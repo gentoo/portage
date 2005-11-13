1eaf47d9ddb16a30fce01869c877bcd1abb58937
Marius Mauch <genone@gentoo.org>
2005-11-13 15:03:44 +0000

    minor glitch: EBUILD_PHASES is a list, no need to split() it
    svn path=/main/trunk/; revision=2308

--- pym/elog_modules/mod_syslog.py
+++ pym/elog_modules/mod_syslog.py
@@ -3,7 +3,7 @@ from portage_const import EBUILD_PHASES
 
 def process(mysettings, cpv, logentries, fulltext):
 	syslog.openlog("portage", syslog.LOG_ERR | syslog.LOG_WARNING | syslog.LOG_INFO | syslog.LOG_NOTICE, syslog.LOG_LOCAL5)
-	for phase in EBUILD_PHASES.split():
+	for phase in EBUILD_PHASES:
 		if not phase in logentries:
 			continue
 		for msgtype,msgcontent in logentries[phase]:
