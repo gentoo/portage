acdd7b0551cdbe07d04f4412c42855900c4003d4
Mike Frysinger <vapier@gentoo.org>
2005-11-13 11:28:19 +0000

    add NetBSD to the BSD list #112393
    svn path=/main/trunk/; revision=2304

--- pym/portage_data.py
+++ pym/portage_data.py
@@ -19,7 +19,7 @@ elif ostype == "Darwin":
 	os.environ["XARGS"]="xargs"
 	def lchown(*pos_args, **key_args):
 		pass
-elif ostype in ["FreeBSD","OpenBSD"]:
+elif ostype in ["FreeBSD","OpenBSD","NetBSD"]:
 	userland="BSD"
 	os.environ["XARGS"]="xargs"
 else:
