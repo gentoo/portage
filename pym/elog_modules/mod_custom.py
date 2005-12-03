d26e3190b6daad186550f7e9378c1feac79e9176
Kito <kito@gentoo.org>
2005-12-03 18:06:31 +0000

    inital import of elog_modules from trunk.
    svn path=/main/branches/prefix/; revision=2331

---
+++ pym/elog_modules/mod_custom.py
@@ -0,0 +1,16 @@
+import elog_modules.mod_save, portage_exec, portage_exception
+
+def process(mysettings, cpv, logentries, fulltext):
+	elog_modules.mod_save.process(mysettings, cpv, logentries, fulltext)
+	
+	if (not "PORTAGE_ELOG_COMMAND" in mysettings.keys()) \
+			or len(mysettings["PORTAGE_ELOG_COMMAND"]) == 0:
+		raise portage_exception.MissingParameter("!!! Custom logging requested but PORTAGE_ELOG_COMMAND is not defined")
+	else:
+		mylogcmd = mysettings["PORTAGE_ELOG_COMMAND"]
+		mylogcmd.replace("${LOGFILE}", elogfilename)
+		mylogcmd.replace("${PACKAGE}", cpv)
+		retval = portage_exec.spawn_bash(mylogcmd)
+		if retval != 0:
+			raise portage_exception.PortageException("!!! PORTAGE_ELOG_COMMAND failed with exitcode %d" % retval)
+	return
