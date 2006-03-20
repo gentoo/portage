gentoo/portage

commit f034aadcfd6887148455973bebd167961eee4b26

Reset the selinux security context before returning from spawn (regression from r2834).  See bug #126711.
svn path=/main/trunk/; revision=2947

pym/portage.py
@@ -1639,20 +1639,22 @@ def spawn(mystring,mysettings,debug=0,free=0,droppriv=0,sesandbox=0,fd_pipes=Non
 		free=((droppriv and "usersandbox" not in features) or \
 			(not droppriv and "sandbox" not in features and "usersandbox" not in features))
 
+	if free:
+		keywords["opt_name"] += " bash"
+	else:
+		keywords["opt_name"] += " sandbox"
+
 	if sesandbox:
 		con = selinux.getcontext()
 		con = string.replace(con, mysettings["PORTAGE_T"], mysettings["PORTAGE_SANDBOX_T"])
 		selinux.setexec(con)
 
-	if not free:
-		keywords["opt_name"] += " sandbox"
-		return portage_exec.spawn_sandbox(mystring,env=env,**keywords)
-	else:
-		keywords["opt_name"] += " bash"
-		return portage_exec.spawn_bash(mystring,env=env,**keywords)
-	
+	retval = portage_exec.spawn_bash(mystring, env=env, **keywords)
+
 	if sesandbox:
 		selinux.setexec(None)
+
+	return retval
 
 def fetch(myuris, mysettings, listonly=0, fetchonly=0, locks_in_subdir=".locks",use_locks=1, try_mirrors=1):
 	"fetch files.  Will use digest file if available."

commit c0462311696cdc8f82b945b4c90070a9fd89ef9f

Fix sandbox feature (broken since r2947).
svn path=/main/trunk/; revision=2963

pym/portage.py
@@ -1641,15 +1641,17 @@ def spawn(mystring,mysettings,debug=0,free=0,droppriv=0,sesandbox=0,fd_pipes=Non
 
 	if free:
 		keywords["opt_name"] += " bash"
+		spawn_func = portage_exec.spawn_bash
 	else:
 		keywords["opt_name"] += " sandbox"
+		spawn_func = portage_exec.spawn_sandbox
 
 	if sesandbox:
 		con = selinux.getcontext()
 		con = string.replace(con, mysettings["PORTAGE_T"], mysettings["PORTAGE_SANDBOX_T"])
 		selinux.setexec(con)
 
-	retval = portage_exec.spawn_bash(mystring, env=env, **keywords)
+	retval = spawn_func(mystring, env=env, **keywords)
 
 	if sesandbox:
 		selinux.setexec(None)