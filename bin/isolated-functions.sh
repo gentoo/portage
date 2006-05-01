cf4a8d3da43c1f2bd5986bedcf22c76b1ad7fb9b
Simon Stelling <blubb@gentoo.org>
2006-05-01 18:34:37 +0000

    allow escaping in elog_base() to fix bug 131913
    svn path=/main/trunk/; revision=3294

--- bin/isolated-functions.sh
+++ bin/isolated-functions.sh
@@ -16,7 +16,7 @@ elog_base() {
 			return 1
 			;;
 	esac
-	echo "$*" >> ${T}/logging/${EBUILD_PHASE:-other}.${messagetype}
+	echo -e "$*" >> ${T}/logging/${EBUILD_PHASE:-other}.${messagetype}
 	return 0
 }
 
