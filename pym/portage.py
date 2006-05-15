gentoo/portage

commit 34fde515c94ee950e2b26e3944bbe112d5c2fb5b

Ensure that ${ROOT} is normalized for bug #90444.
svn path=/main/trunk/; revision=3356

pym/portage.py
@@ -968,8 +968,8 @@ class config:
 			# backupenv is for calculated incremental variables.
 			self.backupenv = os.environ.copy()
 
-			config_root = config_root.rstrip(os.path.sep) + os.path.sep
-			target_root = target_root.rstrip(os.path.sep) + os.path.sep
+			config_root = os.path.normpath(config_root) + os.path.sep
+			target_root = os.path.normpath(target_root) + os.path.sep
 
 			for k, v in (("PORTAGE_CONFIGROOT", config_root),
 				("ROOT", target_root)):

commit 9a16240744addcd32e18a47db9fae5dbba30a598

Add back the rstrip(os.path.sep) which is needed in the case where ROOT=/ (regression from r3356).
svn path=/main/trunk/; revision=3358

pym/portage.py
@@ -968,8 +968,10 @@ class config:
 			# backupenv is for calculated incremental variables.
 			self.backupenv = os.environ.copy()
 
-			config_root = os.path.normpath(config_root) + os.path.sep
-			target_root = os.path.normpath(target_root) + os.path.sep
+			config_root = \
+				os.path.normpath(config_root).rstrip(os.path.sep) + os.path.sep
+			target_root = \
+				os.path.normpath(target_root).rstrip(os.path.sep) + os.path.sep
 
 			for k, v in (("PORTAGE_CONFIGROOT", config_root),
 				("ROOT", target_root)):