24ef47a7da523d4c3a307122763af8c3c6a13e55
Zac Medico <zmedico@gentoo.org>
2006-05-29 11:45:55 +0000

    Use the well know selinux_aux functions for bug #134743.
    svn path=/main/trunk/; revision=3438

--- pym/portage_selinux.py
+++ pym/portage_selinux.py
@@ -5,13 +5,4 @@
 import selinux
 from selinux import is_selinux_enabled
 from selinux_aux import setexec, secure_symlink, secure_rename, \
-	secure_copy, secure_mkdir
-
-def getcontext():
-	return selinux.getcon()[1]
-
-def get_sid(filename):
-	return selinux.getfilecon(filename)[1]
-
-def get_lsid(filename):
-	return selinux.lgetfilecon(filename)[1]
+	secure_copy, secure_mkdir, getcontext, get_sid, get_lsid
