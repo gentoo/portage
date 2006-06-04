10a1a7aab5c679adfdbd8c8a88ba42991daaef95
Zac Medico <zmedico@gentoo.org>
2006-06-04 03:29:01 +0000

    Raise a ParseError if the profile has a parent file containing anything other than 1 parent.
    svn path=/main/trunk/; revision=3454

--- pym/portage.py
+++ pym/portage.py
@@ -1025,8 +1025,14 @@ class config:
 				self.profiles = [os.path.realpath(self.profile_path)]
 				mypath = self.profiles[0]
 				while os.path.exists(os.path.join(mypath, "parent")):
+					parents_file = os.path.join(mypath, "parent")
+					parents = grabfile(parents_file)
+					if len(parents) != 1:
+						raise portage_exception.ParseError(
+							"Expected 1 parent and got %i: '%s'" % \
+							(len(parents), parents_file))
 					mypath = os.path.normpath(os.path.join(
-						mypath, grabfile(os.path.join(mypath, "parent"))[0]))
+						mypath, parents[0]))
 					if os.path.exists(mypath):
 						self.profiles.insert(0, mypath)
 
