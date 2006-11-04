gentoo/portage

commit 4830c53e94c55c2510d6425d6afb5766416cac91

Move all env.d file name filtering into a single loop.
svn path=/main/trunk/; revision=4451

pym/portage.py
@@ -463,15 +463,17 @@ def env_update(makelinks=1, target_root=None, prev_mtimes=None):
 	portage_util.ensure_dirs(envd_dir, mode=0755)
 	fns = listdir(envd_dir, EmptyOnError=1)
 	fns.sort()
-	pos=0
-	while (pos<len(fns)):
-		if len(fns[pos])<=2:
-			del fns[pos]
+	templist = []
+	for x in fns:
+		if len(x) <= 3:
 			continue
-		if (fns[pos][0] not in string.digits) or (fns[pos][1] not in string.digits):
-			del fns[pos]
+		if not x[0].isdigit() or not x[1].isdigit():
 			continue
-		pos=pos+1
+		if x.startswith(".") or x.endswith("~") or x.endswith(".bak"):
+			continue
+		templist.append(x)
+	fns = templist
+	del templist
 
 	specials={
 	  "KDEDIRS":[],"PATH":[],"CLASSPATH":[],"LDPATH":[],"MANPATH":[],
@@ -490,9 +492,6 @@ def env_update(makelinks=1, target_root=None, prev_mtimes=None):
 	env={}
 
 	for x in fns:
-		# don't process backup files
-		if x[-1]=='~' or x[-4:]==".bak":
-			continue
 		file_path = os.path.join(envd_dir, x)
 		try:
 			myconfig = getconfig(file_path)

commit f8095a5711e245b7558985de27c497ae9c6e409f

Allow env.d file names with 3 characters. This is a regression from r4451. (trunk r4558)
svn path=/main/branches/2.1.1/; revision=4932

pym/portage.py
@@ -465,7 +465,7 @@ def env_update(makelinks=1, target_root=None, prev_mtimes=None):
 	fns.sort()
 	templist = []
 	for x in fns:
-		if len(x) <= 3:
+		if len(x) < 3:
 			continue
 		if not x[0].isdigit() or not x[1].isdigit():
 			continue