5a1a95f8dcb9a2fbc38203ae4778e684f3db85aa
Fabian Groffen <grobian@gentoo.org>
2006-11-04 15:11:17 +0000

    autoconf-2.60 is annoying.  Add AC_CONFIG_HEADERS([config.h]) because it wants it, and remove it afterwards because we don't need it.
    svn path=/main/branches/prefix/; revision=4935

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,7 @@ libtoolize --automake -c -f || { echo "failed libtoolize"; exit 1; }
 autoconf || { echo "failed autoconf"; exit 1; }
 touch ChangeLog 
 automake-1.8 -a -c || { "echo failed automake"; exit 1; }
+rm config.h.in
 
 if [ -x ./test.sh ] ; then
 	exec ./test.sh "$@"
