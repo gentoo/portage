b292b759eaf031d591b1c19d53447e3ccb49294e
Fabian Groffen <grobian@gentoo.org>
2006-12-24 11:56:50 +0000

    Make the inter-revision regex tighter, and teach repoman the same trick. (So in total this code seems to be at three places implemented.)
    svn path=/main/branches/prefix/; revision=5377

--- pym/portage_versions.py
+++ pym/portage_versions.py
@@ -5,8 +5,8 @@
 
 import re,string
 
-# PREFIX hack: -r(\\d+) -> -r(\\d+(\\.\\d+)?) (see below)
-ver_regexp = re.compile("^(cvs\\.)?(\\d+)((\\.\\d+)*)([a-z]?)((_(pre|p|beta|alpha|rc)\\d*)*)(-r(\\d+(\\.\\d+)?))?$")
+# PREFIX hack: -r(\\d+) -> -r(\\d+|0\\d+\\.\\d+) (see below)
+ver_regexp = re.compile("^(cvs\\.)?(\\d+)((\\.\\d+)*)([a-z]?)((_(pre|p|beta|alpha|rc)\\d*)*)(-r(\\d+|0\\.\\d+))?$")
 suffix_regexp = re.compile("^(alpha|beta|rc|pre|p)(\\d*)$")
 suffix_value = {"pre": -2, "p": 0, "alpha": -4, "beta": -3, "rc": -1}
 endversion_keys = ["pre", "p", "alpha", "beta", "rc"]
