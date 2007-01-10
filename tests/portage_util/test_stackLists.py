68cee055aba91242c664f4c59606ffce73f4ce37
Alec Warner <antarus@gentoo.org>
2007-01-10 13:45:27 +0000

    split out util tests
    svn path=/main/trunk/; revision=5529

---
+++ tests/portage_util/test_stackLists.py
@@ -0,0 +1,11 @@
+# test_stackLists.py -- Portage Unit Testing Functionality
+# Copyright 2006 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Id: test_vercmp.py 5213 2006-12-08 00:12:41Z antarus $
+
+from unittest import TestCase
+
+class StackListsTestCase(TestCase):
+	
+	def testStackLists(self):
+		pass
