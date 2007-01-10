be41793f50c29aa6c0d17a8616ce214b71d81e0a
Alec Warner <antarus@gentoo.org>
2007-01-10 13:35:03 +0000

    Take Genone's suggestion and break the modules up into module oriented fashion.  Tests for portage.py go in portage/ and so on and so forth...
    svn path=/main/trunk/; revision=5525

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,16 +7,34 @@ import unittest
 
 def main():
 	
-	tests = ["test_atoms", "test_util"]
+	testDirs = ["portage/", "portage_util/"]
 
 	suite = unittest.TestSuite()
 
-	for mod in tests:
-		try:
-			loadMod = __import__(mod)
-			tmpSuite = unittest.TestLoader().loadTestsFromModule(loadMod)
-			suite.addTest(tmpSuite)
-		except ImportError:
-			pass
+	for dir in testDirs:
+		suite.addTests(getTests(dir))
 
 	return unittest.TextTestRunner(verbosity=2).run(suite)
+
+def getTests( path ):
+	"""
+
+	path is the path to a given subdir ( 'portage/' for example)
+	This does a simple filter on files in that dir to give us modules
+	to import
+
+	"""
+	import os
+	files = os.listdir( path )
+	files = [ f[:-3] for f in files if f.startswith("test_") and f.endswith(".py") ]
+
+	result = []
+	for file in files:
+		try:
+			# Make the trailing / a . for module importing
+			path2 = path[:-1] + "." + file
+			mod = __import__( path2, globals(), locals(), [path[-1]])
+			result.append( unittest.TestLoader().loadTestsFromModule(mod) )
+		except ImportError:
+			pass
+	return result
