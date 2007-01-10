5036c5c9353acbb9fb95f7cf2f213c3ba64a1c2b
Alec Warner <antarus@gentoo.org>
2007-01-10 13:37:28 +0000

    shift test cases around
    svn path=/main/trunk/; revision=5526

--- tests/portage/test_atoms.py
+++ tests/portage/test_atoms.py
@@ -4,35 +4,7 @@
 # $Id$
 
 from unittest import TestCase
-from unittest import TestLoader
 from portage_versions import vercmp
-from portage_dep import match_from_list
-
-class AtomCmpEqualGlob(TestCase):
-	""" A simple testcase for =* glob matching
-	"""
-
-	def testEqualGlobPass(self):
-		tests = [ ("=sys-apps/portage-45*", "sys-apps/portage-045" ),
-			  ("=sys-fs/udev-1*", "sys-fs/udev-123"),
-			  ("=sys-fs/udev-4*", "sys-fs/udev-456" ) ]
-
-# I need to look up the cvs syntax
-#			  ("=sys-fs/udev_cvs*","sys-fs/udev_cvs_pre4" ) ]
-
-		for test in tests:
-			self.failIf( len(match_from_list( test[0], [test[1]] )) != 1,
-				msg="%s should match %s!" % (test[0], test[1]) )
-
-	def testEqualGlobFail(self):
-		tests = [ ("=sys-apps/portage-2*", "sys-apps/portage-2.1" ),
-			  ("=sys-apps/portage-2.1*", "sys-apps/portage-2.1.2" ) ]
-		for test in tests:
-			try:
-				self.failIf( len( match_from_list( test[0], [test[1]] ) ) != 1,
-					msg="%s shouldn't match %s!" % (test[0], test[1]) )
-			except TypeError: # failure is ok here
-				pass
 
 class VerCmpTestCase(TestCase):
 	""" A simple testCase for portage_versions.vercmp()
