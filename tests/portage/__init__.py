544952b129dbb7cd0a0e9ab323c2e89c1b4060c9
Alec Warner <antarus@gentoo.org>
2007-01-12 08:08:53 +0000

    set Id on tests
    svn path=/main/trunk/; revision=5596

--- tests/portage/__init__.py
+++ tests/portage/__init__.py
@@ -1,4 +1,4 @@
 # tests/portage/__init__.py -- Portage Unit Test functionality
 # Copyright 2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Id: __init__.py 5522 2007-01-10 12:30:05Z antarus $
+# $Id$
--- tests/portage_util/test_grabdict.py
+++ tests/portage_util/test_grabdict.py
@@ -1,7 +1,7 @@
 # test_grabDict.py -- Portage Unit Testing Functionality
 # Copyright 2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Id: test_vercmp.py 5213 2006-12-08 00:12:41Z antarus $
+# $Id$
 
 from unittest import TestCase, TestLoader
 from portage_util import grabdict
