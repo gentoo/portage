fc8b4972964026c4f151bcc932655de91f29809b
Zac Medico <zmedico@gentoo.org>
2007-02-24 00:56:59 +0000

    Allow --verbose to trigger a merge list even without --pretend, --ask, or --tree.  Thanks to agaffney and zlin for the suggestion.
    svn path=/main/trunk/; revision=6066

--- pym/emerge/__init__.py
+++ pym/emerge/__init__.py
@@ -4680,7 +4680,8 @@ def action_build(settings, trees, mtimedb,
 	ldpath_mtimes = mtimedb["ldpath"]
 	favorites=[]
 	if "--quiet" not in myopts and \
-		("--pretend" in myopts or "--ask" in myopts or "--tree" in myopts):
+		("--pretend" in myopts or "--ask" in myopts or \
+		"--tree" in myopts or "--verbose" in myopts):
 		action = ""
 		if "--fetchonly" in myopts or "--fetch-all-uri" in myopts:
 			action = "fetched"
@@ -4770,7 +4771,8 @@ def action_build(settings, trees, mtimedb,
 			sys.exit(1)
 
 	if "--pretend" not in myopts and \
-		("--ask" in myopts or "--tree" in myopts) and \
+		("--ask" in myopts or "--tree" in myopts or \
+		"--verbose" in myopts) and \
 		not ("--quiet" in myopts and "--ask" not in myopts):
 		if "--resume" in myopts:
 			validate_merge_list(trees, mtimedb["resume"]["mergelist"])
