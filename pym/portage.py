2cdf3c31df50e97c9b871b2518438557c908990b
Zac Medico <zmedico@gentoo.org>
2007-02-24 12:45:42 +0000

    Clean up os.environ["USERLAND"] pollution from the portage_data module so that it doesn't interfere with repoman. (trunk r6067:6068)
    svn path=/main/branches/2.1.2/; revision=6069

--- pym/portage.py
+++ pym/portage.py
@@ -989,6 +989,10 @@ class config:
 
 			# backupenv is for calculated incremental variables.
 			self.backupenv = os.environ.copy()
+			if not local_config:
+				# Clean up pollution from portage_data so that it doesn't
+				# interfere with repoman.
+				self.backupenv.pop("USERLAND", None)
 
 			def check_var_directory(varname, var):
 				if not os.path.isdir(var):
@@ -1231,7 +1235,10 @@ class config:
 
 			self.configlist.append(os.environ.copy())
 			self.configdict["env"]=self.configlist[-1]
-
+			if not local_config:
+				# Clean up pollution from portage_data so that it doesn't
+				# interfere with repoman.
+				self.configdict["env"].pop("USERLAND", None)
 
 			# make lookuplist for loading package.*
 			self.lookuplist=self.configlist[:]
