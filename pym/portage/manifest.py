02c57b76626cd0b8b3688be24cfbf75638f88a91
Fabian Groffen <grobian@gentoo.org>
2007-02-26 21:52:36 +0000

    Merged from trunk 6039:6048
    For bug #167667, use the existence of ${PORTDIR}/manifest1_obsolete to
    trigger the disabling of manifest1/digest file generation.  This toggle
    is repo/overlay specific, allowing a specific overlay to drop manifest1
    buy adding the manifest1_obsolete file.
    
    For bug #167667, add support for ${PORTDIR}/manifest1_obsolete and
    automatically remove ${FILESDIR}/digest-* from cvs when appropriate.
    (prefix: do the same thing for SVN)
    
    Don't bother to parse ${FILESDIR}/digest-* when manifest1_compat is
    disabled.
    
    For bug #167668, use an rsync "hide" filter rule to exclude
    **/files/digest-* and delete them on the receiver.
    
    Remove quotes since rsync doesn't accept them (and there is no shell to
    remove them since we use spawn() for rsync now).
    
    
    
    svn path=/main/branches/prefix/; revision=6083

--- pym/portage/manifest.py
+++ pym/portage/manifest.py
@@ -117,9 +117,9 @@ class Manifest(object):
 		self.hashes = sets.Set(self.hashes)
 		for t in portage.const.MANIFEST2_IDENTIFIERS:
 			self.fhashdict[t] = {}
+		self.compat = manifest1_compat
 		if not from_scratch:
 			self._read()
-		self.compat = manifest1_compat
 		if fetchlist_dict != None:
 			self.fetchlist_dict = fetchlist_dict
 		else:
@@ -181,8 +181,8 @@ class Manifest(object):
 			self._readManifest(self.getFullname(), myhashdict=self.fhashdict)
 		except FileNotFound:
 			pass
-		self._readDigests(myhashdict=self.fhashdict)
-		
+		if self.compat:
+			self._readDigests(myhashdict=self.fhashdict)
 
 	def _parseManifestLines(self, mylines):
 		"""Parse manifest lines and return a list of manifest entries."""
@@ -439,7 +439,8 @@ class Manifest(object):
 		else:
 			distfilehashes = {}
 		self.__init__(self.pkgdir, self.distdir,
-			fetchlist_dict=self.fetchlist_dict, from_scratch=True)
+			fetchlist_dict=self.fetchlist_dict, from_scratch=True,
+			manifest1_compat=self.compat)
 		for pkgdir, pkgdir_dirs, pkgdir_files in os.walk(self.pkgdir):
 			break
 		for f in pkgdir_files:
