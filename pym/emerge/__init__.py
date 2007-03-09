0ecce044fa65b6a2a1bcf0dc967ef3d6200023c4
Marius Mauch <genone@gentoo.org>
2007-03-09 10:31:54 +0000

    Display message about --buildpkgonly restriction in pretend mode as well
    svn path=/main/trunk/; revision=6199

--- pym/emerge/__init__.py
+++ pym/emerge/__init__.py
@@ -4944,6 +4944,11 @@ def action_build(settings, trees, mtimedb,
 		else:
 			mydepgraph.display(
 				mydepgraph.altlist(reversed=("--tree" in myopts)))
+		if ("--buildpkgonly" in myopts):
+			if not mydepgraph.digraph.hasallzeros(ignore_priority=DepPriority.MEDIUM):
+				print "\n!!! --buildpkgonly requires all dependencies to be merged."
+				print "!!! You have to merge the dependencies before you can build this package.\n"
+				sys.exit(1)
 	else:
 		if ("--buildpkgonly" in myopts):
 			if not mydepgraph.digraph.hasallzeros(ignore_priority=DepPriority.MEDIUM):
