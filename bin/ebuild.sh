e6c72375397e0a3f40f3a3be2f6745dc9539d746
Zac Medico <zmedico@gentoo.org>
2007-03-16 01:20:36 +0000

    For bug #163262 and conformance with section 7.2 of the PMS, make the export attribute of variables persist between ebuild phases.
    svn path=/main/trunk/; revision=6212

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -1618,6 +1618,7 @@ if [ -n "${myarg}" ] && \
 	# Save current environment and touch a success file. (echo for success)
 	umask 002
 	set | egrep -v "^SANDBOX_" > "${T}/environment" 2>/dev/null
+	export | egrep -v "^declare -x SANDBOX_" >> "${T}/environment" 2>/dev/null
 	chown portage:portage "${T}/environment" &>/dev/null
 	chmod g+w "${T}/environment" &>/dev/null
 fi
