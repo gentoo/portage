ff0dfb0b1072c1d0e14fdcbf58581941baea7bb6
Zac Medico <zmedico@gentoo.org>
2007-03-27 20:32:16 +0000

    For bug #170178, remove all hardcoded references to '/usr/lib/portage'. (trunk r6202:6203)
    svn path=/main/branches/2.1.2/; revision=6293

--- pym/portage.py
+++ pym/portage.py
@@ -5001,7 +5001,7 @@ class vardbapi(dbapi):
 				except Exception, e:
 					writemsg("!!! COUNTER file is missing for "+str(mycpv)+" in /var/db.\n",
 						noiselevel=-1)
-					writemsg("!!! Please run /usr/lib/portage/bin/fix-db.py or\n",
+					writemsg("!!! Please run %s/fix-db.py or\n" % PORTAGE_BIN_PATH,
 						noiselevel=-1)
 					writemsg("!!! unmerge this exact version.\n", noiselevel=-1)
 					writemsg("!!! %s\n" % e, noiselevel=-1)
@@ -5009,7 +5009,7 @@ class vardbapi(dbapi):
 			else:
 				writemsg("!!! COUNTER file is missing for "+str(mycpv)+" in /var/db.\n",
 					noiselevel=-1)
-				writemsg("!!! Please run /usr/lib/portage/bin/fix-db.py or\n",
+				writemsg("!!! Please run %s/fix-db.py or\n" % PORTAGE_BIN_PATH,
 					noiselevel=-1)
 				writemsg("!!! remerge the package.\n", noiselevel=-1)
 				sys.exit(1)
