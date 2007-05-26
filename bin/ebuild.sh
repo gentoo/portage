994332423c69ef394f5a76d568a693ef82734ad8
Zac Medico <zmedico@gentoo.org>
2007-05-26 00:36:27 +0000

    s/filing a report to/seeking support at/ Thanks to marienz.
    svn path=/main/trunk/; revision=6629

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -497,7 +497,7 @@ econf() {
 
 			if [ -s config.log ]; then
 				echo
-				echo "!!! Please attach the following file when filing a report to bugs.gentoo.org:"
+				echo "!!! Please attach the following file when seeking support at bugs.gentoo.org:"
 				echo "!!! ${PWD}/config.log"
 			fi
 			die "econf failed"
