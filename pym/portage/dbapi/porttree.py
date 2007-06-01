e29a91aad89df64d18b5631035e7cf6e8198939c
Zac Medico <zmedico@gentoo.org>
2007-06-01 15:44:53 +0000

    Don't repeatedly invoke the depend phase for ebuilds that are already known to be broken.
    svn path=/main/trunk/; revision=6707

--- pym/portage/dbapi/porttree.py
+++ pym/portage/dbapi/porttree.py
@@ -111,6 +111,7 @@ class portdbapi(dbapi):
 		# Selectively cache metadata in order to optimize dep matching.
 		self._aux_cache_keys = set(["EAPI", "KEYWORDS", "LICENSE", "SLOT"])
 		self._aux_cache = {}
+		self._broken_ebuilds = set()
 
 	def _init_cache_dirs(self):
 		"""Create /var/cache/edb/dep and adjust permissions for the portage
@@ -301,6 +302,8 @@ class portdbapi(dbapi):
 		writemsg("auxdb is valid: "+str(not doregen)+" "+str(pkg)+"\n", 2)
 
 		if doregen:
+			if myebuild in self._broken_ebuilds:
+				raise KeyError(mycpv)
 			writemsg("doregen: %s %s\n" % (doregen, mycpv), 2)
 			writemsg("Generating cache entry(0) for: "+str(myebuild)+"\n", 1)
 
@@ -310,6 +313,7 @@ class portdbapi(dbapi):
 				self.doebuild_settings["ROOT"], self.doebuild_settings,
 				dbkey=mydata, tree="porttree", mydbapi=self)
 			if myret != os.EX_OK:
+				self._broken_ebuilds.add(myebuild)
 				raise KeyError(mycpv)
 
 			if "EAPI" not in mydata or not mydata["EAPI"].strip():
