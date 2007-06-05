5db79baa543b6ea665f7b57c47997643d2e549b2
Marius Mauch <genone@gentoo.org>
2007-06-05 17:55:03 +0000

    Remove duplicate entries from special variables processed by env_update() (bug #172194)
    svn path=/main/trunk/; revision=6736

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -517,7 +517,9 @@ def env_update(makelinks=1, target_root=None, prev_mtimes=None, contents=None):
 		mylist = []
 		for myconfig in config_list:
 			if var in myconfig:
-				mylist.extend(filter(None, myconfig[var].split()))
+				for item in myconfig[var].split(":"):
+					if item and not item in mylist:
+						mylist.append(item)
 				del myconfig[var] # prepare for env.update(myconfig)
 		if mylist:
 			env[var] = " ".join(mylist)
@@ -527,7 +529,9 @@ def env_update(makelinks=1, target_root=None, prev_mtimes=None, contents=None):
 		mylist = []
 		for myconfig in config_list:
 			if var in myconfig:
-				mylist.extend(filter(None, myconfig[var].split(":")))
+				for item in myconfig[var].split(":"):
+					if item and not item in mylist:
+						mylist.append(item)
 				del myconfig[var] # prepare for env.update(myconfig)
 		if mylist:
 			env[var] = ":".join(mylist)
