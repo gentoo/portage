a1dd09f64ccefbff495d0607aeec5422f32b93fb
Zac Medico <zmedico@gentoo.org>
2007-06-22 23:35:43 +0000

    - Make elog finalize() handling safe for PORTAGE_CONFIGROOT. (trunk r6955) - Dump mod_echo elog output inside post_emerge(), just before the other notifications that happen at exit. (trunk r6953)
    svn path=/main/branches/2.1.2/; revision=6958

--- pym/elog_modules/mod_echo.py
+++ pym/elog_modules/mod_echo.py
@@ -8,18 +8,27 @@ from portage_const import EBUILD_PHASES
 
 _items = {}
 def process(mysettings, key, logentries, fulltext):
-	_items[key] = logentries
+	global _items
+	config_root = mysettings["PORTAGE_CONFIGROOT"]
+	mysettings, items = _items.setdefault(config_root, (mysettings, {}))
+	items[key] = logentries
 
-def finalize(mysettings):
+def finalize():
+	global _items
+	for mysettings, items in _items.itervalues():
+		_finalize(mysettings, items)
+	_items.clear()
+
+def _finalize(mysettings, items):
 	printer = EOutput()
-	for key in _items:
+	for key, logentries in items.iteritems():
 		print
 		printer.einfo("Messages for package %s:" % key)
 		print
 		for phase in EBUILD_PHASES:
-			if not phase in _items[key]:
+			if phase not in logentries:
 				continue
-			for msgtype, msgcontent in _items[key][phase]:
+			for msgtype, msgcontent in logentries[phase]:
 				fmap = {"INFO": printer.einfo,
 						"WARN": printer.ewarn,
 						"ERROR": printer.eerror,
