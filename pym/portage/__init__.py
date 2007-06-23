552428f62a9d821f04b9c308b18ed75bd103647f
Marius Mauch <genone@gentoo.org>
2007-06-23 13:12:07 +0000

    add hashlib support for checksum verification, refactor checksum functions to not look like a complete mess
    svn path=/main/trunk/; revision=6972

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -819,7 +819,7 @@ def autouse(myvartree, use_cache=1, mysettings=None):
 
 def check_config_instance(test):
 	if not isinstance(test, config):
-		raise TypeError("Invalid type for config object: %s" % test.__class__)
+		raise TypeError("Invalid type for config object: %s (should be %s)" % (test.__class__, config))
 
 class config(object):
 	"""
