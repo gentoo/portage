87e21d4ef9a09cac43d6077bd69692ccb57d0fbd
Zac Medico <zmedico@gentoo.org>
2007-07-20 01:11:31 +0000

    Merge --depclean and --prune enhancements from trunk r7304:7310.
    svn path=/main/branches/2.1.2/; revision=7311

--- pym/portage.py
+++ pym/portage.py
@@ -5059,10 +5059,7 @@ class fakedbapi(dbapi):
 			return self.cpdict[mycp]
 
 	def cp_all(self):
-		returnme=[]
-		for x in self.cpdict:
-			returnme.extend(self.cpdict[x])
-		return returnme
+		return list(self.cpdict)
 
 	def cpv_all(self):
 		return self.cpvdict.keys()
