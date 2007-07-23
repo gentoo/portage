e940a98117979ab6d9c4ed367b01ff0598740a3e
Alec Warner <antarus@gentoo.org>
2007-07-23 07:49:38 +0000

    add more tests for keyvaluepairloader (it had a bug previously) and fix up other misc tests
    svn path=/main/trunk/; revision=7371

--- pym/portage/tests/env/config/test_PackageMaskFile.py
+++ pym/portage/tests/env/config/test_PackageMaskFile.py
@@ -6,12 +6,10 @@
 import os
 
 from portage.env.config import PackageMaskFile
-from portage.tests import TestCase
+from portage.tests import TestCase, test_cps
 from tempfile import mkstemp
 
 class PackageMaskFileTestCase(TestCase):
-
-	atoms = ['sys-apps/portage','dev-util/diffball','not@va1id@t0m']
 	
 	def testPackageMaskFile(self):
 		self.BuildFile()
@@ -19,14 +17,14 @@ class PackageMaskFileTestCase(TestCase):
 			f = PackageMaskFile(self.fname)
 			f.load()
 			for atom in f:
-				self.assertTrue(atom in self.atoms)
+				self.assertTrue(atom in test_cps)
 		finally:
 			self.NukeFile()
 	
 	def BuildFile(self):
 		fd, self.fname = mkstemp()
 		f = os.fdopen(fd, 'w')
-		f.write("\n".join(self.atoms))
+		f.write("\n".join(test_cps))
 		f.close()
 	
 	def NukeFile(self):
