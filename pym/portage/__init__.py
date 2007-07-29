51c6672c5251d61ab1278dcb38e63a4a00dc1956
Zac Medico <zmedico@gentoo.org>
2007-07-29 10:12:47 +0000

    Change "output" import to portage.output.
    svn path=/main/trunk/; revision=7426

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -2400,7 +2400,7 @@ def spawn(mystring, mysettings, debug=0, free=0, droppriv=0, sesandbox=0, fakero
 		fd_pipes.setdefault(0, sys.stdin.fileno())
 		fd_pipes_orig = fd_pipes.copy()
 		if got_pty and os.isatty(fd_pipes_orig[1]):
-			from output import get_term_size, set_term_size
+			from portage.output import get_term_size, set_term_size
 			rows, columns = get_term_size()
 			set_term_size(rows, columns, slave_fd)
 		fd_pipes[0] = fd_pipes_orig[0]
