7a3d6b48da77afbaaaef35f5c3d017554eaccea8
Zac Medico <zmedico@gentoo.org>
2007-07-31 04:47:37 +0000

    In dblink.treewalk(), handle the case where the current cpv is already installed but it has a different slot (multislot or unapplied slotmove). (branches/2.1.2 r6834)
    svn path=/main/branches/2.1.2.9/; revision=7532

--- pym/portage.py
+++ pym/portage.py
@@ -7435,6 +7435,11 @@ class dblink:
 
 		slot_matches = self.vartree.dbapi.match(
 			"%s:%s" % (self.mysplit[0], self.settings["SLOT"]))
+		if self.mycpv not in slot_matches and \
+			self.vartree.dbapi.cpv_exists(self.mycpv):
+			# handle multislot or unapplied slotmove
+			slot_matches.append(self.mycpv)
+
 		if slot_matches:
 			# Used by self.isprotected().
 			max_cpv = None
