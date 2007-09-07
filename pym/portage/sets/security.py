4191f68053993645c19fc92dc7cdb8662dd4e4a0
Marius Mauch <genone@gentoo.org>
2007-09-07 11:51:13 +0000

    Undo some unwanted changes
    svn path=/main/trunk/; revision=7756

--- pym/portage/sets/security.py
+++ pym/portage/sets/security.py
@@ -6,7 +6,7 @@ import os
 import portage.glsa as glsa
 from portage.util import grabfile, write_atomic
 from portage.const import CACHE_PATH
-from portage.sets.dbapi import PackageSet
+from portage.sets import PackageSet
 
 class SecuritySet(PackageSet):
 	_operations = ["merge"]
