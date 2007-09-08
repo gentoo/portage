ff8a98f4b34dfd6ff2e589648a1e9020d7417eb3
Zac Medico <zmedico@gentoo.org>
2007-09-08 06:51:59 +0000

    Bug #190268 - Avoid unwanted sandbox violations in src_test().   - Allow SANDBOX_* variables to pass through.   - Don't try to create an sandbox instance inside a test case in order to     interaction with SANDBOX_* variables in src_test().
    svn path=/main/trunk/; revision=7759

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -1201,8 +1201,7 @@ class config(object):
 
 			# Blacklist vars that could interfere with portage internals.
 			for blacklisted in "CATEGORY", "PKGUSE", "PORTAGE_CONFIGROOT", \
-				"ROOT", "SANDBOX_DENY", "SANDBOX_PREDICT", "SANDBOX_READ", \
-				"SANDBOX_WRITE":
+				"ROOT":
 				for cfg in self.lookuplist:
 					cfg.pop(blacklisted, None)
 			del blacklisted, cfg
