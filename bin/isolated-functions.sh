981cea176b91df51587e898cbb552574d6d9d4d8
Fabian Groffen <grobian@gentoo.org>
2007-09-29 20:31:29 +0000

    Merged from trunk 7877:7879
       | 7878      | Fix quoting as reported by Drac in bug 194169              |
       | WarnerBro |                                                            |
       
       | 7879      | revert useless quoting                                     |
       | SpankMan  |                                                            |
    
    svn path=/main/branches/prefix/; revision=7884

--- bin/isolated-functions.sh
+++ bin/isolated-functions.sh
@@ -132,7 +132,7 @@ elog_base() {
 			return 1
 			;;
 	esac
-	echo -e "$*" >> ${T}/logging/${EBUILD_PHASE:-other}.${messagetype}
+	echo -e "$*" >> "${T}/logging/${EBUILD_PHASE:-other}.${messagetype}"
 	return 0
 }
 
