1fbf47530609f917ee3b5973cae1c809bb830cf4
Zac Medico <zmedico@gentoo.org>
2007-10-03 16:19:14 +0000

    Bug #179380 - Make the default src_compile() support ECONF_SOURCE if EAPI > 0. (trunk r6567)
    svn path=/main/branches/2.1.2/; revision=7906

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -685,7 +685,9 @@ src_unpack() {
 }
 
 src_compile() {
-	if [ -x ./configure ]; then
+	if [ "${EAPI:-0}" == 0 ] ; then
+		[ -x ./configure ] && econf
+	elif [ -x "${ECONF_SOURCE:-.}/configure" ] ; then
 		econf
 	fi
 	if [ -f Makefile ] || [ -f GNUmakefile ] || [ -f makefile ]; then
