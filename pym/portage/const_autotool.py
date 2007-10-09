0841e738534bf07613784864c22e888c837effbc
Fabian Groffen <grobian@gentoo.org>
2007-10-09 18:17:36 +0000

    Install sets.conf in datarootdir/portage.
    svn path=/main/branches/prefix/; revision=8020

--- pym/portage/const_autotool.py
+++ pym/portage/const_autotool.py
@@ -4,13 +4,14 @@
 
 # all vars that are to wind up in portage_const must have their name listed in __all__
 
-__all__ = ["EPREFIX", "SYSCONFDIR", "PORTAGE_BASE", "portageuser",
-		"portagegroup", "rootuser", "rootuid"]
+__all__ = ["EPREFIX", "SYSCONFDIR", "DATAROOTDIR", "PORTAGE_BASE",
+		"portageuser", "portagegroup", "rootuser", "rootuid"]
 
 from os import path
 
 EPREFIX=path.normpath("@DOMAIN_PREFIX@")
 SYSCONFDIR=path.normpath("@sysconfdir@")
+DATAROOTDIR=path.normpath("@datarootdir@")
 PORTAGE_BASE=path.normpath("@PORTAGE_BASE@")
 portagegroup="@portagegroup@"
 portageuser="@portageuser@"
