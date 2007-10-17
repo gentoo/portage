1387dfcf15e55ba49aeca7e86cc37a35895706f2
Zac Medico <zmedico@gentoo.org>
2007-10-17 21:44:10 +0000

    Fix the normalize_needed regex to properly match // anywhere in the path. Also add support for detection of . or .. where appropriate.
    svn path=/main/trunk/; revision=8166

--- pym/portage/dbapi/vartree.py
+++ pym/portage/dbapi/vartree.py
@@ -823,7 +823,7 @@ class dblink(object):
 	"""
 
 	import re
-	_normalize_needed = re.compile(r'//|^[^/]|.+/$')
+	_normalize_needed = re.compile(r'.*//.*|^[^/]|.+/$|(^|.*/)\.\.?(/.*|$)')
 
 	def __init__(self, cat, pkg, myroot, mysettings, treetype=None,
 		vartree=None):
