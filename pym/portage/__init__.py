bc31ddb2641a30f6ac28e3bf4b536af8d2a7a022
Zac Medico <zmedico@gentoo.org>
2007-10-26 19:35:01 +0000

    In portage.fetch(), condense digest check ouput to a single line for all digests.
    svn path=/main/trunk/; revision=8316

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -2993,9 +2993,12 @@ def fetch(myuris, mysettings, listonly=0, fetchonly=0, locks_in_subdir=".locks",
 								eout = portage.output.EOutput()
 								eout.quiet = \
 									mysettings.get("PORTAGE_QUIET", None) == "1"
-								for digest_name in mydigests[myfile]:
+								digests = mydigests.get(myfile)
+								if digests:
+									digests = digests.keys()
+									digests.sort()
 									eout.ebegin(
-										"%s %s ;-)" % (myfile, digest_name))
+										"%s %s ;-)" % (myfile, " ".join(digests)))
 									eout.eend(0)
 								continue # fetch any remaining files
 
