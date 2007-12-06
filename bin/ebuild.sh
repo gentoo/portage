416b56226e577d556a9a56a42d7021fd54b53e1c
Zac Medico <zmedico@gentoo.org>
2007-12-06 23:38:00 +0000

    Declare retval as a local variable. (trunk r8859)
    svn path=/main/branches/2.1.2/; revision=8860

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -1464,6 +1464,7 @@ preprocess_ebuild_env() {
 		save_ebuild_env || exit $?
 		touch "${T}/environment.success" || exit $?
 	) | filter_readonly_variables > "${T}/environment.filtered"
+	local retval
 	if [ -e "${T}/environment.success" ] ; then
 		mv "${T}/environment.filtered" "${T}/environment"
 		retval=$?
