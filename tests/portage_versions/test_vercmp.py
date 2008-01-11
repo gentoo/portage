aa0f91ba8b6da92a059830bc2d10c5c8552c733e
Zac Medico <zmedico@gentoo.org>
2008-01-11 11:41:55 +0000

    * Make pkgcmp() pass the ebuild revision directly into vercmp() since   there is code there to handle it already. This eliminates some   redundant revision comparison code. Thanks to peper for the patch.
    * Add some vercmp() test cases for comparison of ebuild revisions.
    
    (trunk r9178)
    
    svn path=/main/branches/2.1.2/; revision=9182

--- tests/portage_versions/test_vercmp.py
+++ tests/portage_versions/test_vercmp.py
@@ -12,7 +12,9 @@ class VerCmpTestCase(TestCase):
 	
 	def testVerCmpGreater(self):
 		
-		tests = [ ( "6.0", "5.0"), ("5.0","5")]
+		tests = [ ( "6.0", "5.0"), ("5.0","5"),
+			("1.0-r1", "1.0-r0"),
+			("1.0-r1", "1.0")]
 		for test in tests:
 			self.failIf( vercmp( test[0], test[1] ) <= 0, msg="%s < %s? Wrong!" % (test[0],test[1]) )
 
@@ -24,20 +26,31 @@ class VerCmpTestCase(TestCase):
 			("1.0_alpha2", "1.0_p2"),("1.0_alpha1", "1.0_beta1"),("1.0_beta3","1.0_rc3"),
 			("1.001000000000000000001", "1.001000000000000000002"),
 			("1.00100000000", "1.0010000000000000001"),
-			("1.01", "1.1")]
+			("1.01", "1.1"),
+			("1.0-r0", "1.0-r1"),
+			("1.0", "1.0-r1")]
 		for test in tests:
 			self.failIf( vercmp( test[0], test[1]) >= 0, msg="%s > %s? Wrong!" % (test[0],test[1]))
 	
 	
 	def testVerCmpEqual(self):
 		
-		tests = [ ("4.0", "4.0") ]
+		tests = [ ("4.0", "4.0"),
+			("1.0", "1.0"),
+			("1.0-r0", "1.0"),
+			("1.0", "1.0-r0"),
+			("1.0-r0", "1.0-r0"),
+			("1.0-r1", "1.0-r1")]
 		for test in tests:
 			self.failIf( vercmp( test[0], test[1]) != 0, msg="%s != %s? Wrong!" % (test[0],test[1]))
 			
 	def testVerNotEqual(self):
 		
 		tests = [ ("1","2"),("1.0_alpha","1.0_pre"),("1.0_beta","1.0_alpha"),
-			("0", "0.0")]
+			("0", "0.0"),
+			("1.0-r0", "1.0-r1"),
+			("1.0-r1", "1.0-r0"),
+			("1.0", "1.0-r1"),
+			("1.0-r1", "1.0")]
 		for test in tests:
 			self.failIf( vercmp( test[0], test[1]) == 0, msg="%s == %s? Wrong!" % (test[0],test[1]))
