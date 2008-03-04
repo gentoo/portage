ad9b962a356ee23f8dc0a3b57ac3a058fadfc0b8
Zac Medico <zmedico@gentoo.org>
2008-03-04 04:51:54 +0000

    Bug #211949 - Fix the regex so that it won't match the = character (equals sign). (trunk r9429)
    svn path=/main/branches/2.1.2/; revision=9430

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -1421,7 +1421,7 @@ filter_readonly_variables() {
 	filtered_vars="${readonly_bash_vars} ${READONLY_PORTAGE_VARS}
 		BASH_[_[:alnum:]]* PATH
 		[[:digit:]][_[:alnum:]]*
-		[^[:space:]]*[^_[:alnum:][:space:]][^[:space:]]*"
+		[^=[:space:]]*[^_[:alnum:]=[:space:]][^=[:space:]]*"
 	if hasq --filter-sandbox $* ; then
 		filtered_vars="${filtered_vars} SANDBOX_[_[:alnum:]]*"
 	else
