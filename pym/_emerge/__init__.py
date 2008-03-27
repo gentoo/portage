1612229e09f321c36bd05643eaf14d695dc89c79
Zac Medico <zmedico@gentoo.org>
2008-03-27 04:23:30 +0000

    Make --noreplace take precedence over --newuse, as suggested by wolf31o2.
    svn path=/main/trunk/; revision=9513

--- pym/_emerge/__init__.py
+++ pym/_emerge/__init__.py
@@ -2574,6 +2574,7 @@ class depgraph(object):
 		usepkgonly = "--usepkgonly" in self.myopts
 		empty = "empty" in self.myparams
 		selective = "selective" in self.myparams
+		noreplace = "--noreplace" in self.myopts
 		reinstall = False
 		# Behavior of the "selective" parameter depends on
 		# whether or not a package matches an argument atom.
@@ -2608,6 +2609,10 @@ class depgraph(object):
 				# descending order
 				cpv_list.reverse()
 				for cpv in cpv_list:
+					# Make --noreplace take precedence over --newuse.
+					if not installed and noreplace and \
+						cpv in vardb.match(atom):
+						continue
 					reinstall_for_flags = None
 					try:
 						metadata = dict(izip(db_keys,
