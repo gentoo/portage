5f2c6418672a6caf4093dc8cb578c9bbaca65a06
Zac Medico <zmedico@gentoo.org>
2008-04-09 07:33:47 +0000

    Create a new depgraph._dep_expand() method for select_files() to expand atoms that have no category. (trunk r9765)
    svn path=/main/branches/2.1.2/; revision=9768

--- pym/portage.py
+++ pym/portage.py
@@ -2519,10 +2519,10 @@ class config:
 
 		self.already_in_regenerate = 0
 
-	def get_virts_p(self, myroot):
+	def get_virts_p(self, myroot=None):
 		if self.virts_p:
 			return self.virts_p
-		virts = self.getvirtuals(myroot)
+		virts = self.getvirtuals()
 		if virts:
 			for x in virts:
 				vkeysplit = x.split("/")
