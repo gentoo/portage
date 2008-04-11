ef04cc11cd149c78b203ba1492535926c2c335da
Zac Medico <zmedico@gentoo.org>
2008-04-11 19:01:45 +0000

    Remove all calls to config.load_infodir() since it doesn't seem to server any purpose now that the complete environment is loaded from environment.bz2.
    svn path=/main/trunk/; revision=9834

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -4788,10 +4788,6 @@ def doebuild(myebuild, mydo, myroot, mysettings, debug=0, listonly=0,
 		elif mydo == "setup":
 			infodir = os.path.join(
 				mysettings["PORTAGE_BUILDDIR"], "build-info")
-			if os.path.isdir(infodir):
-				"""Load USE flags for setup phase of a binary package.
-				Ideally, the environment.bz2 would be used instead."""
-				mysettings.load_infodir(infodir)
 			retval = spawn(
 				_shell_quote(ebuild_sh_binary) + " " + mydo, mysettings,
 				debug=debug, free=1, logfile=logfile)
@@ -4848,7 +4844,6 @@ def doebuild(myebuild, mydo, myroot, mysettings, debug=0, listonly=0,
 						noiselevel=-1)
 			return phase_retval
 		elif mydo in ("prerm", "postrm", "config", "info"):
-			mysettings.load_infodir(mysettings["O"])
 			retval =  spawn(
 				_shell_quote(ebuild_sh_binary) + " " + mydo,
 				mysettings, debug=debug, free=1, logfile=logfile)
