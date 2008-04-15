8a4c728615b26b5298e95131a151780edc758745
Zac Medico <zmedico@gentoo.org>
2008-04-15 23:12:58 +0000

    Inside dblink.unmerge(), call self.updateprotect() just in case anything has changed since the dblink instance was constructed. (trunk r9910)
    svn path=/main/branches/2.1.2/; revision=9911

--- pym/portage.py
+++ pym/portage.py
@@ -8689,6 +8689,7 @@ class dblink:
 		unmerge_orphans = "unmerge-orphans" in self.settings.features
 
 		if pkgfiles:
+			self.updateprotect()
 			mykeys=pkgfiles.keys()
 			mykeys.sort()
 			mykeys.reverse()
