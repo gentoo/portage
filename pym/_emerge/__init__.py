e9224bfc7ad7724fe0b36bef1f09eb606e6338f6
Zac Medico <zmedico@gentoo.org>
2008-04-24 01:49:25 +0000

    Reset the state variables for leaf node selection in depgraph.altlist() when appropriate.
    svn path=/main/trunk/; revision=9950

--- pym/_emerge/__init__.py
+++ pym/_emerge/__init__.py
@@ -3568,6 +3568,12 @@ class depgraph(object):
 						else:
 							ignored_uninstall_tasks.add(node)
 							break
+
+					# After dropping an Uninstall task, reset
+					# the state variables for leaf node selection and
+					# continue trying to select leaf nodes.
+					prefer_asap = True
+					accept_root_node = False
 					continue
 
 			if not selected_nodes:
