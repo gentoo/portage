7900ba47dbc933248a355967fb3e7debb5c079dd
Zac Medico <zmedico@gentoo.org>
2008-04-27 06:31:11 +0000

    Create a digraph.difference_update() method and use it to amortize the cost of removing nodes from the digraph.order list.
    svn path=/main/trunk/; revision=9992

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -389,6 +389,26 @@ class digraph(object):
 		del self.nodes[node]
 		self.order.remove(node)
 
+	def difference_update(self, t):
+		"""
+		Remove all given nodes from node_set. This is more efficient
+		than multiple calls to the remove() method.
+		"""
+		if isinstance(t, (list, tuple)) or \
+			not hasattr(t, "__contains__"):
+			t = frozenset(t)
+		order = []
+		for node in self.order:
+			if node not in t:
+				order.append(node)
+				continue
+			for parent in self.nodes[node][1]:
+				del self.nodes[parent][0][node]
+			for child in self.nodes[node][0]:
+				del self.nodes[child][1][node]
+			del self.nodes[node]
+		self.order = order
+
 	def remove_edge(self, child, parent):
 		"""
 		Remove edge in the direction from child to parent. Note that it is
