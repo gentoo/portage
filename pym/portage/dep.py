cc6093e21371923dbf953b7137d7deae50d5dd65
Zac Medico <zmedico@gentoo.org>
2008-05-30 00:37:34 +0000

    Fix Atom attribute handling in get_operator() and dep_getslot() to account for None return values.
    svn path=/main/trunk/; revision=10497

--- pym/portage/dep.py
+++ pym/portage/dep.py
@@ -427,8 +427,8 @@ def get_operator(mydep):
 	@return: The operator. One of:
 		'~', '=', '>', '<', '=*', '>=', or '<='
 	"""
-	operator = getattr(mydep, "operator", None)
-	if operator is not None:
+	operator = getattr(mydep, "operator", False)
+	if operator is not False:
 		return operator
 	if mydep:
 		mydep = remove_slot(mydep)
@@ -502,8 +502,8 @@ def dep_getslot(mydep):
 	@rtype: String
 	@return: The slot
 	"""
-	slot = getattr(mydep, "slot", None)
-	if slot is not None:
+	slot = getattr(mydep, "slot", False)
+	if slot is not False:
 		return slot
 	colon = mydep.find(":")
 	if colon != -1:
