33975783e067ac5b1336866e4b839bd478aca976
Fabian Groffen <grobian@gentoo.org>
2008-06-19 20:03:58 +0000

    cleanup, set proper version
    svn path=/main/branches/prefix/; revision=10729

--- tarball.sh
+++ tarball.sh
@@ -13,7 +13,6 @@ export PKG="prefix-portage"
 export TMP="/var/tmp"
 export V="$1"
 export DEST="${TMP}/${PKG}-${V}"
-#export PREVEB="2.0.49_pre2"
 
 ./tabcheck.py `grep "#\!@PYTHON@" bin/* | cut -d: -f1` `find ./ -type f -name '*.py'`
 
@@ -25,16 +24,13 @@ fi
 install -d -m0755 ${DEST}
 rsync -a --exclude='.svn' . ${DEST}
 sed -i -e '/^VERSION=/s/^.*$/VERSION="'${V}-prefix'"/' ${DEST}/pym/portage/__init__.py
-sed -i -e "s/##VERSION##/${V}/g" ${DEST}/man/emerge.1
-#rm ${DEST}/man/*.eclass.5
-
-#sed -i -e "s:\t:  :g" ${DEST}/ChangeLog
+sed -i -e "s/##VERSION##/${V}-prefix/g" ${DEST}/man/emerge.1
+sed -i -e "s/@version@/${V}/" ${DEST}/configure.in
 
 cd ${DEST}
 find -name '*~' | xargs --no-run-if-empty rm -f
 find -name '*.pyc' | xargs --no-run-if-empty rm -f
 find -name '*.pyo' | xargs --no-run-if-empty rm -f
-#chown -R root:0 ${DEST}
 cd $TMP
 rm -f ${PKG}-${V}/bin/emerge.py ${PKG}-${V}/bin/{pmake,sandbox} ${PKG}-${V}/{bin,pym}/'.#'* ${PKG}-${V}/{bin,pym}/*.{orig,diff} ${PKG}-${V}/{bin,pym}/*.py[oc]
 cd $TMP/${PKG}-${V}
