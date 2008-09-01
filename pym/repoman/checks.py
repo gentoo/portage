dbed6fa9e5acbbcb52508873ccdcc48384f71bb3
Zac Medico <zmedico@gentoo.org>
2008-09-01 06:31:39 +0000

    Adjust EMakeParallelDisabled.re to require at least one space after emake.
    svn path=/main/trunk/; revision=11482

--- pym/repoman/checks.py
+++ pym/repoman/checks.py
@@ -271,7 +271,7 @@ class IUseUndefined(LineCheck):
 class EMakeParallelDisabled(LineCheck):
 	"""Check for emake -j1 calls which disable parallelization."""
 	repoman_check_name = 'ebuild.minorsyn'
-	re = re.compile(r'^\s*emake\s*-j\s*1\s')
+	re = re.compile(r'^\s*emake\s+-j\s*1\s')
 	error = errors.EMAKE_PARALLEL_DISABLED
 
 class DeprecatedBindnowFlags(LineCheck):
