6a97ca3bfaa4621d00bb337a20af7c821dd09eee
Zac Medico <zmedico@gentoo.org>
2008-09-26 03:13:33 +0000

    Add back code removed in r11553 and fix logic to avoid IndexError.
    svn path=/main/trunk/; revision=11556

--- pym/_emerge/__init__.py
+++ pym/_emerge/__init__.py
@@ -10893,7 +10893,11 @@ def display_preserved_libs(vardbapi):
 				owners = vardbapi._owners.getFileOwnerMap(consumers[:MAX_DISPLAY+2])
 				for c in consumers[:MAX_DISPLAY]:
 					print colorize("WARN", " * ") + "     used by %s (%s)" % (c, ", ".join([x.mycpv for x in owners[c]]))
-				if len(consumers) > MAX_DISPLAY:
+				if len(consumers) == MAX_DISPLAY + 1:
+					print colorize("WARN", " * ") + "     used by %s (%s)" % \
+						(consumers[MAX_DISPLAY], ", ".join(
+						x.mycpv for x in owners[consumers[MAX_DISPLAY]]))
+				elif len(consumers) > MAX_DISPLAY:
 					print colorize("WARN", " * ") + "     used by %d other files" % (len(consumers) - MAX_DISPLAY)
 		print "Use " + colorize("GOOD", "emerge @preserved-rebuild") + " to rebuild packages using these libraries"
 
