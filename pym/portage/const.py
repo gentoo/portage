775ec14f4e2a1086122fdfd54f14c9fd23052119
Fabian Groffen <grobian@gentoo.org>
2008-10-02 15:16:28 +0000

    Allow to set EAPIPREFIX.  Not sure how hazardous this is, but it's darn handy at work (using Prefix repoman to commit on a non-Prefix overlay)
    svn path=/main/branches/prefix/; revision=11612

--- pym/portage/const.py
+++ pym/portage/const.py
@@ -14,6 +14,8 @@ BPREFIX = EPREFIX
 # pick up EPREFIX from the environment if set
 if "EPREFIX" in os.environ:
 	EPREFIX = os.path.normpath(os.environ["EPREFIX"])
+if "EAPIPREFIX" in os.environ:
+	EAPIPREFIX = os.environ["EAPIPREFIX"]
 
 # ===========================================================================
 # START OF CONSTANTS -- START OF CONSTANTS -- START OF CONSTANTS -- START OF
