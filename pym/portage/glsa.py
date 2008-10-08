gentoo/portage

commit ceb5fc5058cc7595ac30f08d912ff3544d0639fb

add support for slot dependencies (original patch by Robert Buchholz <rbu@gentoo.org>)
svn path=/main/trunk/; revision=11593

pym/portage/glsa.py
@@ -226,6 +226,8 @@ def makeAtom(pkgname, versionNode):
 	rValue = opMapping[versionNode.getAttribute("range")] \
 				+ pkgname \
 				+ "-" + getText(versionNode, format="strip")
+	if "slot" in versionNode.attributes and versionNode.getAttribute("slot") != "*":
+		rValue += ":"+versionNode.getAttribute("slot")
 	return str(rValue)
 
 def makeVersion(versionNode):
@@ -239,8 +241,11 @@ def makeVersion(versionNode):
 	@rtype:		String
 	@return:	the version string
 	"""
-	return opMapping[versionNode.getAttribute("range")] \
+	rValue = opMapping[versionNode.getAttribute("range")] \
 			+ getText(versionNode, format="strip")
+	if "slot" in versionNode.attributes and versionNode.getAttribute("slot") != "*":
+		rValue += ":"+versionNode.getAttribute("slot")
+	return rValue
 
 def match(atom, dbapi, match_type="default"):
 	"""
@@ -283,9 +288,9 @@ def revisionMatch(revisionAtom, dbapi, match_type="default"):
 	@return:	a list with the matching versions
 	"""
 	if match_type == "default" or not hasattr(dbapi, "xmatch"):
-		mylist = dbapi.match(re.sub("-r[0-9]+$", "", revisionAtom[2:]))
+		mylist = dbapi.match(re.sub(r'-r[0-9]+(:[^ ]+)?$', r'\1', revisionAtom[2:]))
 	else:
-		mylist = dbapi.xmatch(match_type, re.sub("-r[0-9]+$", "", revisionAtom[2:]))
+		mylist = dbapi.xmatch(match_type, re.sub(r'-r[0-9]+(:[^ ]+)?$', r'\1', revisionAtom[2:]))
 	rValue = []
 	for v in mylist:
 		r1 = pkgsplit(v)[-1][1:]

commit 61e5f233600bb082943eb095554771201d757bce

Fix apparent breakage from r11593 (slot dep support): * Handle KeyError from element.getAttribute() in makeAtom() and makeVersion(). * Avoid 'sre_constants.error: unmatched group' exceptions in revisionMatch()   when the atom does not have a slot.
svn path=/main/trunk/; revision=11659

pym/portage/glsa.py
@@ -226,8 +226,13 @@ def makeAtom(pkgname, versionNode):
 	rValue = opMapping[versionNode.getAttribute("range")] \
 				+ pkgname \
 				+ "-" + getText(versionNode, format="strip")
-	if "slot" in versionNode.attributes and versionNode.getAttribute("slot") != "*":
-		rValue += ":"+versionNode.getAttribute("slot")
+	try:
+		slot = versionNode.getAttribute("slot").strip()
+	except KeyError:
+		pass
+	else:
+		if slot and slot != "*":
+			rValue += ":" + slot
 	return str(rValue)
 
 def makeVersion(versionNode):
@@ -243,8 +248,13 @@ def makeVersion(versionNode):
 	"""
 	rValue = opMapping[versionNode.getAttribute("range")] \
 			+ getText(versionNode, format="strip")
-	if "slot" in versionNode.attributes and versionNode.getAttribute("slot") != "*":
-		rValue += ":"+versionNode.getAttribute("slot")
+	try:
+		slot = versionNode.getAttribute("slot").strip()
+	except KeyError:
+		pass
+	else:
+		if slot and slot != "*":
+			rValue += ":" + slot
 	return rValue
 
 def match(atom, dbapi, match_type="default"):
@@ -288,9 +298,15 @@ def revisionMatch(revisionAtom, dbapi, match_type="default"):
 	@return:	a list with the matching versions
 	"""
 	if match_type == "default" or not hasattr(dbapi, "xmatch"):
-		mylist = dbapi.match(re.sub(r'-r[0-9]+(:[^ ]+)?$', r'\1', revisionAtom[2:]))
+		if ":" in revisionAtom:
+			mylist = dbapi.match(re.sub(r'-r[0-9]+(:[^ ]+)?$', r'\1', revisionAtom[2:]))
+		else:
+			mylist = dbapi.match(re.sub("-r[0-9]+$", "", revisionAtom[2:]))
 	else:
-		mylist = dbapi.xmatch(match_type, re.sub(r'-r[0-9]+(:[^ ]+)?$', r'\1', revisionAtom[2:]))
+		if ":" in revisionAtom:
+			mylist = dbapi.xmatch(match_type, re.sub(r'-r[0-9]+(:[^ ]+)?$', r'\1', revisionAtom[2:]))
+		else:
+			mylist = dbapi.xmatch(match_type, re.sub("-r[0-9]+$", "", revisionAtom[2:]))
 	rValue = []
 	for v in mylist:
 		r1 = pkgsplit(v)[-1][1:]