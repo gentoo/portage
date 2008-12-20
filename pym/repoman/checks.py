8c2d3450a6d0a14cd779057ce8e7b4e2003ca36b
Zac Medico <zmedico@gentoo.org>
2008-12-20 20:24:18 +0000

    Adjust the EbuildNestedDie pattern so that it only matches when ( is surrounded by whitespace. This filters out false positives triggered by sed expressions in net-analyzer/amap-5.2-r1 and media-libs/libsndfile-1.0.17-r1. Thanks to Diego Pettenò <flameeyes@g.o> for reporting. (trunk r12265)
    svn path=/main/branches/2.1.6/; revision=12267

--- pym/repoman/checks.py
+++ pym/repoman/checks.py
@@ -177,7 +177,7 @@ class EbuildNestedDie(LineCheck):
 	"""Check ebuild for nested die statements (die statements in subshells"""
 	
 	repoman_check_name = 'ebuild.nesteddie'
-	nesteddie_re = re.compile(r'^[^#]*\([^)]*\bdie\b')
+	nesteddie_re = re.compile(r'^[^#]*\s\(\s[^)]*\bdie\b')
 	
 	def check(self, num, line):
 		if self.nesteddie_re.match(line):
