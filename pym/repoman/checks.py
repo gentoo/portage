44f33a6c087f8d8c3506d9564db683e425ecb848
Zac Medico <zmedico@gentoo.org>
2008-12-20 19:36:50 +0000

    Bug #251591 - Adjust the InheritAutotools pattern to treat more things similar to whitespace. Now it recognizes escaped newlines, || operators, and && operators.
    svn path=/main/trunk/; revision=12260

--- pym/repoman/checks.py
+++ pym/repoman/checks.py
@@ -242,8 +242,8 @@ class InheritAutotools(LineCheck):
 	_autotools_funcs = (
 		"eaclocal", "eautoconf", "eautoheader",
 		"eautomake", "eautoreconf", "_elibtoolize")
-	_autotools_func_re = re.compile(r'(^|\s)(' + \
-		"|".join(_autotools_funcs) + ')(\s|$)')
+	_autotools_func_re = re.compile(r'(^|\s|\|\||&&)(' + \
+		"|".join(_autotools_funcs) + r')(\s|\|\||&&|\\?$)')
 	# Exempt eclasses:
 	# git - An EGIT_BOOTSTRAP variable may be used to call one of
 	#       the autotools functions.
