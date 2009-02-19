b91752f159ff292b4be59b11c9d1c4a7a16f58cf
Zac Medico <zmedico@gentoo.org>
2009-02-19 03:34:05 +0000

    Implement UserDict.__contains__() and __iter__().
    svn path=/main/trunk/; revision=12630

--- pym/portage/cache/mappings.py
+++ pym/portage/cache/mappings.py
@@ -144,6 +144,12 @@ class UserDict(MutableMapping):
 	def __repr__(self):
 		return repr(self.data)
 
+	def __contains__(self, key):
+		return key in self.data
+
+	def __iter__(self):
+		return iter(self.data)
+
 	def __len__(self):
 		return len(self.data)
 
