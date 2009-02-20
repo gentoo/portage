fb05e64acc278e4395b4209e2219c404b3697252
Zac Medico <zmedico@gentoo.org>
2009-02-20 06:15:34 +0000

    Add a new portage.utils.lazy_import() function which behaves similar to the snakeoil.demandload.demandload() function.
    svn path=/main/trunk/; revision=12661

--- pym/portage/util.py
+++ pym/portage/util.py
@@ -10,6 +10,7 @@ import shlex
 import stat
 import string
 import sys
+import types
 
 from portage.exception import PortageException, FileNotFound, \
        OperationNotPermitted, PermissionDenied, ReadOnlyFileSystem
@@ -341,6 +342,8 @@ class ObjectProxy(object):
 	for implementing lazy initialization.
 	"""
 
+	__slots__ = ()
+
 	def _get_target(self):
 		raise NotImplementedError(self)
 
@@ -391,6 +394,104 @@ class ObjectProxy(object):
 
 	def __nonzero__(self):
 		return bool(object.__getattribute__(self, '_get_target')())
+
+class _LazyImport(ObjectProxy):
+
+	__slots__ = ('_scope', '_alias', '_name', '_target')
+
+	def __init__(self, scope, alias, name):
+		ObjectProxy.__init__(self)
+		object.__setattr__(self, '_scope', scope)
+		object.__setattr__(self, '_alias', alias)
+		object.__setattr__(self, '_name', name)
+
+	def _get_target(self):
+		try:
+			return object.__getattribute__(self, '_target')
+		except AttributeError:
+			pass
+		name = object.__getattribute__(self, '_name')
+		__import__(name)
+		target = sys.modules[name]
+		object.__setattr__(self, '_target', target)
+		object.__getattribute__(self, '_scope')[
+			object.__getattribute__(self, '_alias')] = target
+		return target
+
+class _LazyImportFrom(_LazyImport):
+
+	__slots__ = ()
+
+	def _get_target(self):
+		try:
+			return object.__getattribute__(self, '_target')
+		except AttributeError:
+			pass
+		name = object.__getattribute__(self, '_name')
+		components = name.split('.')
+		parent_name = '.'.join(components[:-1])
+		__import__(parent_name)
+		target = getattr(sys.modules[parent_name], components[-1])
+		object.__setattr__(self, '_target', target)
+		object.__getattribute__(self, '_scope')[
+			object.__getattribute__(self, '_alias')] = target
+		return target
+
+def lazy_import(scope, *args):
+	"""
+	Create a proxy in the given scope in order to performa a lazy import.
+
+	Syntax         Result
+	foo            import foo
+	foo:bar,baz    from foo import bar, baz
+	foo:bar@baz    from foo import bar as baz
+
+	@param scope: the scope in which to place the import, typically globals()
+	@type myfilename: dict
+	@param args: module names to import
+	@type args: strings
+	"""
+
+	for s in args:
+		parts = s.split(':', 1)
+		if len(parts) == 1:
+			name = s
+
+			if not name or not isinstance(name, basestring):
+				raise ValueError(name)
+
+			components = name.split('.')
+			parent_scope = scope
+			for i in xrange(len(components)):
+				alias = components[i]
+				mod = parent_scope.get(alias)
+				if isinstance(mod, types.ModuleType):
+					parent_scope = mod.__dict__
+					continue
+				if i < len(components) - 1:
+					parent_name = ".".join(components[:i+1])
+					__import__(parent_name)
+					mod = sys.modules.get(parent_name)
+					if not isinstance(mod, types.ModuleType):
+						# raise an exception
+						__import__(name)
+					parent_scope[alias] = mod
+					parent_scope = mod.__dict__
+					continue
+				parent_scope[alias] = _LazyImport(parent_scope, alias, name)
+
+		else:
+			name, fromlist = parts
+			fromlist = fromlist.split(',')
+			for s in fromlist:
+				alias = s.split('@', 1)
+				if len(alias) == 1:
+					alias = alias[0]
+					orig = alias
+				else:
+					orig, alias = alias
+				scope[alias] = _LazyImportFrom(scope, alias,
+					name + '.' + orig)
 
 class _tolerant_shlex(shlex.shlex):
 	def sourcehook(self, newfile):
