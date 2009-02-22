354f87f6aacc49c3a958cfa383ecd13cca2b6c19
Zac Medico <zmedico@gentoo.org>
2009-02-22 23:39:03 +0000

    When decoding term codes inside the JobStatusDisplay constructor, use the 'replace' error handling scheme in order to avoid triggering a potential UnicodeError.
    svn path=/main/trunk/; revision=12687

--- pym/_emerge/__init__.py
+++ pym/_emerge/__init__.py
@@ -9638,9 +9638,10 @@ class JobStatusDisplay(object):
 			for k, capname in self._termcap_name_map.iteritems():
 				term_codes[k] = self._default_term_codes[capname]
 			object.__setattr__(self, "_term_codes", term_codes)
+		encoding = sys.getdefaultencoding()
 		for k, v in self._term_codes.items():
 			if not isinstance(v, str):
-				self._term_codes[k] = v.decode()
+				self._term_codes[k] = v.decode(encoding, 'replace')
 
 	def _init_term(self):
 		"""
