77bfa7607a963f19bd3fa27f3469e5f4bf5ef93b
Zac Medico <zmedico@gentoo.org>
2009-03-08 21:27:24 +0000

    Fix typo from previous commit.
    svn path=/main/trunk/; revision=12792

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -1177,7 +1177,7 @@ inherit() {
 		while [[ $__export_funcs_var =~ [-.+] ]] ; do
 			__export_funcs_var=${__export_funcs_var/-/__dash__}
 			__export_funcs_var=${__export_funcs_var/./__dot__}
-			__export_funcs_var=${__export_funcs_var/./__plus__}
+			__export_funcs_var=${__export_funcs_var/+/__plus__}
 		done
 		unset $__export_funcs_var
 
