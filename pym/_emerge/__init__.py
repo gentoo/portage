a2d94563a6659b9fb5f71989d0ea06c42820b1b7
Zac Medico <zmedico@gentoo.org>
2009-04-30 06:47:20 +0000

    Add a --root option that sets $ROOT (complements the --config-root option). (trunk r13246)
    svn path=/main/branches/2.1.6/; revision=13425

--- pym/_emerge/__init__.py
+++ pym/_emerge/__init__.py
@@ -14445,7 +14445,11 @@ def parse_opts(tmpcmdline, silent=False):
 			"help":"specify conditions to trigger package reinstallation",
 			"type":"choice",
 			"choices":["changed-use"]
-		}
+		},
+		"--root": {
+		 "help"   : "specify the target root filesystem for merging packages",
+		 "action" : "store"
+		},
 	}
 
 	from optparse import OptionParser
@@ -14838,6 +14842,8 @@ def emerge_main():
 		os.environ["PORTAGE_DEBUG"] = "1"
 	if "--config-root" in myopts:
 		os.environ["PORTAGE_CONFIGROOT"] = myopts["--config-root"]
+	if "--root" in myopts:
+		os.environ["ROOT"] = myopts["--root"]
 
 	# Portage needs to ensure a sane umask for the files it creates.
 	os.umask(022)
