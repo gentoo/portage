d4efe1af81cffdfb62f29305be0af91ed8d91306
Zac Medico <zmedico@gentoo.org>
2009-04-30 21:14:27 +0000

    Enable use() qa checks via PORTAGE_IUSE for all ebuild phases and for  binary packages since it should work fine now.
    svn path=/main/trunk/; revision=13570

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -142,12 +142,7 @@ useq() {
 	fi
 
 	# Make sure we have this USE flag in IUSE
-	if [[ -n ${PORTAGE_IUSE} ]] && \
-		[[ -n ${EBUILD_PHASE} ]] && \
-		! hasq ${EBUILD_PHASE} config depend info prerm postrm postinst && \
-		[[ ${EMERGE_FROM} != binary ]] ; then
-		# TODO: Implement PORTAGE_IUSE for binary packages. Currently,
-		# it is only valid for build time phases.
+	if [[ -n $PORTAGE_IUSE && -n $EBUILD_PHASE ]] ; then
 		[[ $u =~ $PORTAGE_IUSE ]] || \
 			eqawarn "QA Notice: USE Flag '${u}' not" \
 				"in IUSE for ${CATEGORY}/${PF}"
