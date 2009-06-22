69500d28402817fb70c104501b5c8f4f54650f4a
Zac Medico <zmedico@gentoo.org>
2009-06-22 18:21:56 +0000

    Bug #275047 - Split _emerge/__init__.py into smaller pieces (part 2). Thanks to Sebastian Mingramm (few) <s.mingramm@gmx.de> for this patch.
    svn path=/main/trunk/; revision=13667

---
+++ pym/_emerge/UninstallFailure.py
@@ -0,0 +1,18 @@
+try:
+	import portage
+except ImportError:
+	from os import path as osp
+	import sys
+	sys.path.insert(0, osp.join(osp.dirname(osp.dirname(osp.realpath(__file__))), "pym"))
+	import portage
+	
+class UninstallFailure(portage.exception.PortageException):
+	"""
+	An instance of this class is raised by unmerge() when
+	an uninstallation fails.
+	"""
+	status = 1
+	def __init__(self, *pargs):
+		portage.exception.PortageException.__init__(self, pargs)
+		if pargs:
+			self.status = pargs[0]
