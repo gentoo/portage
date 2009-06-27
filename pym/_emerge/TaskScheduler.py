a9da53056456b311bed17b92eba8caf218d86028
Fabian Groffen <grobian@gentoo.org>
2009-06-27 13:19:35 +0000

    Merged from trunk -r13667:13668
       | 13668   | Bug #275047 - Split _emerge/__init__.py into smaller pieces  |
       | zmedico | (part 3). Thanks to Sebastian Mingramm (few)                 |
       |         | <s.mingramm@gmx.de> for this patch.                          |
    
    svn path=/main/branches/prefix/; revision=13707

---
+++ pym/_emerge/TaskScheduler.py
@@ -0,0 +1,21 @@
+from _emerge.QueueScheduler import QueueScheduler
+from _emerge.SequentialTaskQueue import SequentialTaskQueue
+
+class TaskScheduler(object):
+
+	"""
+	A simple way to handle scheduling of AsynchrousTask instances. Simply
+	add tasks and call run(). The run() method returns when no tasks remain.
+	"""
+
+	def __init__(self, max_jobs=None, max_load=None):
+		self._queue = SequentialTaskQueue(max_jobs=max_jobs)
+		self._scheduler = QueueScheduler(
+			max_jobs=max_jobs, max_load=max_load)
+		self.sched_iface = self._scheduler.sched_iface
+		self.run = self._scheduler.run
+		self._scheduler.add(self._queue)
+
+	def add(self, task):
+		self._queue.add(task)
+
