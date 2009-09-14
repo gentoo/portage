a0c41d09a875e925e52bce2fc1f3a892f5d546ae
Zac Medico <zmedico@gentoo.org>
2009-09-14 05:54:39 +0000

    Add more test cases.
    svn path=/main/trunk/; revision=14253

--- pym/portage/tests/dep/test_isvalidatom.py
+++ pym/portage/tests/dep/test_isvalidatom.py
@@ -70,9 +70,12 @@ class IsValidAtom(TestCase):
 			  ( "null/portage-2.1*", False ),
 			  ( "app-doc/php-docs-20071125", False),
 			  ( "app-doc/php-docs-20071125-r2", False),
+			  ( "=foo/bar-1-r1-1-r1", False ),
+			  ( "foo/-z-1", False ),
 
 			# These are invalid because pkg name must not end in hyphen
 			# followed by numbers
+			  ( "=foo/bar-1-r1-1-r1", False ),
 			  ( "=foo/bar-123-1", False ),
 			  ( "=foo/bar-123-1*", False ),
 			  ( "foo/bar-123", False ),
