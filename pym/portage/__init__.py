3ce278ba6c7fdb53b470124d8dfa8f7faf3f1ea8
Zac Medico <zmedico@gentoo.org>
2009-10-10 01:10:53 +0000

    Disable EAPI 3_pre1.
    svn path=/main/branches/2.1.7/; revision=14537

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -5975,7 +5975,7 @@ def _spawn_misc_sh(mysettings, commands, **kwargs):
 
 	return rval
 
-_testing_eapis = frozenset(["3_pre1"])
+_testing_eapis = frozenset()
 _deprecated_eapis = frozenset(["2_pre3", "2_pre2", "2_pre1"])
 
 def _eapi_is_deprecated(eapi):
