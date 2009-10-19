gentoo/portage

commit fb15e6806685fa8937b4c99d24b48cc7fb62e852

Bug #273648 - EAPI 3 - the following condtions must be met: 1. The A variable contains no items. 2. The phase function in question is not in DEFINED_PHASES. 3. None of the phase functions unpack, prepare, configure, compile or install,    if supported by the EAPI in question and occurring prior to the phase about    to be executed, are in DEFINED_PHASES.
Thanks to Jonathan Callen <abcd@g.o> for this patch.

svn path=/main/trunk/; revision=14616

bin/ebuild.sh
@@ -905,13 +905,17 @@ dyn_prepare() {
 		return 0
 	fi
 
-	local srcdir
 	if [[ -d $S ]] ; then
-		srcdir=$S
+		cd "${S}"
+	elif hasq $EAPI 0 1 2; then
+		cd "${WORKDIR}"
+	elif [[ -z ${A} ]] && \
+			! hasq unpack ${DEFINED_PHASES} && \
+			! hasq prepare ${DEFINED_PHASES} ; then
+		cd "${WORKDIR}"
 	else
-		srcdir=$WORKDIR
+		die "The source directory '${S}' doesn't exist"
 	fi
-	cd "$srcdir"
 
 	trap abort_prepare SIGINT SIGQUIT
 
@@ -932,6 +936,19 @@ dyn_configure() {
 		vecho ">>> It appears that '$PF' is already configured; skipping."
 		vecho ">>> Remove '$PORTAGE_BUILDDIR/.configured' to force configuration."
 		return 0
+	fi
+
+	if [[ -d $S ]] ; then
+		cd "${S}"
+	elif hasq $EAPI 0 1 2; then
+		cd "${WORKDIR}"
+	elif [[ -z ${A} ]] && \
+			! hasq unpack ${DEFINED_PHASES} && \
+			! hasq prepare ${DEFINED_PHASES} && \
+			! hasq configure ${DEFINED_PHASES} ; then
+		cd "${WORKDIR}"
+	else
+		die "The source directory '${S}' doesn't exist"
 	fi
 
 	trap abort_configure SIGINT SIGQUIT
@@ -955,6 +972,20 @@ dyn_compile() {
 		vecho ">>> It appears that '${PF}' is already compiled; skipping."
 		vecho ">>> Remove '$PORTAGE_BUILDDIR/.compiled' to force compilation."
 		return 0
+	fi
+
+	if [[ -d $S ]] ; then
+		cd "${S}"
+	elif hasq $EAPI 0 1 2; then
+		cd "${WORKDIR}"
+	elif [[ -z ${A} ]] && \
+			! hasq unpack ${DEFINED_PHASES} && \
+			! hasq prepare ${DEFINED_PHASES} && \
+			! hasq configure ${DEFINED_PHASES} && \
+			! hasq compile ${DEFINED_PHASES} ; then
+		cd "${WORKDIR}"
+	else
+		die "The source directory '${S}' doesn't exist"
 	fi
 
 	trap abort_compile SIGINT SIGQUIT
@@ -989,6 +1020,7 @@ dyn_test() {
 	else
 		cd "${WORKDIR}"
 	fi
+
 	if ! hasq test $FEATURES && [ "${EBUILD_FORCE_TEST}" != "1" ]; then
 		vecho ">>> Test phase [not enabled]: ${CATEGORY}/${PF}"
 	elif hasq test $RESTRICT; then
@@ -1023,11 +1055,22 @@ dyn_install() {
 	ebuild_phase pre_src_install
 	rm -rf "${PORTAGE_BUILDDIR}/image"
 	mkdir "${PORTAGE_BUILDDIR}/image"
-	if [ -d "${S}" ]; then
+	local srcdir
+	if [[ -d $S ]] ; then
 		cd "${S}"
-	else
+	elif hasq $EAPI 0 1 2; then
 		cd "${WORKDIR}"
+	elif [[ -z ${A} ]] && \
+			! hasq unpack ${DEFINED_PHASES} && \
+			! hasq prepare ${DEFINED_PHASES} && \
+			! hasq configure ${DEFINED_PHASES} && \
+			! hasq compile ${DEFINED_PHASES} && \
+			! hasq install ${DEFINED_PHASES}; then
+		cd "${WORKDIR}"
+	else
+		die "The source directory '${S}' doesn't exist"
 	fi
+
 	vecho
 	vecho ">>> Install ${PF} into ${D} category ${CATEGORY}"
 	#our custom version of libtool uses $S and $D to fix
@@ -2014,13 +2057,6 @@ ebuild_main() {
 				cp "$EBUILD" "build-info/$PF.ebuild"
 			fi
 
-			local srcdir
-			if [[ -d $S ]] ; then
-				srcdir=$S
-			else
-				srcdir=$WORKDIR
-			fi
-			cd "$srcdir"
 			#our custom version of libtool uses $S and $D to fix
 			#invalid paths in .la files
 			export S D

commit 61b870e0f2d3c1193ed0342ef787726751ea3b1e

Fix messages broken in r14616. Thanks to Arfrever for reporting and thanks to Jonathan Callen <abcd@g.o> for this patch. (trunk r14675)
svn path=/main/branches/2.1.7/; revision=14688

bin/ebuild.sh
@@ -915,7 +915,7 @@ dyn_prepare() {
 
 	[ -n "$EBUILD_PHASE" ] && rm -f "$T/logging/$EBUILD_PHASE"
 	ebuild_phase pre_src_prepare
-	vecho ">>> Preparing source in $srcdir ..."
+	vecho ">>> Preparing source in $PWD ..."
 	ebuild_phase src_prepare
 	touch "$PORTAGE_BUILDDIR"/.prepared
 	vecho ">>> Source prepared."
@@ -947,7 +947,7 @@ dyn_configure() {
 	[ -n "$EBUILD_PHASE" ] && rm -f "$T/logging/$EBUILD_PHASE"
 	ebuild_phase pre_src_configure
 
-	vecho ">>> Configuring source in $srcdir ..."
+	vecho ">>> Configuring source in $PWD ..."
 	ebuild_phase src_configure
 	touch "$PORTAGE_BUILDDIR"/.configured
 	vecho ">>> Source configured."
@@ -980,7 +980,7 @@ dyn_compile() {
 	[ -n "$EBUILD_PHASE" ] && rm -f "$T/logging/$EBUILD_PHASE"
 	ebuild_phase pre_src_compile
 
-	vecho ">>> Compiling source in ${srcdir} ..."
+	vecho ">>> Compiling source in $PWD ..."
 	ebuild_phase src_compile
 	touch "$PORTAGE_BUILDDIR"/.compiled
 	vecho ">>> Source compiled."
@@ -1042,7 +1042,6 @@ dyn_install() {
 	ebuild_phase pre_src_install
 	rm -rf "${PORTAGE_BUILDDIR}/image"
 	mkdir "${PORTAGE_BUILDDIR}/image"
-	local srcdir
 	if [[ -d $S ]] ; then
 		cd "${S}"
 	elif hasq $EAPI 0 1 2; then