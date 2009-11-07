0d4856eb936f8c10ef9fdec33aefda94c4490f09
Zac Medico <zmedico@gentoo.org>
2009-11-07 01:11:33 +0000

    Use writemsg instead of print, to send debug messages to stderr. (trunk r14784)
    svn path=/main/branches/2.1.7/; revision=14797

--- pym/_emerge/depgraph.py
+++ pym/_emerge/depgraph.py
@@ -1133,10 +1133,12 @@ class depgraph(object):
 				if not dep_string:
 					continue
 				if debug:
-					print()
-					print("Parent:   ", jbigkey)
-					print("Depstring:", dep_string)
-					print("Priority:", dep_priority)
+					writemsg_level("\nParent:    %s\n" % (pkg,),
+						noiselevel=-1, level=logging.DEBUG)
+					writemsg_level("Depstring: %s\n" % (dep_string,),
+						noiselevel=-1, level=logging.DEBUG)
+					writemsg_level("Priority:  %s\n" % (dep_priority,),
+						noiselevel=-1, level=logging.DEBUG)
 
 				try:
 
@@ -1272,7 +1274,8 @@ class depgraph(object):
 					return 0
 
 		if debug:
-			print("Exiting...", pkg)
+			writemsg_level("Exiting... %s\n" % (pkg,),
+				noiselevel=-1, level=logging.DEBUG)
 
 		return 1
 
