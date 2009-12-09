fe34505cd30265b5826e4a34d9cffae7e82efe78
Zac Medico <zmedico@gentoo.org>
2009-12-09 06:10:53 +0000

    Add localization variables (LANG and LC_*) to the environment whitelist and filter them from the saved environment (so user settings override those of binary packages). Thanks to Fabio Erculiani <lxnay@g.o> for reporting.
    svn path=/main/trunk/; revision=14975

--- bin/isolated-functions.sh
+++ bin/isolated-functions.sh
@@ -506,6 +506,11 @@ save_ebuild_env() {
 		unset ECHANGELOG_USER GPG_AGENT_INFO \
 		SSH_AGENT_PID SSH_AUTH_SOCK STY WINDOW XAUTHORITY
 
+		# localization settings
+		unset LANG LC_COLLATE LC_CTYPE LC_MESSAGES \
+			LC_MONETARY LC_NUMERIC LC_TIME LC_PAPER \
+			LC_ALL
+
 		# CCACHE and DISTCC config
 		unset ${!CCACHE_*} ${!DISTCC_*}
 
