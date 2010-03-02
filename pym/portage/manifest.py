5717e4d7566ea1afda8031229d5b3de0dc3b0f7e
Zac Medico <zmedico@gentoo.org>
2010-03-02 19:49:24 +0000

    Fix concatenation of list + tuple in Manifest._createManifestEntries(). (trunk r15289)
    svn path=/main/branches/2.1.7/; revision=15528

--- pym/portage/manifest.py
+++ pym/portage/manifest.py
@@ -205,6 +205,8 @@ class Manifest(object):
 		return myhashdict
 
 	def _createManifestEntries(self):
+		valid_hashes = set(portage.const.MANIFEST2_HASH_FUNCTIONS)
+		valid_hashes.add('size')
 		mytypes = list(self.fhashdict)
 		mytypes.sort()
 		for t in mytypes:
@@ -213,10 +215,8 @@ class Manifest(object):
 			for f in myfiles:
 				myentry = Manifest2Entry(
 					type=t, name=f, hashes=self.fhashdict[t][f].copy())
-				myhashkeys = list(myentry.hashes)
-				myhashkeys.sort()
-				for h in myhashkeys:
-					if h not in ["size"] + portage.const.MANIFEST2_HASH_FUNCTIONS:
+				for h in list(myentry.hashes):
+					if h not in valid_hashes:
 						del myentry.hashes[h]
 				yield myentry
 
