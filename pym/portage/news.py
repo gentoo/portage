c3dbaac02e716b3b8cc8ad7fa7250fbbb82d1373
Zac Medico <zmedico@gentoo.org>
2010-03-02 19:51:44 +0000

    Deprecate pordbapi.mysettings since the portdbapi.settings alias is now supported by stable portage. (trunk r15306)
    svn path=/main/branches/2.1.7/; revision=15541

--- pym/portage/news.py
+++ pym/portage/news.py
@@ -59,9 +59,9 @@ class NewsManager(object):
 		portdir = portdb.porttree_root
 		profiles_base = os.path.join(portdir, 'profiles') + os.path.sep
 		profile_path = None
-		if portdb.mysettings.profile_path:
+		if portdb.settings.profile_path:
 			profile_path = normalize_path(
-				os.path.realpath(portdb.mysettings.profile_path))
+				os.path.realpath(portdb.settings.profile_path))
 			if profile_path.startswith(profiles_base):
 				profile_path = profile_path[len(profiles_base):]
 		self._profile_path = profile_path
