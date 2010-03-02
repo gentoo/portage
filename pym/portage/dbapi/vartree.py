b6cdee63acbf7ef995f86eaf3f72ea37c94ecd03
Zac Medico <zmedico@gentoo.org>
2010-03-02 21:09:44 +0000

    Fix broken reference to portage._spawn_misc_sh(). Thanks to Arfrever for reporting. (trunk r15496)
    svn path=/main/branches/2.1.7/; revision=15697

--- pym/portage/dbapi/vartree.py
+++ pym/portage/dbapi/vartree.py
@@ -3231,7 +3231,7 @@ class dblink(object):
 					phase = 'die_hooks'
 
 				if self._scheduler is None:
-					portage._spawn_misc_sh(self.settings, [phase],
+					_spawn_misc_sh(self.settings, [phase],
 						phase=phase)
 				else:
 					self._scheduler.dblinkEbuildPhase(
