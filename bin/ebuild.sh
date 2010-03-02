c171240b9aa8b5cb778ba45a06b703f78fb8bc64
Zac Medico <zmedico@gentoo.org>
2010-03-02 20:16:29 +0000

    After src_install, save fewer variables when writing the vdb entry for a virtual package. (trunk r15349)
    svn path=/main/branches/2.1.7/; revision=15576

--- bin/ebuild.sh
+++ bin/ebuild.sh
@@ -1087,13 +1087,19 @@ dyn_install() {
 	set -f
 	local f x
 	IFS=$' \t\n\r'
-	for f in ASFLAGS CATEGORY CBUILD CC CFLAGS CHOST CTARGET CXX \
-		CXXFLAGS DEFINED_PHASES EXTRA_ECONF EXTRA_EINSTALL EXTRA_MAKE \
-		FEATURES INHERITED IUSE LDFLAGS LIBCFLAGS LIBCXXFLAGS \
+	for f in CATEGORY DEFINED_PHASES FEATURES INHERITED IUSE  \
 		PF PKGUSE SLOT KEYWORDS HOMEPAGE DESCRIPTION ; do
 		x=$(echo -n ${!f})
 		[[ -n $x ]] && echo "$x" > $f
 	done
+	if [[ $CATEGORY != virtual ]] ; then
+		for f in ASFLAGS CBUILD CC CFLAGS CHOST CTARGET CXX \
+			CXXFLAGS EXTRA_ECONF EXTRA_EINSTALL EXTRA_MAKE \
+			LDFLAGS LIBCFLAGS LIBCXXFLAGS ; do
+			x=$(echo -n ${!f})
+			[[ -n $x ]] && echo "$x" > $f
+		done
+	fi
 	echo "${USE}"       > USE
 	echo "${EAPI:-0}"   > EAPI
 	set +f
