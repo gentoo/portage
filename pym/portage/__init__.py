611df307a885fbdd2f6cd43b30deba44110743f1
Zac Medico <zmedico@gentoo.org>
2010-03-02 21:09:01 +0000

    Move deprecated_profile_check to portage.package.ebuild submodule. (trunk r15491)
    svn path=/main/branches/2.1.7/; revision=15692

--- pym/portage/__init__.py
+++ pym/portage/__init__.py
@@ -107,6 +107,8 @@ try:
 			'doebuild_environment,spawn,spawnebuild',
 		'portage.package.ebuild.config:autouse,best_from_dict,' + \
 			'check_config_instance,config',
+		'portage.package.ebuild.deprecated_profile_check:' + \
+			'deprecated_profile_check',
 		'portage.package.ebuild.digestcheck:digestcheck',
 		'portage.package.ebuild.digestgen:digestgen',
 		'portage.package.ebuild.fetch:fetch',
@@ -506,36 +508,6 @@ auxdbkeys = (
 	'UNUSED_03', 'UNUSED_02', 'UNUSED_01',
 )
 auxdbkeylen=len(auxdbkeys)
-
-def deprecated_profile_check(settings=None):
-	config_root = "/"
-	if settings is not None:
-		config_root = settings["PORTAGE_CONFIGROOT"]
-	deprecated_profile_file = os.path.join(config_root,
-		DEPRECATED_PROFILE_FILE)
-	if not os.access(deprecated_profile_file, os.R_OK):
-		return False
-	dcontent = codecs.open(_unicode_encode(deprecated_profile_file,
-		encoding=_encodings['fs'], errors='strict'), 
-		mode='r', encoding=_encodings['content'], errors='replace').readlines()
-	writemsg(colorize("BAD", _("\n!!! Your current profile is "
-		"deprecated and not supported anymore.")) + "\n", noiselevel=-1)
-	writemsg(colorize("BAD", _("!!! Use eselect profile to update your "
-		"profile.")) + "\n", noiselevel=-1)
-	if not dcontent:
-		writemsg(colorize("BAD", _("!!! Please refer to the "
-			"Gentoo Upgrading Guide.")) + "\n", noiselevel=-1)
-		return True
-	newprofile = dcontent[0]
-	writemsg(colorize("BAD", _("!!! Please upgrade to the "
-		"following profile if possible:")) + "\n", noiselevel=-1)
-	writemsg(8*" " + colorize("GOOD", newprofile) + "\n", noiselevel=-1)
-	if len(dcontent) > 1:
-		writemsg(_("To upgrade do the following steps:\n"), noiselevel=-1)
-		for myline in dcontent[1:]:
-			writemsg(myline, noiselevel=-1)
-		writemsg("\n\n", noiselevel=-1)
-	return True
 
 def portageexit():
 	if secpass > 1 and os.environ.get("SANDBOX_ON") != "1":
