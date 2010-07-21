ff84a3ce55e7f893b6686a1f7f027f7628238097
Zac Medico <zmedico@gentoo.org>
2010-07-20 18:38:20 -0700

    Move the mod_echo import to the top, so the module is pre-loaded.

--- pym/_emerge/_flush_elog_mod_echo.py
+++ pym/_emerge/_flush_elog_mod_echo.py
@@ -1,5 +1,7 @@
-# Copyright 1999-2009 Gentoo Foundation
+# Copyright 1999-2010 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
+
+from portage.elog import mod_echo
 
 def _flush_elog_mod_echo():
 	"""
@@ -8,13 +10,6 @@ def _flush_elog_mod_echo():
 	@rtype: bool
 	@returns: True if messages were shown, False otherwise.
 	"""
-	messages_shown = False
-	try:
-		from portage.elog import mod_echo
-	except ImportError:
-		pass # happens during downgrade to a version without the module
-	else:
-		messages_shown = bool(mod_echo._items)
-		mod_echo.finalize()
+	messages_shown = bool(mod_echo._items)
+	mod_echo.finalize()
 	return messages_shown
-
