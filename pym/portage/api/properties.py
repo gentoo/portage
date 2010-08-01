a93e8997e0502b54c249c2d5c25e3d67711224c3
Brian Dolbec <brian.dolbec@gmail.com>
2010-08-01 11:01:38 -0700

    define __slots__ and add the needed routines to print properly, etc.

--- pym/portage/api/properties.py
+++ pym/portage/api/properties.py
@@ -7,29 +7,46 @@
 
 """Provides a properties class to hold ebuild variables"""
 
+from portage.api.settings import settings
 
 
-class Properties:
-    """Contains all variables in an ebuild."""
-    def __init__(self, dict = None):
-        self.__dict = dict
-        
-    def __getattr__(self, name):
-        try: return self.__dict[name]
-        except: return ''
-        
-    def get_slot(self):
-        """Return ebuild slot"""
-        return self.slot
+class Properties(object):
+	"""Contains all variables in an ebuild."""
 
-    def get_keywords(self):
-        """Returns a list of strings."""
-        return self.keywords.split()
+	__slots__ = settings.keys + ["_dict", "__doc__", "__str__"]
 
-    def get_use_flags(self):
-        """Returns a list of strings."""
-        return self.iuse.split()
 
-    def get_homepages(self):
-        """Returns a list of strings."""
-        return self.homepage.split()
+	def __init__(self, _dict = None):
+		self._dict = _dict
+
+	def __getattr__(self, name):
+		try: return self._dict[name]
+		except: return ''
+
+	def __str__(self):
+		txt = []
+		for k in self.__slots__[:-3]:
+			txt.append( "	%s: %s" %(k, self.__getattr__(k)))
+		return '\n'.join(txt)
+
+	def keys(self):
+		return self.__slots__[:-3]
+
+	def get(self, name):
+		return getattr(self, name)
+
+	def get_slot(self):
+		"""Return ebuild slot"""
+		return self.slot
+
+	def get_keywords(self):
+		"""Returns a list of strings."""
+		return self.keywords.split()
+
+	def get_use_flags(self):
+		"""Returns a list of strings."""
+		return self.iuse.split()
+
+	def get_homepages(self):
+		"""Returns a list of strings."""
+		return self.homepage.split()
