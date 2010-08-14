724421343eb64987e054c255527e40be73162a83
Zac Medico <zmedico@gentoo.org>
2010-08-14 11:37:31 -0700

    Add support for NOCOLOR variable.

--- runtests.sh
+++ runtests.sh
@@ -2,12 +2,25 @@
 
 PYTHON_VERSIONS="2.6 2.7 3.1 3.2"
 
+case "${NOCOLOR:-false}" in
+	yes|true)
+		GOOD=
+		BAD=
+		NORMAL=
+		;;
+	no|false)
+		GOOD=$'\e[1;32m'
+		BAD=$'\e[1;31m'
+		NORMAL=$'\e[0m'
+		;;
+esac
+
 exit_status="0"
 for version in ${PYTHON_VERSIONS}; do
 	if [[ -x /usr/bin/python${version} ]]; then
-		echo -e "\e[1;32mTesting with Python ${version}...\e[0m"
+		echo -e "${GOOD}Testing with Python ${version}...${NORMAL}"
 		if ! PYTHONPATH="pym${PYTHONPATH:+:}${PYTHONPATH}" /usr/bin/python${version} pym/portage/tests/runTests; then
-			echo -e "\e[1;31mTesting with Python ${version} failed\e[0m"
+			echo -e "${BAD}Testing with Python ${version} failed${NORMAL}"
 			exit_status="1"
 		fi
 		echo
