6099b2436c3142281a9b66edc8cb294f6589e813
Sebastian Luther <SebastianLuther@gmx.de>
2010-09-10 22:18:21 +0200

    Tests: ebuild/test_config: Make sure -atoms in package.mask work as PMS wants it

--- pym/portage/tests/ebuild/test_config.py
+++ pym/portage/tests/ebuild/test_config.py
@@ -6,7 +6,7 @@ from portage import os
 from portage.package.ebuild.config import config
 from portage.package.ebuild._config.LicenseManager import LicenseManager
 from portage.tests import TestCase
-from portage.tests.resolver.ResolverPlayground import ResolverPlayground
+from portage.tests.resolver.ResolverPlayground import ResolverPlayground, ResolverPlaygroundTestCase
 
 class ConfigTestCase(TestCase):
 
@@ -102,4 +102,71 @@ class ConfigTestCase(TestCase):
 			self.assertEqual(lic_man.getMissingLicenses("dev-libs/D-1", [], "", "0"), [])
 		finally:
 			portage.util.noiselimit = 0
+			playground.cleanup()
+
+	def testPackageMaskOrder(self):
+
+		ebuilds = {
+			"dev-libs/A-1": { },
+			"dev-libs/B-1": { },
+			"dev-libs/C-1": { },
+			"dev-libs/D-1": { },
+			"dev-libs/E-1": { },
+		}
+
+		repo_config = {
+			"package.mask":
+				(
+					"dev-libs/A",
+					"dev-libs/C",
+				),
+		}
+
+		profile = {
+			"package.mask":
+				(
+					"-dev-libs/A",
+					"dev-libs/B",
+					"-dev-libs/B",
+					"dev-libs/D",
+				),
+		}
+
+		user_config = {
+			"package.mask":
+				(
+					"-dev-libs/C",
+					"-dev-libs/D",
+					"dev-libs/E",
+				),
+		}
+
+		test_cases = (
+				ResolverPlaygroundTestCase(
+					["dev-libs/A"],
+					success = False),
+				ResolverPlaygroundTestCase(
+					["dev-libs/B"],
+					success = True,
+					mergelist = ["dev-libs/B-1"]),
+				ResolverPlaygroundTestCase(
+					["dev-libs/C"],
+					success = True,
+					mergelist = ["dev-libs/C-1"]),
+				ResolverPlaygroundTestCase(
+					["dev-libs/D"],
+					success = True,
+					mergelist = ["dev-libs/D-1"]),
+				ResolverPlaygroundTestCase(
+					["dev-libs/E"],
+					success = False),
+		)
+
+		playground = ResolverPlayground(ebuilds=ebuilds, repo_config=repo_config, \
+			profile=profile, user_config=user_config)
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True, test_case.fail_msg)
+		finally:
 			playground.cleanup()
