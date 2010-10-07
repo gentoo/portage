c6f7933f0f3aa4b92540fbdbe7135391c2dba246
Zac Medico <zmedico@gentoo.org>
2010-10-07 14:30:23 -0700

    Make repoman ignore sets.conf.

--- pym/portage/repository/config.py
+++ pym/portage/repository/config.py
@@ -460,6 +460,7 @@ class RepoConfigLoader(object):
 def load_repository_config(settings):
 	#~ repoconfigpaths = [os.path.join(settings.global_config_path, "repos.conf")]
 	repoconfigpaths = []
-	repoconfigpaths.append(os.path.join(settings["PORTAGE_CONFIGROOT"],
-		USER_CONFIG_PATH, "repos.conf"))
+	if settings.local_config:
+		repoconfigpaths.append(os.path.join(settings["PORTAGE_CONFIGROOT"],
+			USER_CONFIG_PATH, "repos.conf"))
 	return RepoConfigLoader(repoconfigpaths, settings)
