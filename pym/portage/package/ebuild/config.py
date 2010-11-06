93ee6a847be949e7752c3c052d3eb0619e408b43
Arfrever Frehtes Taifersar Arahesis <Arfrever@Gentoo.Org>
2010-11-06 22:03:55 +0100

    Support use.unsatisfiable and package.use.unsatisfiable files.

--- pym/portage/package/ebuild/config.py
+++ pym/portage/package/ebuild/config.py
@@ -204,6 +204,7 @@ class config(object):
 			self._non_user_variables = clone._non_user_variables
 			self.usemask = clone.usemask
 			self.useforce = clone.useforce
+			self.useunsatisfiable = clone.useunsatisfiable
 			self.puse = clone.puse
 			self.user_profile_dir = clone.user_profile_dir
 			self.local_config = clone.local_config
@@ -519,6 +520,7 @@ class config(object):
 			#Initialize all USE related variables we track ourselves.
 			self.usemask = self._use_manager.getUseMask()
 			self.useforce = self._use_manager.getUseForce()
+			self.useunsatisfiable = self._use_manager.getUseUnsatisfiable()
 			self.configdict["conf"]["USE"] = \
 				self._use_manager.extract_global_USE_changes( \
 					self.configdict["conf"].get("USE", ""))
@@ -894,6 +896,7 @@ class config(object):
 				" ".join(self.make_defaults_use)
 			self.usemask = self._use_manager.getUseMask()
 			self.useforce = self._use_manager.getUseForce()
+			self.useunsatisfiable = self._use_manager.getUseUnsatisfiable()
 		self.regenerate()
 
 	class _lazy_vars(object):
@@ -1133,6 +1136,11 @@ class config(object):
 			self.usemask = usemask
 			has_changed = True
 
+		useunsatisfiable = self._use_manager.getUseUnsatisfiable(cpv_slot)
+		if useunsatisfiable != self.useunsatisfiable:
+			self.useunsatisfiable = useunsatisfiable
+			has_changed = True
+
 		oldpuse = self.puse
 		self.puse = self._use_manager.getPUSE(cpv_slot)
 		if oldpuse != self.puse:
@@ -1364,6 +1372,9 @@ class config(object):
 	def _getUseForce(self, pkg):
 		return self._use_manager.getUseForce(pkg)
 
+	def _getUseUnsatisfiable(self, pkg):
+		return self._use_manager.getUseUnsatisfiable(pkg)
+
 	def _getMaskAtom(self, cpv, metadata):
 		"""
 		Take a package and return a matching package.mask atom, or None if no
@@ -1375,7 +1386,7 @@ class config(object):
 		@param metadata: A dictionary of raw package metadata
 		@type metadata: dict
 		@rtype: String
-		@return: An matching atom string or None if one is not found.
+		@return: A matching atom string or None if one is not found.
 		"""
 		return self._mask_manager.getMaskAtom(cpv, metadata["SLOT"], metadata.get('repository'))
 
@@ -1391,7 +1402,7 @@ class config(object):
 		@param metadata: A dictionary of raw package metadata
 		@type metadata: dict
 		@rtype: String
-		@return: An matching profile atom string or None if one is not found.
+		@return: A matching profile atom string or None if one is not found.
 		"""
 
 		cp = cpv_getkey(cpv)
