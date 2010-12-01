d634d736e6cdafefa39bedf53f64113bea650ef9
Zac Medico <zmedico@gentoo.org>
2010-11-30 16:46:52 -0800

    fix AttributeError in recent use dep changes

--- pym/portage/dep/__init__.py
+++ pym/portage/dep/__init__.py
@@ -1936,26 +1936,28 @@ def match_from_list(mydep, candidate_list):
 					mydep.unevaluated_atom.use.required):
 					continue
 
-				missing_enabled = mydep.use.missing_enabled.difference(x.iuse.all)
-				missing_disabled = mydep.use.missing_disabled.difference(x.iuse.all)
+				if mydep.use:
 
-				if mydep.use.enabled:
-					if mydep.use.enabled.intersection(missing_disabled):
-						continue
-					need_enabled = mydep.use.enabled.difference(use.enabled)
-					if need_enabled:
-						need_enabled = need_enabled.difference(missing_enabled)
+					missing_enabled = mydep.use.missing_enabled.difference(x.iuse.all)
+					missing_disabled = mydep.use.missing_disabled.difference(x.iuse.all)
+
+					if mydep.use.enabled:
+						if mydep.use.enabled.intersection(missing_disabled):
+							continue
+						need_enabled = mydep.use.enabled.difference(use.enabled)
 						if need_enabled:
-							continue
+							need_enabled = need_enabled.difference(missing_enabled)
+							if need_enabled:
+								continue
 
-				if mydep.use.disabled:
-					if mydep.use.disabled.intersection(missing_enabled):
-						continue
-					need_disabled = mydep.use.disabled.intersection(use.enabled)
-					if need_disabled:
-						need_disabled = need_disabled.difference(missing_disabled)
-						if need_disabled:
+					if mydep.use.disabled:
+						if mydep.use.disabled.intersection(missing_enabled):
 							continue
+						need_disabled = mydep.use.disabled.intersection(use.enabled)
+						if need_disabled:
+							need_disabled = need_disabled.difference(missing_disabled)
+							if need_disabled:
+								continue
 
 			mylist.append(x)
 
