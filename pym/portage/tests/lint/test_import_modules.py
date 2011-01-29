1b8569c5acc898a2b7edb768034829dd1a495de1
Zac Medico <zmedico@gentoo.org>
2011-01-28 23:54:50 -0800

    Test import of all modules.

---
+++ pym/portage/tests/lint/test_import_modules.py
@@ -0,0 +1,35 @@
+# Copyright 2011 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+from portage.const import PORTAGE_PYM_PATH
+from portage.tests import TestCase
+from portage import os
+from portage import _encodings
+from portage import _unicode_decode
+
+class ImportModulesTestCase(TestCase):
+
+	def testImportModules(self):
+		for mod in self._list_modules(PORTAGE_PYM_PATH):
+			__import__(mod)
+
+	def _list_modules(self, base_dir):
+		all_modules = []
+		for parent, dirs, files in os.walk(base_dir):
+			parent = _unicode_decode(parent,
+				encoding=_encodings['fs'], errors='strict')
+			parent_mod = parent[len(PORTAGE_PYM_PATH)+1:]
+			parent_mod = parent_mod.replace("/", ".")
+			for x in files:
+				x = _unicode_decode(x,
+					encoding=_encodings['fs'], errors='strict')
+				if x[-3:] != '.py':
+					continue
+				x = x[:-3]
+				if x[-8:] == '__init__':
+					x = parent_mod
+				else:
+					x = parent_mod + "." + x
+				all_modules.append(x)
+
+		return all_modules
