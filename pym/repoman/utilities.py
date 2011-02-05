c202f11dc844edc8dea4ececc6ac8590af903cf0
Zac Medico <zmedico@gentoo.org>
2011-02-04 16:56:14 -0800

    Revert "parse_metadata_use(): omit empty text tokens."
    This reverts commit 52f6b55d51e2accaa446bc5d6920421959289baa.
    
    Since split() never return empty tokens, this patch does nothing.

--- pym/repoman/utilities.py
+++ pym/repoman/utilities.py
@@ -157,8 +157,7 @@ def parse_metadata_use(xml_tree):
 				uselist[pkg_flag] = {}
 
 			# (flag_restrict can be None)
-			uselist[pkg_flag][flag_restrict] = " ".join( \
-					[x for x in "".join(inner_text).split() if x])
+			uselist[pkg_flag][flag_restrict] = " ".join("".join(inner_text).split())
 
 	return uselist
 
