ec113fa7a8b9e8f13c4b4dfd110b10fe9a0c7cfc
Zac Medico <zmedico@gentoo.org>
2011-02-13 18:20:16 -0800

    Display: tweak output and docs for bug 58416
    Now the masking display is disabled by the --quiet option if the
    --verbose  option is not enabled simultaneously. Also, docs are
    updated as per bug 58416, comment #11 and #12.

--- pym/_emerge/resolver/output.py
+++ pym/_emerge/resolver/output.py
@@ -158,6 +158,9 @@ class Display(object):
 			self.pkgsettings["USE_EXPAND_HIDDEN"].lower().split()
 		return
 
+	def include_mask_str(self):
+		return self.conf.verbosity > 1
+
 	def gen_mask_str(self, pkg):
 		"""
 		@param pkg: _emerge.Package instance
@@ -181,7 +184,7 @@ class Display(object):
 
 	def empty_space_in_brackets(self):
 		space = ""
-		if self.conf.verbosity == 3:
+		if self.include_mask_str():
 			# add column for mask status
 			space += " "
 		return space
@@ -815,7 +818,7 @@ class Display(object):
 					self.check_system_world(pkg)
 				addl = self.set_interactive(pkg, pkg_info.ordered, addl)
 
-				if self.conf.verbosity == 3:
+				if self.include_mask_str():
 					addl += self.gen_mask_str(pkg)
 
 				if pkg.root != "/":
