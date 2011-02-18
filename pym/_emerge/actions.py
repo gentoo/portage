9fc5c27af699de5d5f5ded2b51a5e73c99e983ac
Zac Medico <zmedico@gentoo.org>
2011-02-18 08:44:31 -0800

    emerge: fix --sync cvs error message

--- pym/_emerge/actions.py
+++ pym/_emerge/actions.py
@@ -2379,9 +2379,9 @@ def action_sync(settings, trees, mtimedb, myopts, myaction):
 			retval = portage.process.spawn_bash(
 				"cd %s; exec cvs -z0 -q update -dP" % \
 				(portage._shell_quote(myportdir),), **spawn_kwargs)
-			writemsg_level("!!! cvs update error; exiting.\n",
-				noiselevel=-1, level=logging.ERROR)
 			if retval != os.EX_OK:
+				writemsg_level("!!! cvs update error; exiting.\n",
+					noiselevel=-1, level=logging.ERROR)
 				sys.exit(retval)
 		dosyncuri = syncuri
 	else:
