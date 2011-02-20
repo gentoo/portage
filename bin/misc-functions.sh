ed5aa31ee605fe6efbdcb0c0ab7deabcb12f4b77
Zac Medico <zmedico@gentoo.org>
2011-02-19 16:00:16 -0800

    Update date headers in modified files.

--- bin/misc-functions.sh
+++ bin/misc-functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 1999-2010 Gentoo Foundation
+# Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 #
 # Miscellaneous shell functions that make use of the ebuild env but don't need
