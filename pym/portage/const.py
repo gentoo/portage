1e8889b6625fc52890e838125528c7ebb4fef0a1
Zac Medico <zmedico@gentoo.org>
2011-04-13 00:59:38 -0700

    Update timestamps in headers of modified files.

--- pym/portage/const.py
+++ pym/portage/const.py
@@ -1,5 +1,5 @@
 # portage: Constants
-# Copyright 1998-2010 Gentoo Foundation
+# Copyright 1998-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 import os
