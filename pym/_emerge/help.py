16cca624a56e9f5032c83b88e9b58349ac87319f
Zac Medico <zmedico@gentoo.org>
2011-04-27 15:58:16 -0700

    emerge: add --help for 3 new options

--- pym/_emerge/help.py
+++ pym/_emerge/help.py
@@ -550,6 +550,12 @@ def help(myopts, havecolor=1):
 		print("       "+green("--nospinner"))
 		print("              Disables the spinner regardless of terminal type.")
 		print()
+		print("       " + green("--nousepkg-atoms") + " " + turquoise("ATOMS"))
+		desc = "A space separated list of package names or slot atoms." + \
+			" Emerge will ignore matching binary packages."
+		for line in wrap(desc, desc_width):
+			print(desc_indent + line)
+		print()
 		print("       "+green("--oneshot")+" ("+green("-1")+" short option)")
 		print("              Emerge as normal, but don't add packages to the world profile.")
 		print("              This package will only be updated if it is depended upon by")
@@ -626,6 +632,13 @@ def help(myopts, havecolor=1):
 		print("              changed since installation.  Unlike --newuse, this option does")
 		print("              not trigger reinstallation when flags that the user has not")
 		print("              enabled are added or removed.")
+		print()
+		print("       " + green("--reinstall-atoms") + " " + turquoise("ATOMS"))
+		desc = "A space separated list of package names or slot atoms. " + \
+			"Emerge will treat matching packages as if they are not " + \
+			"installed, and reinstall them if necessary."
+		for line in wrap(desc, desc_width):
+			print(desc_indent + line)
 		print()
 		print("       "+green("--root=DIR"))
 		desc = "Set the ROOT environment variable " + \
@@ -706,6 +719,13 @@ def help(myopts, havecolor=1):
 			(turquoise("y"), turquoise("n")))
 		desc = "Use unbuilt ebuild metadata for visibility " + \
 			"checks on built packages."
+		for line in wrap(desc, desc_width):
+			print(desc_indent + line)
+		print()
+		print("       " + green("--useoldpkg-atoms") + " " + turquoise("ATOMS"))
+		desc = "A space separated list of package names or slot atoms." + \
+			" Emerge will prefer matching binary packages over newer" + \
+			" unbuilt packages."
 		for line in wrap(desc, desc_width):
 			print(desc_indent + line)
 		print()
