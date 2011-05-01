92d5eba065f12c38cdbb2e24e13ee3b123f92245
Zac Medico <zmedico@gentoo.org>
2011-05-01 07:18:42 -0700

    runtests.sh: propagate arguments to runTests

--- runtests.sh
+++ runtests.sh
@@ -26,7 +26,7 @@ exit_status="0"
 for version in ${PYTHON_VERSIONS}; do
 	if [[ -x /usr/bin/python${version} ]]; then
 		echo -e "${GOOD}Testing with Python ${version}...${NORMAL}"
-		if ! /usr/bin/python${version} pym/portage/tests/runTests; then
+		if ! /usr/bin/python${version} pym/portage/tests/runTests "$@" ; then
 			echo -e "${BAD}Testing with Python ${version} failed${NORMAL}"
 			exit_status="1"
 		fi
