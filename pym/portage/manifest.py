80380311abe71536ac4dda8db54bf9d0b0ec03c1
Mike Gilbert <floppymaster@gmail.com>
2011-05-03 23:28:21 -0400

    Fix Manifest.updateAllHashes()

--- pym/portage/manifest.py
+++ pym/portage/manifest.py
@@ -481,7 +481,7 @@ class Manifest(object):
 	
 	def updateAllHashes(self, checkExisting=False, ignoreMissingFiles=True):
 		""" Regenerate all hashes for all files in this Manifest. """
-		for ftype in portage.const.MANIFEST2_IDENTIFIERS:
+		for idtype in portage.const.MANIFEST2_IDENTIFIERS:
 			self.updateTypeHashes(idtype, fname, checkExisting)
 
 	def updateCpvHashes(self, cpv, ignoreMissingFiles=True):
