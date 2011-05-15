2160fefc6b7e670906e5e76188daccf0e6c9d659
Zac Medico <zmedico@gentoo.org>
2011-05-15 16:28:32 -0700

    Revert "counter_tick_core: don't lock if parallel-install"
    This reverts commit 405ad9eed65393205ec28af8772f7ea45ce0371e.
    The root problem is fixed by commit
    8380ec9d7590a47b912e0d1e1db2ebb6049b903b.

--- pym/portage/dbapi/vartree.py
+++ pym/portage/dbapi/vartree.py
@@ -815,18 +815,7 @@ class vardbapi(dbapi):
 		"""
 		myroot = None
 		mycpv = None
-		locked_vdb = False
-		if "parallel-install" not in self.settings.features:
-			# If parallel-install is enabled, it's unsafe to
-			# lock the vdb here because it can deadlock the
-			# Scheduler by preventing it from servicing its
-			# poll loop which is essential for at least a
-			# couple of reasons:
-			# 1) releasing locks held by the scheduler
-			# 2) handling output of subprocesses so that they
-			#    don't deadlock due to blocking on stdout
-			self.lock()
-			locked_vdb = True
+		self.lock()
 		try:
 			counter = self.get_counter_tick_core() - 1
 			if self._cached_counter != counter:
@@ -840,8 +829,7 @@ class vardbapi(dbapi):
 					write_atomic(self._counter_path, str(counter))
 				self._cached_counter = counter
 		finally:
-			if locked_vdb:
-				self.unlock()
+			self.unlock()
 
 		return counter
 
