2dec6268b2114a147e10503d6ddc4713443a2f99
Zac Medico <zmedico@gentoo.org>
2011-05-17 22:38:07 -0700

    _find_libs_to_preserve: always return set

--- pym/portage/dbapi/vartree.py
+++ pym/portage/dbapi/vartree.py
@@ -2350,7 +2350,7 @@ class dblink(object):
 			self.vartree.dbapi._plib_registry is None or \
 			(not unmerge and self._installed_instance is None) or \
 			"preserve-libs" not in self.settings.features:
-			return None
+			return set()
 
 		os = _os_merge
 		linkmap = self.vartree.dbapi._linkmap
