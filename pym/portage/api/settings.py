4d1521a7508c8ea86a1af8fb3628448c1b44ea4b
dol-sen <brian.dolbec@gmail.com>
2011-05-25 07:45:08 -0700

    fix missing os import. Thanks, Detlev Casanova

--- pym/portage/api/settings.py
+++ pym/portage/api/settings.py
@@ -5,6 +5,7 @@
 
 """Portage API settings class for consumer apps."""
 
+import os
 
 from _emerge.actions import load_emerge_config
 import portage
@@ -15,12 +16,12 @@ class PortageSettings:
 	"""This class hold an instance of the portage settings as
 	well as some other commonly used data used in/from the  api.
 	"""
-	
+
 	def __init__(self, config_root=None):
 		# declare some globals
 		self.portdir = None
 		self.portdir_overlay = None
-		self.ACCEPT_KEYWORDS = None 
+		self.ACCEPT_KEYWORDS = None
 		self.user_config_dir = None
 		self._world = None
 		self.SystemUseFlags = None
@@ -30,24 +31,27 @@ class PortageSettings:
 		if config_root is None:
 			self.reset()
 		else:
-			
+			self.config_root = config_root
+
 
 
 	def reset_use_flags(self):
 		"""Resets the SystemUseFlags to any new
 		changes to their setting.
 		"""
-		#debug.dprint("SETTINGS: Settings.reset_use_flags();")
+		#print("SETTINGS: Settings.reset_use_flags();")
 		self.SystemUseFlags = self.settings["USE"].split()
-		#debug.dprint("SETTINGS: Settings.reset_use_flags(); SystemUseFlags = " + str(SystemUseFlags))
+		#print("SETTINGS: Settings.reset_use_flags(); SystemUseFlags = " +
+			#str(self.SystemUseFlags))
 
 
 	def reset(self):
 		"""Reset remaining run once variables after a sync or other mods
 		"""
-		#debug.dprint("SETTINGS: reset_globals();")
+		#print("SETTINGS: reset_globals();")
 		self.settings, self.trees, self.mtimedb = load_emerge_config()
 		self._load_dbapis()
+		self.root_config = self.trees[self.settings['ROOT']]['root_config']
 		self.portdir = self.settings.environ()['PORTDIR']
 		self.config_root = self.settings['PORTAGE_CONFIGROOT']
 		# is PORTDIR_OVERLAY always defined?
@@ -114,14 +118,14 @@ class PortageSettings:
 		"""Reloads the world file into memory for quick access
 		@return boolean
 		"""
-		#debug.dprint("SETTINGS: reset_world();")
+		#print("SETTINGS: reset_world();")
 		world = []
 		try:
 			file = open(os.path.join(portage.root, portage.WORLD_FILE), "r")
 			world = file.read().split()
 			file.close()
 		except:
-			#debug.dprint("SETTINGS: get_world(); Failure to locate file: '%s'" %portage.WORLD_FILE)
+			print("SETTINGS: get_world(); Failure to locate file: '%s'" %portage.WORLD_FILE)
 			return False
 		self._world = world
 		return True
@@ -157,12 +161,12 @@ def reload_portage():
 	Caution, it may not always work correctly due to python caching if
 	program files are added/deleted between versions. In those cases the
 	consumer app may need to be closed and restarted."""
-	#debug.dprint('SETTINGS: reloading portage')
-	#debug.dprint("SETTINGS: old portage version = " + portage.VERSION)
+	#print('SETTINGS: reloading portage')
+	#print("SETTINGS: old portage version = " + portage.VERSION)
 	try:
 		reload(portage)
 	except ImportError:
 		return False
-	#debug.dprint("SETTINGS: new portage version = " + portage.VERSION)
+	#print("SETTINGS: new portage version = " + portage.VERSION)
 	settings.reset()
 	return True
