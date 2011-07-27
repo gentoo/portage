9a31128ea29f2fb3f4bde03273d671a0e01332bf
Zac Medico <zmedico@gentoo.org>
2011-07-27 02:53:40 -0700

    config: fix AttributeError: 'NoneType' errors
    This is a regression from commit
    1fdeb78d08bac14f3f999f112499d179a87cf342 and it can only be triggered
    when repo-level package.use is present.

--- pym/portage/package/ebuild/config.py
+++ pym/portage/package/ebuild/config.py
@@ -1148,7 +1148,7 @@ class config(object):
 					d = d.copy()
 				cpdict = self._use_manager._repo_puse_dict.get(repo, {}).get(cp)
 				if cpdict:
-					repo_puse = ordered_by_atom_specificity(cpdict, pkg)
+					repo_puse = ordered_by_atom_specificity(cpdict, cpv_slot)
 					if repo_puse:
 						for x in repo_puse:
 							d["USE"] = d.get("USE", "") + " " + " ".join(x)
