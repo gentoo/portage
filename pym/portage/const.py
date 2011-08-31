377720b203f2f22609da0429ec866ba671fc2da0
Zac Medico <zmedico@gentoo.org>
2011-08-31 10:54:52 -0700

    Add FEATURES=clean-logs support.
    Enable automatic execution of the command specified by the
    PORT_LOGDIR_CLEAN variable. The default PORT_LOGDIR_CLEAN setting will
    remove all files from PORT_LOGDIR that were last modified at least 7
    days ago.

--- pym/portage/const.py
+++ pym/portage/const.py
@@ -88,7 +88,8 @@ EBUILD_PHASES            = ("pretend", "setup", "unpack", "prepare", "configure"
 SUPPORTED_FEATURES       = frozenset([
                            "allow-missing-manifests",
                            "assume-digests", "binpkg-logs", "buildpkg", "buildsyspkg", "candy",
-                           "ccache", "chflags", "collision-protect", "compress-build-logs",
+                           "ccache", "chflags", "clean-logs",
+                           "collision-protect", "compress-build-logs",
                            "digest", "distcc", "distcc-pump", "distlocks", "ebuild-locks", "fakeroot",
                            "fail-clean", "fixpackages", "force-mirror", "getbinpkg",
                            "installsources", "keeptemp", "keepwork", "fixlafiles", "lmirror",
