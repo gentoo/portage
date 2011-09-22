64899b4f3bfcce72b487428d36b279454b298c88
Zac Medico <zmedico@gentoo.org>
2011-09-22 12:06:18 -0700

    Test direct virtual circular dep for bug #384107.

--- pym/portage/tests/resolver/test_circular_choices.py
+++ pym/portage/tests/resolver/test_circular_choices.py
@@ -30,3 +30,32 @@ class CircularChoicesTestCase(TestCase):
 				self.assertEqual(test_case.test_success, True, test_case.fail_msg)
 		finally:
 			playground.cleanup()
+
+class VirtualCircularChoicesTestCase(TestCase):
+	def testDirectVirtualCircularDependency(self):
+
+		# Bug #384107
+		self.todo = True
+
+		ebuilds = {
+			"dev-java/icedtea-6.1.10.3": { "SLOT" : "6", "DEPEND": "virtual/jdk" },
+			"dev-java/icedtea6-bin-1.10.3": {},
+			"virtual/jdk-1.6.0": { "SLOT" : "1.6", "RDEPEND": "|| ( dev-java/icedtea6-bin =dev-java/icedtea-6* )" },
+		}
+
+		test_cases = (
+			# Automatically pull in icedtea6-bin to solve a circular dep
+			ResolverPlaygroundTestCase(
+				["dev-java/icedtea"],
+				mergelist = ["dev-java/icedtea6-bin-1.10.3", "virtual/jdk-1.6.0", "dev-java/icedtea-6.1.10.3"],
+				success = True,
+			),
+		)
+
+		playground = ResolverPlayground(ebuilds=ebuilds)
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True, test_case.fail_msg)
+		finally:
+			playground.cleanup()
