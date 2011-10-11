gentoo/portage

commit 8a119ea94ecc6668797e3a1358465ef3733f3a3e

elog_process: add newlines for bug #386627
This fixes omission of newlines in saved messages since commit
ca7bef7821876791a26a5f9df6938f4c77fe571f.

pym/portage/elog/__init__.py
@@ -52,11 +52,11 @@ def _combine_logentries(logentries):
 		for msgtype, msgcontent in logentries[phase]:
 			if previous_type != msgtype:
 				previous_type = msgtype
-				rValue.append("%s: %s\n" % (msgtype, phase))
+				rValue.append("%s: %s" % (msgtype, phase))
 			for line in msgcontent:
-				rValue.append(line)
-			rValue.append("\n")
-	return "".join(rValue)
+				rValue.append(line.rstrip("\n"))
+			rValue.append("")
+	return "\n".join(rValue)
 
 _elog_mod_imports = {}
 def _load_mod(name):

commit 12cb62465b130c21080281d78f7bfac077c17dfb

elog_process: fix ridicoulus newlines bug #386771
This fixes a regression since commit 8a119ea94ecc6668797e3a1358465ef3733f3a3e
which added a newline after each character. This boosts efficiency since we no
longer convert a strings to lists of characters.

pym/portage/elog/__init__.py
@@ -1,6 +1,10 @@
 # elog/__init__.py - elog core functions
-# Copyright 2006-2009 Gentoo Foundation
+# Copyright 2006-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
+
+import sys
+if sys.hexversion >= 0x3000000:
+	basestring = str
 
 import portage
 portage.proxy.lazyimport.lazyimport(globals(),
@@ -53,9 +57,13 @@ def _combine_logentries(logentries):
 			if previous_type != msgtype:
 				previous_type = msgtype
 				rValue.append("%s: %s" % (msgtype, phase))
-			for line in msgcontent:
-				rValue.append(line.rstrip("\n"))
-			rValue.append("")
+			if isinstance(msgcontent, basestring):
+				rValue.append(msgcontent.rstrip("\n"))
+			else:
+				for line in msgcontent:
+					rValue.append(line.rstrip("\n"))
+	if rValue:
+		rValue.append("")
 	return "\n".join(rValue)
 
 _elog_mod_imports = {}