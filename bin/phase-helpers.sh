gentoo/portage

commit ab484dc9a2612aa6709fad3ff926c8589a706637

phase-helpers.sh: fix best/has_version for prefix

bin/phase-helpers.sh
@@ -600,11 +600,13 @@ _eapi4_src_install() {
 # Takes single depend-type atoms.
 has_version() {
 
+	local eroot=${EROOT}
+	case "$EAPI" in 0|1|2) eroot=${ROOT} ;; esac
 	if [[ -n $PORTAGE_IPC_DAEMON ]] ; then
-		"$PORTAGE_BIN_PATH"/ebuild-ipc has_version "$ROOT" "$1"
+		"$PORTAGE_BIN_PATH"/ebuild-ipc has_version "${eroot}" "$1"
 	else
 		PYTHONPATH=${PORTAGE_PYM_PATH}${PYTHONPATH:+:}${PYTHONPATH} \
-		"${PORTAGE_PYTHON:-/usr/bin/python}" "${PORTAGE_BIN_PATH}/portageq" has_version "${ROOT}" "$1"
+		"${PORTAGE_PYTHON:-/usr/bin/python}" "${PORTAGE_BIN_PATH}/portageq" has_version "${eroot}" "$1"
 	fi
 	local retval=$?
 	case "${retval}" in
@@ -621,11 +623,13 @@ has_version() {
 # Takes single depend-type atoms.
 best_version() {
 
+	local eroot=${EROOT}
+	case "$EAPI" in 0|1|2) eroot=${ROOT} ;; esac
 	if [[ -n $PORTAGE_IPC_DAEMON ]] ; then
-		"$PORTAGE_BIN_PATH"/ebuild-ipc best_version "$ROOT" "$1"
+		"$PORTAGE_BIN_PATH"/ebuild-ipc best_version "${eroot}" "$1"
 	else
 		PYTHONPATH=${PORTAGE_PYM_PATH}${PYTHONPATH:+:}${PYTHONPATH} \
-		"${PORTAGE_PYTHON:-/usr/bin/python}" "${PORTAGE_BIN_PATH}/portageq" best_version "${ROOT}" "$1"
+		"${PORTAGE_PYTHON:-/usr/bin/python}" "${PORTAGE_BIN_PATH}/portageq" best_version "${eroot}" "$1"
 	fi
 	local retval=$?
 	case "${retval}" in

commit b58e5fd407c8726fd2ffe674444255aab9ae0010

Fix best/has_version ROOT override for EAPI 3-4.
This has been broken since commit
ab484dc9a2612aa6709fad3ff926c8589a706637. In order to support prefix,
callers will have to override EROOT instead.

bin/phase-helpers.sh
@@ -596,12 +596,29 @@ _eapi4_src_install() {
 	fi
 }
 
-# Return true if given package is installed. Otherwise return false.
-# Takes single depend-type atoms.
+# @FUNCTION: has_version
+# @USAGE: <DEPEND ATOM>
+# @DESCRIPTION:
+# Returns the best/most-current match. Callers may override the ROOT
+# variable in order match packages from an alternative ROOT. In
+# EAPI 3 and later, override EROOT instead (ROOT override is supported
+# in this case only if EPREFIX is empty).
 has_version() {
 
 	local eroot=${EROOT}
-	case "$EAPI" in 0|1|2) eroot=${ROOT} ;; esac
+	case "$EAPI" in
+		0|1|2)
+			eroot=${ROOT}
+			;;
+		*)
+			if [[ -z ${EPREFIX} && ${EROOT} != ${ROOT} ]] ; then
+				# Handle ROOT environment override, which ebuilds
+				# sometimes use for stage1/cross-compiling.
+				# In order to support prefix, they'll have to
+				# override EROOT instead.
+				eroot=${ROOT}
+			fi
+	esac
 	if [[ -n $PORTAGE_IPC_DAEMON ]] ; then
 		"$PORTAGE_BIN_PATH"/ebuild-ipc has_version "${eroot}" "$1"
 	else
@@ -619,12 +636,29 @@ has_version() {
 	esac
 }
 
-# Returns the best/most-current match.
-# Takes single depend-type atoms.
+# @FUNCTION: best_version
+# @USAGE: <DEPEND ATOM>
+# @DESCRIPTION:
+# Returns the best/most-current match. Callers may override the ROOT
+# variable in order match packages from an alternative ROOT. In
+# EAPI 3 and later, override EROOT instead (ROOT override is supported
+# in this case only if EPREFIX is empty).
 best_version() {
 
 	local eroot=${EROOT}
-	case "$EAPI" in 0|1|2) eroot=${ROOT} ;; esac
+	case "$EAPI" in
+		0|1|2)
+			eroot=${ROOT}
+			;;
+		*)
+			if [[ -z ${EPREFIX} && ${EROOT} != ${ROOT} ]] ; then
+				# Handle ROOT environment override, which ebuilds
+				# sometimes use for stage1/cross-compiling.
+				# In order to support prefix, they'll have to
+				# override EROOT instead.
+				eroot=${ROOT}
+			fi
+	esac
 	if [[ -n $PORTAGE_IPC_DAEMON ]] ; then
 		"$PORTAGE_BIN_PATH"/ebuild-ipc best_version "${eroot}" "$1"
 	else