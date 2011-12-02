1776d5db74e7065b409b7e2d521323e8d7ea2bb5
Zac Medico <zmedico@gentoo.org>
2011-12-01 18:07:21 -0800

    test_uniqueArray.py: fix 'is' usage PyPy incompat

--- pym/portage/tests/util/test_uniqueArray.py
+++ pym/portage/tests/util/test_uniqueArray.py
@@ -1,5 +1,5 @@
 # test_uniqueArray.py -- Portage Unit Testing Functionality
-# Copyright 2006 Gentoo Foundation
+# Copyright 2006-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from portage import os
@@ -20,5 +20,5 @@ class UniqueArrayTestCase(TestCase):
 			result = unique_array( test[0] )
 			for item in test[1]:
 				number = result.count(item)
-				self.assertFalse( number is not 1, msg="%s contains %s of %s, \
-					should be only 1" % (result, number, item) )
+				self.assertFalse( number != 1, msg=("%s contains %s of %s, "
+					"should be only 1") % (result, number, item) )
