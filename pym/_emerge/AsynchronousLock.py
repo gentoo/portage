gentoo/portage

commit b432a1b3051d91546649e8f3190675767461d8e8

_emerge/EbuildMetadataPhase.py: use os.read()
There's no need for a file object, and file objects introduce
complexity that can lead to bugs as mentioned in bug 337465 comment 31,
so use os.read() directly on the file descriptor.

pym/_emerge/EbuildMetadataPhase.py
@@ -10,6 +10,7 @@ from portage import os
 from portage import _encodings
 from portage import _unicode_decode
 from portage import _unicode_encode
+import errno
 import fcntl
 import io
 
@@ -75,7 +76,6 @@ class EbuildMetadataPhase(SubProcess):
 			if fd == sys.stderr.fileno():
 				sys.stderr.flush()
 
-		fd_pipes_orig = fd_pipes.copy()
 		self._files = self._files_dict()
 		files = self._files
 
@@ -86,8 +86,8 @@ class EbuildMetadataPhase(SubProcess):
 		fd_pipes[self._metadata_fd] = slave_fd
 
 		self._raw_metadata = []
-		files.ebuild = os.fdopen(master_fd, 'rb', 0)
-		self._reg_id = self.scheduler.register(files.ebuild.fileno(),
+		files.ebuild = master_fd
+		self._reg_id = self.scheduler.register(files.ebuild,
 			self._registered_events, self._output_handler)
 		self._registered = True
 
@@ -112,10 +112,16 @@ class EbuildMetadataPhase(SubProcess):
 	def _output_handler(self, fd, event):
 
 		if event & PollConstants.POLLIN:
-			self._raw_metadata.append(self._files.ebuild.read())
-			if not self._raw_metadata[-1]:
-				self._unregister()
-				self.wait()
+			try:
+				self._raw_metadata.append(
+					os.read(self._files.ebuild, self._bufsize))
+			except IOError as e:
+				if e.errno not in (errno.EAGAIN,):
+					raise
+			else:
+				if not self._raw_metadata[-1]:
+					self._unregister()
+					self.wait()
 
 		self._unregister_if_appropriate(event)
 

commit c6c71eefb9532af0d4505de70e58f45ad8e8fe0a

Handle OSError from os.read and loop if needed.
Looping fixes EbuildMetadataPhase failures for ebuilds that produce
more than 4096 bytes of metadata, broken since commit
b432a1b3051d91546649e8f3190675767461d8e8.

pym/_emerge/AsynchronousLock.py
@@ -138,7 +138,7 @@ class _LockThread(AbstractPollTask):
 		if event & PollConstants.POLLIN:
 			try:
 				buf = os.read(self._files['pipe_read'], self._bufsize)
-			except IOError as e:
+			except OSError as e:
 				if e.errno not in (errno.EAGAIN,):
 					raise
 		if buf:
@@ -274,7 +274,7 @@ class _LockProcess(AbstractPollTask):
 		if event & PollConstants.POLLIN:
 			try:
 				buf = os.read(self._files['pipe_in'], self._bufsize)
-			except IOError as e:
+			except OSError as e:
 				if e.errno not in (errno.EAGAIN,):
 					raise
 		if buf:

pym/_emerge/EbuildMetadataPhase.py
@@ -112,16 +112,19 @@ class EbuildMetadataPhase(SubProcess):
 	def _output_handler(self, fd, event):
 
 		if event & PollConstants.POLLIN:
-			try:
-				self._raw_metadata.append(
-					os.read(self._files.ebuild, self._bufsize))
-			except IOError as e:
-				if e.errno not in (errno.EAGAIN,):
-					raise
-			else:
-				if not self._raw_metadata[-1]:
-					self._unregister()
-					self.wait()
+			while True:
+				try:
+					self._raw_metadata.append(
+						os.read(self._files.ebuild, self._bufsize))
+				except OSError as e:
+					if e.errno not in (errno.EAGAIN,):
+						raise
+					break
+				else:
+					if not self._raw_metadata[-1]:
+						self._unregister()
+						self.wait()
+						break
 
 		self._unregister_if_appropriate(event)
 

pym/_emerge/PipeReader.py
@@ -59,18 +59,21 @@ class PipeReader(AbstractPollTask):
 
 		if event & PollConstants.POLLIN:
 
-			data = None
-			try:
-				data = os.read(fd, self._bufsize)
-			except IOError as e:
-				if e.errno not in (errno.EAGAIN,):
-					raise
-			else:
-				if data:
-					self._read_data.append(data)
+			while True:
+				data = None
+				try:
+					data = os.read(fd, self._bufsize)
+				except OSError as e:
+					if e.errno not in (errno.EAGAIN,):
+						raise
+					break
 				else:
-					self._unregister()
-					self.wait()
+					if data:
+						self._read_data.append(data)
+					else:
+						self._unregister()
+						self.wait()
+						break
 
 		self._unregister_if_appropriate(event)
 