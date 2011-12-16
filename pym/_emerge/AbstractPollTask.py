gentoo/portage

commit 915348ce34fc499ac295b8f0ffee9f0829803542

test_poll: test different sizes, and pty too

pym/_emerge/PipeReader.py
@@ -64,7 +64,9 @@ class PipeReader(AbstractPollTask):
 				try:
 					data = os.read(fd, self._bufsize)
 				except OSError as e:
-					if e.errno not in (errno.EAGAIN,):
+					# EIO happens with pty on Linux after the
+					# slave end of the pty has been closed.
+					if e.errno not in (errno.EAGAIN, errno.EIO):
 						raise
 					break
 				else:

pym/portage/tests/process/test_poll.py
@@ -3,25 +3,28 @@
 
 from portage import os
 from portage.tests import TestCase
+from portage.util._pty import _create_pty_or_pipe
 from _emerge.PollScheduler import PollScheduler
 from _emerge.PipeReader import PipeReader
 from _emerge.SpawnProcess import SpawnProcess
 
 class PipeReaderTestCase(TestCase):
 
-	def testPipeReader(self):
+	def _testPipeReader(self, test_string, use_pty):
 		"""
 		Use a poll loop to read data from a pipe and assert that
 		the data written to the pipe is identical to the data
 		read from the pipe.
 		"""
 
-		test_string = 2 * "blah blah blah\n"
-
 		scheduler = PollScheduler().sched_iface
-		master_fd, slave_fd = os.pipe()
+		if use_pty:
+			got_pty, master_fd, slave_fd = _create_pty_or_pipe()
+		else:
+			got_pty = False
+			master_fd, slave_fd = os.pipe()
 		master_file = os.fdopen(master_fd, 'rb', 0)
-		slave_file = os.fdopen(slave_fd, 'wb')
+		slave_file = os.fdopen(slave_fd, 'wb', 0)
 		producer = SpawnProcess(
 			args=["bash", "-c", "echo -n '%s'" % test_string],
 			env=os.environ, fd_pipes={1:slave_fd},
@@ -44,4 +47,12 @@ class PipeReaderTestCase(TestCase):
 		self.assertEqual(consumer.returncode, os.EX_OK)
 
 		output = consumer.getvalue().decode('ascii', 'replace')
-		self.assertEqual(test_string, output)
+		return (output, got_pty)
+
+	def testPipeReader(self):
+		for use_pty in (False, True):
+			for x in (1, 2, 5, 6, 7, 8, 2**5, 2**10, 2**12, 2**13, 2**14):
+				test_string = x * "a"
+				output, got_pty = self._testPipeReader(test_string, use_pty)
+				self.assertEqual(test_string, output,
+					"x = %s, use_pty = %s, got_pty = %s" % (x, use_pty, got_pty))

commit 36632047bebbd435610944ad9e7850163b260645

Fix EIO handling for PollSelectAdapter.
In PipeReader and AbstractPollTask, we have to treat EIO as EOF, in
order to terminate the select loop at the correct time when using
PollSelectAdapter. This fixes a regression in EIO handling since
commits 915348ce34fc499ac295b8f0ffee9f0829803542 adn
30d2d0a9db486c5a70848ad5d27b37a3ec48f271.

pym/_emerge/AbstractPollTask.py
@@ -50,9 +50,14 @@ class AbstractPollTask(AsynchronousTask):
 			except OSError as e:
 				# EIO happens with pty on Linux after the
 				# slave end of the pty has been closed.
-				if e.errno not in (errno.EAGAIN, errno.EIO):
+				if e.errno == errno.EIO:
+					# EOF: return empty buffer
+					pass
+				elif e.errno == errno.EAGAIN:
+					# EAGAIN: return None
+					buf = None
+				else:
 					raise
-				buf = None
 
 		return buf
 

pym/_emerge/PipeReader.py
@@ -66,9 +66,14 @@ class PipeReader(AbstractPollTask):
 				except OSError as e:
 					# EIO happens with pty on Linux after the
 					# slave end of the pty has been closed.
-					if e.errno not in (errno.EAGAIN, errno.EIO):
+					if e.errno == errno.EIO:
+						self._unregister()
+						self.wait()
+						break
+					elif e.errno == errno.EAGAIN:
+						break
+					else:
 						raise
-					break
 				else:
 					if data:
 						self._read_data.append(data)