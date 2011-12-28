gentoo/portage

commit 10246cd535f909dda8bd05de617c32d2b8a56b4a

Parse layout.conf files in portage.repository.config.RepoConfig.__init__() instead of portage.repository.config.RepoConfigLoader.__init__().

pym/portage/repository/config.py
@@ -125,6 +125,33 @@ class RepoConfig(object):
 		self.portage1_profiles = True
 		self.portage1_profiles_compat = False
 
+		# Parse layout.conf.
+		if self.location:
+			layout_filename = os.path.join(self.location, "metadata", "layout.conf")
+			layout_data = parse_layout_conf(self.location, self.name)[0]
+
+			# layout.conf masters may be overridden here if we have a masters
+			# setting from the user's repos.conf
+			if self.masters is None:
+				self.masters = layout_data['masters']
+
+			if layout_data['aliases']:
+				aliases = self.aliases
+				if aliases is None:
+					aliases = ()
+				# repos.conf aliases come after layout.conf aliases, giving
+				# them the ability to do incremental overrides
+				self.aliases = layout_data['aliases'] + tuple(aliases)
+
+			for value in ('allow-missing-manifest', 'cache-formats',
+				'create-manifest', 'disable-manifest', 'manifest-hashes',
+				'sign-manifest', 'thin-manifest', 'update-changelog'):
+				setattr(self, value.lower().replace("-", "_"), layout_data[value])
+
+			self.portage1_profiles = any(x.startswith("portage-1") \
+				for x in layout_data['profile-formats'])
+			self.portage1_profiles_compat = layout_data['profile-formats'] == ('portage-1-compat',)
+
 	def iter_pregenerated_caches(self, auxdbkeys, readonly=True, force=False):
 		"""
 		Reads layout.conf cache-formats from left to right and yields cache
@@ -411,35 +438,6 @@ class RepoConfigLoader(object):
 		self.missing_repo_names = frozenset(repo.location
 			for repo in prepos.values()
 			if repo.location is not None and repo.missing_repo_name)
-
-		#Parse layout.conf and read masters key.
-		for repo in prepos.values():
-			if not repo.location:
-				continue
-			layout_filename = os.path.join(repo.location, "metadata", "layout.conf")
-			layout_data, layout_errors = parse_layout_conf(repo.location, repo.name)
-
-			# layout.conf masters may be overridden here if we have a masters
-			# setting from the user's repos.conf
-			if repo.masters is None:
-				repo.masters = layout_data['masters']
-
-			if layout_data['aliases']:
-				aliases = repo.aliases
-				if aliases is None:
-					aliases = ()
-				# repos.conf aliases come after layout.conf aliases, giving
-				# them the ability to do incremental overrrides
-				repo.aliases = layout_data['aliases'] + tuple(aliases)
-
-			for value in ('allow-missing-manifest', 'cache-formats',
-				'create-manifest', 'disable-manifest', 'manifest-hashes',
-				'sign-manifest', 'thin-manifest', 'update-changelog'):
-				setattr(repo, value.lower().replace("-", "_"), layout_data[value])
-
-			repo.portage1_profiles = any(x.startswith("portage-1") \
-				for x in layout_data['profile-formats'])
-			repo.portage1_profiles_compat = layout_data['profile-formats'] == ('portage-1-compat',)
 
 		#Take aliases into account.
 		new_prepos = {}

commit e626628a338aa90f9cf321ec89789da72f67a9e0

RepoConfig.update(): copy more attributes
This fixes a regression since commit
10246cd535f909dda8bd05de617c32d2b8a56b4a which caused layout.conf
settings such as thin-manifests to be ignored for repositories that
had repos.conf settings that did not specify the repository location.
In order to trigger this case, ResolverPlayground has been modified to
omit the location of each repository in the repos.conf file that it
generates.

pym/portage/repository/config.py
@@ -199,8 +199,9 @@ class RepoConfig(object):
 	def update(self, new_repo):
 		"""Update repository with options in another RepoConfig"""
 
-		for k in ('aliases', 'eclass_overrides', 'location', 'masters',
-			'name', 'priority', 'sync', 'user_location'):
+		keys = set(self.__slots__)
+		keys.discard("missing_repo_name")
+		for k in keys:
 			v = getattr(new_repo, k, None)
 			if v is not None:
 				setattr(self, k, v)
@@ -324,7 +325,13 @@ class RepoConfigLoader(object):
 					repo_opts = default_repo_opts.copy()
 					repo_opts['location'] = ov
 					repo = RepoConfig(None, repo_opts)
-					# repos_conf_opts contains options from /etc/portage/repos.conf
+					# repos_conf_opts may contain options from various places:
+					# 1) /etc/portage/repos.conf
+					# 2) $location/metadata/layout.conf if repos.conf specified
+					#    the repo location
+					# 3) A RepoConfig instance corresponding to a previously
+					#    processed path in the current list of overlays which
+					#    referred to a repository with the same name.
 					repos_conf_opts = prepos.get(repo.name)
 					if repos_conf_opts is not None:
 						if repos_conf_opts.aliases is not None:

pym/portage/tests/ebuild/test_config.py
@@ -243,6 +243,8 @@ class ConfigTestCase(TestCase):
 		self.assertTrue(len(new_repo_config.masters) > 0, "new_repo has no default master")
 		self.assertEqual(new_repo_config.masters[0].user_location, playground.portdir,
 			"new_repo default master is not PORTDIR")
+		self.assertEqual(new_repo_config.thin_manifest, True,
+			"new_repo_config.thin_manifest != True")
 
 		new_manifest_file = os.path.join(playground.repo_dirs["new_repo"], "dev-libs", "A", "Manifest")
 		self.assertEqual(os.path.exists(new_manifest_file), False)

pym/portage/tests/resolver/ResolverPlayground.py
@@ -376,17 +376,13 @@ class ResolverPlayground(object):
 				with open(os.path.join(metadata_dir, "metadata.xml"), 'w') as f:
 					f.write(herds_xml)
 
+		# Write empty entries for each repository, in order to exercise
+		# RepoConfigLoader's repos.conf processing.
 		repos_conf_file = os.path.join(user_config_dir, "repos.conf")		
 		f = open(repos_conf_file, "w")
-		priority = 0
 		for repo in sorted(self.repo_dirs.keys()):
 			f.write("[%s]\n" % repo)
-			f.write("LOCATION=%s\n" % self.repo_dirs[repo])
-			if repo == "test_repo":
-				f.write("PRIORITY=%s\n" % -1000)
-			else:
-				f.write("PRIORITY=%s\n" % priority)
-				priority += 1
+			f.write("\n")
 		f.close()
 
 		for config_file, lines in user_config.items():