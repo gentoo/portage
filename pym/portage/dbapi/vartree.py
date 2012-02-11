gentoo/portage

commit 93b654199a32fc3df1299b030317720b9294b0c3

Apply INSTALL_MASK prior to collision-protect.
It may be useful to avoid collisions in some scenarios.

pym/portage/dbapi/vartree.py
@@ -3506,14 +3506,6 @@ class dblink(object):
 			if installed_files:
 				return 1
 
-		# check for package collisions
-		blockers = self._blockers
-		if blockers is None:
-			blockers = []
-		collisions, symlink_collisions, plib_collisions = \
-			self._collision_protect(srcroot, destroot,
-			others_in_slot + blockers, myfilelist, mylinklist)
-
 		# Make sure the ebuild environment is initialized and that ${T}/elog
 		# exists for logging of collision-protect eerror messages.
 		if myebuild is None:
@@ -3524,6 +3516,23 @@ class dblink(object):
 			[portage.versions.cpv_getversion(other.mycpv)
 			for other in others_in_slot])
 		prepare_build_dirs(settings=self.settings, cleanup=cleanup)
+
+		if self.settings.get("INSTALL_MASK"):
+			# Apply INSTALL_MASK before collision-protect, since it may
+			# be useful to avoid collisions in some scenarios.
+			phase = MiscFunctionsProcess(background=False,
+				commands=["preinst_mask"], phase="preinst",
+				scheduler=self._scheduler, settings=self.settings)
+			phase.start()
+			phase.wait()
+
+		# check for package collisions
+		blockers = self._blockers
+		if blockers is None:
+			blockers = []
+		collisions, symlink_collisions, plib_collisions = \
+			self._collision_protect(srcroot, destroot,
+			others_in_slot + blockers, myfilelist, mylinklist)
 
 		if collisions:
 			collision_protect = "collision-protect" in self.settings.features

pym/_emerge/MiscFunctionsProcess.py
@@ -29,5 +29,11 @@ class MiscFunctionsProcess(AbstractEbuildProcess):
 		AbstractEbuildProcess._start(self)
 
 	def _spawn(self, args, **kwargs):
-		self.settings.pop("EBUILD_PHASE", None)
-		return spawn(" ".join(args), self.settings, **kwargs)
+		# Temporarily unset EBUILD_PHASE so that bashrc code doesn't
+		# think this is a real phase.
+		phase_backup = self.settings.pop("EBUILD_PHASE", None)
+		try:
+			return spawn(" ".join(args), self.settings, **kwargs)
+		finally:
+			if phase_backup is not None:
+				self.settings["EBUILD_PHASE"] = phase_backup

pym/portage/package/ebuild/doebuild.py
@@ -1487,7 +1487,7 @@ _post_phase_cmds = {
 		"preinst_sfperms",
 		"preinst_selinux_labels",
 		"preinst_suid_scan",
-		"preinst_mask"]
+		]
 }
 
 def _post_phase_userpriv_perms(mysettings):

commit a7071bd82676ffdf3020b2f71effa2512ed803d4

Fix FEATURES=no{doc,info,man} for bug #403181
This has been broken since commit
93b654199a32fc3df1299b030317720b9294b0c3.

pym/portage/dbapi/vartree.py
@@ -3381,7 +3381,10 @@ class dblink(object):
 					max_dblnk = dblnk
 			self._installed_instance = max_dblnk
 
-		if self.settings.get("INSTALL_MASK"):
+		if self.settings.get("INSTALL_MASK") or \
+			"nodoc" in self.settings.features or \
+			"noinfo" in self.settings.features or \
+			"noman" in self.settings.features:
 			# Apply INSTALL_MASK before collision-protect, since it may
 			# be useful to avoid collisions in some scenarios.
 			phase = MiscFunctionsProcess(background=False,