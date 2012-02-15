b39ff144bd47952ee06bba0b549069c635d31a31
Zac Medico <zmedico@gentoo.org>
2012-02-15 14:28:41 -0800

    MergeProcess: inherit stdin for use with pdb

--- pym/portage/dbapi/_MergeProcess.py
+++ pym/portage/dbapi/_MergeProcess.py
@@ -4,6 +4,7 @@
 import io
 import platform
 import signal
+import sys
 import traceback
 
 import errno
@@ -39,6 +40,12 @@ class MergeProcess(SpawnProcess):
 			settings.reload()
 			settings.reset()
 			settings.setcpv(cpv, mydb=self.mydbapi)
+
+		# Inherit stdin by default, so that the pdb SIGUSR1
+		# handler is usable for the subprocess.
+		if self.fd_pipes is None:
+			self.fd_pipes = {}
+		self.fd_pipes.setdefault(0, sys.stdin.fileno())
 
 		super(MergeProcess, self)._start()
 
