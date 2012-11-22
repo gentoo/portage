da268c268e7fb91318a4676025ec841a4444b362
Arfrever Frehtes Taifersar Arahesis <Arfrever@Apache.Org>
2012-11-22 22:53:09 +0100

    Bug #440680: Add boost-utils to list of deprecated eclasses.

--- pym/repoman/checks.py
+++ pym/repoman/checks.py
@@ -378,6 +378,7 @@ class InheritDeprecated(LineCheck):
 	# deprecated eclass : new eclass (False if no new eclass)
 	deprecated_classes = {
 		"bash-completion": "bash-completion-r1",
+		"boost-utils": False,
 		"gems": "ruby-fakegem",
 		"git": "git-2",
 		"mozconfig-2": "mozconfig-3",
