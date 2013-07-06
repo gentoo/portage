92d9c95c83ddc6ac7b59395db92c0c8d81d7687a
Zac Medico <zmedico@gentoo.org>
2013-07-06 14:45:04 -0700

    depgraph: tweak slot-operator merge order
    This handles circular DEPEND/RDEPEND with one := operator, so that when
    both deps are already satisfied by installed packages, the := dep is
    given higher priority in merge order.

--- pym/_emerge/UnmergeDepPriority.py
+++ pym/_emerge/UnmergeDepPriority.py
@@ -1,4 +1,4 @@
-# Copyright 1999-2011 Gentoo Foundation
+# Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from _emerge.AbstractDepPriority import AbstractDepPriority
@@ -7,15 +7,16 @@ class UnmergeDepPriority(AbstractDepPriority):
 	"""
 	Combination of properties           Priority  Category
 
-	runtime                                0       HARD
-	runtime_post                          -1       HARD
-	buildtime                             -2       SOFT
-	(none of the above)                   -2       SOFT
+	runtime_slot_op                        0       HARD
+	runtime                               -1       HARD
+	runtime_post                          -2       HARD
+	buildtime                             -3       SOFT
+	(none of the above)                   -3       SOFT
 	"""
 
 	MAX    =  0
-	SOFT   = -2
-	MIN    = -2
+	SOFT   = -3
+	MIN    = -3
 
 	def __init__(self, **kwargs):
 		AbstractDepPriority.__init__(self, **kwargs)
@@ -23,13 +24,15 @@ class UnmergeDepPriority(AbstractDepPriority):
 			self.optional = True
 
 	def __int__(self):
-		if self.runtime:
+		if self.runtime_slot_op:
 			return 0
-		if self.runtime_post:
+		if self.runtime:
 			return -1
-		if self.buildtime:
+		if self.runtime_post:
 			return -2
-		return -2
+		if self.buildtime:
+			return -3
+		return -3
 
 	def __str__(self):
 		if self.ignored:
