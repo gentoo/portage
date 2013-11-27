719b1c87918426fa3ae8c389779dece18e12a9ea
Mike Frysinger <vapier@gentoo.org>
2013-11-26 22:23:45 -0500

    countdown: clean up & simplify a bit

--- pym/_emerge/countdown.py
+++ pym/_emerge/countdown.py
@@ -1,4 +1,4 @@
-# Copyright 1999-2009 Gentoo Foundation
+# Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import print_function
@@ -8,15 +8,15 @@ import time
 
 from portage.output import colorize
 
-def countdown(secs=5, doing="Starting"):
+
+def countdown(secs=5, doing='Starting'):
 	if secs:
-		print(">>> Waiting",secs,"seconds before starting...")
-		print(">>> (Control-C to abort)...\n"+doing+" in: ", end=' ')
-		ticks=list(range(secs))
-		ticks.reverse()
-		for sec in ticks:
-			sys.stdout.write(colorize("UNMERGE_WARN", str(sec+1)+" "))
+		print(
+			'>>> Waiting %s seconds before starting...\n'
+			'>>> (Control-C to abort)...\n'
+			'%s in:' % (secs, doing), end='')
+		for sec in range(secs, 0, -1):
+			sys.stdout.write(colorize('UNMERGE_WARN', ' %i' % sec))
 			sys.stdout.flush()
 			time.sleep(1)
 		print()
-
