349407da332968b09ec641ffcf4b1ea190b02b6c
Chris Reffett <2011creffett@tjhsst.edu>
2014-02-09 23:06:46 -0500

    Update module spec for git

--- pym/portage/sync/modules/git/__init__.py
+++ pym/portage/sync/modules/git/__init__.py
@@ -14,10 +14,21 @@ module_spec = {
 			'name': "git",
 			'class': "GitSync",
 			'description': __doc__,
-			'functions': ['sync',],
+			'functions': ['sync', 'new', 'exists'],
 			'func_desc': {
-				'sync', 'Performs a git pull on the repository',
-				}
-			}
+				'sync': 'Performs a git pull on the repository',
+				'new': 'Creates the new repository at the specified location',
+				'exists': 'Returns a boolean of whether the specified dir ' +
+					'exists and is a valid Git repository',
+			},
+			'func_parameters': {
+				'kwargs': {
+					'type': dict,
+					'description': 'Standard python **kwargs parameter format' +
+						'Please refer to the sync modules specs at ' +
+						'"https://wiki.gentoo.org:Project:Portage" for details',
+				},
+			},
 		}
 	}
+}
