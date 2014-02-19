99921a561ca964f6b4156b1688fe45b19a1316ac
Brian Dolbec <dolsen@gentoo.org>
2014-02-19 00:22:42 -0800

    Initial stubbing out of a common SyncBase class
    Add copyright
    Optimize bin_command handling in __init__().

---
+++ pym/portage/sync/syncbase.py
@@ -0,0 +1,100 @@
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+'''
+Base class for performing sync operations.
+This class contains common initialization code and functions.
+'''
+
+
+import os
+
+import portage
+from portage.util import writemsg_level
+
+
+class SyncBase(object):
+	'''Base Sync class for subclassing'''
+
+	short_desc = "Perform sync operations on repositories"
+
+	@staticmethod
+	def name():
+		return "BlankSync"
+
+
+	def can_progressbar(self, func):
+		return False
+
+
+	def __init__(self, bin_command, bin_pkg):
+		self.options = None
+		self.settings = None
+		self.logger = None
+		self.repo = None
+		self.xterm_titles = None
+		self.spawn_kwargs = None
+		self.bin_command = portage.process.find_binary(bin_command)
+
+		self.has_bin = True
+		if bin_command and self.bin_command is None:
+			msg = ["Command not found: %s" % bin_command,
+			"Type \"emerge %s\" to enable %s support." % (bin_pkg, bin_command)]
+			for l in msg:
+				writemsg_level("!!! %s\n" % l,
+					level=self.logger.ERROR, noiselevel=-1)
+			self.has_bin = False
+
+
+	def _kwargs(self, kwargs):
+		'''Sets internal variables from kwargs'''
+		self.options = kwargs.get('options', {})
+		self.settings = self.options.get('settings', None)
+		self.logger = self.options.get('logger', None)
+		self.repo = self.options.get('repo', None)
+		self.xterm_titles = self.options.get('xterm_titles', False)
+		self.spawn_kwargs = self.options.get('spawn_kwargs', None)
+
+
+	def exists(self, **kwargs):
+		'''Tests whether the repo actually exists'''
+		if kwargs:
+			self._kwargs(kwargs)
+		elif not self.repo:
+			return False
+
+
+		if not os.path.exists(self.repo.location):
+			return False
+		return True
+
+
+	def sync(self, **kwargs):
+		'''Sync the repository'''
+		if kwargs:
+			self._kwargs(kwargs)
+
+		if not self.has_bin:
+			return (1, False)
+
+		if not self.exists():
+			return self.new()
+		return self._sync()
+
+
+	def new(self, **kwargs):
+		'''Do the initial download and install of the repository'''
+		pass
+
+	def _sync(self):
+		'''Update existing repository
+		'''
+		pass
+
+	def post_sync(self, portdb, location, emerge_config):
+		'''repo.sync_type == "Blank":
+		# NOTE: Do this after reloading the config, in case
+		# it did not exist prior to sync, so that the config
+		# and portdb properly account for its existence.
+		'''
+		pass
