4a4f8e59af5a3c482cfb3c9f69bef967416cc56b
Brian Dolbec <dolsen@gentoo.org>
2014-05-23 17:58:28 -0700

    Repoman: move profile functions to their own file

--- pym/repoman/main.py
+++ pym/repoman/main.py
@@ -402,29 +402,7 @@ def caterror(mycat):
 		"if it is a new category." % (mycat, catdir, repodir))
 
 
-class ProfileDesc(object):
-	__slots__ = ('abs_path', 'arch', 'status', 'sub_path', 'tree_path',)
-
-	def __init__(self, arch, status, sub_path, tree_path):
-		self.arch = arch
-		self.status = status
-		if sub_path:
-			sub_path = normalize_path(sub_path.lstrip(os.sep))
-		self.sub_path = sub_path
-		self.tree_path = tree_path
-		if tree_path:
-			self.abs_path = os.path.join(tree_path, 'profiles', self.sub_path)
-		else:
-			self.abs_path = tree_path
-
-	def __str__(self):
-		if self.sub_path:
-			return self.sub_path
-		return 'empty profile'
-
-
 profile_list = []
-valid_profile_types = frozenset(['dev', 'exp', 'stable'])
 
 # get lists of valid keywords, licenses, and use
 kwlist = set()
@@ -665,26 +643,6 @@ def git_supports_gpg_sign():
 				return True
 	return False
 
-
-def dev_keywords(profiles):
-	"""
-	Create a set of KEYWORDS values that exist in 'dev'
-	profiles. These are used
-	to trigger a message notifying the user when they might
-	want to add the --include-dev option.
-	"""
-	type_arch_map = {}
-	for arch, arch_profiles in profiles.items():
-		for prof in arch_profiles:
-			arch_set = type_arch_map.get(prof.status)
-			if arch_set is None:
-				arch_set = set()
-				type_arch_map[prof.status] = arch_set
-			arch_set.add(arch)
-
-	dev_keywords = type_arch_map.get('dev', set())
-	dev_keywords.update(['~' + arch for arch in dev_keywords])
-	return frozenset(dev_keywords)
 
 dev_keywords = dev_keywords(profiles)
 
