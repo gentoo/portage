7bc992a02c89c6c3f76b09bfc978c104fb1c2b9a
Zac Medico <zmedico@gentoo.org>
2014-11-01 08:03:26 -0700

    Add IndexStreamIterator and MultiIterGroupBy.
    This IndexStreamIterator class can be used together with the
    pkg_desc_index_line_read function to read an index file incrementally
    as a stream.
    
    The MultiIterGroupBy class can be used to iterate over multiple
    IndexStreamIterator instances at once, incrementally grouping results
    for a particular package from multiple indices, while limiting the
    amount of any given index that must be in memory at once.
    
    Both of these classes are used by the  IndexedPortdb class in the next
    patch of this series.
    
    X-Gentoo-Bug: 525718
    X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=525718

---
+++ pym/portage/util/iterators/MultiIterGroupBy.py
@@ -0,0 +1,94 @@
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+import bisect
+
+class MultiIterGroupBy(object):
+	"""
+	This class functions similarly to the itertools.groupby function,
+	except that it takes multiple source iterators as input. The source
+	iterators must yield objects in sorted order. A group is yielded as
+	soon as the progress of all iterators reaches a state which
+	guarantees that there can not be any remaining (unseen) elements of
+	the group. This is useful for incremental display of grouped search
+	results.
+	"""
+
+	def __init__(self, iterators, key=None):
+		self._iterators = iterators
+		self._key = key
+
+	def __iter__(self):
+
+		trackers = []
+		for iterator in self._iterators:
+			trackers.append(_IteratorTracker(iterator))
+
+		key_map = {}
+		key_list = []
+		eof = []
+		key_getter = self._key
+		if key_getter is None:
+			key_getter = lambda x: x
+		min_progress = None
+
+		while trackers:
+
+			for tracker in trackers:
+
+				if tracker.current is not None and \
+					tracker.current != min_progress:
+					# The trackers are sorted by progress, so the
+					# remaining trackers are guaranteed to have
+					# sufficient progress.
+					break
+
+				# In order to avoid over-buffering (waste of memory),
+				# only grab a single entry.
+				try:
+					entry = next(tracker.iterator)
+				except StopIteration:
+					eof.append(tracker)
+				else:
+					tracker.current = key_getter(entry)
+					key_group = key_map.get(tracker.current)
+					if key_group is None:
+						key_group = []
+						key_map[tracker.current] = key_group
+						bisect.insort(key_list, tracker.current)
+					key_group.append(entry)
+
+			if eof:
+				for tracker in eof:
+					trackers.remove(tracker)
+				del eof[:]
+
+			if trackers:
+				trackers.sort()
+				min_progress = trackers[0].current
+				# yield if key <= min_progress
+				i = bisect.bisect_right(key_list, min_progress)
+				yield_these = key_list[:i]
+				del key_list[:i]
+			else:
+				yield_these = key_list
+				key_list = []
+
+			if yield_these:
+				for k in yield_these:
+					yield key_map.pop(k)
+
+class _IteratorTracker(object):
+
+	__slots__ = ('current', 'iterator')
+
+	def __init__(self, iterator):
+
+		self.iterator = iterator
+		self.current = None
+
+	def __lt__(self, other):
+		if self.current is None:
+			return other.current is not None
+		return other.current is not None and \
+			self.current < other.current
