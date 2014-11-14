gentoo/portage

commit c9f6aa9f0151adb3c86706eaef1914cdbdcf2b6d

Add cross-prefix support
This patch series adds support for using a portage installed in one prefix
to build packages with a different prefix.

The current portage has a single EPREFIX variable specifying both the prefix
of the portage installation, and the prefix of the packages portage is
building. This patch series splits it into two parts: the
portage.const.EPREFIX variable specifying the prefix of the portage
installation, used for constructing the PATH as well as the paths to files
belonging to a portage installation itself rather than a target root; and the
 EPREFIX setting in config instances, specifying the prefix of the
to-be-built packages and being used for almost everything else.

The EPREFIX config setting defaults to const.EPREFIX, but can be overridden
by the EPREFIX environment variable, as well as the emerge --prefix option.
This allows one to install systems with different prefixes using
`EPREFIX=/foo emerge @system`, though some unrelated changes need to happen
elsewhere first in order to make that a reality.

Ruud Koolen (3):
  Distinguish between portage prefix and package prefix
  Based GLOBAL_CONFIG_PATH and DEPCACHE_PATH on portage prefix
  Pick up EPREFIX environment variable

bin/portageq
@@ -1232,7 +1232,7 @@ def main(argv):
 			sys.stderr.write("Run portageq with --help for info\n")
 			sys.stderr.flush()
 			sys.exit(os.EX_USAGE)
-		eprefix = portage.const.EPREFIX
+		eprefix = portage.settings["EPREFIX"]
 		eroot = portage.util.normalize_path(argv[2])
 
 		if eprefix:

bin/dispatch-conf
@@ -79,7 +79,7 @@ class dispatch:
         confs = []
         count = 0
 
-        config_root = portage.const.EPREFIX or os.sep
+        config_root = portage.settings["EPREFIX"] or os.sep
         self.options = portage.dispatch_conf.read_config(MANDATORY_OPTS)
 
         if "log-file" in self.options:

bin/phase-helpers.sh
@@ -676,7 +676,11 @@ has_version() {
 	fi
 
 	if ___eapi_has_prefix_variables; then
-		eroot=${root%/}${EPREFIX}/
+		if [[ ${root} == / ]] ; then
+			eroot=${root%/}${PORTAGE_OVERRIDE_EPREFIX}/
+		else
+			eroot=${root%/}${EPREFIX}/
+		fi
 	else
 		eroot=${root}
 	fi
@@ -728,7 +732,11 @@ best_version() {
 	fi
 
 	if ___eapi_has_prefix_variables; then
-		eroot=${root%/}${EPREFIX}/
+		if [[ ${root} == / ]] ; then
+			eroot=${root%/}${PORTAGE_OVERRIDE_EPREFIX}/
+		else
+			eroot=${root%/}${EPREFIX}/
+		fi
 	else
 		eroot=${root}
 	fi

man/emerge.1
@@ -631,6 +631,9 @@ exhaustively apply the entire history of package moves,
 regardless of whether or not any of the package moves have
 been previously applied.
 .TP
+.BR \-\-prefix=DIR
+Set the \fBEPREFIX\fR environment variable.
+.TP
 .BR "\-\-pretend " (\fB\-p\fR)
 Instead of actually performing the merge, simply display what *would*
 have been installed if \fB\-\-pretend\fR weren't used.  Using \fB\-\-pretend\fR
@@ -846,6 +849,13 @@ This setting can be added to
 \fBEMERGE_DEFAULT_OPTS\fR (see make.conf(5)) and later overridden via the
 command line.
 .SH "ENVIRONMENT OPTIONS"
+.TP
+\fBEPREFIX\fR = \fI[path]\fR
+Use \fBEPREFIX\fR to specify the target prefix to be used for merging packages
+or ebuilds. This variable can be set via the \fB\-\-prefix\fR
+option or in \fBmake.conf\fR(5) (the command line overrides other settings).
+.br
+Defaults to the prefix where portage is currently installed.
 .TP
 \fBROOT\fR = \fI[path]\fR
 Use \fBROOT\fR to specify the target root filesystem to be used for

pym/_emerge/actions.py
@@ -2017,8 +2017,8 @@ def action_sync(settings, trees, mtimedb, myopts, myaction):
 			myportdir = None
 	out = portage.output.EOutput()
 	global_config_path = GLOBAL_CONFIG_PATH
-	if settings['EPREFIX']:
-		global_config_path = os.path.join(settings['EPREFIX'],
+	if portage.const.EPREFIX:
+		global_config_path = os.path.join(portage.const.EPREFIX,
 				GLOBAL_CONFIG_PATH.lstrip(os.sep))
 	if not myportdir:
 		sys.stderr.write("!!! PORTDIR is undefined.  " + \
@@ -3150,7 +3150,8 @@ def load_emerge_config(emerge_config=None, **kargs):
 		emerge_config = _emerge_config(**kargs)
 
 	kwargs = {}
-	for k, envvar in (("config_root", "PORTAGE_CONFIGROOT"), ("target_root", "ROOT")):
+	for k, envvar in (("config_root", "PORTAGE_CONFIGROOT"), ("target_root", "ROOT"),
+			("eprefix", "EPREFIX")):
 		v = os.environ.get(envvar, None)
 		if v and v.strip():
 			kwargs[k] = v
@@ -3334,8 +3335,8 @@ def missing_sets_warning(root_config, missing_sets):
 	if root_config.sets:
 		msg.append("        sets defined: %s" % ", ".join(root_config.sets))
 	global_config_path = portage.const.GLOBAL_CONFIG_PATH
-	if root_config.settings['EPREFIX']:
-		global_config_path = os.path.join(root_config.settings['EPREFIX'],
+	if portage.const.EPREFIX:
+		global_config_path = os.path.join(portage.const.EPREFIX,
 				portage.const.GLOBAL_CONFIG_PATH.lstrip(os.sep))
 	msg.append("        This usually means that '%s'" % \
 		(os.path.join(global_config_path, "sets/portage.conf"),))

pym/_emerge/main.py
@@ -541,6 +541,11 @@ def parse_opts(tmpcmdline, silent=False):
 			"choices"  : true_y_or_n
 		},
 
+		"--prefix": {
+			"help"     : "specify the installation prefix",
+			"action"   : "store"
+		},
+
 		"--quiet": {
 			"shortopt" : "-q",
 			"help"     : "reduced or condensed output",
@@ -1024,6 +1029,8 @@ def emerge_main(args=None):
 		os.environ["PORTAGE_CONFIGROOT"] = myopts["--config-root"]
 	if "--root" in myopts:
 		os.environ["ROOT"] = myopts["--root"]
+	if "--prefix" in myopts:
+		os.environ["EPREFIX"] = myopts["--prefix"]
 	if "--accept-properties" in myopts:
 		os.environ["ACCEPT_PROPERTIES"] = myopts["--accept-properties"]
 	if "--accept-restrict" in myopts:

pym/portage/_legacy_globals.py
@@ -27,7 +27,8 @@ def _get_legacy_global(name):
 	os.umask(0o22)
 
 	kwargs = {}
-	for k, envvar in (("config_root", "PORTAGE_CONFIGROOT"), ("target_root", "ROOT")):
+	for k, envvar in (("config_root", "PORTAGE_CONFIGROOT"),
+			("target_root", "ROOT"), ("eprefix", "EPREFIX")):
 		kwargs[k] = os.environ.get(envvar)
 
 	portage._initializing_globals = True

pym/portage/_sets/__init__.py
@@ -17,6 +17,7 @@ try:
 		from configparser import SafeConfigParser
 except ImportError:
 	from ConfigParser import SafeConfigParser, NoOptionError, ParsingError
+import portage
 from portage import os
 from portage import load_mod
 from portage import _unicode_decode
@@ -295,8 +296,8 @@ def load_default_config(settings, trees):
 		return SetConfig(None, settings, trees)
 
 	global_config_path = GLOBAL_CONFIG_PATH
-	if settings['EPREFIX']:
-		global_config_path = os.path.join(settings['EPREFIX'],
+	if portage.const.EPREFIX:
+		global_config_path = os.path.join(portage.const.EPREFIX,
 			GLOBAL_CONFIG_PATH.lstrip(os.sep))
 	def _getfiles():
 		for path, dirs, files in os.walk(os.path.join(global_config_path, "sets")):

commit 3b08575233ecf1d3e6f31f959741a4826aeac4a9

portageq: fix eroot parameter (bug #529200)
The portageq eroot parameter has been broken since commit
c9f6aa9f0151adb3c86706eaef1914cdbdcf2b6d, due to premature instantiation
of portage.settings (before the ROOT variable was set). Premature access
to the portage.settings attribute must be avoided by using other
available means to determine the EPREFIX.

Fixes: c9f6aa9f0151 ("Add cross-prefix support")
X-Gentoo-Bug: 529200
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=529200
Acked-by: Alexander Berntsen <bernalex@gentoo.org>

bin/portageq
@@ -1392,7 +1392,14 @@ def main(argv):
 			sys.stderr.write("Run portageq with --help for info\n")
 			sys.stderr.flush()
 			sys.exit(os.EX_USAGE)
-		eprefix = portage.settings["EPREFIX"]
+		# Calculate EPREFIX and ROOT that will be used to construct
+		# portage.settings later. It's tempting to use
+		# portage.settings["EPREFIX"] here, but that would force
+		# instantiation of portage.settings, which we don't want to do
+		# until after we've calculated ROOT (see bug #529200).
+		eprefix = os.environ.get("EPREFIX", portage.const.EPREFIX)
+		if eprefix:
+			eprefix = portage.util.normalize_path(eprefix)
 		eroot = portage.util.normalize_path(argv[2])
 
 		if eprefix:

pym/portage/tests/emerge/test_simple.py
@@ -217,6 +217,8 @@ pkg_preinst() {
 		self.assertFalse(test_ebuild is None)
 
 		cross_prefix = os.path.join(eprefix, "cross_prefix")
+		cross_root = os.path.join(eprefix, "cross_root")
+		cross_eroot = os.path.join(cross_root, eprefix.lstrip(os.sep))
 
 		test_commands = (
 			env_update_cmd,
@@ -318,6 +320,10 @@ pkg_preinst() {
 				portageq_cmd + ("has_version", cross_prefix, "dev-libs/A"),
 			({"EPREFIX" : cross_prefix},) + \
 				portageq_cmd + ("has_version", cross_prefix, "dev-libs/B"),
+
+			# Test ROOT support
+			({"ROOT": cross_root},) + emerge_cmd + ("dev-libs/B",),
+			portageq_cmd + ("has_version", cross_eroot, "dev-libs/B"),
 		)
 
 		distdir = playground.distdir
@@ -372,8 +378,8 @@ pkg_preinst() {
 				os.environ["__PORTAGE_TEST_HARDLINK_LOCKS"]
 
 		updates_dir = os.path.join(test_repo_location, "profiles", "updates")
-		dirs = [cachedir, cachedir_pregen, cross_prefix, distdir, fake_bin,
-			portage_tmpdir, updates_dir,
+		dirs = [cachedir, cachedir_pregen, cross_eroot, cross_prefix,
+			distdir, fake_bin, portage_tmpdir, updates_dir,
 			user_config_dir, var_cache_edb]
 		etc_symlinks = ("dispatch-conf.conf", "etc-update.conf")
 		# Override things that may be unavailable, or may have portability