a4784ffb9406e69811aaf21818459aff00fadb6b
Zac Medico <zmedico@gentoo.org>
2014-12-07 16:42:03 -0800

    WorldSelectedSet._load: load all nested atoms/sets
    Since commit 3e327e8c849cf6bfb84a3a3ec5c080ab4bc4653c,
    AutounmaskTestCase.testAutounmaskAndSets fails due to incorrect logic
    in WorldSelectedSet._load. Fix it to load all nested atoms/sets.
    
    Fixes: 3e327e8c849c ("Split @selected into @selected-packages and @selected-sets")

--- pym/portage/_sets/files.py
+++ pym/portage/_sets/files.py
@@ -222,7 +222,7 @@ class WorldSelectedSet(EditablePackageSet):
 	def load(self):
 		self._pkgset.load()
 		self._setset.load()
-		self._setAtoms(self._pkgset._atoms | self._pkgset._nonatoms)
+		self._setAtoms(chain(self._pkgset, self._setset))
 
 	def lock(self):
 		self._pkgset.lock()
