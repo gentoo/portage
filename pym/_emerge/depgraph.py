gentoo/portage

commit 17bc89c5db424c3f04af28a1da5bf90a943ac368

Update links to Handbooks

pym/_emerge/depgraph.py
@@ -9059,7 +9059,7 @@ def show_mask_docs():
 def show_blocker_docs_link():
 	writemsg("\nFor more information about " + bad("Blocked Packages") + ", please refer to the following\n", noiselevel=-1)
 	writemsg("section of the Gentoo Linux x86 Handbook (architecture is irrelevant):\n\n", noiselevel=-1)
-	writemsg("http://www.gentoo.org/doc/en/handbook/handbook-x86.xml?full=1#blocked\n\n", noiselevel=-1)
+        writemsg("https://wiki.gentoo.org/wiki/Handbook:X86/Working/Portage#Blocked_packages\n\n", noiselevel=-1)
 
 def get_masking_status(pkg, pkgsettings, root_config, myrepo=None, use=None):
 	return [mreason.message for \

cnf/make.conf.example
@@ -11,7 +11,7 @@
 # example, quite a few packages have optional X, gtk or GNOME functionality
 # that can only be enabled or disabled at compile-time. Gentoo Linux has a
 # very extensive set of USE variables described in our USE variable HOWTO at
-# http://www.gentoo.org/doc/en/handbook/handbook-x86.xml?part=2&chap=1
+# https://wiki.gentoo.org/wiki/Handbook:X86/Working/USE
 #
 # The available list of use flags with descriptions is in your portage tree.
 # Use 'less' to view them:  --> less /usr/portage/profiles/use.desc <--

commit 6370873af5844fa7e76ddb603a3cad17cff99eae

Fix "TabError: inconsistent use of tabs and spaces in indentation" with Python 3.
Regression was introduced in commit 17bc89c5db424c3f04af28a1da5bf90a943ac368.

pym/_emerge/depgraph.py
@@ -9059,7 +9059,7 @@ def show_mask_docs():
 def show_blocker_docs_link():
 	writemsg("\nFor more information about " + bad("Blocked Packages") + ", please refer to the following\n", noiselevel=-1)
 	writemsg("section of the Gentoo Linux x86 Handbook (architecture is irrelevant):\n\n", noiselevel=-1)
-        writemsg("https://wiki.gentoo.org/wiki/Handbook:X86/Working/Portage#Blocked_packages\n\n", noiselevel=-1)
+	writemsg("https://wiki.gentoo.org/wiki/Handbook:X86/Working/Portage#Blocked_packages\n\n", noiselevel=-1)
 
 def get_masking_status(pkg, pkgsettings, root_config, myrepo=None, use=None):
 	return [mreason.message for \