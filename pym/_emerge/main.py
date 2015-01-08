gentoo/portage

commit c8a850685232b938cf0896fc2d80e72d57edad17

Add --sync-submodule <glsa|news|profiles> (534070)
This adds support for a new --sync-submodule option to both emerge and
emaint. When this option is used with the sync action, only the selected
submodules are synced. Each submodule is referenced using an abstract
identifier, which serves to hide the implementation details involving
the precise locations of specific submodules within each repository.

Currently, --sync-submodule has no effect for sync protocols other than
rsync, but the new SyncBase._get_submodule_paths() method will be useful
for implementing support in other SyncBase subclasses.

X-Gentoo-Bug: 534070
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=534070
Acked-by: Alexander Berntsen <bernalex@gentoo.org>

pym/_emerge/main.py
@@ -1,4 +1,4 @@
-# Copyright 1999-2014 Gentoo Foundation
+# Copyright 1999-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import print_function
@@ -631,6 +631,13 @@ def parse_opts(tmpcmdline, silent=False):
 		"--selective": {
 			"help"    : "identical to --noreplace",
 			"choices" : true_y_or_n
+		},
+
+		"--sync-submodule": {
+			"help"    : ("Restrict sync to the specified submodule(s)."
+				" (--sync action only)"),
+			"choices" : ("glsa", "news", "profiles"),
+			"action" : "append",
 		},
 
 		"--use-ebuild-visibility": {

pym/portage/emaint/modules/sync/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Gentoo Foundation
+# Copyright 2014-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 doc = """Check repos.conf settings and sync repositories."""
@@ -36,6 +36,16 @@ module_spec = {
 					'action': 'store_true',
 					'dest': 'auto',
 					'func': 'auto_sync',
+					},
+				},
+			'opt_desc': {
+				'sync-submodule': {
+					"long": "--sync-submodule",
+					"help": ("(sync module only): Restrict sync "
+						"to the specified submodule(s)"),
+					"choices": ("glsa", "news", "profiles"),
+					"action": "append",
+					"dest": "sync_submodule",
 					},
 				}
 			}

pym/portage/sync/syncbase.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Gentoo Foundation
+# Copyright 2014-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 '''
@@ -12,6 +12,11 @@ import os
 import portage
 from portage.util import writemsg_level
 
+_SUBMODULE_PATH_MAP = {
+	'glsa': 'metadata/glsa',
+	'news': 'metadata/news',
+	'profiles': 'profiles',
+}
 
 class SyncBase(object):
 	'''Base Sync class for subclassing'''
@@ -57,7 +62,6 @@ class SyncBase(object):
 		self.xterm_titles = self.options.get('xterm_titles', False)
 		self.spawn_kwargs = self.options.get('spawn_kwargs', None)
 
-
 	def exists(self, **kwargs):
 		'''Tests whether the repo actually exists'''
 		if kwargs:
@@ -100,3 +104,11 @@ class SyncBase(object):
 		# and portdb properly account for its existence.
 		'''
 		pass
+
+	def _get_submodule_paths(self):
+		paths = []
+		emerge_config = self.options.get('emerge_config')
+		if emerge_config is not None:
+			for name in emerge_config.opts.get('--sync-submodule', []):
+				paths.append(_SUBMODULE_PATH_MAP[name])
+		return tuple(paths)

pym/portage/emaint/main.py
@@ -1,4 +1,4 @@
-# Copyright 2005-2014 Gentoo Foundation
+# Copyright 2005-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import print_function
@@ -34,6 +34,7 @@ class OptionItem(object):
 		self.action = opt.get('action')
 		self.type = opt.get('type')
 		self.dest = opt.get('dest')
+		self.choices = opt.get('choices')
 
 	@property
 	def pargs(self):
@@ -58,6 +59,8 @@ class OptionItem(object):
 			kwargs['type'] = self.type
 		if self.dest is not None:
 			kwargs['dest'] = self.dest
+		if self.choices is not None:
+			kwargs['choices'] = self.choices
 		return kwargs
 
 def usage(module_controller):
@@ -89,7 +92,10 @@ def module_opts(module_controller, module):
 		opts = DEFAULT_OPTIONS
 	for opt in sorted(opts):
 		optd = opts[opt]
-		opto = "  %s, %s" % (optd['short'], optd['long'])
+		if 'short' in optd:
+			opto = "  %s, %s" % (optd['short'], optd['long'])
+		else:
+			opto = "  %s" % (optd['long'],)
 		_usage += '%s %s\n' % (opto.ljust(15), optd['help'])
 	_usage += '\n'
 	return _usage
@@ -171,6 +177,10 @@ def emaint_main(myargv):
 		parser_options.append(OptionItem(DEFAULT_OPTIONS[opt]))
 	for mod in module_names[1:]:
 		desc = module_controller.get_func_descriptions(mod)
+		if desc:
+			for opt in desc:
+				parser_options.append(OptionItem(desc[opt]))
+		desc = module_controller.get_opt_descriptions(mod)
 		if desc:
 			for opt in desc:
 				parser_options.append(OptionItem(desc[opt]))

man/emaint.1
@@ -1,4 +1,4 @@
-.TH "EMAINT" "1" "Nov 2008" "Portage VERSION" "Portage"
+.TH "EMAINT" "1" "Jan 2015" "Portage VERSION" "Portage"
 .SH NAME
 emaint \- performs system health checks and maintenance
 .SH SYNOPSIS
@@ -71,6 +71,13 @@ Sync all repositories which have a sync\-uri specified. (sync command only)
 .TP
 .B \-r, \-\-repo REPO
 Sync the repository specified. (sync command only)
+.TP
+.BR "\-\-sync-submodule <glsa|news|profiles>"
+Restrict sync to the specified submodule(s). This option may be
+specified multiple times, in order to sync multiple submodules.
+Currently, this option has no effect for sync protocols other
+than rsync.
+(sync command only)
 .SH "REPORTING BUGS"
 Please report bugs via http://bugs.gentoo.org/
 .SH AUTHORS

man/emerge.1
@@ -1,4 +1,4 @@
-.TH "EMERGE" "1" "Dec 2014" "Portage VERSION" "Portage"
+.TH "EMERGE" "1" "Jan 2015" "Portage VERSION" "Portage"
 .SH "NAME"
 emerge \- Command\-line interface to the Portage system
 .SH "SYNOPSIS"
@@ -835,6 +835,13 @@ first package in the resume list. Dependencies are recalculated for
 remaining packages and any that have unsatisfied dependencies or are
 masked will be automatically dropped. Also see the related
 \fB\-\-keep\-going\fR option.
+.TP
+.BR "\-\-sync\-submodule <glsa|news|profiles>"
+Restrict sync to the specified submodule(s). This option may be
+specified multiple times, in order to sync multiple submodules.
+Currently, this option has no effect for sync protocols other
+than rsync.
+(--sync action only)
 .TP
 .BR "\-\-tree " (\fB\-t\fR)
 Shows the dependency tree for the given target by indenting dependencies.

pym/portage/emaint/modules/sync/sync.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Gentoo Foundation
+# Copyright 2014-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 import logging
@@ -90,7 +90,8 @@ class SyncRepos(object):
 			return_messages = options.get('return-messages', False)
 		else:
 			return_messages = False
-		return self._sync(selected, return_messages)
+		return self._sync(selected, return_messages,
+			emaint_opts=options)
 
 
 	def all_repos(self, **kwargs):
@@ -101,7 +102,8 @@ class SyncRepos(object):
 			return_messages = options.get('return-messages', False)
 		else:
 			return_messages = False
-		return self._sync(selected, return_messages)
+		return self._sync(selected, return_messages,
+			emaint_opts=options)
 
 
 	def repo(self, **kwargs):
@@ -123,7 +125,8 @@ class SyncRepos(object):
 			if return_messages:
 				return msgs
 			return
-		return self._sync(selected, return_messages)
+		return self._sync(selected, return_messages,
+			emaint_opts=options)
 
 
 	@staticmethod
@@ -189,7 +192,15 @@ class SyncRepos(object):
 		return selected
 
 
-	def _sync(self, selected_repos, return_messages):
+	def _sync(self, selected_repos, return_messages,
+		emaint_opts=None):
+
+		if emaint_opts is not None:
+			for k, v in emaint_opts.items():
+				if v is not None:
+					k = "--" + k.replace("_", "-")
+					self.emerge_config.opts[k] = v
+
 		msgs = []
 		if not selected_repos:
 			msgs.append("Emaint sync, nothing to sync... returning")

pym/portage/module.py
@@ -1,4 +1,4 @@
-# Copyright 2005-2014 Gentoo Foundation
+# Copyright 2005-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 
@@ -178,4 +178,19 @@ class Modules(object):
 		else:
 			raise InvalidModuleName("Module name '%s' was invalid or not"
 				%modname + "found")
+		return desc
+
+	def get_opt_descriptions(self, modname):
+		"""Retrieves the module class exported options descriptions
+
+		@type modname: string
+		@param modname: the module class name
+		@type dictionary
+		@return: the modules class exported options descriptions
+		"""
+		if modname and modname in self.module_names:
+			desc = self._modules[modname].get('opt_desc')
+		else:
+			raise InvalidModuleName(
+				"Module name '%s' was invalid or not found" % modname)
 		return desc

commit b75a834d421fecc1a3962c047cce9f110bf7da70

Use _SUBMODULE_PATH_MAP keys for option choices
Fix commit c8a850685232b938cf0896fc2d80e72d57edad17 to re-use the keys
from _SUBMODULE_PATH_MAP for option choices.

Fixes: c8a850685232 ("Add --sync-submodule <glsa|news|profiles> (534070)")
Suggested-by: Brian Dolbec <dolsen@gentoo.org>
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/main.py
@@ -20,6 +20,7 @@ portage.proxy.lazyimport.lazyimport(globals(),
 )
 from portage import os
 from portage.util._argparse import ArgumentParser
+from portage.sync import _SUBMODULE_PATH_MAP
 
 if sys.hexversion >= 0x3000000:
 	long = int
@@ -636,7 +637,7 @@ def parse_opts(tmpcmdline, silent=False):
 		"--sync-submodule": {
 			"help"    : ("Restrict sync to the specified submodule(s)."
 				" (--sync action only)"),
-			"choices" : ("glsa", "news", "profiles"),
+			"choices" : tuple(_SUBMODULE_PATH_MAP),
 			"action" : "append",
 		},
 

pym/portage/emaint/modules/sync/__init__.py
@@ -1,6 +1,8 @@
 # Copyright 2014-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
+from ....sync import _SUBMODULE_PATH_MAP
+
 doc = """Check repos.conf settings and sync repositories."""
 __doc__ = doc[:]
 
@@ -43,7 +45,7 @@ module_spec = {
 					"long": "--sync-submodule",
 					"help": ("(sync module only): Restrict sync "
 						"to the specified submodule(s)"),
-					"choices": ("glsa", "news", "profiles"),
+					"choices": tuple(_SUBMODULE_PATH_MAP),
 					"action": "append",
 					"dest": "sync_submodule",
 					},

pym/portage/sync/__init__.py
@@ -1,11 +1,18 @@
-# Copyright 2014 Gentoo Foundation
+# Copyright 2014-2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 import os
 
+from portage import OrderedDict
 from portage.module import Modules
 from portage.sync.controller import SyncManager
 from portage.sync.config_checks import check_type
+
+_SUBMODULE_PATH_MAP = OrderedDict([
+	('glsa', 'metadata/glsa'),
+	('news', 'metadata/news'),
+	('profiles', 'profiles'),
+])
 
 path = os.path.join(os.path.dirname(__file__), "modules")
 # initial development debug info

pym/portage/sync/syncbase.py
@@ -11,12 +11,7 @@ import os
 
 import portage
 from portage.util import writemsg_level
-
-_SUBMODULE_PATH_MAP = {
-	'glsa': 'metadata/glsa',
-	'news': 'metadata/news',
-	'profiles': 'profiles',
-}
+from . import _SUBMODULE_PATH_MAP
 
 class SyncBase(object):
 	'''Base Sync class for subclassing'''