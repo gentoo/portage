e30ad8803d82f11776f2da55b6650dee715154fa
Michał Górny <mgorny@gentoo.org>
2015-01-17 12:10:23 +0100

    compression_probe: support lz4, lzip, lzop

--- pym/portage/util/compression_probe.py
+++ pym/portage/util/compression_probe.py
@@ -14,13 +14,21 @@ from portage.exception import FileNotFound, PermissionDenied
 _decompressors = {
 	"bzip2": "${PORTAGE_BUNZIP2_COMMAND:-${PORTAGE_BZIP2_COMMAND} -d}",
 	"gzip": "gzip -d",
+	"lz4": "lz4 -d",
+	"lzip": "lzip -d",
+	"lzop": "lzop -d",
 	"xz": "xz -d",
 }
 
 _compression_re = re.compile(b'^(' +
 	b'(?P<bzip2>\x42\x5a\x68\x39)|' +
 	b'(?P<gzip>\x1f\x8b)|' +
+	b'(?P<lz4>(?:\x04\x22\x4d\x18|\x02\x21\x4c\x18))|' +
+	b'(?P<lzip>LZIP)|' +
+	b'(?P<lzop>\x89LZO\x00\x0d\x0a\x1a\x0a)|' +
 	b'(?P<xz>\xfd\x37\x7a\x58\x5a\x00))')
+
+_max_compression_re_len = 9
 
 def compression_probe(f):
 	"""
@@ -29,6 +37,9 @@ def compression_probe(f):
 
 		bzip2
 		gzip
+		lz4
+		lzip
+		lzop
 		xz
 
 	@param f: a file path, or file-like object
@@ -59,7 +70,7 @@ def compression_probe(f):
 
 def _compression_probe_file(f):
 
-	m = _compression_re.match(f.read(6))
+	m = _compression_re.match(f.read(_max_compression_re_len))
 	if m is not None:
 		for k, v in m.groupdict().items():
 			if v is not None:
