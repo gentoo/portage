70a41bd4404cccc502a3eacf6335c8d2c9503129
Michał Górny <mgorny@gentoo.org>
2015-01-30 23:53:50 +0100

    test_compile_modules: skip files that require newer Python version
    Support skipping Python modules and scripts that require newer Python
    version than currently used during compile tests. Add a metadata db that
    can be used to store additional information about Python files, and
    store the required language version there.

---
+++ pym/portage/tests/lint/metadata.py
@@ -0,0 +1,11 @@
+# Copyright 2015 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+module_metadata = {
+}
+
+script_metadata = {
+	'socks5-server.py': {
+		'required_python': '3.3',
+	},
+}
