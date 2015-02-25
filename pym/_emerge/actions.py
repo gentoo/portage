gentoo/portage

commit 206efe5f6341bce99a5e9994a0458c304513b2c3

emerge: check for writable PKGDIR (490732)
If there are packages to be merged and "buildpkg" or "buildsyspkg" is
enabled, then bail out early if PKGDIR is not writable (in order to
avoid a fatal EROFS error which would otherwise occur later on).
Behavior remains unchanged for --pretend, --fetchonly and
--fetch-all-uri. For --ask, it will bail out just after the last
relevant --ask prompt.

X-Gentoo-Bug: 490732
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=490732
Acked-by: Alexander Berntsen <bernalex@gentoo.org>

pym/_emerge/actions.py
@@ -428,7 +428,18 @@ def action_build(settings, trees, mtimedb,
 			# least show warnings about missed updates and such.
 			mydepgraph.display_problems()
 
-		if not Scheduler._opts_no_self_update.intersection(myopts):
+
+		need_write_vardb = not Scheduler. \
+			_opts_no_self_update.intersection(myopts)
+
+		need_write_bindb = not any(x in myopts for x in
+			("--fetchonly", "--fetch-all-uri", "--pretend")) and \
+			(any("buildpkg" in trees[eroot]["root_config"].
+				settings.features for eroot in trees) or
+			any("buildsyspkg" in trees[eroot]["root_config"].
+				settings.features for eroot in trees))
+
+		if need_write_bindb or need_write_vardb:
 
 			eroots = set()
 			for x in mydepgraph.altlist():
@@ -436,10 +447,23 @@ def action_build(settings, trees, mtimedb,
 					eroots.add(x.root)
 
 			for eroot in eroots:
-				if not trees[eroot]["vartree"].dbapi.writable:
+				if need_write_vardb and \
+					not trees[eroot]["vartree"].dbapi.writable:
 					writemsg_level("!!! %s\n" %
 						_("Read-only file system: %s") %
 						trees[eroot]["vartree"].dbapi._dbroot,
+						level=logging.ERROR, noiselevel=-1)
+					return 1
+
+				if need_write_bindb and \
+					("buildpkg" in trees[eroot]["root_config"].
+					settings.features or
+					"buildsyspkg" in trees[eroot]["root_config"].
+					settings.features) and \
+					not trees[eroot]["bintree"].dbapi.writable:
+					writemsg_level("!!! %s\n" %
+						_("Read-only file system: %s") %
+						trees[eroot]["bintree"].pkgdir,
 						level=logging.ERROR, noiselevel=-1)
 					return 1
 

pym/portage/dbapi/bintree.py
@@ -18,6 +18,7 @@ portage.proxy.lazyimport.lazyimport(globals(),
 	'portage.util:atomic_ofstream,ensure_dirs,normalize_path,' + \
 		'writemsg,writemsg_stdout',
 	'portage.util.listdir:listdir',
+	'portage.util.path:first_existing',
 	'portage.util._urlopen:urlopen@_urlopen',
 	'portage.versions:best,catpkgsplit,catsplit,_pkg_str',
 )
@@ -84,6 +85,17 @@ class bindbapi(fakedbapi):
 			])
 		self._aux_cache_slot_dict = slot_dict_class(self._aux_cache_keys)
 		self._aux_cache = {}
+
+	@property
+	def writable(self):
+		"""
+		Check if PKGDIR is writable, or permissions are sufficient
+		to create it if it does not exist yet.
+		@rtype: bool
+		@return: True if PKGDIR is writable or can be created,
+			False otherwise
+		"""
+		return os.access(first_existing(self.bintree.pkgdir), os.W_OK)
 
 	def match(self, *pargs, **kwargs):
 		if self.bintree and not self.bintree.populated:

commit 0b453300f4da44a8a32e05f6d75da18847806739

actions.py: fix missing localization import (bug 541302)
Since commit 206efe5f6341bce99a5e9994a0458c304513b2c3, FEATURES=buildpkg
triggers a NameError due to a missing localization import. Fix the
NameError, and also allow read-only PKGDIR if the --usepkgonly option
is enabled.

Fixes: 206efe5f6341 ("emerge: check for writable PKGDIR (490732)")
X-Gentoo-Bug: 541302
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=541302
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/actions.py
@@ -63,6 +63,7 @@ from portage.util._async.SchedulerInterface import SchedulerInterface
 from portage.util._eventloop.global_event_loop import global_event_loop
 from portage._global_updates import _global_updates
 from portage.sync.old_tree_timestamp import old_tree_timestamp_warn
+from portage.localization import _
 from portage.metadata import action_metadata
 
 from _emerge.clear_caches import clear_caches
@@ -433,7 +434,8 @@ def action_build(settings, trees, mtimedb,
 			_opts_no_self_update.intersection(myopts)
 
 		need_write_bindb = not any(x in myopts for x in
-			("--fetchonly", "--fetch-all-uri", "--pretend")) and \
+			("--fetchonly", "--fetch-all-uri",
+			"--pretend", "--usepkgonly")) and \
 			(any("buildpkg" in trees[eroot]["root_config"].
 				settings.features for eroot in trees) or
 			any("buildsyspkg" in trees[eroot]["root_config"].