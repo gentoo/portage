gentoo/portage

commit c3586c5e15c8373d08f9192713a2b03d4542faaf

WorldSelectedSet: fix breakage for bug #539746
Since commit 3e327e8c849cf6bfb84a3a3ec5c080ab4bc4653c,
AutounmaskTestCase.testAutounmaskAndSets fails due to incorrect logic
in WorldSelectedSet._load. Fix it to load all nested atoms/sets. Also,
fix WorldSelectedSet.write so that self._pkgset and self._setset have
separate data structures. Shared data structures triggered the
corruption reported in bug #539746.

Fixes: 3e327e8c849c ("Split @selected into @selected-packages and @selected-sets")

X-Gentoo-Bug: 539746
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=539746

pym/portage/_sets/files.py
@@ -215,15 +215,14 @@ class WorldSelectedSet(EditablePackageSet):
 		self._setset = WorldSelectedSetsSet(eroot)
 
 	def write(self):
-		self._pkgset._atoms = self._atoms
+		self._pkgset._atoms = self._atoms.copy()
 		self._pkgset.write()
-		self._setset._nonatoms = self._nonatoms
+		self._setset._nonatoms = self._nonatoms.copy()
 		self._setset.write()
 
 	def load(self):
-		self._pkgset.load()
-		self._setset.load()
-		self._setAtoms(self._pkgset._atoms | self._pkgset._nonatoms)
+		# Iterating over these sets loads them automatically.
+		self._setAtoms(chain(self._pkgset, self._setset))
 
 	def lock(self):
 		self._pkgset.lock()

commit 0bdfd62184d3224c23f5293683ba6fb5c0769e6a

WorldSelectedSet: fix load method for bug 542732
Since commit c3586c5e15c8373d08f9192713a2b03d4542faaf, the WorldSelectedSet
load method does not force the set to load if it has already been loaded.
Fix it to do so.

Fixes: c3586c5e15c8 ("WorldSelectedSet: fix breakage for bug #539746")
X-Gentoo-Bug: 542732
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=542732
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/portage/_sets/files.py
@@ -221,7 +221,10 @@ class WorldSelectedSet(EditablePackageSet):
 		self._setset.write()
 
 	def load(self):
-		# Iterating over these sets loads them automatically.
+		# Iterating over these sets does not force them to load if they
+		# have been loaded previously.
+		self._pkgset.load()
+		self._setset.load()
 		self._setAtoms(chain(self._pkgset, self._setset))
 
 	def lock(self):