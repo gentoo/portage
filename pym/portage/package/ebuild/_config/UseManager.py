gentoo/portage

commit b65e1fc5fe723bd07bf034f6e08878fa3eb033cb

Support USE_EXPAND prefixes in package.use and relevant files
Support prefixing groups of USE_EXPAND flags with 'USE_EXPAND:' in user
configuration package.use and similar files. This provides a convenient
way of declaring specifying multiple USE_EXPAND flags and matches the
syntax supported by Paludis. Example:

  dev-util/netbeans NETBEANS_MODULES: php webcommon extide
  media-libs/mesa osmesa VIDEO_CARDS: intel nouveau

X-Gentoo-Bug: 471776
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=471776

pym/portage/package/ebuild/_config/UseManager.py
@@ -202,11 +202,18 @@ class UseManager(object):
 		useflag_re = _get_useflag_re(eapi)
 		for k, v in file_dict.items():
 			useflags = []
+			use_expand_prefix = ''
 			for prefixed_useflag in v:
+				if extended_syntax and prefixed_useflag[-1] == ":":
+					use_expand_prefix = prefixed_useflag[:-1].lower() + "_"
+					continue
+
 				if prefixed_useflag[:1] == "-":
-					useflag = prefixed_useflag[1:]
+					useflag = use_expand_prefix + prefixed_useflag[1:]
+					prefixed_useflag = "-" + useflag
 				else:
-					useflag = prefixed_useflag
+					useflag = use_expand_prefix + prefixed_useflag
+					prefixed_useflag = useflag
 				if useflag_re.match(useflag) is None:
 					writemsg(_("--- Invalid USE flag for '%s' in '%s': '%s'\n") %
 						(k, file_name, prefixed_useflag), noiselevel=-1)
@@ -227,7 +234,18 @@ class UseManager(object):
 			pusedict = grabdict_package(
 				os.path.join(location, file_name), recursive=1, allow_wildcard=True, allow_repo=True, verify_eapi=False)
 			for k, v in pusedict.items():
-				ret.setdefault(k.cp, {})[k] = tuple(v)
+				l = []
+				use_expand_prefix = ''
+				for flag in v:
+					if flag[-1] == ":":
+						use_expand_prefix = flag[:-1].lower() + "_"
+						continue
+					if flag[0] == "-":
+						nv = "-" + use_expand_prefix + flag[1:]
+					else:
+						nv = use_expand_prefix + flag
+					l.append(nv)
+				ret.setdefault(k.cp, {})[k] = tuple(l)
 
 		return ret
 

man/portage.5
@@ -792,6 +792,10 @@ documentation for QT.  Easy as pie my friend!
 .nf
 \- comments begin with # (no inline comments)
 \- one DEPEND atom per line with space-delimited USE flags
+\- USE flags can be prefixed with USE_EXPAND name followed by a colon (:)
+and a space. In this case, all the names following it are treated
+as values for the USE_EXPAND. Note that if you need to remove earlier
+assignments or defaults, you need to explicitly specify '\-*'.
 .fi
 
 .I Example:
@@ -800,6 +804,8 @@ documentation for QT.  Easy as pie my friend!
 =x11\-libs/gtk+\-2* doc
 # disable mysql support for QT
 x11\-libs/qt \-mysql
+# set preferred video card for all packages
+*/* VIDEO_CARDS: \-* radeon
 .fi
 .TP
 .BR repos.conf

commit 5c53c0831dc34b7c1edab2ab8726e54cb5957289

UseManager: handle newlines for USE_EXPAND prefixes (bug 546512)
Since commit b65e1fc5fe723bd07bf034f6e08878fa3eb033cb, USE_EXPAND
prefixes affected subsequent lines of the same atom in package.use.

Fixes: b65e1fc5fe72 ("Support USE_EXPAND prefixes in package.use and relevant files")
X-Gentoo-Bug: 546512
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=546512
Signed-off-by: Zac Medico <zmedico@gentoo.org>
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/portage/package/ebuild/_config/UseManager.py
@@ -208,6 +208,9 @@ class UseManager(object):
 			useflags = []
 			use_expand_prefix = ''
 			for prefixed_useflag in v:
+				if extended_syntax and prefixed_useflag == "\n":
+					use_expand_prefix = ""
+					continue
 				if extended_syntax and prefixed_useflag[-1] == ":":
 					use_expand_prefix = prefixed_useflag[:-1].lower() + "_"
 					continue
@@ -236,11 +239,14 @@ class UseManager(object):
 		ret = ExtendedAtomDict(dict)
 		if user_config:
 			pusedict = grabdict_package(
-				os.path.join(location, file_name), recursive=1, allow_wildcard=True, allow_repo=True, verify_eapi=False)
+				os.path.join(location, file_name), recursive=1, newlines=1, allow_wildcard=True, allow_repo=True, verify_eapi=False)
 			for k, v in pusedict.items():
 				l = []
 				use_expand_prefix = ''
 				for flag in v:
+					if flag == "\n":
+						use_expand_prefix = ""
+						continue
 					if flag[-1] == ":":
 						use_expand_prefix = flag[:-1].lower() + "_"
 						continue

pym/portage/util/__init__.py
@@ -340,7 +340,7 @@ def stack_lists(lists, incremental=1, remember_source_file=False,
 	else:
 		return list(new_list)
 
-def grabdict(myfilename, juststrings=0, empty=0, recursive=0, incremental=1):
+def grabdict(myfilename, juststrings=0, empty=0, recursive=0, incremental=1, newlines=0):
 	"""
 	This function grabs the lines in a file, normalizes whitespace and returns lines in a dictionary
 
@@ -354,6 +354,8 @@ def grabdict(myfilename, juststrings=0, empty=0, recursive=0, incremental=1):
 	@type recursive: Boolean (integer)
 	@param incremental: Append to the return list, don't overwrite
 	@type incremental: Boolean (integer)
+	@param newlines: Append newlines
+	@type newlines: Boolean (integer)
 	@rtype: Dictionary
 	@return:
 	1.  Returns the lines in a file in a dictionary, for example:
@@ -379,6 +381,8 @@ def grabdict(myfilename, juststrings=0, empty=0, recursive=0, incremental=1):
 			continue
 		if len(myline) < 1 and empty == 1:
 			continue
+		if newlines:
+			myline.append("\n")
 		if incremental:
 			newdict.setdefault(myline[0], []).extend(myline[1:])
 		else:
@@ -424,7 +428,7 @@ def read_corresponding_eapi_file(filename, default="0"):
 		return default
 	return eapi
 
-def grabdict_package(myfilename, juststrings=0, recursive=0,
+def grabdict_package(myfilename, juststrings=0, recursive=0, newlines=0,
 	allow_wildcard=False, allow_repo=False, allow_build_id=False,
 	verify_eapi=False, eapi=None, eapi_default="0"):
 	""" Does the same thing as grabdict except it validates keys
@@ -438,7 +442,7 @@ def grabdict_package(myfilename, juststrings=0, recursive=0,
 	atoms = {}
 	for filename in file_list:
 		d = grabdict(filename, juststrings=False,
-			empty=True, recursive=False, incremental=True)
+			empty=True, recursive=False, incremental=True, newlines=newlines)
 		if not d:
 			continue
 		if verify_eapi and eapi is None: