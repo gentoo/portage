e148b48dd42c910d833af51f0b81f3b929f23375
Brian Dolbec <dolsen@gentoo.org>
2015-05-18 15:41:28 -0700

    Updates for a release

--- setup.py
+++ setup.py
@@ -612,7 +612,7 @@ def get_manpages():
 
 setup(
 	name = 'portage',
-	version = '2.2.19',
+	version = '2.2.20',
 	url = 'https://wiki.gentoo.org/wiki/Project:Portage',
 	author = 'Gentoo Portage Development Team',
 	author_email = 'dev-portage@gentoo.org',
