gentoo/portage

commit b01a1b90d8c5c9ebc9b2c956520f3096cc07342d

Add FEATURES=cgroup to isolate phase processes.
We create a cgroup for each ebuild. The ebuild processes are put in that
cgroup, therefore allowing us to keep track of their replication. Once
ebuild phase is over, we can happily kill all orphans.

pym/_emerge/AbstractEbuildProcess.py
@@ -2,7 +2,9 @@
 # Distributed under the terms of the GNU General Public License v2
 
 import io
+import platform
 import stat
+import subprocess
 import textwrap
 from _emerge.SpawnProcess import SpawnProcess
 from _emerge.EbuildBuildDir import EbuildBuildDir
@@ -20,8 +22,10 @@ class AbstractEbuildProcess(SpawnProcess):
 
 	__slots__ = ('phase', 'settings',) + \
 		('_build_dir', '_ipc_daemon', '_exit_command', '_exit_timeout_id')
+
 	_phases_without_builddir = ('clean', 'cleanrm', 'depend', 'help',)
 	_phases_interactive_whitelist = ('config',)
+	_phases_without_cgroup = ('preinst', 'postinst', 'prerm', 'postrm', 'config')
 
 	# Number of milliseconds to allow natural exit of the ebuild
 	# process after it has called the exit command via IPC. It
@@ -58,6 +62,41 @@ class AbstractEbuildProcess(SpawnProcess):
 			self._set_returncode((self.pid, 1 << 8))
 			self._async_wait()
 			return
+
+		# Check if the cgroup hierarchy is in place. If it's not, mount it.
+		if (os.geteuid() == 0 and platform.system() == 'Linux'
+				and 'cgroup' in self.settings.features
+				and self.phase not in self._phases_without_cgroup):
+			cgroup_root = '/sys/fs/cgroup'
+			cgroup_portage = os.path.join(cgroup_root, 'portage')
+			cgroup_path = os.path.join(cgroup_portage,
+					'%s:%s' % (self.settings["CATEGORY"],
+						self.settings["PF"]))
+			try:
+				# cgroup tmpfs
+				if not os.path.ismount(cgroup_root):
+					# we expect /sys/fs to be there already
+					if not os.path.isdir(cgroup_root):
+						os.mkdir(cgroup_root, 0o755)
+					subprocess.check_call(['mount', '-t', 'tmpfs',
+						'-o', 'rw,nosuid,nodev,noexec,mode=0755',
+						'tmpfs', cgroup_root])
+
+				# portage subsystem
+				if not os.path.ismount(cgroup_portage):
+					if not os.path.isdir(cgroup_portage):
+						os.mkdir(cgroup_portage, 0o755)
+					subprocess.check_call(['mount', '-t', 'cgroup',
+						'-o', 'rw,nosuid,nodev,noexec,none,name=portage',
+						'tmpfs', cgroup_portage])
+
+				# the ebuild cgroup
+				if not os.path.isdir(cgroup_path):
+					os.mkdir(cgroup_path)
+			except (subprocess.CalledProcessError, OSError):
+				pass
+			else:
+				self.cgroup = cgroup_path
 
 		if self.background:
 			# Automatically prevent color codes from showing up in logs,

man/make.conf.5
@@ -276,6 +276,10 @@ like "File not recognized: File truncated"), try recompiling the application
 with ccache disabled before reporting a bug. Unless you are doing development
 work, do not enable ccache.
 .TP
+.B cgroup
+Use Linux control group to control processes spawned by ebuilds. This allows
+emerge to safely kill all subprocesses when ebuild phase exits.
+.TP
 .B clean\-logs
 Enable automatic execution of the command specified by the
 PORT_LOGDIR_CLEAN variable. The default PORT_LOGDIR_CLEAN setting will

pym/_emerge/SpawnProcess.py
@@ -7,7 +7,9 @@ except ImportError:
 	# http://bugs.jython.org/issue1074
 	fcntl = None
 
+import errno
 import platform
+import signal
 import sys
 
 from _emerge.SubProcess import SubProcess
@@ -29,7 +31,7 @@ class SpawnProcess(SubProcess):
 
 	_spawn_kwarg_names = ("env", "opt_name", "fd_pipes",
 		"uid", "gid", "groups", "umask", "logfile",
-		"path_lookup", "pre_exec", "close_fds")
+		"path_lookup", "pre_exec", "close_fds", "cgroup")
 
 	__slots__ = ("args",) + \
 		_spawn_kwarg_names + ("_pipe_logger", "_selinux_type",)
@@ -179,3 +181,36 @@ class SpawnProcess(SubProcess):
 			pipe_logger.removeExitListener(self._pipe_logger_exit)
 			pipe_logger.cancel()
 			pipe_logger.wait()
+
+	def _set_returncode(self, wait_retval):
+		SubProcess._set_returncode(self, wait_retval)
+
+		if self.cgroup:
+			def get_pids(cgroup):
+				try:
+					with open(os.path.join(cgroup, 'cgroup.procs'), 'r') as f:
+						return f.read().split()
+				except OSError:
+					# cgroup removed already?
+					return []
+
+			def kill_all(pids, sig):
+				for p in pids:
+					try:
+						os.kill(int(p), sig)
+					except OSError as e:
+						if e.errno != errno.ESRCH:
+							raise
+
+			# step 1: kill all orphans
+			pids = get_pids(self.cgroup)
+			if pids:
+				kill_all(pids, signal.SIGTERM)
+
+			# step 2: remove the cgroup
+			try:
+				os.rmdir(self.cgroup)
+			except OSError:
+				# it may be removed already, or busy
+				# we can't do anything good about it
+				pass

pym/portage/const.py
@@ -96,7 +96,7 @@ EBUILD_PHASES            = ("pretend", "setup", "unpack", "prepare", "configure"
                            "nofetch", "config", "info", "other")
 SUPPORTED_FEATURES       = frozenset([
                            "assume-digests", "binpkg-logs", "buildpkg", "buildsyspkg", "candy",
-                           "ccache", "chflags", "clean-logs",
+                           "ccache", "cgroup", "chflags", "clean-logs",
                            "collision-protect", "compress-build-logs", "compressdebug",
                            "compress-index", "config-protect-if-modified",
                            "digest", "distcc", "distcc-pump", "distlocks",

pym/portage/process.py
@@ -185,7 +185,7 @@ def cleanup():
 def spawn(mycommand, env={}, opt_name=None, fd_pipes=None, returnpid=False,
           uid=None, gid=None, groups=None, umask=None, logfile=None,
           path_lookup=True, pre_exec=None, close_fds=True, unshare_net=False,
-          unshare_ipc=False):
+          unshare_ipc=False, cgroup=None):
 	"""
 	Spawns a given command.
 	
@@ -222,6 +222,8 @@ def spawn(mycommand, env={}, opt_name=None, fd_pipes=None, returnpid=False,
 	@type unshare_net: Boolean
 	@param unshare_ipc: If True, IPC will be unshared from the spawned process
 	@type unshare_ipc: Boolean
+	@param cgroup: CGroup path to bind the process to
+	@type cgroup: String
 
 	logfile requires stdout and stderr to be assigned to this process (ie not pointed
 	   somewhere else.)
@@ -300,7 +302,7 @@ def spawn(mycommand, env={}, opt_name=None, fd_pipes=None, returnpid=False,
 			try:
 				_exec(binary, mycommand, opt_name, fd_pipes,
 					env, gid, groups, uid, umask, pre_exec, close_fds,
-					unshare_net, unshare_ipc)
+					unshare_net, unshare_ipc, cgroup)
 			except SystemExit:
 				raise
 			except Exception as e:
@@ -370,7 +372,7 @@ def spawn(mycommand, env={}, opt_name=None, fd_pipes=None, returnpid=False,
 	return 0
 
 def _exec(binary, mycommand, opt_name, fd_pipes, env, gid, groups, uid, umask,
-	pre_exec, close_fds, unshare_net, unshare_ipc):
+	pre_exec, close_fds, unshare_net, unshare_ipc, cgroup):
 
 	"""
 	Execute a given binary with options
@@ -399,6 +401,8 @@ def _exec(binary, mycommand, opt_name, fd_pipes, env, gid, groups, uid, umask,
 	@type unshare_net: Boolean
 	@param unshare_ipc: If True, IPC will be unshared from the spawned process
 	@type unshare_ipc: Boolean
+	@param cgroup: CGroup path to bind the process to
+	@type cgroup: String
 	@rtype: None
 	@return: Never returns (calls os.execve)
 	"""
@@ -434,6 +438,13 @@ def _exec(binary, mycommand, opt_name, fd_pipes, env, gid, groups, uid, umask,
 	signal.signal(signal.SIGQUIT, signal.SIG_DFL)
 
 	_setup_pipes(fd_pipes, close_fds=close_fds)
+
+	# Add to cgroup
+	# it's better to do it from the child since we can guarantee
+	# it is done before we start forking children
+	if cgroup:
+		with open(os.path.join(cgroup, 'cgroup.procs'), 'a') as f:
+			f.write('%d\n' % os.getpid())
 
 	# Unshare (while still uid==0)
 	if unshare_net or unshare_ipc:

commit 551837f0de95cf8e3e741e76094b31cfc0d68bd5

AbstractEbuildProcess: use mkdtemp to avoid cgroup interference (bug 554108)
This fixes parallel builds of the same package, so they don't try
to kill eachothers processes.

Fixes: b01a1b90d8c5 ("Add FEATURES=cgroup to isolate phase processes.")
X-Gentoo-Bug: 554108
X-Gentoo-Bug-url: https://bugs.gentoo.org/show_bug.cgi?id=554108
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/AbstractEbuildProcess.py
@@ -1,10 +1,12 @@
 # Copyright 1999-2012 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
+import errno
 import io
 import platform
 import stat
 import subprocess
+import tempfile
 import textwrap
 from _emerge.SpawnProcess import SpawnProcess
 from _emerge.EbuildBuildDir import EbuildBuildDir
@@ -14,7 +16,7 @@ from portage.elog import messages as elog_messages
 from portage.localization import _
 from portage.package.ebuild._ipc.ExitCommand import ExitCommand
 from portage.package.ebuild._ipc.QueryCommand import QueryCommand
-from portage import os
+from portage import shutil, os
 from portage.util._pty import _create_pty_or_pipe
 from portage.util import apply_secpass_permissions
 
@@ -69,9 +71,7 @@ class AbstractEbuildProcess(SpawnProcess):
 				and self.phase not in self._phases_without_cgroup):
 			cgroup_root = '/sys/fs/cgroup'
 			cgroup_portage = os.path.join(cgroup_root, 'portage')
-			cgroup_path = os.path.join(cgroup_portage,
-					'%s:%s' % (self.settings["CATEGORY"],
-						self.settings["PF"]))
+
 			try:
 				# cgroup tmpfs
 				if not os.path.ismount(cgroup_root):
@@ -90,9 +90,9 @@ class AbstractEbuildProcess(SpawnProcess):
 						'-o', 'rw,nosuid,nodev,noexec,none,name=portage',
 						'tmpfs', cgroup_portage])
 
-				# the ebuild cgroup
-				if not os.path.isdir(cgroup_path):
-					os.mkdir(cgroup_path)
+				cgroup_path = tempfile.mkdtemp(dir=cgroup_portage,
+					prefix='%s:%s.' % (self.settings["CATEGORY"],
+					self.settings["PF"]))
 			except (subprocess.CalledProcessError, OSError):
 				pass
 			else:
@@ -312,6 +312,13 @@ class AbstractEbuildProcess(SpawnProcess):
 
 	def _set_returncode(self, wait_retval):
 		SpawnProcess._set_returncode(self, wait_retval)
+
+		if self.cgroup is not None:
+			try:
+				shutil.rmtree(self.cgroup)
+			except EnvironmentError as e:
+				if e.errno != errno.ENOENT:
+					raise
 
 		if self._exit_timeout_id is not None:
 			self.scheduler.source_remove(self._exit_timeout_id)