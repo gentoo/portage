472d351506e518148e130a45ea011de191c36967
Brian Dolbec <dolsen@gentoo.org>
2015-08-13 07:49:22 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -612,7 +612,7 @@ def get_manpages():
 
 setup(
 	name = 'portage',
-	version = '2.2.20',
+	version = '2.2.20.1',
 	url = 'https://wiki.gentoo.org/wiki/Project:Portage',
 	author = 'Gentoo Portage Development Team',
 	author_email = 'dev-portage@gentoo.org',
