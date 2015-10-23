713611a67455d2f38b0b452d0eaed17fd68bd517
Zac Medico <zmedico@gentoo.org>
2015-10-22 23:48:34 -0700

    repoman: check deps of stable ebuilds for unstable configurations (bug 563546)
    For ebuilds with stable keywords, check if the dependencies are
    satisfiable for unstable configurations, since use.stable.mask
    is not applied for unstable configurations. Fix the
    KeywordsManager.isStable method to behave consistently for both
    repoman and emerge.
    
    X-Gentoo-Bug: 563546
    X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=563546
    Acked-by: Alexander Berntsen <bernalex@gentoo.org>

--- pym/repoman/scanner.py
+++ pym/repoman/scanner.py
@@ -379,14 +379,24 @@ class Scanner(object):
 						else:
 							arches.add((keyword, arch, (arch, keyword)))
 					else:
+						# For ebuilds with stable keywords, check if the
+						# dependencies are satisfiable for unstable
+						# configurations, since use.stable.mask is not
+						# applied for unstable configurations (see bug
+						# 563546).
 						if keyword == "*":
 							for expanded_arch in self.profiles:
 								if expanded_arch == "**":
 									continue
 								arches.add(
 									(keyword, expanded_arch, (expanded_arch,)))
+								arches.add(
+									(keyword, expanded_arch,
+										(expanded_arch, "~" + expanded_arch)))
 						else:
 							arches.add((keyword, keyword, (keyword,)))
+							arches.add((keyword, keyword,
+								(keyword, "~" + keyword)))
 				if not arches:
 					# Use an empty profile for checking dependencies of
 					# packages that have empty KEYWORDS.
