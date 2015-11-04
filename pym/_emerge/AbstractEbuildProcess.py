gentoo/portage

commit e6be71af3f67ea274db455e9c24d5f84bd372c39

AbstractEbuildProcess: remove cgroup with release_agent (bug 561264)
Use the cgroup release_agent hook to remove cgroups automatically
upon release.

X-Gentoo-bug: 561264
X-Gentoo-bug-url: https://bugs.gentoo.org/show_bug.cgi?id=561264
Acked-by: Alexander Berntsen <bernalex@gentoo.org>

pym/_emerge/AbstractEbuildProcess.py
@@ -89,6 +89,13 @@ class AbstractEbuildProcess(SpawnProcess):
 					subprocess.check_call(['mount', '-t', 'cgroup',
 						'-o', 'rw,nosuid,nodev,noexec,none,name=portage',
 						'tmpfs', cgroup_portage])
+					with open(os.path.join(
+						cgroup_portage, 'release_agent'), 'w') as f:
+						f.write(os.path.join(self.settings['PORTAGE_BIN_PATH'],
+							'cgroup-release-agent'))
+					with open(os.path.join(
+						cgroup_portage, 'notify_on_release'), 'w') as f:
+						f.write('1')
 
 				cgroup_path = tempfile.mkdtemp(dir=cgroup_portage,
 					prefix='%s:%s.' % (self.settings["CATEGORY"],
@@ -312,13 +319,6 @@ class AbstractEbuildProcess(SpawnProcess):
 
 	def _set_returncode(self, wait_retval):
 		SpawnProcess._set_returncode(self, wait_retval)
-
-		if self.cgroup is not None:
-			try:
-				shutil.rmtree(self.cgroup)
-			except EnvironmentError as e:
-				if e.errno != errno.ENOENT:
-					raise
 
 		if self._exit_timeout_id is not None:
 			self.scheduler.source_remove(self._exit_timeout_id)

bin/cgroup-release-agent
@@ -1,0 +1,2 @@
+#!/bin/bash
+exec rmdir "/sys/fs/cgroup/portage/${1}"

commit 21930f70b7c3b98c9b3c1ac7b3ebdd31581ec00b

AbstractEbuildProcess: validate cgroup release agent
Since commit e6be71af3f67ea274db455e9c24d5f84bd372c39, the release agent
can refer to a temporary file if it was set while portage was updating
itself. Fix it to reset the release agent when necessary.

Fixes: e6be71af3f67 ("AbstractEbuildProcess: remove cgroup with release_agent (bug 561264)")
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/AbstractEbuildProcess.py
@@ -96,6 +96,24 @@ class AbstractEbuildProcess(SpawnProcess):
 					with open(os.path.join(
 						cgroup_portage, 'notify_on_release'), 'w') as f:
 						f.write('1')
+				else:
+					# Update release_agent if it no longer exists, because
+					# it refers to a temporary path when portage is updating
+					# itself.
+					release_agent = os.path.join(
+						cgroup_portage, 'release_agent')
+					try:
+						with open(release_agent) as f:
+							release_agent_path = f.readline().rstrip('\n')
+					except EnvironmentError:
+						release_agent_path = None
+
+					if (release_agent_path is None or
+						not os.path.exists(release_agent_path)):
+						with open(release_agent, 'w') as f:
+							f.write(os.path.join(
+								self.settings['PORTAGE_BIN_PATH'],
+								'cgroup-release-agent'))
 
 				cgroup_path = tempfile.mkdtemp(dir=cgroup_portage,
 					prefix='%s:%s.' % (self.settings["CATEGORY"],