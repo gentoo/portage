gentoo/portage

commit 9b19ac5696c487dab58d7c10990fe07fd7f1f731

Convert funcs of isolated-functions.sh to __ prefixed namespace.

bin/save-ebuild-env.sh
@@ -46,11 +46,11 @@ save_ebuild_env() {
 	done
 	unset x
 
-	unset -f assert assert_sigpipe_ok \
-		dump_trace die \
-		quiet_mode vecho elog_base eqawarn elog \
-		einfo einfon ewarn eerror ebegin _eend eend KV_major \
-		KV_minor KV_micro KV_to_int get_KV unset_colors set_colors has \
+	unset -f assert __assert_sigpipe_ok \
+		__dump_trace die \
+		__quiet_mode __vecho __elog_base eqawarn elog \
+		einfo einfon ewarn eerror ebegin __eend eend KV_major \
+		KV_minor KV_micro KV_to_int get_KV __1 __1 has \
 		has_phase_defined_up_to \
 		hasv hasq qa_source qa_call \
 		addread addwrite adddeny addpredict _sb_append_var \
@@ -63,7 +63,7 @@ save_ebuild_env() {
 		abort_test abort_install dyn_prepare dyn_configure \
 		dyn_compile dyn_test dyn_install \
 		dyn_preinst dyn_pretend dyn_help debug-print debug-print-function \
-		debug-print-section helpers_die inherit EXPORT_FUNCTIONS \
+		debug-print-section __helpers_die inherit EXPORT_FUNCTIONS \
 		nonfatal register_success_hook \
 		save_ebuild_env filter_readonly_variables preprocess_ebuild_env \
 		source_all_bashrcs \

bin/ebuild-helpers/dobin
@@ -5,7 +5,7 @@
 source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}"/isolated-functions.sh
 
 if [[ $# -lt 1 ]] ; then
-	helpers_die "${0##*/}: at least one argument needed"
+	__helpers_die "${0##*/}: at least one argument needed"
 	exit 1
 fi
 
@@ -13,7 +13,7 @@ fi
 	case "$EAPI" in 0|1|2) ED=${D} ;; esac
 
 if [[ ! -d ${ED}${DESTTREE}/bin ]] ; then
-	install -d "${ED}${DESTTREE}/bin" || { helpers_die "${0##*/}: failed to install ${ED}${DESTTREE}/bin"; exit 2; }
+	install -d "${ED}${DESTTREE}/bin" || { __helpers_die "${0##*/}: failed to install ${ED}${DESTTREE}/bin"; exit 2; }
 fi
 
 ret=0
@@ -28,5 +28,5 @@ for x in "$@" ; do
 	((ret|=$?))
 done
 
-[[ $ret -ne 0 ]] && helpers_die "${0##*/} failed"
+[[ $ret -ne 0 ]] && __helpers_die "${0##*/} failed"
 exit ${ret}

bin/ebuild-helpers/doconfd
@@ -4,7 +4,7 @@
 
 if [[ $# -lt 1 ]] ; then
 	source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}"/isolated-functions.sh
-	helpers_die "${0##*/}: at least one argument needed"
+	__helpers_die "${0##*/}: at least one argument needed"
 	exit 1
 fi
 

bin/ebuild-helpers/dodir
@@ -9,5 +9,5 @@ source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}"/isolated-functions.sh
 
 install -d ${DIROPTIONS} "${@/#/${ED}/}"
 ret=$?
-[[ $ret -ne 0 ]] && helpers_die "${0##*/} failed"
+[[ $ret -ne 0 ]] && __helpers_die "${0##*/} failed"
 exit $ret

bin/ebuild-helpers/dodoc
@@ -16,7 +16,7 @@ esac
 source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}"/isolated-functions.sh
 
 if [ $# -lt 1 ] ; then
-	helpers_die "${0##*/}: at least one argument needed"
+	__helpers_die "${0##*/}: at least one argument needed"
 	exit 1 	
 fi
 
@@ -41,5 +41,5 @@ for x in "$@" ; do
 	fi
 done
 
-[[ $ret -ne 0 ]] && helpers_die "${0##*/} failed"
+[[ $ret -ne 0 ]] && __helpers_die "${0##*/} failed"
 exit ${ret}

bin/ebuild-helpers/doenvd
@@ -4,7 +4,7 @@
 
 if [[ $# -lt 1 ]] ; then
 	source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}"/isolated-functions.sh
-	helpers_die "${0##*/}: at least one argument needed"
+	__helpers_die "${0##*/}: at least one argument needed"
 	exit 1
 fi
 

bin/ebuild-helpers/doexe
@@ -5,7 +5,7 @@
 source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}"/isolated-functions.sh
 
 if [[ $# -lt 1 ]] ; then
-	helpers_die "${0##*/}: at least one argument needed"
+	__helpers_die "${0##*/}: at least one argument needed"
 	exit 1
 fi
 
@@ -26,7 +26,7 @@ for x in "$@" ; do
 		cp "$x" "$TMP"
 		mysrc=$TMP/${x##*/}
 	elif [ -d "${x}" ] ; then
-		vecho "doexe: warning, skipping directory ${x}"
+		__vecho "doexe: warning, skipping directory ${x}"
 		continue
 	else
 		mysrc="${x}"
@@ -42,5 +42,5 @@ done
 
 rm -rf "$TMP"
 
-[[ $ret -ne 0 ]] && helpers_die "${0##*/} failed"
+[[ $ret -ne 0 ]] && __helpers_die "${0##*/} failed"
 exit $ret

bin/ebuild-helpers/doheader
@@ -11,7 +11,7 @@ case "${EAPI}" in
 esac
 
 if [[ $# -lt 1 ]] ; then
-	helpers_die "${0##*/}: at least one argument needed"
+	__helpers_die "${0##*/}: at least one argument needed"
 	exit 1
 fi
 

commit d806eea86c6e2be5b17abeed6e930ea4ab32aeb2

bin/save-ebuild-env.sh: remove bogus __1 function names
In commit 9b19ac5696c487dab58d7c10990fe07fd7f1f731, these bogus __1
values were inserted where unset_colors and set_colors used to be. The
unset_colors and set_colors function have been renamed to have __
prefixes, and they are correctly unset further down.

Fixes: 9b19ac5696c4 ("Convert funcs of isolated-functions.sh to __ prefixed namespace.")

bin/save-ebuild-env.sh
@@ -51,7 +51,7 @@ __save_ebuild_env() {
 		__dump_trace die \
 		__quiet_mode __vecho __elog_base eqawarn elog \
 		einfo einfon ewarn eerror ebegin __eend eend KV_major \
-		KV_minor KV_micro KV_to_int get_KV __1 __1 has \
+		KV_minor KV_micro KV_to_int get_KV has \
 		__has_phase_defined_up_to \
 		hasv hasq __qa_source __qa_call \
 		addread addwrite adddeny addpredict __sb_append_var \