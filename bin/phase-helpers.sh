dddeca6e4409e2a8a0dedc907d208ce352a48b72
Zac Medico <zmedico@gentoo.org>
2015-11-23 16:59:37 -0800

    einstalldocs: use lazy docinto calls (bug 566654)
    This avoids unnecessary creation of an empty documentation directory.
    
    X-Gentoo-Bug: 566654
    X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=566654

--- bin/phase-helpers.sh
+++ bin/phase-helpers.sh
@@ -954,26 +954,26 @@ fi
 if ___eapi_has_einstalldocs; then
 	einstalldocs() {
 		(
-			docinto .
 			if ! declare -p DOCS &>/dev/null ; then
 				local d
 				for d in README* ChangeLog AUTHORS NEWS TODO CHANGES \
 						THANKS BUGS FAQ CREDITS CHANGELOG ; do
-					[[ -s ${d} ]] && dodoc "${d}"
+					[[ -s ${d} ]] && docinto / && dodoc "${d}"
 				done
 			elif [[ $(declare -p DOCS) == "declare -a"* ]] ; then
-				[[ ${DOCS[@]} ]] && dodoc -r "${DOCS[@]}"
+				[[ ${DOCS[@]} ]] && docinto / && dodoc -r "${DOCS[@]}"
 			else
-				[[ ${DOCS} ]] && dodoc -r ${DOCS}
+				[[ ${DOCS} ]] && docinto / && dodoc -r ${DOCS}
 			fi
 		)
 
 		(
-			docinto html
 			if [[ $(declare -p HTML_DOCS 2>/dev/null) == "declare -a"* ]] ; then
-				[[ ${HTML_DOCS[@]} ]] && dodoc -r "${HTML_DOCS[@]}"
+				[[ ${HTML_DOCS[@]} ]] && \
+					docinto html && dodoc -r "${HTML_DOCS[@]}"
 			else
-				[[ ${HTML_DOCS} ]] && dodoc -r ${HTML_DOCS}
+				[[ ${HTML_DOCS} ]] && \
+					docinto html && dodoc -r ${HTML_DOCS}
 			fi
 		)
 	}
