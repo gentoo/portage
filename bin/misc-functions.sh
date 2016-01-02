gentoo/portage

commit 5a1b870fe54ac06f864a648c3ea5cc118f6ce911

MEDIUM: misc-functions: Be more quiet when removing non existing INSTALL_MASK
When FEATURES="nodoc noinfo noman" is used, you will get the following
output for every packages, even virtual that contain no file:

  # FEATURES="nodoc noinfo noman" emerge -va1t virtual/cron
  ..
  >>> Installing (1 of 1) virtual/cron-0::gentoo
   * Removing /usr/share/man
   * Removing /usr/share/info
   * Removing /usr/share/doc
   * checking 0 files for package collisions
  >>> Merging virtual/cron-0 to /

The following patch makes the output as:

  # FEATURES="nodoc noinfo noman" emerge -va1t virtual/cron
  ..
  >>> Installing (1 of 1) virtual/cron-0::gentoo
   * checking 0 files for package collisions
  >>> Merging virtual/cron-0 to /

Signed-off-by: Bertrand Jacquin <bertrand@jacquin.bzh>
Acked-by: Brian Dolbec <dolsen@gentoo.org>

bin/misc-functions.sh
@@ -267,9 +267,12 @@ install_mask() {
 	local no_inst
 	for no_inst in ${install_mask}; do
 		set +o noglob
-		__quiet_mode || einfo "Removing ${no_inst}"
+
 		# normal stuff
-		rm -Rf "${root}"/${no_inst} >&/dev/null
+		if [[ -e "${root}"/${no_inst} || "${root}"/${no_inst} != $(echo "${root}"/${no_inst}) ]] ; then
+			__quiet_mode || einfo "Removing ${no_inst}"
+			rm -Rf "${root}"/${no_inst} >&/dev/null
+		fi
 
 		# we also need to handle globs (*.a, *.h, etc)
 		find "${root}" \( -path "${no_inst}" -or -name "${no_inst}" \) \

commit f9d1791dbb8f61e836d1b453258502d800776bf2

INSTALL_MASK: enable matching of broken symlinks (bug 570530)
Since commit 5a1b870fe54ac06f864a648c3ea5cc118f6ce911, INSTALL_MASK
does not match broken symlinks. Fix it to do so.

Fixes: 5a1b870fe54a ("MEDIUM: misc-functions: Be more quiet when removing non existing INSTALL_MASK")
X-Gentoo-Bug: 570530
X-Gentoo-Bug-url: https://bugs.gentoo.org/show_bug.cgi?id=570530

bin/misc-functions.sh
@@ -275,7 +275,8 @@ install_mask() {
 
 		# The standard case where $no_inst is something that
 		# the shell could expand on its own.
-		if [[ -e "${root}"/${no_inst} || "${root}"/${no_inst} != $(echo "${root}"/${no_inst}) ]] ; then
+		if [[ -e "${root}"/${no_inst} || -L "${root}"/${no_inst} ||
+			"${root}"/${no_inst} != $(echo "${root}"/${no_inst}) ]] ; then
 			__quiet_mode || einfo "Removing ${no_inst}"
 			rm -Rf "${root}"/${no_inst} >&/dev/null
 		fi