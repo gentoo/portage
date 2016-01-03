832cc1cc734858dec86c7041d8f5af2a29677a65
Brian Dolbec <dolsen@gentoo.org>
2016-01-03 11:11:22 -0800

    repoman: Create a new ArchChecks class plugin

---
+++ pym/repoman/modules/scan/arches/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Arches plug-in module for repoman.
+Performs archs checks on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'arches',
+	'description': doc,
+	'provides':{
+		'archs-module': {
+			'name': "arches",
+			'sourcefile': "arches",
+			'class': "ArchChecks",
+			'description': doc,
+			'functions': ['check'],
+			'func_desc': {
+			},
+		},
+	}
+}
+
---
+++ pym/repoman/modules/scan/arches/arches.py
@@ -0,0 +1,64 @@
+# -*- coding:utf-8 -*-
+
+
+class ArchChecks(object):
+
+	def __init__(self, **kwargs):
+		self.options = kwargs.get('options')
+		self.repo_settings = kwargs.get('repo_settings')
+		self.profiles = kwargs.get('profiles')
+
+	def check(self, **kwargs):
+		ebuild = kwargs.get('ebuild')
+		if self.options.ignore_arches:
+			arches = [[
+				self.repo_settings.repoman_settings["ARCH"], self.repo_settings.repoman_settings["ARCH"],
+				self.repo_settings.repoman_settings["ACCEPT_KEYWORDS"].split()]]
+		else:
+			arches = set()
+			for keyword in ebuild.keywords:
+				if keyword[0] == "-":
+					continue
+				elif keyword[0] == "~":
+					arch = keyword[1:]
+					if arch == "*":
+						for expanded_arch in self.profiles:
+							if expanded_arch == "**":
+								continue
+							arches.add(
+								(keyword, expanded_arch, (
+									expanded_arch, "~" + expanded_arch)))
+					else:
+						arches.add((keyword, arch, (arch, keyword)))
+				else:
+					# For ebuilds with stable keywords, check if the
+					# dependencies are satisfiable for unstable
+					# configurations, since use.stable.mask is not
+					# applied for unstable configurations (see bug
+					# 563546).
+					if keyword == "*":
+						for expanded_arch in self.profiles:
+							if expanded_arch == "**":
+								continue
+							arches.add(
+								(keyword, expanded_arch, (expanded_arch,)))
+							arches.add(
+								(keyword, expanded_arch,
+									(expanded_arch, "~" + expanded_arch)))
+					else:
+						arches.add((keyword, keyword, (keyword,)))
+						arches.add((keyword, keyword,
+							(keyword, "~" + keyword)))
+			if not arches:
+				# Use an empty profile for checking dependencies of
+				# packages that have empty KEYWORDS.
+				arches.add(('**', '**', ('**',)))
+		return {'continue': False, 'arches': arches}
+
+	@property
+	def runInPkgs(self):
+		return (False, [])
+
+	@property
+	def runInEbuilds(self):
+		return (True, [self.check])
