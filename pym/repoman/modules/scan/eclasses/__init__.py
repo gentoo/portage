a6492d319e8a78bd3e4671c4e5cbb8765a77a4f9
Brian Dolbec <dolsen@gentoo.org>
2016-01-08 00:15:22 -0800

    repoman: Create LiveEclassChecks class plugin

---
+++ pym/repoman/modules/scan/eclasses/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Eclasses plug-in module for repoman.
+Performs an live and ruby eclass checks on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'eclasses',
+	'description': doc,
+	'provides':{
+		'live-module': {
+			'name': "live",
+			'sourcefile': "live",
+			'class': "LiveEclassChecks",
+			'description': doc,
+			'functions': ['check'],
+			'func_kwargs': {
+			},
+		},
+	}
+}
+
