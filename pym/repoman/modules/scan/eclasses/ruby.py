a4d34a66ff329073ec72d1aa9fbe347f6b8d8def
Brian Dolbec <dolsen@gentoo.org>
2016-01-08 00:46:01 -0800

    repoman: Create RubyEclassChecks class plugin

--- pym/repoman/modules/scan/eclasses/ruby.py
+++ pym/repoman/modules/scan/eclasses/ruby.py
@@ -4,19 +4,23 @@ Performs Ruby eclass checks
 '''
 
 from repoman.qa_data import ruby_deprecated
+from repoman.modules.scan.scanbase import ScanBase
 
 
-class RubyEclassChecks(object):
+class RubyEclassChecks(ScanBase):
 	'''Performs checks for the usage of Ruby eclasses in ebuilds'''
 
-	def __init__(self, qatracker):
+	def __init__(self, **kwargs):
 		'''
 		@param qatracker: QATracker instance
 		'''
-		self.qatracker = qatracker
+		super(RubyEclassChecks, self).__init__(**kwargs)
+		self.qatracker = kwargs.get('qatracker')
 		self.old_ruby_eclasses = ["ruby-ng", "ruby-fakegem", "ruby"]
 
-	def check(self, pkg, ebuild):
+	def check(self, **kwargs):
+		pkg = kwargs.get('pkg')
+		ebuild = kwargs.get('ebuild')
 		is_inherited = lambda eclass: eclass in pkg.inherited
 		is_old_ruby_eclass_inherited = filter(
 			is_inherited, self.old_ruby_eclasses)
@@ -30,3 +34,9 @@ class RubyEclassChecks(object):
 						"IUSE.rubydeprecated",
 						(ebuild.relative_path + ": Deprecated ruby target: %s")
 						% myruby)
+		return {'continue': False}
+
+	@property
+	def runInEbuilds(self):
+		'''Ebuild level scans'''
+		return (True, [self.check])
