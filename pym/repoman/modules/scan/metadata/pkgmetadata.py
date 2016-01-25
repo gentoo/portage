04727cddf1d59b4ed5d2a51a9e111e4995c4a187
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-01-25 21:27:51 +0100

    repoman: inline checks.herds.metadata module into pkgmetadata

--- pym/repoman/modules/scan/metadata/pkgmetadata.py
+++ pym/repoman/modules/scan/metadata/pkgmetadata.py
@@ -24,7 +24,6 @@ except (ImportError, SystemError, RuntimeError, Exception):
 from repoman._portage import portage
 from repoman.metadata import metadata_dtd_uri
 from repoman.checks.herds.herdbase import get_herd_base
-from repoman.checks.herds.metadata import check_metadata, UnknownHerdsError
 from repoman._xml import _XMLParser, _MetadataTreeBuilder, XmlLint
 from repoman.modules.scan.scanbase import ScanBase
 
@@ -38,6 +37,31 @@ metadata_xml_encoding = 'UTF-8'
 metadata_xml_declaration = '<?xml version="1.0" encoding="%s"?>' \
 	% (metadata_xml_encoding,)
 metadata_doctype_name = 'pkgmetadata'
+
+
+class UnknownHerdsError(ValueError):
+	def __init__(self, herd_names):
+		_plural = len(herd_names) != 1
+		super(UnknownHerdsError, self).__init__(
+			'Unknown %s %s' % (
+				_plural and 'herds' or 'herd',
+				','.join('"%s"' % e for e in herd_names)))
+
+
+def check_metadata_herds(xml_tree, herd_base):
+	herd_nodes = xml_tree.findall('herd')
+	unknown_herds = [
+		name for name in (
+			e.text.strip() for e in herd_nodes if e.text is not None)
+		if not herd_base.known_herd(name)]
+
+	if unknown_herds:
+		raise UnknownHerdsError(unknown_herds)
+
+
+def check_metadata(xml_tree, herd_base):
+	if herd_base is not None:
+		check_metadata_herds(xml_tree, herd_base)
 
 
 def parse_metadata_use(xml_tree):
