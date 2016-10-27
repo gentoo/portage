f9ba03db1207c2e130bf821d39b7e78637fe76ac
hackers.terabit <terabit.funtoo@Yandex.com>
2016-10-27 04:09:27 +0000

    Fix tab indentation error

--- pym/_emerge/depgraph.py
+++ pym/_emerge/depgraph.py
@@ -8237,11 +8237,10 @@ class depgraph(object):
 									continue
 								stack.append(os.path.join(p, child))
 			#If the directory is empty add a file with name  pattern file_name.default
-                        if last_file_path == None:
-                                last_file_path=file_path+"/autonmask"
-                                with open(last_file_path,"a+") as default:
-                                        default.write("#"+file_name)
-
+			if last_file_path == None:
+				last_file_path=file_path+"/autonmask"
+				with open(last_file_path,"a+") as default:
+					default.write("#"+file_name)
 			return last_file_path
 
 		write_to_file = autounmask_write and not pretend
