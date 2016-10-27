e65d014706c674bdb31ff2e167a5a46cb3f38bd1
hackers.terabit <terabit.funtoo@Yandex.com>
2016-10-27 03:45:19 +0000

    Change the default autonmask file to just 'autounmask'

--- pym/_emerge/depgraph.py
+++ pym/_emerge/depgraph.py
@@ -8238,7 +8238,7 @@ class depgraph(object):
 								stack.append(os.path.join(p, child))
 			#If the directory is empty add a file with name  pattern file_name.default
                         if last_file_path == None:
-                                last_file_path=file_path+"/"+file_name+".default"
+                                last_file_path=file_path+"/autonmask"
                                 with open(last_file_path,"a+") as default:
                                         default.write("#"+file_name)
 
