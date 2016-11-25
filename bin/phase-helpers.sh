gentoo/portage

commit daa65a336102050396482f08c77524fe99e48c9f

Add tentative EAPI6 .txz unpack support
Support unpacking .txz-suffixed archives.

bin/phase-helpers.sh
@@ -492,6 +492,19 @@ unpack() {
 					__vecho "unpack ${x}: file format not recognized. Ignoring."
 				fi
 				;;
+			txz)
+				if ___eapi_unpack_is_case_sensitive && \
+					[[ " txz " != *" ${suffix} "* ]] ; then
+					eqawarn "QA Notice: unpack called with" \
+						"suffix '${suffix}' which is unofficially supported" \
+						"with EAPI '${EAPI}'. Instead use 'txz'."
+				fi
+				if ___eapi_supports_txz; then
+					__unpack_tar "xz -d" || return 1
+				else
+					__vecho "unpack ${x}: file format not recognized. Ignoring."
+				fi
+				;;
 			*)
 				__vecho "unpack ${x}: file format not recognized. Ignoring."
 				;;

bin/eapi.sh
@@ -118,6 +118,10 @@ ___eapi_unpack_supports_xz() {
 	[[ ! ${1-${EAPI}} =~ ^(0|1|2)$ ]]
 }
 
+___eapi_unpack_supports_txz() {
+	[[ ! ${1-${EAPI}} =~ ^(0|1|2|3|4|4-python|4-slot-abi|5|5-hdepend|5-progress)$ ]]
+}
+
 ___eapi_econf_passes_--disable-dependency-tracking() {
 	[[ ! ${1-${EAPI}} =~ ^(0|1|2|3)$ ]]
 }

commit d5851278daea670910cdcf1f8864b9c4494d0b31

unpack: fix txz unpack support (bug 600660)
Since txz unpack support was added in commit
daa65a336102050396482f08c77524fe99e48c9f, it has been non-functional,
as follows:

phase-helpers.sh: line 521: ___eapi_supports_txz: command not found
unpack portage-2.3.2.txz: file format not recognized. Ignoring.

Fixes: daa65a336102 ("Add tentative EAPI6 .txz unpack support")
X-Gentoo-Bug: 600660
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=600660
Acked-by: Brian Dolbec <dolsen@gentoo.org>

bin/phase-helpers.sh
@@ -518,8 +518,11 @@ unpack() {
 						"suffix '${suffix}' which is unofficially supported" \
 						"with EAPI '${EAPI}'. Instead use 'txz'."
 				fi
-				if ___eapi_supports_txz; then
-					__unpack_tar "xz -d" || return 1
+				if ___eapi_unpack_supports_txz; then
+					if ! tar xof "$srcdir$x"; then
+						__helpers_die "$myfail"
+						return 1
+					fi
 				else
 					__vecho "unpack ${x}: file format not recognized. Ignoring."
 				fi