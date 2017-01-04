gentoo/portage

commit ab07ac68fa1e04ed64e2e0f6c753ff169a32d517

depgraph: select highest version involved in slot conflict (bug 554070)
Fix depgraph's package selection logic to choose the highest version
involved in a slot conflict, for correct operation of conflict_downgrade
logic in the dep_zapdeps function which was introduced in commit
a9064d08ef4c92a5d0d1bfb3dc8a01b7850812b0. This will prevent incorrect
re-ordering of || deps in dep_zapdeps, as reported in bug 554070.

X-Gentoo-Bug: 554070
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=554070
Fixes a9064d08ef4c ("Solve more slot-operator conflicts (531656)")
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/depgraph.py
@@ -6071,8 +6071,15 @@ class depgraph(object):
 					# will always end with a break statement below
 					# this point.
 					if find_existing_node:
-						e_pkg = next(self._dynamic_config._package_tracker.match(
-							root, pkg.slot_atom, installed=False), None)
+						# Use reversed iteration in order to get
+						# descending order here, so that the highest
+						# version involved in a slot conflict is
+						# selected. This is needed for correct operation
+						# of conflict_downgrade logic in the dep_zapdeps
+						# function (see bug 554070).
+						e_pkg = next(reversed(list(
+							self._dynamic_config._package_tracker.match(
+							root, pkg.slot_atom, installed=False))), None)
 
 						if not e_pkg:
 							break

commit 033477d6a4cc1adcabed966b8aa82b51b42af33c

_dep_check_composite_db: select highest in slot conflict (bug 554070)
Fix the _dep_check_composite_db._visible method to select the highest
version involved in a slot conflict, for consistency with the change
in ab07ac68fa1e04ed64e2e0f6c753ff169a32d517. The included unit test
fails without this fix.

Fixes: ab07ac68fa1e ("depgraph: select highest version involved in slot conflict (bug 554070)")
X-Gentoo-Bug: 554070
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=554070
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/depgraph.py
@@ -1,4 +1,4 @@
-# Copyright 1999-2016 Gentoo Foundation
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import division, print_function, unicode_literals
@@ -9130,8 +9130,12 @@ class _dep_check_composite_db(dbapi):
 			# into the same slot.
 			return True
 
-		in_graph = next(self._depgraph._dynamic_config._package_tracker.match(
-			self._root, pkg.slot_atom, installed=False), None)
+		# Use reversed iteration in order to get descending order here,
+		# so that the highest version involved in a slot conflict is
+		# selected (see bug 554070).
+		in_graph = next(reversed(list(
+			self._depgraph._dynamic_config._package_tracker.match(
+			self._root, pkg.slot_atom, installed=False))), None)
 
 		if in_graph is None:
 			# Mask choices for packages which are not the highest visible

pym/portage/tests/resolver/test_imagemagick_graphicsmagick.py
@@ -1,0 +1,104 @@
+# Copyright 2017 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+from portage.tests import TestCase
+from portage.tests.resolver.ResolverPlayground import (
+	ResolverPlayground,
+	ResolverPlaygroundTestCase,
+)
+
+class ImageMagickGraphicsMagickTestCase(TestCase):
+
+	def testImageMagickUpdate(self):
+
+		ebuilds = {
+			"media-gfx/imagemagick-6.9.7.0" : {
+				"EAPI": "6",
+				"SLOT": "0/6.9.7.0",
+			},
+
+			"media-gfx/imagemagick-6.9.6.6" : {
+				"EAPI": "6",
+				"SLOT": "0/6.9.6.6",
+			},
+
+			"media-gfx/inkscape-0.91-r3" : {
+				"EAPI": "6",
+				"DEPEND": "media-gfx/imagemagick:=",
+				"RDEPEND": "media-gfx/imagemagick:=",
+			},
+
+			"media-video/dvdrip-0.98.11-r3" : {
+				"EAPI": "6",
+				"DEPEND": "|| ( media-gfx/graphicsmagick[imagemagick] media-gfx/imagemagick )",
+				"RDEPEND": "|| ( media-gfx/graphicsmagick[imagemagick] media-gfx/imagemagick )",
+			},
+
+			"media-gfx/graphicsmagick-1.3.25" : {
+				"EAPI": "6",
+				"SLOT": "0/1.3",
+				"IUSE": "imagemagick",
+				"RDEPEND": "imagemagick? ( !media-gfx/imagemagick )",
+			},
+		}
+
+		installed = {
+			"media-gfx/imagemagick-6.9.6.6" : {
+				"EAPI": "6",
+				"SLOT": "0/6.9.6.6",
+			},
+
+			"media-gfx/inkscape-0.91-r3" : {
+				"EAPI": "6",
+				"DEPEND": "media-gfx/imagemagick:0/6.9.6.6=",
+				"RDEPEND": "media-gfx/imagemagick:0/6.9.6.6=",
+			},
+
+			"media-video/dvdrip-0.98.11-r3" : {
+				"EAPI": "6",
+				"DEPEND": "|| ( media-gfx/graphicsmagick[imagemagick] media-gfx/imagemagick )",
+				"RDEPEND": "|| ( media-gfx/graphicsmagick[imagemagick] media-gfx/imagemagick )",
+			},
+
+			"media-gfx/graphicsmagick-1.3.25" : {
+				"EAPI": "6",
+				"SLOT": "0/1.3",
+				"IUSE": "imagemagick",
+				"USE": "",
+				"RDEPEND": "imagemagick? ( !media-gfx/imagemagick )",
+			},
+		}
+
+		world = (
+			"media-gfx/inkscape",
+			"media-video/dvdrip",
+			"media-gfx/graphicsmagick",
+		)
+
+		test_cases = (
+
+			# bug #554070: imagemagick upgrade triggered erroneous
+			# autounmask USE change for media-gfx/graphicsmagick[imagemagick]
+			ResolverPlaygroundTestCase(
+				["media-gfx/imagemagick", "@world"],
+				options = {"--update": True, "--deep": True},
+				success = True,
+				mergelist = [
+					"media-gfx/imagemagick-6.9.7.0",
+					"media-gfx/inkscape-0.91-r3"
+				]
+			),
+
+		)
+
+		playground = ResolverPlayground(debug=False,
+			ebuilds=ebuilds, installed=installed, world=world)
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			# Disable debug so that cleanup works.
+			playground.debug = False
+			playground.cleanup()