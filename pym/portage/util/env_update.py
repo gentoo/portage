gentoo/portage

commit 1bc49bead14ddd31c94921fe9c3d1972f0737056

env-update: call ldconfig if found in EROOT
prefix-standalone has a glibc installed by portage with ldconfig
under EROOT/sbin.  It should be called during env-update.

For prefix-rpath, host glibc is used and env-update should not
care about ldconfig.  In this case, no ldconfig is in EROOT/sbin
and ldconfig is skipped.

X-Gentoo-Bug: 532100
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=532100

pym/portage/util/env_update.py
@@ -306,13 +306,17 @@ def _env_update(makelinks, target_root, prev_mtimes, contents, env,
 		if not libdir_contents_changed:
 			makelinks = False
 
-	ldconfig = "/sbin/ldconfig"
 	if "CHOST" in settings and "CBUILD" in settings and \
 		settings["CHOST"] != settings["CBUILD"]:
 		ldconfig = find_binary("%s-ldconfig" % settings["CHOST"])
+	else:
+		ldconfig = os.path.join(eroot, "sbin", "ldconfig")
+
+	if not (os.access(ldconfig, os.X_OK) and os.path.isfile(ldconfig)):
+		ldconfig = None
 
 	# Only run ldconfig as needed
-	if makelinks and ldconfig and not eprefix:
+	if makelinks and ldconfig:
 		# ldconfig has very different behaviour between FreeBSD and Linux
 		if ostype == "Linux" or ostype.lower().endswith("gnu"):
 			# We can't update links if we haven't cleaned other versions first, as

commit b3401efea5820c525c57a1b52c38404054593e0a

env-update: skip os.access call when ldconfig is None (bug 606832)
Since commit 1bc49bead14ddd31c94921fe9c3d1972f0737056, env_update
would raise the following exception with crossdev configurations
when ${CHOST}-ldconfig is not available:

    TypeError: access: can't specify None for path argument

Fixes: 1bc49bead14d ("env-update: call ldconfig if found in EROOT")
X-Gentoo-Bug: 606832
X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=606832

pym/portage/util/env_update.py
@@ -312,7 +312,9 @@ def _env_update(makelinks, target_root, prev_mtimes, contents, env,
 	else:
 		ldconfig = os.path.join(eroot, "sbin", "ldconfig")
 
-	if not (os.access(ldconfig, os.X_OK) and os.path.isfile(ldconfig)):
+	if ldconfig is None:
+		pass
+	elif not (os.access(ldconfig, os.X_OK) and os.path.isfile(ldconfig)):
 		ldconfig = None
 
 	# Only run ldconfig as needed