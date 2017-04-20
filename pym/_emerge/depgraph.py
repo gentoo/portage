gentoo/portage

commit a83bb83909c5a6ac232c8eb5931b28027f4175af

depgraph: trigger slot operator rebuilds via _complete_graph (bug 614390)
Fix _complete_graph to trigger rebuilds of parent packages when they
pull in installed packages that had already been scheduled for rebuild
by the previous calculation.

X-Gentoo-bug: 614390
X-Gentoo-bug-url: https://bugs.gentoo.org/show_bug.cgi?id=614390
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/depgraph.py
@@ -6649,6 +6649,21 @@ class depgraph(object):
 				# will be appropriately reported as a slot collision
 				# (possibly solvable via backtracking).
 				pkg = matches[-1] # highest match
+
+				if (self._dynamic_config._allow_backtracking and
+					not self._want_installed_pkg(pkg) and (dep.atom.soname or (
+					dep.atom.package and dep.atom.slot_operator_built))):
+					# If pkg was already scheduled for rebuild by the previous
+					# calculation, then pulling in the installed instance will
+					# trigger a slot conflict that may go unsolved. Therefore,
+					# trigger a rebuild of the parent if appropriate.
+					dep.child = pkg
+					new_dep = self._slot_operator_update_probe(dep)
+					if new_dep is not None:
+						self._slot_operator_update_backtrack(
+							dep, new_dep=new_dep)
+						continue
+
 				if not self._add_pkg(pkg, dep):
 					return 0
 				if not self._create_graph(allow_unsatisfied=True):

pym/portage/tests/resolver/test_slot_operator_complete_graph.py
@@ -1,0 +1,141 @@
+# Copyright 2017 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+from portage.tests import TestCase
+from portage.tests.resolver.ResolverPlayground import (
+	ResolverPlayground,
+	ResolverPlaygroundTestCase,
+)
+
+class SlotOperatorCompleteGraphTestCase(TestCase):
+
+	def testSlotOperatorCompleteGraph(self):
+
+		ebuilds = {
+			"app-misc/meta-pkg-2" : {
+				"EAPI": "6",
+				"DEPEND": "=app-misc/B-2 =app-misc/C-1  =app-misc/D-1 =dev-libs/foo-2",
+				"RDEPEND": "=app-misc/B-2 =app-misc/C-1 =app-misc/D-1 =dev-libs/foo-2",
+			},
+
+			"app-misc/meta-pkg-1" : {
+				"EAPI": "6",
+				"DEPEND": "=app-misc/B-1 =app-misc/C-1  =app-misc/D-1 =dev-libs/foo-1",
+				"RDEPEND": "=app-misc/B-1 =app-misc/C-1 =app-misc/D-1 =dev-libs/foo-1",
+			},
+
+			"app-misc/B-1" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:=",
+				"RDEPEND": "dev-libs/foo:=",
+			},
+
+			"app-misc/B-2" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:=",
+				"RDEPEND": "dev-libs/foo:=",
+			},
+
+			"app-misc/C-1" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:= app-misc/B",
+				"RDEPEND": "dev-libs/foo:= app-misc/B",
+			},
+
+			"app-misc/C-2" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:= app-misc/B",
+				"RDEPEND": "dev-libs/foo:= app-misc/B",
+			},
+
+			"app-misc/D-1" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:=",
+				"RDEPEND": "dev-libs/foo:=",
+			},
+
+			"app-misc/D-2" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:=",
+				"RDEPEND": "dev-libs/foo:=",
+			},
+
+			"dev-libs/foo-1" : {
+				"EAPI": "6",
+				"SLOT": "0/1",
+			},
+
+			"dev-libs/foo-2" : {
+				"EAPI": "6",
+				"SLOT": "0/2",
+			},
+		}
+
+		installed = {
+			"app-misc/meta-pkg-1" : {
+				"EAPI": "6",
+				"DEPEND": "=app-misc/B-1 =app-misc/C-1  =app-misc/D-1 =dev-libs/foo-1",
+				"RDEPEND": "=app-misc/B-1 =app-misc/C-1 =app-misc/D-1 =dev-libs/foo-1",
+			},
+
+			"app-misc/B-1" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:0/1=",
+				"RDEPEND": "dev-libs/foo:0/1=",
+			},
+
+			"app-misc/C-1" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:0/1= app-misc/B",
+				"RDEPEND": "dev-libs/foo:0/1= app-misc/B",
+			},
+
+			"app-misc/D-1" : {
+				"EAPI": "6",
+				"DEPEND": "dev-libs/foo:0/1=",
+				"RDEPEND": "dev-libs/foo:0/1=",
+			},
+
+			"dev-libs/foo-1" : {
+				"EAPI": "6",
+				"SLOT": "0/1",
+			},
+		}
+
+		world = (
+			"app-misc/meta-pkg",
+		)
+
+		test_cases = (
+			# Test bug 614390, where the depgraph._complete_graph
+			# method pulled in an installed package that had been
+			# scheduled for rebuild by the previous calculation,
+			# triggering an unsolved slot conflict and preventing
+			# slot operator rebuilds.
+			ResolverPlaygroundTestCase(
+				["=app-misc/meta-pkg-2", "app-misc/C"],
+				options = {
+					"--backtrack": 5,
+				},
+				success = True,
+				ambiguous_merge_order = True,
+				mergelist = [
+					'dev-libs/foo-2',
+					('app-misc/D-1', 'app-misc/C-1', 'app-misc/B-2'),
+					'app-misc/meta-pkg-2',
+				]
+			),
+		)
+
+		playground = ResolverPlayground(debug=False,
+			ebuilds=ebuilds, installed=installed,
+			world=world)
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			# Disable debug so that cleanup works.
+			playground.debug = False
+			playground.cleanup()

commit 2366c903bab8f44463106e878f4e0c1ba81f42f8

depgraph._in_blocker_conflict: call _validate_blockers if needed (bug 615982)
Sometimes _complete_graph calls _slot_operator_update_probe, which
sometimes calls _in_blocker_conflict. This case occurs infrequently,
so call _validate_blockers only if needed.

Fixes: a83bb83909c5 ("depgraph: trigger slot operator rebuilds via _complete_graph (bug 614390)")
X-Gentoo-bug: 615982
X-Gentoo-bug-url: https://bugs.gentoo.org/show_bug.cgi?id=615982
Acked-by: Brian Dolbec <dolsen@gentoo.org>

pym/_emerge/depgraph.py
@@ -2176,9 +2176,9 @@ class depgraph(object):
 		only works after the _validate_blockers method has been called.
 		"""
 
-		if self._dynamic_config._blocked_pkgs is None:
-			raise AssertionError(
-				'_in_blocker_conflict called before _validate_blockers')
+		if (self._dynamic_config._blocked_pkgs is None
+			and not self._validate_blockers()):
+			raise self._unknown_internal_error()
 
 		if pkg in self._dynamic_config._blocked_pkgs:
 			return True
@@ -6728,7 +6728,14 @@ class depgraph(object):
 		packages within the graph.  If necessary, create hard deps to ensure
 		correct merge order such that mutually blocking packages are never
 		installed simultaneously. Also add runtime blockers from all installed
-		packages if any of them haven't been added already (bug 128809)."""
+		packages if any of them haven't been added already (bug 128809).
+
+		Normally, this method is called only after the graph is complete, and
+		after _solve_non_slot_operator_slot_conflicts has had an opportunity
+		to solve slot conflicts (possibly removing some blockers). It can also
+		be called earlier, in order to get a preview of the blocker data, but
+		then it needs to be called again after the graph is complete.
+		"""
 
 		# The _in_blocker_conflict method needs to assert that this method
 		# has been called before it, by checking that it is not None.