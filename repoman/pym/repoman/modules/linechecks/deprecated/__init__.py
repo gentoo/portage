dff21e5edda66fc12bc56da347f196a9fdcc09c1
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 00:19:12 +0000

    repoman: New linechecks module, deprecated

---
+++ repoman/pym/repoman/modules/linechecks/deprecated/__init__.py
@@ -0,0 +1,46 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Deprecated plug-in module for repoman LineChecks.
+Performs miscelaneous deprecation checks on ebuilds not covered by
+specialty modules."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'deprecated',
+	'description': doc,
+	'provides':{
+		'useq-check': {
+			'name': "useq",
+			'sourcefile': "deprecated",
+			'class': "DeprecatedUseq",
+			'description': doc,
+		},
+		'hasq-check': {
+			'name': "hasq",
+			'sourcefile': "deprecated",
+			'class': "DeprecatedHasq",
+			'description': doc,
+		},
+		'preserve-check': {
+			'name': "preservelib",
+			'sourcefile': "deprecated",
+			'class': "PreserveOldLib",
+			'description': doc,
+		},
+		'bindnow-check': {
+			'name': "bindnow",
+			'sourcefile': "deprecated",
+			'class': "DeprecatedBindnowFlags",
+			'description': doc,
+		},
+		'inherit-check': {
+			'name': "inherit",
+			'sourcefile': "inherit",
+			'class': "InheritDeprecated",
+			'description': doc,
+		},
+	}
+}
+
