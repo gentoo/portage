ee1a1b4621b2a276d228014a57d4a25ca90575b0
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:04:00 +0000

    repoman: New linechecks module, portage

---
+++ repoman/pym/repoman/modules/linechecks/portage/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Portage plug-in module for repoman LineChecks.
+Performs checks for internal portage variable usage in ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'do',
+	'description': doc,
+	'provides':{
+		'internal-check': {
+			'name': "portageinternal",
+			'sourcefile': "internal",
+			'class': "PortageInternal",
+			'description': doc,
+		},
+		'portageinternalvariableassignment-check': {
+			'name': "portageinternalvariableassignment",
+			'sourcefile': "internal",
+			'class': "PortageInternalVariableAssignment",
+			'description': doc,
+		},
+	}
+}
+
