176b456c8725432346affcfdbbd449203c308d88
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:01:52 +0000

    repoman: New linechecks module, helpers

---
+++ repoman/pym/repoman/modules/linechecks/helpers/offset.py
@@ -0,0 +1,22 @@
+
+import re
+
+from repoman.modules.linechecks.base import LineCheck
+
+
+class NoOffsetWithHelpers(LineCheck):
+	""" Check that the image location, the alternate root offset, and the
+	offset prefix (D, ROOT, ED, EROOT and EPREFIX) are not used with
+	helpers """
+
+	repoman_check_name = 'variable.usedwithhelpers'
+	# Ignore matches in quoted strings like this:
+	# elog "installed into ${ROOT}usr/share/php5/apc/."
+	_install_funcs = (
+		'docinto|do(compress|dir|hard)'
+		'|exeinto|fowners|fperms|insinto|into')
+	_quoted_vars = 'D|ROOT|ED|EROOT|EPREFIX'
+	re = re.compile(
+		r'^[^#"\']*\b(%s)\s+"?\$\{?(%s)\b.*' %
+		(_install_funcs, _quoted_vars))
+	error = 'NO_OFFSET_WITH_HELPERS'
