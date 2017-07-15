ac1df05ac0d49c3c98518d77a892d2c18ad38195
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:00:30 +0000

    repoman: New linechecks module, emake

---
+++ repoman/pym/repoman/modules/linechecks/emake/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Emake plug-in module for repoman LineChecks.
+Performs emake checks on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'do',
+	'description': doc,
+	'provides':{
+		'paralleldisabled-check': {
+			'name': "paralleldisabled",
+			'sourcefile': "emake",
+			'class': "EMakeParallelDisabledViaMAKEOPTS",
+			'description': doc,
+		},
+		'autodefault-check': {
+			'name': "autodefault",
+			'sourcefile': "emake",
+			'class': "WantAutoDefaultValue",
+			'description': doc,
+		},
+	}
+}
+
