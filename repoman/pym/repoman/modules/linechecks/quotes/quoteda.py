7802d8ede8fe099334e62ad5f1be776d184ae9e8
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:04:31 +0000

    repoman: New linechecks module, quotes

---
+++ repoman/pym/repoman/modules/linechecks/quotes/quoteda.py
@@ -0,0 +1,16 @@
+
+import re
+
+from repoman.modules.linechecks.base import LineCheck
+
+
+class EbuildQuotedA(LineCheck):
+	"""Ensure ebuilds have no quoting around ${A}"""
+
+	repoman_check_name = 'ebuild.minorsyn'
+	a_quoted = re.compile(r'.*\"\$(\{A\}|A)\"')
+
+	def check(self, num, line):
+		match = self.a_quoted.match(line)
+		if match:
+			return "Quoted \"${A}\" on line: %d"
