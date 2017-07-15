bfb7f2160ff12dc4757e2dde898cae55f22ff144
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 00:18:32 +0000

    repoman: New linechecks module, depend

---
+++ repoman/pym/repoman/modules/linechecks/depend/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Depend plug-in module for repoman LineChecks.
+Performs dependency checks on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'depend',
+	'description': doc,
+	'provides':{
+		'implicit-check': {
+			'name': "implicitdepend",
+			'sourcefile': "implicit",
+			'class': "ImplicitRuntimeDeps",
+			'description': doc,
+		},
+	}
+}
+
