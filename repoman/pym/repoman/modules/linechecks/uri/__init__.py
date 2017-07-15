d6a80ae7971c995ff770a36793f0d0ed6a13b7a1
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:05:03 +0000

    repoman: New linechecks module, uri

---
+++ repoman/pym/repoman/modules/linechecks/uri/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Uri plug-in module for repoman LineChecks.
+Performs HOMEPAGE variable checks on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'do',
+	'description': doc,
+	'provides':{
+		'httpsuri-check': {
+			'name': "httpsuri",
+			'sourcefile': "uri",
+			'class': "UriUseHttps",
+			'description': doc,
+		},
+	}
+}
+
