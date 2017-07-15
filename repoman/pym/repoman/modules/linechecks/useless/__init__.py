2b4ba2b10d9b4a1417b5f34a884aaad3b07a0a25
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:06:38 +0000

    repoman: New linechecks module, useless

---
+++ repoman/pym/repoman/modules/linechecks/useless/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Useless plug-in module for repoman LineChecks.
+Performs checks for useless operations on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'do',
+	'description': doc,
+	'provides':{
+		'uselesscds-check': {
+			'name': "uselesscds",
+			'sourcefile': "cd",
+			'class': "EbuildUselessCdS",
+			'description': doc,
+		},
+		'uselessdodoc-check': {
+			'name': "uselessdodoc",
+			'sourcefile': "dodoc",
+			'class': "EbuildUselessDodoc",
+			'description': doc,
+		},
+	}
+}
+
