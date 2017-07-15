3a2c108052c22bb753dfb2e363f086c4e78dcf1a
Brian Dolbec <dolsen@gentoo.org>
2017-07-15 01:05:56 +0000

    repoman: New linechecks module, use

---
+++ repoman/pym/repoman/modules/linechecks/use/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2015-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+doc = """Use plug-in module for repoman LineChecks.
+Performs Built-With-Use checks on ebuilds."""
+__doc__ = doc[:]
+
+
+module_spec = {
+	'name': 'do',
+	'description': doc,
+	'provides':{
+		'builtwith-check': {
+			'name': "builtwith",
+			'sourcefile': "builtwith",
+			'class': "BuiltWithUse",
+			'description': doc,
+		},
+	}
+}
+
