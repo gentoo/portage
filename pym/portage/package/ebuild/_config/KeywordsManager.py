gentoo/portage

commit c6e3af2b1e419d70443a74575e9b762a101a3912

binpkg-multi-instance 7 of 7
Support "profile-formats = build-id" setting for layout.conf. When
this is enabled in layout.conf of the containing repository, a
dependency atom in the profile can refer to a specific build, using the
build-id that is assigned when FEATURES=binpkg-multi-instance is
enabled. A build-id atom is identical to a version-specific atom,
except that the version is followed by a hyphen and an integer build-id.

With the build-id profile format, it is possible to assemble a system
using specific builds of binary packages, as users of "binary"
distros might be accustomed to. For example, an atom in the "packages"
file can pull a specific build of a package into the @system set, and
an atom in the "package.keywords" file can be used to modify the
effective KEYWORDS of a specific build of a package.

Refering to specific builds can be useful for a number of reasons. For
example, if a particular build needs to undergo a large amount of
testing in a complex environment in order to verify reliability, then
it can be useful to lock a profile to a specific build that has been
thoroughly tested.

pym/portage/package/ebuild/_config/KeywordsManager.py
@@ -22,7 +22,8 @@ class KeywordsManager(object):
 		rawpkeywords = [grabdict_package(
 			os.path.join(x.location, "package.keywords"),
 			recursive=x.portage1_directories,
-			verify_eapi=True, eapi=x.eapi, eapi_default=None)
+			verify_eapi=True, eapi=x.eapi, eapi_default=None,
+			allow_build_id=x.allow_build_id)
 			for x in profiles]
 		for pkeyworddict in rawpkeywords:
 			if not pkeyworddict:

pym/portage/package/ebuild/_config/UseManager.py
@@ -153,7 +153,8 @@ class UseManager(object):
 		return tuple(ret)
 
 	def _parse_file_to_dict(self, file_name, juststrings=False, recursive=True,
-		eapi_filter=None, user_config=False, eapi=None, eapi_default="0"):
+		eapi_filter=None, user_config=False, eapi=None, eapi_default="0",
+		allow_build_id=False):
 		"""
 		@param file_name: input file name
 		@type file_name: str
@@ -176,6 +177,9 @@ class UseManager(object):
 		@param eapi_default: the default EAPI which applies if the
 			current profile node does not define a local EAPI
 		@type eapi_default: str
+		@param allow_build_id: allow atoms to specify a particular
+			build-id
+		@type allow_build_id: bool
 		@rtype: tuple
 		@return: collection of USE flags
 		"""
@@ -192,7 +196,7 @@ class UseManager(object):
 		file_dict = grabdict_package(file_name, recursive=recursive,
 			allow_wildcard=extended_syntax, allow_repo=extended_syntax,
 			verify_eapi=(not extended_syntax), eapi=eapi,
-			eapi_default=eapi_default)
+			eapi_default=eapi_default, allow_build_id=allow_build_id)
 		if eapi is not None and eapi_filter is not None and not eapi_filter(eapi):
 			if file_dict:
 				writemsg(_("--- EAPI '%s' does not support '%s': '%s'\n") %
@@ -262,7 +266,8 @@ class UseManager(object):
 		for repo in repositories.repos_with_profiles():
 			ret[repo.name] = self._parse_file_to_dict(
 				os.path.join(repo.location, "profiles", file_name),
-				eapi_filter=eapi_filter, eapi_default=repo.eapi)
+				eapi_filter=eapi_filter, eapi_default=repo.eapi,
+				allow_build_id=("build-id" in repo.profile_formats))
 		return ret
 
 	def _parse_profile_files_to_tuple_of_tuples(self, file_name, locations,
@@ -279,7 +284,8 @@ class UseManager(object):
 			os.path.join(profile.location, file_name), juststrings,
 			recursive=profile.portage1_directories, eapi_filter=eapi_filter,
 			user_config=profile.user_config, eapi=profile.eapi,
-			eapi_default=None) for profile in locations)
+			eapi_default=None, allow_build_id=profile.allow_build_id)
+			for profile in locations)
 
 	def _parse_repository_usealiases(self, repositories):
 		ret = {}

man/portage.5
@@ -1220,7 +1220,7 @@ and the newer/faster "md5-dict" format.  Default is to detect dirs.
 The EAPI to use for profiles when unspecified. This attribute is
 supported only if profile-default-eapi is included in profile-formats.
 .TP
-.BR profile\-formats " = [pms] [portage-1] [portage-2] [profile-bashrcs] [profile-set] [profile-default-eapi]"
+.BR profile\-formats " = [pms] [portage-1] [portage-2] [profile-bashrcs] [profile-set] [profile-default-eapi] [build-id]"
 Control functionality available to profiles in this repo such as which files
 may be dirs, or the syntax available in parent files.  Use "portage-2" if you're
 unsure.  The default is "portage-1-compat" mode which is meant to be compatible
@@ -1230,7 +1230,11 @@ Setting profile-bashrcs will enable the per-profile bashrc mechanism
 profile \fBpackages\fR file to add atoms to the @profile package set.
 See the profile \fBpackages\fR section for more information.
 Setting profile-default-eapi enables support for the
-profile_eapi_when_unspecified attribute.
+profile_eapi_when_unspecified attribute. Setting build\-id allows
+dependency atoms in the profile to refer to specific builds (see the
+binpkg\-multi\-instance FEATURES setting in \fBmake.conf\fR(5)). A
+build\-id atom is identical to a version-specific atom, except that the
+version is followed by a hyphen and an integer build\-id.
 .RE
 .RE
 

pym/_emerge/is_valid_package_atom.py
@@ -14,9 +14,10 @@ def insert_category_into_atom(atom, category):
 		ret = None
 	return ret
 
-def is_valid_package_atom(x, allow_repo=False):
+def is_valid_package_atom(x, allow_repo=False, allow_build_id=True):
 	if "/" not in x.split(":")[0]:
 		x2 = insert_category_into_atom(x, 'cat')
 		if x2 != None:
 			x = x2
-	return isvalidatom(x, allow_blockers=False, allow_repo=allow_repo)
+	return isvalidatom(x, allow_blockers=False, allow_repo=allow_repo,
+		allow_build_id=allow_build_id)

pym/portage/_sets/ProfilePackageSet.py
@@ -23,7 +23,8 @@ class ProfilePackageSet(PackageSet):
 	def load(self):
 		self._setAtoms(x for x in stack_lists(
 			[grabfile_package(os.path.join(y.location, "packages"),
-			verify_eapi=True, eapi=y.eapi, eapi_default=None)
+			verify_eapi=True, eapi=y.eapi, eapi_default=None,
+			allow_build_id=y.allow_build_id)
 			for y in self._profiles
 			if "profile-set" in y.profile_formats],
 			incremental=1) if x[:1] != "*")

pym/portage/_sets/profiles.py
@@ -34,7 +34,8 @@ class PackagesSystemSet(PackageSet):
 				(self._profiles,), level=logging.DEBUG, noiselevel=-1)
 
 		mylist = [grabfile_package(os.path.join(x.location, "packages"),
-			verify_eapi=True, eapi=x.eapi, eapi_default=None)
+			verify_eapi=True, eapi=x.eapi, eapi_default=None,
+			allow_build_id=x.allow_build_id)
 			for x in self._profiles]
 
 		if debug:

pym/portage/dep/__init__.py
@@ -1193,11 +1193,11 @@ class Atom(_unicode):
 			self.overlap = self._overlap(forbid=forbid_overlap)
 
 	def __new__(cls, s, unevaluated_atom=None, allow_wildcard=False, allow_repo=None,
-		_use=None, eapi=None, is_valid_flag=None):
+		_use=None, eapi=None, is_valid_flag=None, allow_build_id=None):
 		return _unicode.__new__(cls, s)
 
 	def __init__(self, s, unevaluated_atom=None, allow_wildcard=False, allow_repo=None,
-		_use=None, eapi=None, is_valid_flag=None):
+		_use=None, eapi=None, is_valid_flag=None, allow_build_id=None):
 		if isinstance(s, Atom):
 			# This is an efficiency assertion, to ensure that the Atom
 			# constructor is not called redundantly.
@@ -1218,8 +1218,13 @@ class Atom(_unicode):
 			# Ignore allow_repo when eapi is specified.
 			allow_repo = eapi_attrs.repo_deps
 		else:
+			# These parameters have "smart" defaults that are only
+			# applied when the caller does not explicitly pass in a
+			# True or False value.
 			if allow_repo is None:
 				allow_repo = True
+			if allow_build_id is None:
+				allow_build_id = True
 
 		blocker_prefix = ""
 		if "!" == s[:1]:
@@ -1234,6 +1239,7 @@ class Atom(_unicode):
 			blocker = False
 		self.__dict__['blocker'] = blocker
 		m = atom_re.match(s)
+		build_id = None
 		extended_syntax = False
 		extended_version = None
 		if m is None:
@@ -1270,8 +1276,22 @@ class Atom(_unicode):
 			slot = m.group(atom_re.groups - 2)
 			repo = m.group(atom_re.groups - 1)
 			use_str = m.group(atom_re.groups)
-			if m.group(base + 4) is not None:
-				raise InvalidAtom(self)
+			version = m.group(base + 4)
+			if version is not None:
+				if allow_build_id:
+					cpv_build_id = cpv
+					cpv = cp
+					cp = cp[:-len(version)]
+					build_id = cpv_build_id[len(cpv)+1:]
+					if len(build_id) > 1 and build_id[:1] == "0":
+						# Leading zeros are not allowed.
+						raise InvalidAtom(self)
+					try:
+						build_id = int(build_id)
+					except ValueError:
+						raise InvalidAtom(self)
+				else:
+					raise InvalidAtom(self)
 		elif m.group('star') is not None:
 			base = atom_re.groupindex['star']
 			op = '=*'
@@ -1334,6 +1354,7 @@ class Atom(_unicode):
 				self.__dict__['slot_operator'] = None
 		self.__dict__['operator'] = op
 		self.__dict__['extended_syntax'] = extended_syntax
+		self.__dict__['build_id'] = build_id
 
 		if not (repo is None or allow_repo):
 			raise InvalidAtom(self)
@@ -1879,7 +1900,7 @@ def dep_getusedeps( depend ):
 	return tuple(use_list)
 
 def isvalidatom(atom, allow_blockers=False, allow_wildcard=False,
-	allow_repo=False, eapi=None):
+	allow_repo=False, eapi=None, allow_build_id=False):
 	"""
 	Check to see if a depend atom is valid
 
@@ -1904,7 +1925,8 @@ def isvalidatom(atom, allow_blockers=False, allow_wildcard=False,
 	try:
 		if not isinstance(atom, Atom):
 			atom = Atom(atom, allow_wildcard=allow_wildcard,
-				allow_repo=allow_repo, eapi=eapi)
+				allow_repo=allow_repo, eapi=eapi,
+				allow_build_id=allow_build_id)
 		if not allow_blockers and atom.blocker:
 			return False
 		return True
@@ -2109,6 +2131,7 @@ def match_from_list(mydep, candidate_list):
 	mycpv     = mydep.cpv
 	mycpv_cps = catpkgsplit(mycpv) # Can be None if not specific
 	slot      = mydep.slot
+	build_id  = mydep.build_id
 
 	if not mycpv_cps:
 		cat, pkg = catsplit(mycpv)
@@ -2182,6 +2205,9 @@ def match_from_list(mydep, candidate_list):
 			if xcpv is None:
 				xcpv = remove_slot(x)
 			if not cpvequal(xcpv, mycpv):
+				continue
+			if (build_id is not None and
+				getattr(xcpv, "build_id", None) != build_id):
 				continue
 			mylist.append(x)
 

pym/portage/package/ebuild/_config/LocationsManager.py
@@ -31,7 +31,8 @@ _PORTAGE1_DIRECTORIES = frozenset([
 	'use.mask', 'use.force'])
 
 _profile_node = collections.namedtuple('_profile_node',
-	'location portage1_directories user_config profile_formats eapi')
+	('location', 'portage1_directories', 'user_config',
+	'profile_formats', 'eapi', 'allow_build_id'))
 
 _allow_parent_colon = frozenset(
 	["portage-2"])
@@ -142,7 +143,8 @@ class LocationsManager(object):
 					_profile_node(custom_prof, True, True,
 					('profile-bashrcs', 'profile-set'),
 					read_corresponding_eapi_file(
-					custom_prof + os.sep, default=None)))
+					custom_prof + os.sep, default=None),
+					True))
 			del custom_prof
 
 		self.profiles = tuple(self.profiles)
@@ -253,7 +255,7 @@ class LocationsManager(object):
 		self.profiles.append(currentPath)
 		self.profiles_complex.append(
 			_profile_node(currentPath, allow_directories, False,
-				current_formats, eapi))
+				current_formats, eapi, 'build-id' in current_formats))
 
 	def _expand_parent_colon(self, parentsFile, parentPath,
 		repo_loc, repositories):

commit 9c819f72399d4fdae731883d9d915aa824d30fcc

config: allow_build_id=True for user config keywords and use
Fixes: c6e3af2b1e41 ("binpkg-multi-instance 7 of 7")
Closes: https://github.com/gentoo/portage/pull/185

pym/portage/package/ebuild/_config/KeywordsManager.py
@@ -57,12 +57,12 @@ class KeywordsManager(object):
 			pkgdict = grabdict_package(
 				os.path.join(abs_user_config, "package.keywords"),
 				recursive=1, allow_wildcard=True, allow_repo=True,
-				verify_eapi=False)
+				verify_eapi=False, allow_build_id=True)
 
 			for k, v in grabdict_package(
 				os.path.join(abs_user_config, "package.accept_keywords"),
 				recursive=1, allow_wildcard=True, allow_repo=True,
-				verify_eapi=False).items():
+				verify_eapi=False, allow_build_id=True).items():
 				pkgdict.setdefault(k, []).extend(v)
 
 			accept_keywords_defaults = global_accept_keywords.split()

pym/portage/package/ebuild/_config/UseManager.py
@@ -239,7 +239,10 @@ class UseManager(object):
 		ret = ExtendedAtomDict(dict)
 		if user_config:
 			pusedict = grabdict_package(
-				os.path.join(location, file_name), recursive=1, newlines=1, allow_wildcard=True, allow_repo=True, verify_eapi=False)
+				os.path.join(location, file_name),
+				recursive=1, newlines=1, allow_wildcard=True,
+				allow_repo=True, verify_eapi=False,
+				allow_build_id=True)
 			for k, v in pusedict.items():
 				l = []
 				use_expand_prefix = ''