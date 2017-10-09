gentoo/portage

commit a62ebb4a2f759da85aaab7a214c3f8c4ac7f4d64

elog mod_echo: Print log path if PORT_LOGDIR is used
Include the path to the log file if PORT_LOGDIR is being used
(and therefore the log is going to be preserved past the build). This is
useful when elog messages contain QA warnings or other errors that
require reporting a bug. In such case, having a path to the log is
handy.

After this commit, the mod_echo output becomes:

 * Messages for package dev-foo/bar-1:
 * Log file: /var/log/portage/dev-foo:bar-1:20170816-100533.log

 * test

Reviewed-by: Brian Dolbec <dolsen@gentoo.org>

pym/portage/elog/mod_echo.py
@@ -1,5 +1,5 @@
 # elog/mod_echo.py - elog dispatch module
-# Copyright 2007-2014 Gentoo Foundation
+# Copyright 2007-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import print_function
@@ -16,7 +16,12 @@ if sys.hexversion >= 0x3000000:
 _items = []
 def process(mysettings, key, logentries, fulltext):
 	global _items
-	_items.append((mysettings["ROOT"], key, logentries))
+	logfile = None
+	# output logfile explicitly only if it isn't in tempdir, otherwise
+	# it will be removed anyway
+	if "PORT_LOGDIR" in mysettings:
+		logfile = mysettings["PORTAGE_LOG_FILE"]
+	_items.append((mysettings["ROOT"], key, logentries, logfile))
 
 def finalize():
 	# For consistency, send all message types to stdout.
@@ -34,7 +39,7 @@ def finalize():
 def _finalize():
 	global _items
 	printer = EOutput()
-	for root, key, logentries in _items:
+	for root, key, logentries, logfile in _items:
 		print()
 		if root == "/":
 			printer.einfo(_("Messages for package %s:") %
@@ -42,6 +47,8 @@ def _finalize():
 		else:
 			printer.einfo(_("Messages for package %(pkg)s merged to %(root)s:") %
 				{"pkg": colorize("INFORM", key), "root": root})
+		if logfile is not None:
+			printer.einfo(_("Log file: %s") % colorize("INFORM", logfile))
 		print()
 		for phase in EBUILD_PHASES:
 			if phase not in logentries:

commit fc69af818c5208ef153ca0c41ac8784afba7e30e

elog mod_echo: fix PORTAGE_LOG_FILE KeyError (bug 633842)
In some cases such as Scheduler._calc_resume_list, the
elog_process function can be called when mysettings is
not in a suitable state to use the PORTAGE_LOG_FILE
variable.

Fixes: a62ebb4a2f75 ("elog mod_echo: Print log path if PORT_LOGDIR is used")
Bug: https://bugs.gentoo.org/show_bug.cgi?id=633842

pym/portage/elog/mod_echo.py
@@ -19,7 +19,9 @@ def process(mysettings, key, logentries, fulltext):
 	logfile = None
 	# output logfile explicitly only if it isn't in tempdir, otherwise
 	# it will be removed anyway
-	if "PORT_LOGDIR" in mysettings:
+	if (key == mysettings.mycpv and
+		"PORT_LOGDIR" in mysettings and
+		"PORTAGE_LOG_FILE" in mysettings):
 		logfile = mysettings["PORTAGE_LOG_FILE"]
 	_items.append((mysettings["ROOT"], key, logentries, logfile))
 