ac2dc22e38c1525df701e74f97c2af15177f3af3
Zac Medico <zmedico@gentoo.org>
2017-10-27 12:59:42 -0700

    Updates for portage-2.3.13 release

--- setup.py
+++ setup.py
@@ -659,7 +659,7 @@ class build_ext(_build_ext):
 
 setup(
 	name = 'portage',
-	version = '2.3.12',
+	version = '2.3.13',
 	url = 'https://wiki.gentoo.org/wiki/Project:Portage',
 	author = 'Gentoo Portage Development Team',
 	author_email = 'dev-portage@gentoo.org',
