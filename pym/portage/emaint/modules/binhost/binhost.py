gentoo/portage

commit 694419fc11af6cbda4944b21632acec9c641d1c3

emaint binhost: use _populate_local instead of _populate (bug 638320)
Fixes: 8267445cf2f8 ("binarytree.populate: avoid lock when possible (bug 607872)")
Bug: https://bugs.gentoo.org/638320

pym/portage/emaint/modules/binhost/binhost.py
@@ -123,7 +123,7 @@ class BinhostHandler(object):
 				self._pkgindex_file, wantnewlockfile=1)
 			try:
 				# Repopulate with lock held.
-				bintree._populate()
+				bintree._populate_local()
 				cpv_all = self._bintree.dbapi.cpv_all()
 				cpv_all.sort()
 

pym/portage/dbapi/bintree.py
@@ -532,7 +532,6 @@ class binarytree(object):
 		# prior to performing package moves since it only wants to
 		# operate on local packages (getbinpkgs=0).
 		self._remotepkgs = None
-		self.dbapi.clear()
 
 		self._populating = True
 		try:
@@ -568,6 +567,7 @@ class binarytree(object):
 		self.populated = True
 
 	def _populate_local(self):
+		self.dbapi.clear()
 		_instance_key = self.dbapi._instance_key
 		if True:
 			pkg_paths = {}

commit b1d467ad9984c8fb100aeccb7de084f395a099f4

emaint binhost: use _populate_local return value when appropriate
Since _populate_local does not write the Packages file to disk,
its return value must be used because _load_pkgindex would return
stale data.

Fixes: 694419fc11af ("emaint binhost: use _populate_local instead of _populate (bug 638320)")
Bug: https://bugs.gentoo.org/638320

pym/portage/emaint/modules/binhost/binhost.py
@@ -122,13 +122,13 @@ class BinhostHandler(object):
 			pkgindex_lock = locks.lockfile(
 				self._pkgindex_file, wantnewlockfile=1)
 			try:
-				# Repopulate with lock held.
-				bintree._populate_local()
+				# Repopulate with lock held. If _populate_local returns
+				# data then use that, since _load_pkgindex would return
+				# stale data in this case.
+				self._pkgindex = pkgindex = (bintree._populate_local() or
+					bintree._load_pkgindex())
 				cpv_all = self._bintree.dbapi.cpv_all()
 				cpv_all.sort()
-
-				pkgindex = bintree._load_pkgindex()
-				self._pkgindex = pkgindex
 
 				# Recount stale/missing packages, with lock held.
 				missing = []