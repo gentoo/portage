c040200175b33e637a4807278925e92cf61eb725
Zac Medico <zmedico@gentoo.org>
2018-01-09 12:28:27 -0800

    testDoInsOption: fix timestamp comparison to work in travis
    Use integer comparison in order to avoid failures like the
    following:
    
    AssertionError: 1515530127.479328 != 1515530127.479327

--- pym/portage/tests/bin/test_doins.py
+++ pym/portage/tests/bin/test_doins.py
@@ -45,8 +45,9 @@ class DoIns(setup_env.BinTestCase):
 			st = os.lstat(env['D'] + '/test')
 			if stat.S_IMODE(st.st_mode) != 0o644:
 				raise tests.TestCase.failureException
-			if os.stat(os.path.join(env['S'], 'test')).st_mtime != st.st_mtime:
-				raise tests.TestCase.failureException
+			self.assertEqual(
+				os.stat(os.path.join(env['S'], 'test'))[stat.ST_MTIME],
+				st[stat.ST_MTIME])
 		finally:
 			self.cleanup()
 
