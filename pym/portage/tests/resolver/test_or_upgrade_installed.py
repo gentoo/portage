gentoo/portage

commit 86ba22da7a2f34848cdb5a6f1090c22c264e577e

dep_zapdeps: install new package, allow upgrade (bug 643974)
Prefer to install a new package in order to allow upgrade of an
installed package. This generalizes the code from bug 635540 so
that it both allows desirable upgrades and prevents unwanted
downgrades.

Fixes: 7c58e3737616 ("dep_zapdeps: install new package, avoid downgrade (bug 635540)")
Bug: https://bugs.gentoo.org/643974
Reviewed-by: Alec Warner <antarus@gentoo.org>

pym/portage/dep/dep_check.py
@@ -366,10 +366,8 @@ def dep_zapdeps(unreduced, reduced, myroot, use_binaries=0, trees=None):
 	want_update_pkg = trees[myroot].get("want_update_pkg")
 	downgrade_probe = trees[myroot].get("downgrade_probe")
 	vardb = None
-	vardb_match_pkgs = None
 	if "vartree" in trees[myroot]:
 		vardb = trees[myroot]["vartree"].dbapi
-		vardb_match_pkgs = getattr(vardb, 'match_pkgs', None)
 	if use_binaries:
 		mydbapi = trees[myroot]["bintree"].dbapi
 	else:
@@ -465,10 +463,11 @@ def dep_zapdeps(unreduced, reduced, myroot, use_binaries=0, trees=None):
 						avail_pkg = avail_pkg_use
 					avail_slot = Atom("%s:%s" % (atom.cp, avail_pkg.slot))
 
-			if vardb_match_pkgs is not None and downgrade_probe is not None:
-				inst_pkg = vardb_match_pkgs(avail_slot)
-				if (inst_pkg and avail_pkg < inst_pkg[-1] and
-					not downgrade_probe(inst_pkg[-1])):
+			if downgrade_probe is not None:
+				highest_in_slot = mydbapi_match_pkgs(avail_slot)
+				if (avail_pkg and highest_in_slot and
+					avail_pkg < highest_in_slot[-1] and
+					not downgrade_probe(avail_pkg)):
 					installed_downgrade = True
 
 			slot_map[avail_slot] = avail_pkg

pym/portage/tests/resolver/test_or_upgrade_installed.py
@@ -1,0 +1,101 @@
+# Copyright 2018 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+from portage.tests import TestCase
+from portage.tests.resolver.ResolverPlayground import (
+	ResolverPlayground,
+	ResolverPlaygroundTestCase,
+)
+
+class OrUpgradeInstalledTestCase(TestCase):
+
+	def testOrUpgradeInstalled(self):
+		ebuilds = {
+			'net-misc/foo-1': {
+				'EAPI': '6',
+				'RDEPEND': '|| ( sys-libs/glibc[rpc(-)]  net-libs/libtirpc )'
+			},
+			'net-libs/libtirpc-1': {
+				'EAPI': '6',
+			},
+			'sys-libs/glibc-2.26': {
+				'EAPI': '6',
+				'IUSE': ''
+			},
+			'sys-libs/glibc-2.24': {
+				'EAPI': '6',
+				'IUSE': '+rpc'
+			},
+		}
+
+		installed = {
+			'sys-libs/glibc-2.24': {
+				'EAPI': '6',
+				'IUSE': '+rpc',
+				'USE': 'rpc',
+			},
+		}
+
+		world = ['sys-libs/glibc']
+
+		test_cases = (
+			# Test bug 643974, where we need to install libtirpc
+			# in order to upgrade glibc.
+			ResolverPlaygroundTestCase(
+				['net-misc/foo', '@world'],
+				options={'--update': True, '--deep': True},
+				success=True,
+				ambiguous_merge_order=True,
+				mergelist=(
+					(
+						'net-libs/libtirpc-1',
+						'sys-libs/glibc-2.26',
+						'net-misc/foo-1',
+					),
+				)
+			),
+		)
+
+		playground = ResolverPlayground(debug=False,
+			ebuilds=ebuilds, installed=installed, world=world)
+
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			playground.debug = False
+			playground.cleanup()
+
+		# In some cases it's necessary to avoid upgrade due to
+		# the package being masked.
+		user_config = {
+			"package.mask" : (
+				">=sys-libs/glibc-2.26",
+			),
+		}
+
+		test_cases = (
+			ResolverPlaygroundTestCase(
+				['net-misc/foo', '@world'],
+				options={'--update': True, '--deep': True},
+				success=True,
+				mergelist=[
+					'net-misc/foo-1',
+				]
+			),
+		)
+
+		playground = ResolverPlayground(debug=False,
+			ebuilds=ebuilds, installed=installed, world=world,
+			user_config=user_config)
+
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			playground.debug = False
+			playground.cleanup()

commit 5c888fcd2e87270523f55fb180ced5c3c8689f76

dep_zapdeps: fix virtual/rust handling (bug 645416)
Fix the code from bug 643974 to set the installed_downgrade flag only
if the selected newer package is either installed or in the graph. This
is currently needed for appropriate handling of virtual/rust-1.19.0,
since there's an upgrade to dev-lang/rust-1.23.0 available which may
not be desired since it would mean that dev-lang/rust-bin-1.19.0 has
to be installed in order to satisfy virtual/rust-1.19.0:

 || ( =dev-lang/rust-1.19.0* =dev-lang/rust-bin-1.19.0* )

So, the rust-bin choice is desirable only if the rust-1.23.0 package is
already installed or in the graph.

Fixes: 86ba22da7a2f ("dep_zapdeps: install new package, allow upgrade (bug 643974)")
Bug: https://bugs.gentoo.org/645416

pym/portage/dep/dep_check.py
@@ -463,11 +463,15 @@ def dep_zapdeps(unreduced, reduced, myroot, use_binaries=0, trees=None):
 						avail_pkg = avail_pkg_use
 					avail_slot = Atom("%s:%s" % (atom.cp, avail_pkg.slot))
 
-			if downgrade_probe is not None:
+			if downgrade_probe is not None and graph is not None:
 				highest_in_slot = mydbapi_match_pkgs(avail_slot)
+				highest_in_slot = (highest_in_slot[-1]
+					if highest_in_slot else None)
 				if (avail_pkg and highest_in_slot and
-					avail_pkg < highest_in_slot[-1] and
-					not downgrade_probe(avail_pkg)):
+					avail_pkg < highest_in_slot and
+					not downgrade_probe(avail_pkg) and
+					(highest_in_slot.installed or
+					highest_in_slot in graph)):
 					installed_downgrade = True
 
 			slot_map[avail_slot] = avail_pkg

pym/portage/tests/resolver/test_or_upgrade_installed.py
@@ -99,3 +99,62 @@ class OrUpgradeInstalledTestCase(TestCase):
 		finally:
 			playground.debug = False
 			playground.cleanup()
+
+	def testVirtualRust(self):
+		ebuilds = {
+			'dev-lang/rust-1.19.0': {},
+			'dev-lang/rust-1.23.0': {},
+			'dev-lang/rust-bin-1.19.0': {},
+			'virtual/rust-1.19.0': {
+				'RDEPEND': '|| ( =dev-lang/rust-1.19.0* =dev-lang/rust-bin-1.19.0* )'
+			},
+		}
+
+		installed = {
+			'dev-lang/rust-1.19.0': {},
+			'virtual/rust-1.19.0': {
+				'RDEPEND': '|| ( =dev-lang/rust-1.19.0* =dev-lang/rust-bin-1.19.0* )'
+			},
+		}
+
+		world = ['virtual/rust']
+
+		test_cases = (
+			# Test bug 645416, where rust-bin-1.19.0 was pulled in
+			# inappropriately due to the rust-1.23.0 update being
+			# available.
+			ResolverPlaygroundTestCase(
+				['virtual/rust'],
+				options={'--update': True, '--deep': True},
+				success=True,
+				mergelist=[]
+			),
+			# Test upgrade to rust-1.23.0, which is only possible
+			# if rust-bin-1.19.0 is installed in order to satisfy
+			# virtual/rust-1.19.0.
+			ResolverPlaygroundTestCase(
+				['=dev-lang/rust-1.23.0', 'virtual/rust'],
+				options={'--update': True, '--deep': True},
+				all_permutations=True,
+				success=True,
+				ambiguous_merge_order=True,
+				mergelist=(
+					(
+						'dev-lang/rust-1.23.0',
+						'dev-lang/rust-bin-1.19.0',
+					),
+				),
+			),
+		)
+
+		playground = ResolverPlayground(debug=False,
+			ebuilds=ebuilds, installed=installed, world=world)
+
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			playground.debug = False
+			playground.cleanup()