gentoo/portage

commit c3b98e6b66364a08d1dadd3df7d84ad5f133fb1c

_solve_..slot_conflicts: Remove double-check of unexplored stack.
Closes: https://github.com/gentoo/portage/pull/272

pym/_emerge/depgraph.py
@@ -1539,9 +1539,11 @@ class depgraph(object):
 		explored_nodes = set()
 
 		while unexplored:
-			# Handle all unexplored packages.
-			while unexplored:
-				node = unexplored.pop()
+			while True:
+				try:
+				    node = unexplored.pop()
+				except IndexError:
+					break
 				for child in conflict_graph.child_nodes(node):
 					# Don't explore a node more than once, in order
 					# to avoid infinite recursion. The forced set

commit 9de7c259ed4f75abbd09cfdbdfa183cec36c1286

_solve_..slot_conflicts: fix KeyError: 'pop from an empty set'
Fixes: c3b98e6b6636 (_solve_..slot_conflicts: Remove double-check of unexplored stack.)

pym/_emerge/depgraph.py
@@ -1542,7 +1542,7 @@ class depgraph(object):
 			while True:
 				try:
 					node = unexplored.pop()
-				except IndexError:
+				except KeyError:
 					break
 				for child in conflict_graph.child_nodes(node):
 					# Don't explore a node more than once, in order