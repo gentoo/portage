919ed310fb78265100cd798f1c1ad01c5a1d8552
Manuel Rüger <mrueg@gentoo.org>
2018-03-30 15:43:44 +0200

    FileNotFoundError is not defined in python2.7

--- repoman/pym/repoman/config.py
+++ repoman/pym/repoman/config.py
@@ -8,6 +8,11 @@ import stat
 
 import yaml
 
+try:
+	FileNotFoundError
+except NameError:
+	FileNotFoundError = IOError
+
 
 class ConfigError(Exception):
 	"""Raised when a config file fails to load"""
