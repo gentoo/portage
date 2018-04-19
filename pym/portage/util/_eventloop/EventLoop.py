gentoo/portage

commit 25245d7eb86ed197b7d7cfead0dbe4ce8ad4bc5b

EventLoop.run_until_complete: wait for done callbacks
Since done callbacks are executed via call_soon, it's desirable to
continue iterating until those callbacks have executed, which is easily
achieved by registering a done callback and waiting for it to execute.

pym/portage/util/_eventloop/EventLoop.py
@@ -812,7 +812,14 @@ class EventLoop(object):
 		@raise: the Future's exception
 		"""
 		future = asyncio.ensure_future(future, loop=self._asyncio_wrapper)
-		while not future.done():
+
+		# Since done callbacks are executed via call_soon, it's desirable
+		# to continue iterating until those callbacks have executed, which
+		# is easily achieved by registering a done callback and waiting for
+		# it to execute.
+		waiter = self.create_future()
+		future.add_done_callback(functools.partial(waiter.set_result))
+		while not waiter.done():
 			self.iteration()
 
 		return future.result()

commit da5efb5e677d12c850ff02140e24a095a8efdafb

EventLoop.run_until_complete: remove unneeded partial
Fixes: 25245d7eb86e ("EventLoop.run_until_complete: wait for done callbacks")

pym/portage/util/_eventloop/EventLoop.py
@@ -818,7 +818,7 @@ class EventLoop(object):
 		# is easily achieved by registering a done callback and waiting for
 		# it to execute.
 		waiter = self.create_future()
-		future.add_done_callback(functools.partial(waiter.set_result))
+		future.add_done_callback(waiter.set_result)
 		while not waiter.done():
 			self.iteration()
 