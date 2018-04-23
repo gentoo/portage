gentoo/portage

commit db4dca876cdb004b12a944f5323a51bc4bbde770

EbuildBuild._start(): fix event loop recursion (bug 653844)
Use async_aux_get to fetch SRC_URI metadata (it's not cached in
self.pkg.metadata because some packages have an extremely large
SRC_URI value).

Bug: https://bugs.gentoo.org/653844

pym/_emerge/EbuildBuild.py
@@ -35,23 +35,32 @@ class EbuildBuild(CompositeTask):
 		("_build_dir", "_buildpkg", "_ebuild_path", "_issyspkg", "_tree")
 
 	def _start(self):
-
-		pkg = self.pkg
-		settings = self.settings
-
 		if not self.opts.fetchonly:
-			rval = _check_temp_dir(settings)
+			rval = _check_temp_dir(self.settings)
 			if rval != os.EX_OK:
 				self.returncode = rval
 				self._current_task = None
 				self._async_wait()
 				return
 
+		# First get the SRC_URI metadata (it's not cached in self.pkg.metadata
+		# because some packages have an extremely large SRC_URI value).
+		self._start_task(
+			AsyncTaskFuture(
+				future=self.pkg.root_config.trees["porttree"].dbapi.\
+				async_aux_get(self.pkg.cpv, ["SRC_URI"], myrepo=self.pkg.repo)),
+			self._start_with_metadata)
+
+	def _start_with_metadata(self, aux_get_task):
+		self._assert_current(aux_get_task)
+		pkg = self.pkg
+		settings = self.settings
 		root_config = pkg.root_config
 		tree = "porttree"
 		self._tree = tree
 		portdb = root_config.trees[tree].dbapi
 		settings.setcpv(pkg)
+		settings.configdict["pkg"]["SRC_URI"], = aux_get_task.future.result()
 		settings.configdict["pkg"]["EMERGE_FROM"] = "ebuild"
 		if self.opts.buildpkgonly:
 			settings.configdict["pkg"]["MERGE_TYPE"] = "buildonly"

pym/portage/package/ebuild/doebuild.py
@@ -450,8 +450,10 @@ def doebuild_environment(myebuild, mydo, myroot=None, settings=None,
 		if hasattr(mydbapi, "getFetchMap") and \
 			("A" not in mysettings.configdict["pkg"] or \
 			"AA" not in mysettings.configdict["pkg"]):
-			src_uri, = mydbapi.aux_get(mysettings.mycpv,
-				["SRC_URI"], mytree=mytree)
+			src_uri = mysettings.configdict["pkg"].get("SRC_URI")
+			if src_uri is None:
+				src_uri, = mydbapi.aux_get(mysettings.mycpv,
+					["SRC_URI"], mytree=mytree)
 			metadata = {
 				"EAPI"    : eapi,
 				"SRC_URI" : src_uri,

commit b58ba964c79f8ec7e450b871059306bbc433a7ca

EbuildBuild: pass loop parameter to async_fetch_map
Fixes: db4dca876cdb ("EbuildBuild._start(): fix event loop recursion (bug 653844)")

pym/_emerge/EbuildBuild.py
@@ -48,7 +48,8 @@ class EbuildBuild(CompositeTask):
 		self._start_task(
 			AsyncTaskFuture(
 				future=self.pkg.root_config.trees["porttree"].dbapi.\
-				async_aux_get(self.pkg.cpv, ["SRC_URI"], myrepo=self.pkg.repo)),
+				async_aux_get(self.pkg.cpv, ["SRC_URI"], myrepo=self.pkg.repo,
+				loop=self.scheduler)),
 			self._start_with_metadata)
 
 	def _start_with_metadata(self, aux_get_task):