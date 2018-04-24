gentoo/portage

commit 7f1a362fea4ca0bf4cd0476ceb3a61afb8ef1a7e

EbuildMetadataPhase._output_handler: fix event loop recursion (bug 591760)
When pid exit status is not yet available, use the Subprocess
_async_waitpid() method to avoid event loop recursion.

Bug: https://bugs.gentoo.org/591760

pym/_emerge/EbuildMetadataPhase.py
@@ -144,8 +144,11 @@ class EbuildMetadataPhase(SubProcess):
 					break
 				else:
 					if not self._raw_metadata[-1]:
-						self._unregister()
-						self.wait()
+						if self.pid is None:
+							self._unregister()
+							self._async_wait()
+						else:
+							self.self._async_waitpid()
 						break
 
 		self._unregister_if_appropriate(event)

commit 292f81f17ada179f1edf367cd9faddc299b3bf15

EbuildMetadataPhase: fix self.self reference
Fixes: 7f1a362fea4c ("EbuildMetadataPhase._output_handler: fix event loop recursion (bug 591760)")

pym/_emerge/EbuildMetadataPhase.py
@@ -148,7 +148,7 @@ class EbuildMetadataPhase(SubProcess):
 							self._unregister()
 							self._async_wait()
 						else:
-							self.self._async_waitpid()
+							self._async_waitpid()
 						break
 
 		self._unregister_if_appropriate(event)