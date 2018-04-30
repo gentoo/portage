gentoo/portage

commit c11a6ec05f02601a7c2cb5455d1289b9dfed5f10

AbstractPollTask: add_reader asyncio compat (bug 654382)
Migration to add_reader is now complete, so remove unused code related
to io_add_watch (and source_remove).

Bug: https://bugs.gentoo.org/654382

pym/_emerge/AbstractPollTask.py
@@ -44,18 +44,16 @@ class AbstractPollTask(AsynchronousTask):
 		| ---------------------------------------------------
 		| 0      | None
 		"""
-		buf = None
-		if event & self.scheduler.IO_IN:
-			buf = array.array('B')
-			try:
+		buf = array.array('B')
+		try:
 				buf.fromfile(f, self._bufsize)
-			except EOFError:
+		except EOFError:
 				pass
-			except TypeError:
+		except TypeError:
 				# Python 3.2:
 				# TypeError: read() didn't return bytes
 				pass
-			except IOError as e:
+		except IOError as e:
 				# EIO happens with pty on Linux after the
 				# slave end of the pty has been closed.
 				if e.errno == errno.EIO:
@@ -120,23 +118,6 @@ class AbstractPollTask(AsynchronousTask):
 
 	def _unregister(self):
 		self._registered = False
-
-	def _log_poll_exception(self, event):
-		writemsg_level(
-			"!!! %s received strange poll event: %s\n" % \
-			(self.__class__.__name__, event,),
-			level=logging.ERROR, noiselevel=-1)
-
-	def _unregister_if_appropriate(self, event):
-		if self._registered:
-			if event & self._exceptional_events:
-				self._log_poll_exception(event)
-				self.cancel()
-				self.returncode = self.returncode or os.EX_OK
-				self._async_wait()
-			elif event & self.scheduler.IO_HUP:
-				self.returncode = self.returncode or os.EX_OK
-				self._async_wait()
 
 	def _wait_loop(self, timeout=None):
 		loop = getattr(self.scheduler, '_asyncio_wrapper', self.scheduler)

pym/_emerge/AbstractEbuildProcess.py
@@ -347,11 +347,6 @@ class AbstractEbuildProcess(SpawnProcess):
 				log_path = self.settings.get("PORTAGE_LOG_FILE")
 			self.scheduler.output(msg, log_path=log_path)
 
-	def _log_poll_exception(self, event):
-		self._elog("eerror",
-			["%s received strange poll event: %s\n" % \
-			(self.__class__.__name__, event,)])
-
 	def _async_waitpid_cb(self, *args, **kwargs):
 		"""
 		Override _async_waitpid_cb to perform cleanup that is

pym/_emerge/PipeReader.py
@@ -79,7 +79,7 @@ class PipeReader(AbstractPollTask):
 				break
 
 		while True:
-			data = self._read_array(f, self.scheduler.IO_IN)
+			data = self._read_array(f, None)
 			if data is None:
 				break
 			if data:

pym/_emerge/SubProcess.py
@@ -12,7 +12,7 @@ import errno
 class SubProcess(AbstractPollTask):
 
 	__slots__ = ("pid",) + \
-		("_dummy_pipe_fd", "_files", "_reg_id", "_waitpid_id")
+		("_dummy_pipe_fd", "_files", "_waitpid_id")
 
 	# This is how much time we allow for waitpid to succeed after
 	# we've sent a kill signal to our subprocess.
@@ -70,10 +70,6 @@ class SubProcess(AbstractPollTask):
 
 		self._registered = False
 
-		if self._reg_id is not None:
-			self.scheduler.source_remove(self._reg_id)
-			self._reg_id = None
-
 		if self._waitpid_id is not None:
 			self.scheduler._asyncio_child_watcher.\
 				remove_child_handler(self._waitpid_id)
@@ -86,19 +82,3 @@ class SubProcess(AbstractPollTask):
 				else:
 					f.close()
 			self._files = None
-
-	def _unregister_if_appropriate(self, event):
-		"""
-		Override the AbstractPollTask._unregister_if_appropriate method to
-		call _async_waitpid instead of wait(), so that event loop recursion
-		is not triggered when the pid exit status is not yet available.
-		"""
-		if self._registered:
-			if event & self._exceptional_events:
-				self._log_poll_exception(event)
-				self._unregister()
-				self.cancel()
-				self._async_waitpid()
-			elif event & self.scheduler.IO_HUP:
-				self._unregister()
-				self._async_waitpid()

pym/portage/util/_async/SchedulerInterface.py
@@ -11,11 +11,7 @@ from ..SlotObject import SlotObject
 
 class SchedulerInterface(SlotObject):
 
-	_event_loop_attrs = ("IO_ERR", "IO_HUP", "IO_IN",
-		"IO_NVAL", "IO_OUT", "IO_PRI",
-		"io_add_watch",
-		"source_remove",
-
+	_event_loop_attrs = (
 		"add_reader",
 		"add_writer",
 		"call_at",

commit 2bd3d97d18644f2dc348df808a0f32d4d36e7120

AbstractPollTask: remove unused IO_* references
Fixes: c11a6ec05f02 ("AbstractPollTask: add_reader asyncio compat (bug 654382)")

pym/_emerge/AbstractPollTask.py
@@ -16,15 +16,6 @@ class AbstractPollTask(AsynchronousTask):
 
 	_bufsize = 4096
 
-	@property
-	def _exceptional_events(self):
-		return self.scheduler.IO_ERR | self.scheduler.IO_NVAL
-
-	@property
-	def _registered_events(self):
-		return self.scheduler.IO_IN | self.scheduler.IO_HUP | \
-			self._exceptional_events
-
 	def isAlive(self):
 		return bool(self._registered)
 