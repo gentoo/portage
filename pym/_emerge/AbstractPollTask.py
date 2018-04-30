gentoo/portage

commit c11a6ec05f02601a7c2cb5455d1289b9dfed5f10

AbstractPollTask: add_reader asyncio compat (bug 654382)
Migration to add_reader is now complete, so remove unused code related
to io_add_watch (and source_remove).

Bug: https://bugs.gentoo.org/654382

pym/_emerge/AbstractPollTask.py
@@ -44,18 +44,16 @@ class AbstractPollTask(AsynchronousTask):
 		| ---------------------------------------------------
 		| 0      | None
 		"""
-		buf = None
-		if event & self.scheduler.IO_IN:
-			buf = array.array('B')
-			try:
+		buf = array.array('B')
+		try:
 				buf.fromfile(f, self._bufsize)
-			except EOFError:
+		except EOFError:
 				pass
-			except TypeError:
+		except TypeError:
 				# Python 3.2:
 				# TypeError: read() didn't return bytes
 				pass
-			except IOError as e:
+		except IOError as e:
 				# EIO happens with pty on Linux after the
 				# slave end of the pty has been closed.
 				if e.errno == errno.EIO:
@@ -120,23 +118,6 @@ class AbstractPollTask(AsynchronousTask):
 
 	def _unregister(self):
 		self._registered = False
-
-	def _log_poll_exception(self, event):
-		writemsg_level(
-			"!!! %s received strange poll event: %s\n" % \
-			(self.__class__.__name__, event,),
-			level=logging.ERROR, noiselevel=-1)
-
-	def _unregister_if_appropriate(self, event):
-		if self._registered:
-			if event & self._exceptional_events:
-				self._log_poll_exception(event)
-				self.cancel()
-				self.returncode = self.returncode or os.EX_OK
-				self._async_wait()
-			elif event & self.scheduler.IO_HUP:
-				self.returncode = self.returncode or os.EX_OK
-				self._async_wait()
 
 	def _wait_loop(self, timeout=None):
 		loop = getattr(self.scheduler, '_asyncio_wrapper', self.scheduler)

pym/_emerge/PipeReader.py
@@ -79,7 +79,7 @@ class PipeReader(AbstractPollTask):
 				break
 
 		while True:
-			data = self._read_array(f, self.scheduler.IO_IN)
+			data = self._read_array(f, None)
 			if data is None:
 				break
 			if data:

pym/_emerge/AbstractEbuildProcess.py
@@ -347,11 +347,6 @@ class AbstractEbuildProcess(SpawnProcess):
 				log_path = self.settings.get("PORTAGE_LOG_FILE")
 			self.scheduler.output(msg, log_path=log_path)
 
-	def _log_poll_exception(self, event):
-		self._elog("eerror",
-			["%s received strange poll event: %s\n" % \
-			(self.__class__.__name__, event,)])
-
 	def _async_waitpid_cb(self, *args, **kwargs):
 		"""
 		Override _async_waitpid_cb to perform cleanup that is

pym/_emerge/SubProcess.py
@@ -12,7 +12,7 @@ import errno
 class SubProcess(AbstractPollTask):
 
 	__slots__ = ("pid",) + \
-		("_dummy_pipe_fd", "_files", "_reg_id", "_waitpid_id")
+		("_dummy_pipe_fd", "_files", "_waitpid_id")
 
 	# This is how much time we allow for waitpid to succeed after
 	# we've sent a kill signal to our subprocess.
@@ -70,10 +70,6 @@ class SubProcess(AbstractPollTask):
 
 		self._registered = False
 
-		if self._reg_id is not None:
-			self.scheduler.source_remove(self._reg_id)
-			self._reg_id = None
-
 		if self._waitpid_id is not None:
 			self.scheduler._asyncio_child_watcher.\
 				remove_child_handler(self._waitpid_id)
@@ -86,19 +82,3 @@ class SubProcess(AbstractPollTask):
 				else:
 					f.close()
 			self._files = None
-
-	def _unregister_if_appropriate(self, event):
-		"""
-		Override the AbstractPollTask._unregister_if_appropriate method to
-		call _async_waitpid instead of wait(), so that event loop recursion
-		is not triggered when the pid exit status is not yet available.
-		"""
-		if self._registered:
-			if event & self._exceptional_events:
-				self._log_poll_exception(event)
-				self._unregister()
-				self.cancel()
-				self._async_waitpid()
-			elif event & self.scheduler.IO_HUP:
-				self._unregister()
-				self._async_waitpid()

pym/portage/util/_async/SchedulerInterface.py
@@ -11,11 +11,7 @@ from ..SlotObject import SlotObject
 
 class SchedulerInterface(SlotObject):
 
-	_event_loop_attrs = ("IO_ERR", "IO_HUP", "IO_IN",
-		"IO_NVAL", "IO_OUT", "IO_PRI",
-		"io_add_watch",
-		"source_remove",
-
+	_event_loop_attrs = (
 		"add_reader",
 		"add_writer",
 		"call_at",

commit 8b3dd3537a2c50035d6369946e65305d448d7f1b

AbstractPollTask: remove deprecated _read_* event parameter
Fixes: c11a6ec05f02 ("AbstractPollTask: add_reader asyncio compat (bug 654382)")

pym/_emerge/AbstractPollTask.py
@@ -19,7 +19,7 @@ class AbstractPollTask(AsynchronousTask):
 	def isAlive(self):
 		return bool(self._registered)
 
-	def _read_array(self, f, event):
+	def _read_array(self, f):
 		"""
 		NOTE: array.fromfile() is used here only for testing purposes,
 		because it has bugs in all known versions of Python (including
@@ -65,7 +65,7 @@ class AbstractPollTask(AsynchronousTask):
 
 		return buf
 
-	def _read_buf(self, fd, event):
+	def _read_buf(self, fd):
 		"""
 		Read self._bufsize into a string of bytes, handling EAGAIN and
 		EIO. This will only call os.read() once, so the caller should
@@ -78,8 +78,6 @@ class AbstractPollTask(AsynchronousTask):
 
 		@param fd: file descriptor (non-blocking mode required)
 		@type fd: int
-		@param event: poll event flags
-		@type event: int
 		@rtype: bytes or None
 		@return: A string of bytes, or None
 		"""

pym/_emerge/AsynchronousLock.py
@@ -266,7 +266,7 @@ class _LockProcess(AbstractPollTask):
 		return self.returncode
 
 	def _output_handler(self):
-		buf = self._read_buf(self._files['pipe_in'], None)
+		buf = self._read_buf(self._files['pipe_in'])
 		if buf:
 			self._acquired = True
 			self._unregister()

pym/_emerge/EbuildIpcDaemon.py
@@ -34,7 +34,7 @@ class EbuildIpcDaemon(FifoIpcDaemon):
 
 	def _input_handler(self):
 		# Read the whole pickle in a single atomic read() call.
-		data = self._read_buf(self._files.pipe_in, None)
+		data = self._read_buf(self._files.pipe_in)
 		if data is None:
 			pass # EAGAIN
 		elif data:

pym/_emerge/EbuildMetadataPhase.py
@@ -130,7 +130,7 @@ class EbuildMetadataPhase(SubProcess):
 
 	def _output_handler(self):
 		while True:
-			buf = self._read_buf(self._files.ebuild, None)
+			buf = self._read_buf(self._files.ebuild)
 			if buf is None:
 				break # EAGAIN
 			elif buf:

pym/_emerge/PipeReader.py
@@ -61,7 +61,7 @@ class PipeReader(AbstractPollTask):
 	def _output_handler(self, fd):
 
 		while True:
-			data = self._read_buf(fd, None)
+			data = self._read_buf(fd)
 			if data is None:
 				break
 			if data:
@@ -79,7 +79,7 @@ class PipeReader(AbstractPollTask):
 				break
 
 		while True:
-			data = self._read_array(f, None)
+			data = self._read_array(f)
 			if data is None:
 				break
 			if data:

pym/portage/dbapi/_MergeProcess.py
@@ -79,7 +79,7 @@ class MergeProcess(ForkProcess):
 			self._locked_vdb = False
 
 	def _elog_output_handler(self):
-		output = self._read_buf(self._elog_reader_fd, None)
+		output = self._read_buf(self._elog_reader_fd)
 		if output:
 			lines = _unicode_decode(output).split('\n')
 			if len(lines) == 1:

pym/portage/util/_async/PipeLogger.py
@@ -72,7 +72,7 @@ class PipeLogger(AbstractPollTask):
 		log_file = self._log_file 
 
 		while True:
-			buf = self._read_buf(fd, None)
+			buf = self._read_buf(fd)
 
 			if buf is None:
 				# not a POLLIN event, EAGAIN, etc...