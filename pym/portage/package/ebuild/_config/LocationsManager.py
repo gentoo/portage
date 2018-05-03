gentoo/portage

commit a41dacf7926c0242678300a8e1770df84029e8a4

Export SYSROOT and ESYSROOT in ebuild env in EAPI 7
SYSROOT is the new destination for DEPEND, however it must currently
be equal either / or ROOT. This is partly to simplify the
implementation and partly because supporting a third separate
destination would not be of much practical benefit anyway.

Bug: https://bugs.gentoo.org/317337

pym/portage/package/ebuild/_config/LocationsManager.py
@@ -16,7 +16,7 @@ from portage import os, eapi_is_supported, _encodings, _unicode_encode
 from portage.const import CUSTOM_PROFILE_PATH, GLOBAL_CONFIG_PATH, \
 	PROFILE_PATH, USER_CONFIG_PATH
 from portage.eapi import eapi_allows_directories_on_profile_level_and_repository_level
-from portage.exception import DirectoryNotFound, ParseError
+from portage.exception import DirectoryNotFound, InvalidLocation, ParseError
 from portage.localization import _
 from portage.util import ensure_dirs, grabfile, \
 	normalize_path, read_corresponding_eapi_file, shlex_split, writemsg
@@ -40,12 +40,13 @@ _allow_parent_colon = frozenset(
 class LocationsManager(object):
 
 	def __init__(self, config_root=None, eprefix=None, config_profile_path=None, local_config=True, \
-		target_root=None):
+		target_root=None, sysroot=None):
 		self.user_profile_dir = None
 		self._local_repo_conf_path = None
 		self.eprefix = eprefix
 		self.config_root = config_root
 		self.target_root = target_root
+		self.sysroot = sysroot
 		self._user_config = local_config
 
 		if self.eprefix is None:
@@ -64,6 +65,13 @@ class LocationsManager(object):
 		self._check_var_directory("PORTAGE_CONFIGROOT", self.config_root)
 		self.abs_user_config = os.path.join(self.config_root, USER_CONFIG_PATH)
 		self.config_profile_path = config_profile_path
+
+		if self.sysroot is None:
+			self.sysroot = "/"
+		else:
+			self.sysroot = normalize_path(os.path.abspath(self.sysroot)).rstrip(os.sep) + os.sep
+
+		self.esysroot = self.sysroot.rstrip(os.sep) + self.eprefix + os.sep
 
 	def load_profiles(self, repositories, known_repository_paths):
 		known_repository_paths = set(os.path.realpath(x)
@@ -297,6 +305,13 @@ class LocationsManager(object):
 
 		self.target_root = normalize_path(os.path.abspath(
 			self.target_root)).rstrip(os.path.sep) + os.path.sep
+
+		if self.sysroot != "/" and self.sysroot != self.target_root:
+			writemsg(_("!!! Error: SYSROOT (currently %s) must "
+				"equal / or ROOT (currently %s).\n") %
+				(self.sysroot, self.target_root),
+				noiselevel=-1)
+			raise InvalidLocation(self.sysroot)
 
 		ensure_dirs(self.target_root)
 		self._check_var_directory("ROOT", self.target_root)

bin/eapi.sh
@@ -30,6 +30,10 @@ ___eapi_has_prefix_variables() {
 	[[ ! ${1-${EAPI-0}} =~ ^(0|1|2)$ || " ${FEATURES} " == *" force-prefix "* ]]
 }
 
+___eapi_has_SYSROOT() {
+	[[ ! ${1-${EAPI-0}} =~ ^(0|1|2|3|4|4-python|4-slot-abi|5|5-hdepend|5-progress|6)$ ]]
+}
+
 ___eapi_has_HDEPEND() {
 	[[ ${1-${EAPI-0}} =~ ^(5-hdepend)$ ]]
 }

bin/phase-functions.sh
@@ -100,10 +100,16 @@ __filter_readonly_variables() {
 	filtered_vars="$readonly_bash_vars $bash_misc_vars
 		$PORTAGE_READONLY_VARS $misc_garbage_vars"
 
+	if ___eapi_has_SYSROOT; then
+		filtered_vars+=" SYSROOT"
+	fi
 	# Don't filter/interfere with prefix variables unless they are
 	# supported by the current EAPI.
 	if ___eapi_has_prefix_variables; then
 		filtered_vars+=" ED EPREFIX EROOT"
+		if ___eapi_has_SYSROOT; then
+			filtered_vars+=" ESYSROOT"
+		fi
 	fi
 	if ___eapi_has_PORTDIR_ECLASSDIR; then
 		filtered_vars+=" PORTDIR ECLASSDIR"

pym/_emerge/actions.py
@@ -2438,7 +2438,7 @@ def load_emerge_config(emerge_config=None, env=None, **kargs):
 	env = os.environ if env is None else env
 	kwargs = {'env': env}
 	for k, envvar in (("config_root", "PORTAGE_CONFIGROOT"), ("target_root", "ROOT"),
-			("eprefix", "EPREFIX")):
+			("sysroot", "SYSROOT"), ("eprefix", "EPREFIX")):
 		v = env.get(envvar)
 		if v and v.strip():
 			kwargs[k] = v

pym/_emerge/depgraph.py
@@ -3322,8 +3322,10 @@ class depgraph(object):
 		if removal_action:
 			depend_root = myroot
 		else:
-			if eapi_attrs.bdepend or eapi_attrs.hdepend:
+			if eapi_attrs.hdepend:
 				depend_root = myroot
+			elif eapi_attrs.bdepend:
+				depend_root = pkg.root_config.settings["ESYSROOT"]
 			else:
 				depend_root = self._frozen_config._running_root.root
 				root_deps = self._frozen_config.myopts.get("--root-deps")

pym/portage/__init__.py
@@ -525,7 +525,7 @@ class _trees_dict(dict):
 		self._target_eroot = None
 
 def create_trees(config_root=None, target_root=None, trees=None, env=None,
-	eprefix=None):
+	sysroot=None, eprefix=None):
 
 	if trees is None:
 		trees = _trees_dict()
@@ -538,7 +538,7 @@ def create_trees(config_root=None, target_root=None, trees=None, env=None,
 		env = os.environ
 
 	settings = config(config_root=config_root, target_root=target_root,
-		env=env, eprefix=eprefix)
+		env=env, sysroot=sysroot, eprefix=eprefix)
 	settings.lock()
 
 	depcachedir = settings.get('PORTAGE_DEPCACHEDIR')
@@ -562,7 +562,7 @@ def create_trees(config_root=None, target_root=None, trees=None, env=None,
 		if depcachedir is not None:
 			clean_env['PORTAGE_DEPCACHEDIR'] = depcachedir
 		settings = config(config_root=None, target_root="/",
-			env=clean_env, eprefix=None)
+			env=clean_env, sysroot="/", eprefix=None)
 		settings.lock()
 		trees._running_eroot = settings['EROOT']
 		myroots.append((settings['EROOT'], settings))

pym/portage/_legacy_globals.py
@@ -28,7 +28,8 @@ def _get_legacy_global(name):
 
 	kwargs = {}
 	for k, envvar in (("config_root", "PORTAGE_CONFIGROOT"),
-			("target_root", "ROOT"), ("eprefix", "EPREFIX")):
+			("target_root", "ROOT"), ("sysroot", "SYSROOT"),
+			("eprefix", "EPREFIX")):
 		kwargs[k] = os.environ.get(envvar)
 
 	portage._initializing_globals = True

pym/portage/eapi.py
@@ -123,6 +123,10 @@ def eapi_path_variables_end_with_trailing_slash(eapi):
 	return eapi in ("0", "1", "2", "3", "4", "4-python", "4-slot-abi",
 			"5", "5-progress", "6")
 
+def eapi_has_sysroot(eapi):
+	return eapi not in ("0", "1", "2", "3", "4", "4-python", "4-slot-abi",
+			"5", "5-progress", "5-hdepend", "6")
+
 _eapi_attrs = collections.namedtuple('_eapi_attrs',
 	'allows_package_provided '
 	'bdepend dots_in_PN dots_in_use_flags exports_EBUILD_PHASE_FUNC '
@@ -132,7 +136,7 @@ _eapi_attrs = collections.namedtuple('_eapi_attrs',
 	'path_variables_end_with_trailing_slash '
 	'repo_deps required_use required_use_at_most_one_of slot_operator slot_deps '
 	'src_uri_arrows strong_blocks use_deps use_dep_defaults '
-	'empty_groups_always_true')
+	'empty_groups_always_true sysroot')
 
 _eapi_attrs_cache = {}
 
@@ -176,6 +180,7 @@ def _get_eapi_attrs(eapi):
 		slot_operator = (eapi is None or eapi_has_slot_operator(eapi)),
 		src_uri_arrows = (eapi is None or eapi_has_src_uri_arrows(eapi)),
 		strong_blocks = (eapi is None or eapi_has_strong_blocks(eapi)),
+		sysroot = (eapi is None or eapi_has_sysroot(eapi)),
 		use_deps = (eapi is None or eapi_has_use_deps(eapi)),
 		use_dep_defaults = (eapi is None or eapi_has_use_dep_defaults(eapi))
 	)

commit b69a756099973abbf9719717ea3726519b32ce60

LocationsManager: fix SYSROOT normalization to handle empty SYSROOT
This issue was exposed when building portage-2.3.34 with USE="epydoc"
enabled, since epydoc triggers instantiation of portage.settings.
The empty SYSROOT change for bug 654600 (in commit
1b5110557d1dd725f7c12bbed4b7ceaaec29f2a3) triggered incorrect
normalization behavior here.

Fixes: a41dacf7926c ("Export SYSROOT and ESYSROOT in ebuild env in EAPI 7")

pym/portage/package/ebuild/_config/LocationsManager.py
@@ -1,4 +1,4 @@
-# Copyright 2010-2014 Gentoo Foundation
+# Copyright 2010-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import unicode_literals
@@ -69,7 +69,7 @@ class LocationsManager(object):
 		if self.sysroot is None:
 			self.sysroot = "/"
 		else:
-			self.sysroot = normalize_path(os.path.abspath(self.sysroot)).rstrip(os.sep) + os.sep
+			self.sysroot = normalize_path(os.path.abspath(self.sysroot or os.sep)).rstrip(os.sep) + os.sep
 
 		self.esysroot = self.sysroot.rstrip(os.sep) + self.eprefix + os.sep
 