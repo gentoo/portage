gentoo/portage

commit 9abebd3e6aae049d2eb9c8039453d7f187bca00f

phase-helpers.sh: fix ROOT overrides for has/best_version (bug 655860)
Since it's common for code to override the ROOT variable, use
${ROOT%/}/${EPREFIX} instead of ${EROOT}. Thanks to Mike Gilbert
<floppym@gentoo.org> for finding the cause.

Fixes: 43b6be7423aa ("phase-helpers.sh: Implement -r|-d|-b options for best/has_version")
Bug: https://bugs.gentoo.org/655860

bin/phase-helpers.sh
@@ -891,7 +891,7 @@ ___best_version_and_has_version_common() {
 
 	case ${root_arg} in
 		"") if ___eapi_has_prefix_variables; then
-				root=${EROOT}
+				root=${ROOT%/}/${EPREFIX}
 			else
 				root=${ROOT}
 			fi ;;
@@ -910,7 +910,7 @@ ___best_version_and_has_version_common() {
 			fi
 			if ___eapi_has_prefix_variables; then
 				case ${root_arg} in
-					-r) root=${EROOT} ;;
+					-r) root=${ROOT%/}/${EPREFIX} ;;
 					-d) root=${ESYSROOT} ;;
 					-b) root=${BROOT:-/} ;;
 				esac

commit 1d1a20f48f5f37316b7827428e95714137078ea1

phase-helpers.sh: normalize has/best_version root more (cosmetic)
This eliminates a double slash when ${EPREFIX} is non-empty,
though it's only a cosmetic fix since both portageq and
QueryCommand normalize the root argument.

Fixes: 9abebd3e6aae ("phase-helpers.sh: fix ROOT overrides for has/best_version (bug 655860)")
Reported-by: Douglas Freed <dwfreed@mtu.edu>
Reported-by: Michał Górny <mgorny@gentoo.org>

bin/phase-helpers.sh
@@ -891,7 +891,7 @@ ___best_version_and_has_version_common() {
 
 	case ${root_arg} in
 		"") if ___eapi_has_prefix_variables; then
-				root=${ROOT%/}/${EPREFIX}
+				root=${ROOT%/}/${EPREFIX#/}
 			else
 				root=${ROOT}
 			fi ;;
@@ -910,7 +910,7 @@ ___best_version_and_has_version_common() {
 			fi
 			if ___eapi_has_prefix_variables; then
 				case ${root_arg} in
-					-r) root=${ROOT%/}/${EPREFIX} ;;
+					-r) root=${ROOT%/}/${EPREFIX#/} ;;
 					-d) root=${ESYSROOT} ;;
 					-b) root=${BROOT:-/} ;;
 				esac