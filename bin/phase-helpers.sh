gentoo/portage

commit 17fce85669beec41747d65b37a55747b2f3ddb3c

phase-helpers.sh: fix has/best_version for cross-prefix portageq (bug 655414)
When the portageq root argument refers to ${PORTAGE_OVERRIDE_EPREFIX},
also export a consistent ${EPREFIX}, since portageq asserts that the
root argument ends with ${EPREFIX}.

Bug: https://bugs.gentoo.org/655414

bin/phase-helpers.sh
@@ -900,7 +900,10 @@ ___best_version_and_has_version_common() {
 				die "${FUNCNAME[1]}: option ${root_arg} is not supported with EAPI ${EAPI}"
 			fi
 			if ___eapi_has_prefix_variables; then
+				# Since portageq requires the root argument be consistent
+				# with EPREFIX, ensure consistency here (bug 655414).
 				root=/${PORTAGE_OVERRIDE_EPREFIX#/}
+				local -x EPREFIX=${PORTAGE_OVERRIDE_EPREFIX}
 			else
 				root=/
 			fi ;;

commit 1582103b2f0f64e5dc57eddc4217360eac230b8a

phase-helpers.sh: handle readonly EPREFIX for local override (bug 655414)
Since ebuild.sh calls "declare -r ED EPREFIX EROOT", use env to
override EPREFIX for the called command.

Fixes: 17fce85669be ("phase-helpers.sh: fix has/best_version for cross-prefix portageq (bug 655414)")

bin/phase-helpers.sh
@@ -880,6 +880,7 @@ __eapi6_src_install() {
 
 ___best_version_and_has_version_common() {
 	local atom root root_arg
+	local -a cmd=()
 	case $1 in
 		--host-root|-r|-d|-b)
 			root_arg=$1
@@ -903,7 +904,7 @@ ___best_version_and_has_version_common() {
 				# Since portageq requires the root argument be consistent
 				# with EPREFIX, ensure consistency here (bug 655414).
 				root=/${PORTAGE_OVERRIDE_EPREFIX#/}
-				local -x EPREFIX=${PORTAGE_OVERRIDE_EPREFIX}
+				cmd+=(env EPREFIX="${PORTAGE_OVERRIDE_EPREFIX}")
 			else
 				root=/
 			fi ;;
@@ -927,10 +928,11 @@ ___best_version_and_has_version_common() {
 	esac
 
 	if [[ -n $PORTAGE_IPC_DAEMON ]] ; then
-		"${PORTAGE_BIN_PATH}"/ebuild-ipc "${FUNCNAME[1]}" "${root}" "${atom}"
+		cmd+=("${PORTAGE_BIN_PATH}"/ebuild-ipc "${FUNCNAME[1]}" "${root}" "${atom}")
 	else
-		"${PORTAGE_BIN_PATH}"/ebuild-helpers/portageq "${FUNCNAME[1]}" "${root}" "${atom}"
+		cmd+=("${PORTAGE_BIN_PATH}"/ebuild-helpers/portageq "${FUNCNAME[1]}" "${root}" "${atom}")
 	fi
+	"${cmd[@]}"
 	local retval=$?
 	case "${retval}" in
 		0|1)