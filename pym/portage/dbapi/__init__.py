e691f07bf0572fad7686a54d628c1a29aec4ebe4
Zac Medico <zmedico@gentoo.org>
2018-07-12 02:48:32 -0700

    dbapi: fix repoman implicit IUSE (bug 660982)
    Account for repoman modifications of the portdbapi self.settings
    reference, and treat all flags as valid for the empty profile
    because it does not have any implicit IUSE settings.
    
    Bug: https://bugs.gentoo.org/660982
    Reviewed-by: Brian Dolbec <dolsen@gentoo.org>

--- pym/portage/dbapi/__init__.py
+++ pym/portage/dbapi/__init__.py
@@ -216,6 +216,22 @@ class dbapi(object):
 
 			yield cpv
 
+	def _repoman_iuse_implicit_cnstr(self, pkg, metadata):
+		"""
+		In repoman's version of _iuse_implicit_cnstr, account for modifications
+		of the self.settings reference between calls, and treat all flags as
+		valid for the empty profile because it does not have any implicit IUSE
+		settings. See bug 660982.
+		"""
+		eapi_attrs = _get_eapi_attrs(metadata["EAPI"])
+		if eapi_attrs.iuse_effective:
+			iuse_implicit_match = lambda flag: (True if not self.settings.profile_path
+				else self.settings._iuse_effective_match(flag))
+		else:
+			iuse_implicit_match = lambda flag: (True if not self.settings.profile_path
+				else self.settings._iuse_implicit_match(flag))
+		return iuse_implicit_match
+
 	def _iuse_implicit_cnstr(self, pkg, metadata):
 		"""
 		Construct a callable that checks if a given USE flag should
