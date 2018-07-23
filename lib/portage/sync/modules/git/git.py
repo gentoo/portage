gentoo/portage

commit dc1ba7729c42cbe738ae7ec34e2f13f79c48d41c

git: fix key refresh failure to trigger abort (bug 661906)
Fix the verify_head method to appropriately return a bool
value instead of a tuple.

Fixes: d30191b887bb ("git: Support running the verification against sync-openpgp-key-path")
Bug: https://bugs.gentoo.org/661906

lib/portage/sync/modules/git/git.py
@@ -227,7 +227,7 @@ class GitSync(NewBase):
 					writemsg_level("!!! Verification impossible due to keyring problem:\n%s\n"
 							% (e,),
 							level=logging.ERROR, noiselevel=-1)
-					return (1, False)
+					False
 
 				env = os.environ.copy()
 				env['GNUPGHOME'] = openpgp_env.home

commit de9801740fcbdb80c582308d7a4f2fef6b2b6aed

git: add missing return statement in fix for bug 661906
Fixes: dc1ba7729c42 ("git: fix key refresh failure to trigger abort (bug 661906)")
Bug: https://bugs.gentoo.org/661906

lib/portage/sync/modules/git/git.py
@@ -227,7 +227,7 @@ class GitSync(NewBase):
 					writemsg_level("!!! Verification impossible due to keyring problem:\n%s\n"
 							% (e,),
 							level=logging.ERROR, noiselevel=-1)
-					False
+					return False
 
 				env = os.environ.copy()
 				env['GNUPGHOME'] = openpgp_env.home
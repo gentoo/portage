de9801740fcbdb80c582308d7a4f2fef6b2b6aed
Zac Medico <zmedico@gentoo.org>
2018-07-23 12:58:03 -0700

    git: add missing return statement in fix for bug 661906
    Fixes: dc1ba7729c42 ("git: fix key refresh failure to trigger abort (bug 661906)")
    Bug: https://bugs.gentoo.org/661906

--- lib/portage/sync/modules/git/git.py
+++ lib/portage/sync/modules/git/git.py
@@ -227,7 +227,7 @@ class GitSync(NewBase):
 					writemsg_level("!!! Verification impossible due to keyring problem:\n%s\n"
 							% (e,),
 							level=logging.ERROR, noiselevel=-1)
-					False
+					return False
 
 				env = os.environ.copy()
 				env['GNUPGHOME'] = openpgp_env.home
