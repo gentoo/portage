3389191e9e58e73be3f41bdb251fbb962b834890
Fabian Groffen <grobian@gentoo.org>
2018-12-06 14:07:56 +0100

    build-sys: update after pym->lib move
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- tarball.sh
+++ tarball.sh
@@ -13,6 +13,11 @@ export TMP="/var/tmp"
 export V="$1"
 export DEST="${TMP}/${PKG}-${V}"
 
+if [[ -e ${DEST} ]]; then
+	echo ${DEST} already exists, please remove first
+	exit 1
+fi
+
 ./tabcheck.py $(
 	find ./ -name .git -o -name .hg -prune -o -type f ! -name '*.py' -print \
 		| xargs grep -l "#\!@PREFIX_PORTAGE_PYTHON@" \
@@ -22,14 +27,10 @@ export DEST="${TMP}/${PKG}-${V}"
 
 )
 
-if [[ -e ${DEST} ]]; then
-	echo EXISTS ALREADY
-	exit 1
-fi
-
 install -d -m0755 ${DEST}
 rsync -a --exclude='.git' --exclude='.hg' --exclude="repoman/" . ${DEST}
-sed -i -e '/^VERSION\s*=/s/^.*$/VERSION = "'${V}-prefix'"/' ${DEST}/pym/portage/__init__.py
+sed -i -e '/^VERSION\s*=/s/^.*$/VERSION = "'${V}-prefix'"/' \
+	${DEST}/lib/portage/__init__.py
 sed -i -e "/version = /s/'[^']\+'/'${V}-prefix'/" ${DEST}/setup.py
 sed -i -e "1s/VERSION/${V}-prefix/" ${DEST}/man/{,ru/}*.[15]
 sed -i -e "s/@version@/${V}/" ${DEST}/configure.ac
@@ -39,7 +40,12 @@ find -name '*~' | xargs --no-run-if-empty rm -f
 find -name '*.pyc' | xargs --no-run-if-empty rm -f
 find -name '*.pyo' | xargs --no-run-if-empty rm -f
 cd $TMP
-rm -f ${PKG}-${V}/bin/emerge.py ${PKG}-${V}/bin/{pmake,sandbox} ${PKG}-${V}/{bin,pym}/'.#'* ${PKG}-${V}/{bin,pym}/*.{orig,diff} ${PKG}-${V}/{bin,pym}/*.py[oc]
+rm -f \
+	${PKG}-${V}/bin/emerge.py \
+	${PKG}-${V}/bin/{pmake,sandbox} \
+	${PKG}-${V}/{bin,lib}/'.#'* \
+	${PKG}-${V}/{bin,lib}/*.{orig,diff} \
+	${PKG}-${V}/{bin,lib}/*.py[oc]
 cd $TMP/${PKG}-${V}
 chmod a+x autogen.sh && ./autogen.sh || { echo "autogen failed!"; exit -1; };
 rm -f autogen.sh tabcheck.py tarball.sh commit
