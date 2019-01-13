fb406579b1d13c1ba23b28e0bb794c22878a58c0
Zac Medico <zmedico@gentoo.org>
2019-01-13 15:06:35 -0800

    pid-sandbox: execute pid-ns-init as pid 1 (bug 675312)
    Execute pid-ns-init as the first fork after unshare, as
    required for it to have pid 1 and become the default reaper
    of orphaned descendant processes. In _exec, exec a separate
    pid-ns-init process to behave as a supervisor which will
    forward signals to init and forward exit status to the parent
    process.
    
    Fixes: a75d5546e3a4 ("Introduce a tiny init replacement for inside pid namespace")
    Bug: https://bugs.gentoo.org/675312
    Reviewed-by: Brian Dolbec <dolsen@gentoo.org>
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- lib/portage/process.py
+++ lib/portage/process.py
@@ -564,15 +564,28 @@ def _exec(binary, mycommand, opt_name, fd_pipes,
 							noiselevel=-1)
 					else:
 						if unshare_pid:
-							# pid namespace requires us to become init
-							fork_ret = os.fork()
-							if fork_ret != 0:
-								os.execv(portage._python_interpreter, [
+							main_child_pid = os.fork()
+							if main_child_pid == 0:
+								# pid namespace requires us to become init
+								binary, myargs = portage._python_interpreter, [
 									portage._python_interpreter,
 									os.path.join(portage._bin_path,
 										'pid-ns-init'),
-									'%s' % fork_ret,
-									])
+									binary] + myargs
+							else:
+								# Execute a supervisor process which will forward
+								# signals to init and forward exit status to the
+								# parent process. The supervisor process runs in
+								# the global pid namespace, so skip /proc remount
+								# and other setup that's intended only for the
+								# init process.
+								binary, myargs = portage._python_interpreter, [
+									portage._python_interpreter,
+									os.path.join(portage._bin_path,
+									'pid-ns-init'), str(main_child_pid)]
+
+								os.execve(binary, myargs, env)
+
 						if unshare_mount:
 							# mark the whole filesystem as slave to avoid
 							# mounts escaping the namespace
