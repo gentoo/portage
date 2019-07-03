gentoo/portage

commit 7f1aac1113203cb43d2a44bfafd6b8deb045efea

ebuild.sh: unexport ___in_portage_iuse function (bug 680810)
The exported BASH_FUNC____in_portage_iuse%% variable can trigger
problems for some shells, so do not export it.

Reported-by: Fabian Groffen <grobian@gentoo.org>
Bug: https://bugs.gentoo.org/680810
Fixes: 9cac3bfa782f ("Speed up testing against IUSE by not using regexp")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

bin/ebuild.sh
@@ -8,6 +8,7 @@ unalias -a
 
 # Make sure this isn't exported to scripts we execute.
 unset BASH_COMPAT
+export -n -f ___in_portage_iuse
 
 source "${PORTAGE_BIN_PATH}/isolated-functions.sh" || exit 1
 

commit f4aa49bc1ba210a1257ae6291a60d0944c32691d

ebuild.sh: suppress export error messages for eix-update
Suppress export error messages like this for eix-update:

/usr/lib/portage/python3.6/ebuild.sh: line 11: export: ___in_portage_iuse: not a function

Fixes: 7f1aac111320 ("ebuild.sh: unexport ___in_portage_iuse function (bug 680810)")
Bug: https://bugs.gentoo.org/680810
Bug: https://bugs.gentoo.org/689128
Signed-off-by: Zac Medico <zmedico@gentoo.org>

bin/ebuild.sh
@@ -8,7 +8,7 @@ unalias -a
 
 # Make sure this isn't exported to scripts we execute.
 unset BASH_COMPAT
-export -n -f ___in_portage_iuse
+declare -F ___in_portage_iuse >/dev/null && export -n -f ___in_portage_iuse
 
 source "${PORTAGE_BIN_PATH}/isolated-functions.sh" || exit 1
 