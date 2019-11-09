c6d2f47243bcedd4f491db5cbe316c2a261e4da3
Zac Medico <zmedico@gentoo.org>
2019-11-09 11:56:54 -0800

    FileCopier: native zero-copy and sparse file support
    Use native copyfile for zero-copy and sparse file support, and copy
    permission bits like shutil.copy.
    
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- lib/portage/util/_async/FileCopier.py
+++ lib/portage/util/_async/FileCopier.py
@@ -1,7 +1,11 @@
 # Copyright 2013-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-from portage import shutil
+import os as _os
+
+from portage import _encodings, _unicode_encode
+from portage.util import apply_stat_permissions
+from portage.util.file_copy import copyfile
 from portage.util.futures import asyncio
 from portage.util.futures.executor.fork import ForkExecutor
 from portage.util._async.AsyncTaskFuture import AsyncTaskFuture
@@ -14,6 +18,12 @@ class FileCopier(AsyncTaskFuture):
 	__slots__ = ('src_path', 'dest_path')
 
 	def _start(self):
-		self.future = asyncio.ensure_future(self.scheduler.run_in_executor(ForkExecutor(loop=self.scheduler),
-			shutil.copy, self.src_path, self.dest_path))
+		self.future = asyncio.ensure_future(self.scheduler.run_in_executor(
+			ForkExecutor(loop=self.scheduler), self._run))
 		super(FileCopier, self)._start()
+
+	def _run(self):
+		src_path = _unicode_encode(self.src_path, encoding=_encodings['fs'], errors='strict')
+		dest_path = _unicode_encode(self.dest_path, encoding=_encodings['fs'], errors='strict')
+		copyfile(src_path, dest_path)
+		apply_stat_permissions(dest_path, _os.stat(src_path))
