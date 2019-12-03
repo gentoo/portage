gentoo/portage

commit a561ac910331657ffb281ca2b16bb1c8d075770e

repoman commit: ignore unadded hidden files
Bug: https://bugs.gentoo.org/541076
Copyright: Sony Interactive Entertainment Inc.
Signed-off-by: Zac Medico <zmedico@gentoo.org>

repoman/lib/repoman/actions.py
@@ -334,7 +334,8 @@ the whole commit message to abort.
 		if myunadded:
 			for x in range(len(myunadded) - 1, -1, -1):
 				xs = myunadded[x].split("/")
-				if self.repo_settings.repo_config.find_invalid_path_char(myunadded[x]) != -1:
+				if (any(token.startswith('.') for token in xs) or
+					self.repo_settings.repo_config.find_invalid_path_char(myunadded[x]) != -1):
 					# The Manifest excludes this file,
 					# so it's safe to ignore.
 					del myunadded[x]

commit 1c2e99fbf21675c8335ab8a3eee0ed58e89c4342

repoman commit: ignore unadded hidden files except '.' itself
Fixes: a561ac910331 ("repoman commit: ignore unadded hidden files")
Bug: https://bugs.gentoo.org/541076
Copyright: Sony Interactive Entertainment Inc.
Signed-off-by: Zac Medico <zmedico@gentoo.org>

repoman/lib/repoman/actions.py
@@ -334,7 +334,7 @@ the whole commit message to abort.
 		if myunadded:
 			for x in range(len(myunadded) - 1, -1, -1):
 				xs = myunadded[x].split("/")
-				if (any(token.startswith('.') for token in xs) or
+				if (any(token.startswith('.') and token != '.' for token in xs) or
 					self.repo_settings.repo_config.find_invalid_path_char(myunadded[x]) != -1):
 					# The Manifest excludes this file,
 					# so it's safe to ignore.