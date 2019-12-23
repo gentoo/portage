gentoo/portage

commit fa7b6ea6ecdc135a01a65e249276e6d75b92791e

_queue_disjunctive_deps: group disjunctions (bug 701996)
When disjunctive dependencies are queued, group together disjunctions
from the same dependency string so that any overlap between them will
trigger expansion to DNF.

Bug: https://bugs.gentoo.org/701996
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/depgraph.py
@@ -3850,10 +3850,11 @@ class depgraph(object):
 		Yields non-disjunctive deps. Raises InvalidDependString when
 		necessary.
 		"""
+		disjunctions = []
 		for x in dep_struct:
 			if isinstance(x, list):
 				if x and x[0] == "||":
-					self._queue_disjunction(pkg, dep_root, dep_priority, [x])
+					disjunctions.append(x)
 				else:
 					for y in self._queue_disjunctive_deps(
 						pkg, dep_root, dep_priority, x):
@@ -3863,9 +3864,12 @@ class depgraph(object):
 				# or whatever other metadata gets implemented for this
 				# purpose.
 				if x.cp.startswith('virtual/'):
-					self._queue_disjunction(pkg, dep_root, dep_priority, [x])
+					disjunctions.append(x)
 				else:
 					yield x
+
+		if disjunctions:
+			self._queue_disjunction(pkg, dep_root, dep_priority, disjunctions)
 
 	def _queue_disjunction(self, pkg, dep_root, dep_priority, dep_struct):
 		self._dynamic_config._dep_disjunctive_stack.append(

lib/portage/tests/resolver/test_virtual_minimize_children.py
@@ -285,3 +285,42 @@ class VirtualMinimizeChildrenTestCase(TestCase):
 		finally:
 			playground.debug = False
 			playground.cleanup()
+
+
+	def testVirtualWine(self):
+		ebuilds = {
+			'virtual/wine-0-r6': {
+				'RDEPEND': '|| ( app-emulation/wine-staging app-emulation/wine-any ) '
+					'|| ( app-emulation/wine-vanilla app-emulation/wine-staging app-emulation/wine-any )'
+			},
+			'app-emulation/wine-staging-4': {},
+			'app-emulation/wine-any-4': {},
+			'app-emulation/wine-vanilla-4': {},
+		}
+
+		test_cases = (
+			# Test bug 701996, where separate disjunctions where not
+			# converted to DNF, causing both wine-vanilla and
+			# wine-staging to be pulled in.
+			ResolverPlaygroundTestCase(
+				[
+					'virtual/wine',
+				],
+				success=True,
+				mergelist=(
+					'app-emulation/wine-staging-4',
+					'virtual/wine-0-r6',
+				),
+			),
+		)
+
+		playground = ResolverPlayground(debug=False, ebuilds=ebuilds)
+
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			playground.debug = False
+			playground.cleanup()

commit f68752b7020c66e501b49acc580d023654a96949

_queue_disjunctive_deps: group disjunctions recursively (bug 701996)
When disjunctive dependencies are queued, recursively group together
disjunctions from the same dependency string so that any overlap between
them will trigger expansion to DNF.

Fixes: fa7b6ea6ecdc ("_queue_disjunctive_deps: group disjunctions (bug 701996)")
Bug: https://bugs.gentoo.org/701996
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/depgraph.py
@@ -3865,20 +3865,20 @@ class depgraph(object):
 					child_pkgs.sort()
 				yield (atom, child_pkgs[-1])
 
-	def _queue_disjunctive_deps(self, pkg, dep_root, dep_priority, dep_struct):
+	def _queue_disjunctive_deps(self, pkg, dep_root, dep_priority, dep_struct, _disjunctions_recursive=None):
 		"""
 		Queue disjunctive (virtual and ||) deps in self._dynamic_config._dep_disjunctive_stack.
 		Yields non-disjunctive deps. Raises InvalidDependString when
 		necessary.
 		"""
-		disjunctions = []
+		disjunctions = [] if _disjunctions_recursive is None else _disjunctions_recursive
 		for x in dep_struct:
 			if isinstance(x, list):
 				if x and x[0] == "||":
 					disjunctions.append(x)
 				else:
 					for y in self._queue_disjunctive_deps(
-						pkg, dep_root, dep_priority, x):
+						pkg, dep_root, dep_priority, x, _disjunctions_recursive=disjunctions):
 						yield y
 			else:
 				# Note: Eventually this will check for PROPERTIES=virtual
@@ -3889,7 +3889,7 @@ class depgraph(object):
 				else:
 					yield x
 
-		if disjunctions:
+		if _disjunctions_recursive is None and disjunctions:
 			self._queue_disjunction(pkg, dep_root, dep_priority, disjunctions)
 
 	def _queue_disjunction(self, pkg, dep_root, dep_priority, dep_struct):