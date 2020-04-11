f049604664bff616b297a5c022b6a8277542eac9
Zac Medico <zmedico@gentoo.org>
2020-04-11 16:45:33 -0700

    Add test case to reproduce bug 717140
    Bug: https://bugs.gentoo.org/717140
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- lib/portage/tests/resolver/test_slot_operator_reverse_deps.py
+++ lib/portage/tests/resolver/test_slot_operator_reverse_deps.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Gentoo Foundation
+# Copyright 2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 from portage.tests import TestCase
@@ -110,4 +110,96 @@ class SlotOperatorReverseDepsTestCase(TestCase):
 				self.assertEqual(test_case.test_success, True,
 					test_case.fail_msg)
 		finally:
+			playground.cleanup()
+
+
+class SlotOperatorReverseDepsLibGit2TestCase(TestCase):
+
+	def testSlotOperatorReverseDepsLibGit2(self):
+		"""
+		Test bug #717140, where the depgraph _slot_operator_update_probe
+		method ignored <dev-libs/libgit2-1:0= dependency and tried to
+		trigger an upgrade to dev-libs/libgit2-1.0.0-r1, ultimately
+		resulting in an undesirable downgrade to dev-libs/libgit2-0.28.4-r1.
+		"""
+		self.todo = True
+
+		ebuilds = {
+
+			"dev-libs/libgit2-0.28.4-r1" : {
+				"EAPI": "7",
+				"SLOT": "0/28",
+			},
+
+			"dev-libs/libgit2-0.99.0-r1" : {
+				"EAPI": "7",
+				"SLOT": "0/0.99",
+			},
+
+			"dev-libs/libgit2-1.0.0-r1" : {
+				"EAPI": "7",
+				"SLOT": "0/1.0",
+			},
+
+			"dev-libs/libgit2-glib-0.28.0.1" : {
+				"EAPI": "7",
+				"SLOT": "0",
+				"RDEPEND": "<dev-libs/libgit2-0.29:0= >=dev-libs/libgit2-0.26.0",
+			},
+
+			"dev-libs/libgit2-glib-0.99.0.1" : {
+				"EAPI": "7",
+				"SLOT": "0",
+				"RDEPEND": "<dev-libs/libgit2-1:0= >=dev-libs/libgit2-0.26.0",
+			},
+
+			"dev-vcs/gitg-3.32.1-r1" : {
+				"EAPI": "7",
+				"SLOT": "0",
+				"RDEPEND": "dev-libs/libgit2:= >=dev-libs/libgit2-glib-0.27 <dev-libs/libgit2-glib-1",
+			},
+		}
+
+		installed = {
+
+			"dev-libs/libgit2-0.99.0-r1" : {
+				"EAPI": "7",
+				"SLOT": "0/0.99",
+			},
+
+			"dev-libs/libgit2-glib-0.99.0.1" : {
+				"EAPI": "7",
+				"SLOT": "0",
+				"RDEPEND": "<dev-libs/libgit2-1:0/0.99= >=dev-libs/libgit2-0.26.0",
+			},
+
+			"dev-vcs/gitg-3.32.1-r1" : {
+				"EAPI": "7",
+				"SLOT": "0",
+				"RDEPEND": "dev-libs/libgit2:0/0.99= >=dev-libs/libgit2-glib-0.27 <dev-libs/libgit2-glib-1",
+			},
+
+		}
+
+		world = ["dev-vcs/gitg"]
+
+		test_cases = (
+			ResolverPlaygroundTestCase(
+				["@world"],
+				options = {"--update": True, "--deep": True},
+				success = True,
+				#mergelist = ['dev-libs/libgit2-0.28.4-r1', 'dev-libs/libgit2-glib-0.99.0.1', 'dev-vcs/gitg-3.32.1-r1'],
+				mergelist = [],
+			),
+		)
+
+		playground = ResolverPlayground(ebuilds=ebuilds,
+			installed=installed, world=world, debug=False)
+		try:
+			for test_case in test_cases:
+				playground.run_TestCase(test_case)
+				self.assertEqual(test_case.test_success, True,
+					test_case.fail_msg)
+		finally:
+			playground.debug = False
 			playground.cleanup()
