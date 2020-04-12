52681b48125abb38cc2f16e52ece2955c24682d1
Zac Medico <zmedico@gentoo.org>
2020-04-11 18:29:35 -0700

    Updates for portage-2.3.99 release
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- setup.py
+++ setup.py
@@ -657,7 +657,7 @@ class build_ext(_build_ext):
 
 setup(
 	name = 'portage',
-	version = '2.3.98',
+	version = '2.3.99',
 	url = 'https://wiki.gentoo.org/wiki/Project:Portage',
 	author = 'Gentoo Portage Development Team',
 	author_email = 'dev-portage@gentoo.org',
