gentoo/portage

commit 4c18f523bb86a8be4c148f365dabee06fca2e4fa

Escape percent-signs in filename when fetching from mirrors
Bug: https://bugs.gentoo.org/719810
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/package/ebuild/fetch.py
@@ -26,6 +26,11 @@ try:
 except ImportError:
 	from urlparse import urlparse
 
+try:
+	from urllib.parse import quote as urlquote
+except ImportError:
+	from urllib import quote as urlquote
+
 import portage
 portage.proxy.lazyimport.lazyimport(globals(),
 	'portage.package.ebuild.config:check_config_instance,config',
@@ -351,7 +356,7 @@ _size_suffix_map = {
 
 class FlatLayout(object):
 	def get_path(self, filename):
-		return filename
+		return urlquote(filename)
 
 	def get_filenames(self, distdir):
 		for dirpath, dirnames, filenames in os.walk(distdir,
@@ -382,7 +387,7 @@ class FilenameHashLayout(object):
 			c = c // 4
 			ret += fnhash[:c] + '/'
 			fnhash = fnhash[c:]
-		return ret + filename
+		return ret + urlquote(filename)
 
 	def get_filenames(self, distdir):
 		pattern = ''

commit c238d5f7ed264179c263f5a2da983c4ee50b4f00

Escape percent-signs in portage.package.ebuild.fetch.get_mirror_url()
This avoids double-escaping in emirrordist. We only want to escape the
path when fetching the file from the mirror, not when mirroring the
file.

Bug: https://bugs.gentoo.org/719810
Fixes: 4c18f523bb86a8be4c148f365dabee06fca2e4fa
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/package/ebuild/fetch.py
@@ -26,6 +26,11 @@ try:
 except ImportError:
 	from urlparse import urlparse
 
+try:
+	from urllib.parse import quote as urlquote
+except ImportError:
+	from urllib import quote as urlquote
+
 import portage
 portage.proxy.lazyimport.lazyimport(globals(),
 	'portage.package.ebuild.config:check_config_instance,config',
@@ -520,7 +525,7 @@ def get_mirror_url(mirror_url, filename, mysettings, cache_path=None):
 				f.close()
 
 	return (mirror_url + "/distfiles/" +
-			mirror_conf.get_best_supported_layout().get_path(filename))
+			urlquote(mirror_conf.get_best_supported_layout().get_path(filename)))
 
 
 def fetch(myuris, mysettings, listonly=0, fetchonly=0,