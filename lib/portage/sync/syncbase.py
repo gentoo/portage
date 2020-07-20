gentoo/portage

commit 788c0e8bb07b40d45bee243f563267444af791ac

Remove from __future__ import unicode_literals
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/sync/syncbase.py
@@ -6,8 +6,6 @@ Base class for performing sync operations.
 This class contains common initialization code and functions.
 '''
 
-from __future__ import unicode_literals
-import functools
 import logging
 import os
 

commit d65e759fc26ea0301ba2020e0993d431e9354e7b

syncbase: Add back missing functools import
Fixes: 788c0e8bb07b ("Remove from __future__ import unicode_literals")
Bug: https://bugs.gentoo.org/733370
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/sync/syncbase.py
@@ -6,6 +6,7 @@ Base class for performing sync operations.
 This class contains common initialization code and functions.
 '''
 
+import functools
 import logging
 import os
 