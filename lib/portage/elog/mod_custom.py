87443505412763cbd974174ed1249d4c25df57a0
Aaron Bauman <bman@gentoo.org>
2020-08-03 18:42:32 -0400

    lib/portage/elog/mod_custom.py: fix whitespace
    Signed-off-by: Aaron Bauman <bman@gentoo.org>
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- lib/portage/elog/mod_custom.py
+++ lib/portage/elog/mod_custom.py
@@ -1,12 +1,12 @@
 # elog/mod_custom.py - elog dispatch module
-# Copyright 2006-2007 Gentoo Foundation
+# Copyright 2006-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import portage.elog.mod_save, portage.process, portage.exception
 
 def process(mysettings, key, logentries, fulltext):
 	elogfilename = portage.elog.mod_save.process(mysettings, key, logentries, fulltext)
-	
+
 	if not mysettings.get("PORTAGE_ELOG_COMMAND"):
 		raise portage.exception.MissingParameter("!!! Custom logging requested but PORTAGE_ELOG_COMMAND is not defined")
 	else:
