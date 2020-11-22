gentoo/portage

commit a2e7bf7d1c7d518fe8d7cf2c0e6cb30020b8aa94

compare_files: handle missing xattr support
Bug: https://bugs.gentoo.org/755950
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/util/_compare_files.py
@@ -45,7 +45,8 @@ def compare_files(file1, file2, skipped_types=()):
 	if "device_number" not in skipped_types and file1_stat.st_rdev != file2_stat.st_rdev:
 		differences.append("device_number")
 
-	if "xattr" not in skipped_types and sorted(xattr.get_all(file1, nofollow=True)) != sorted(xattr.get_all(file2, nofollow=True)):
+	if (xattr.XATTRS_WORKS and "xattr" not in skipped_types and
+		sorted(xattr.get_all(file1, nofollow=True)) != sorted(xattr.get_all(file2, nofollow=True))):
 		differences.append("xattr")
 
 	if "atime" not in skipped_types and file1_stat.st_atime_ns != file2_stat.st_atime_ns:

commit ae8b18f868c9bd039643f89f28f9d92ce8966c3c

compare_files: fix missing xattr handling
Fixes: a2e7bf7d1c7d ("compare_files: handle missing xattr support")
Bug: https://bugs.gentoo.org/755950
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/util/_compare_files.py
@@ -9,7 +9,7 @@ import stat
 
 from portage import _encodings
 from portage import _unicode_encode
-from portage.util._xattr import xattr
+from portage.util._xattr import XATTRS_WORKS, xattr
 
 def compare_files(file1, file2, skipped_types=()):
 	"""
@@ -45,7 +45,7 @@ def compare_files(file1, file2, skipped_types=()):
 	if "device_number" not in skipped_types and file1_stat.st_rdev != file2_stat.st_rdev:
 		differences.append("device_number")
 
-	if (xattr.XATTRS_WORKS and "xattr" not in skipped_types and
+	if (XATTRS_WORKS and "xattr" not in skipped_types and
 		sorted(xattr.get_all(file1, nofollow=True)) != sorted(xattr.get_all(file2, nofollow=True))):
 		differences.append("xattr")
 