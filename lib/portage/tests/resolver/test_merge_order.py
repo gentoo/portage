gentoo/portage

commit 5095c2023595a75e2848f1ad3dbe25b5fb451a44

find_smallest_cycle: enhance search prioritization
Enhance the find_smallest_cycle function to prioritize its
search so that it will minimize the use of installed packages
to break cycles. When installed packages must be used to
break cycles, it will now prefer to do this for runtime
dependencies over buildtime dependencies, since it's
preferable to build against latest versions of buildtime
dependencies whenever possible. This should solve some cases
of bug 199856 which have been triggered by unsafe reliance
on installed packages to break cycles.

The included unit test case demonstrates correct merge order
for a dependency calculation involving 6 independent cycles.
This test case fails with the master branch, due to a buildtime
dependency cycle of 3 packages being merged earlier than cycles
of 2 packages. We can generalize this to say that the master
branch may use an installed package to break an arbitrarily
sized cycle in a somewhat random location, even though that
cycle may be composed of smaller independent cycles which
would be safer to break individually.

Bug: https://bugs.gentoo.org/754903
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/DepPrioritySatisfiedRange.py
@@ -8,17 +8,18 @@ class DepPrioritySatisfiedRange:
 
 	not satisfied and buildtime                    HARD
 	not satisfied and runtime              7       MEDIUM
-	not satisfied and runtime_post         6       MEDIUM_SOFT
-	satisfied and buildtime_slot_op        5       SOFT
-	satisfied and buildtime                4       SOFT
-	satisfied and runtime                  3       SOFT
-	satisfied and runtime_post             2       SOFT
+	satisfied and buildtime_slot_op        6       MEDIUM_SOFT
+	satisfied and buildtime                5       MEDIUM_SOFT
+	satisfied and runtime                  4       MEDIUM_SOFT
+	runtime_post                           3       MEDIUM_POST
+	satisfied and runtime_post             2       MEDIUM_POST
 	optional                               1       SOFT
 	(none of the above)                    0       NONE
 	"""
 	MEDIUM      = 7
 	MEDIUM_SOFT = 6
-	SOFT        = 5
+	MEDIUM_POST = 3
+	SOFT        = 1
 	NONE        = 0
 
 	@classmethod
@@ -35,6 +36,18 @@ class DepPrioritySatisfiedRange:
 			return True
 		if not priority.satisfied:
 			return False
+		if priority.buildtime or priority.runtime:
+			return False
+		return bool(priority.runtime_post)
+
+	@classmethod
+	def _ignore_runtime_post(cls, priority):
+		if priority.__class__ is not DepPriority:
+			return False
+		if priority.optional:
+			return True
+		if priority.buildtime or priority.runtime:
+			return False
 		return bool(priority.runtime_post)
 
 	@classmethod
@@ -43,9 +56,11 @@ class DepPrioritySatisfiedRange:
 			return False
 		if priority.optional:
 			return True
-		if not priority.satisfied:
+		if priority.buildtime:
 			return False
-		return not priority.buildtime
+		if not priority.runtime:
+			return True
+		return bool(priority.satisfied)
 
 	@classmethod
 	def _ignore_satisfied_buildtime(cls, priority):
@@ -61,16 +76,11 @@ class DepPrioritySatisfiedRange:
 	def _ignore_satisfied_buildtime_slot_op(cls, priority):
 		if priority.__class__ is not DepPriority:
 			return False
-		return bool(priority.optional or \
-			priority.satisfied)
-
-	@classmethod
-	def _ignore_runtime_post(cls, priority):
-		if priority.__class__ is not DepPriority:
-			return False
-		return bool(priority.optional or \
-			priority.satisfied or \
-			priority.runtime_post)
+		if priority.optional:
+			return True
+		if priority.satisfied:
+			return True
+		return not priority.buildtime and not priority.runtime
 
 	@classmethod
 	def _ignore_runtime(cls, priority):
@@ -81,17 +91,18 @@ class DepPrioritySatisfiedRange:
 			not priority.buildtime)
 
 	ignore_medium      = _ignore_runtime
-	ignore_medium_soft = _ignore_runtime_post
-	ignore_soft        = _ignore_satisfied_buildtime
+	ignore_medium_soft = _ignore_satisfied_buildtime_slot_op
+	ignore_medium_post = _ignore_runtime_post
+	ignore_soft        = _ignore_optional
 
 
 DepPrioritySatisfiedRange.ignore_priority = (
 	None,
 	DepPrioritySatisfiedRange._ignore_optional,
 	DepPrioritySatisfiedRange._ignore_satisfied_runtime_post,
+	DepPrioritySatisfiedRange._ignore_runtime_post,
 	DepPrioritySatisfiedRange._ignore_satisfied_runtime,
 	DepPrioritySatisfiedRange._ignore_satisfied_buildtime,
 	DepPrioritySatisfiedRange._ignore_satisfied_buildtime_slot_op,
-	DepPrioritySatisfiedRange._ignore_runtime_post,
 	DepPrioritySatisfiedRange._ignore_runtime
 )

lib/_emerge/depgraph.py
@@ -7905,7 +7905,7 @@ class depgraph:
 								if check_asap_parent:
 									for node in nodes:
 										parents = mygraph.parent_nodes(node,
-											ignore_priority=DepPrioritySatisfiedRange.ignore_soft)
+											ignore_priority=DepPrioritySatisfiedRange.ignore_medium_soft)
 										if any(x in asap_nodes for x in parents):
 											selected_nodes = [node]
 											break
@@ -7921,7 +7921,7 @@ class depgraph:
 
 			if not selected_nodes:
 
-				def find_smallest_cycle(mergeable_nodes, priority_ranges):
+				def find_smallest_cycle(mergeable_nodes, local_priority_range):
 					if prefer_asap and asap_nodes:
 						nodes = asap_nodes
 					else:
@@ -7938,18 +7938,27 @@ class depgraph:
 					# these smaller independent cycles.
 					smallest_cycle = None
 					ignore_priority = None
-					for node in nodes:
-						if not mygraph.parent_nodes(node):
-							continue
-						for local_priority_range in priority_ranges:
+
+					# Sort nodes for deterministic results.
+					nodes = sorted(nodes)
+					for priority in (local_priority_range.ignore_priority[i] for i in range(
+						local_priority_range.MEDIUM_POST,
+						local_priority_range.MEDIUM_SOFT + 1)):
+						for node in nodes:
+							if not mygraph.parent_nodes(node):
+								continue
 							selected_nodes = set()
-							if gather_deps(local_priority_range.ignore_medium_soft,
+							if gather_deps(priority,
 								mergeable_nodes, selected_nodes, node):
 								if smallest_cycle is None or \
 									len(selected_nodes) < len(smallest_cycle):
 									smallest_cycle = selected_nodes
-									ignore_priority = local_priority_range.ignore_medium_soft
-								break
+									ignore_priority = priority
+
+						# Exit this loop with the lowest possible priority, which
+						# minimizes the use of installed packages to break cycles.
+						if smallest_cycle is not None:
+							break
 
 					return smallest_cycle, ignore_priority
 
@@ -7963,7 +7972,7 @@ class depgraph:
 				for local_priority_range in priority_ranges:
 					mergeable_nodes = set(get_nodes(ignore_priority=local_priority_range.ignore_medium))
 					if mergeable_nodes:
-						selected_nodes, ignore_priority = find_smallest_cycle(mergeable_nodes, priority_ranges)
+						selected_nodes, ignore_priority = find_smallest_cycle(mergeable_nodes, local_priority_range)
 						if selected_nodes:
 							break
 
@@ -8001,19 +8010,19 @@ class depgraph:
 									(selected_nodes[0],), noiselevel=-1)
 
 			if selected_nodes and ignore_priority is not None:
-				# Try to merge ignored medium_soft deps as soon as possible
+				# Try to merge ignored medium_post deps as soon as possible
 				# if they're not satisfied by installed packages.
 				for node in selected_nodes:
 					children = set(mygraph.child_nodes(node))
 					soft = children.difference(
 						mygraph.child_nodes(node,
-						ignore_priority=DepPrioritySatisfiedRange.ignore_soft))
-					medium_soft = children.difference(
-						mygraph.child_nodes(node,
 							ignore_priority = \
-							DepPrioritySatisfiedRange.ignore_medium_soft))
-					medium_soft -= soft
-					for child in medium_soft:
+							DepPrioritySatisfiedRange.ignore_soft))
+					medium_post = children.difference(
+						mygraph.child_nodes(node,
+						ignore_priority=DepPrioritySatisfiedRange.ignore_medium_post))
+					medium_post -= soft
+					for child in medium_post:
 						if child in selected_nodes:
 							continue
 						if child in asap_nodes:

lib/portage/tests/resolver/test_merge_order.py
@@ -490,6 +490,16 @@ class MergeOrderTestCase(TestCase):
 				success=True,
 				all_permutations = True,
 				mergelist = ['x11-base/xorg-server-1.14.1', 'media-libs/mesa-9.1.3']),
+			# Test prioritization of the find_smallest_cycle function, which should
+			# minimize the use of installed packages to break cycles. If installed
+			# packages must be used to break cycles, then it should prefer to do this
+			# for runtime dependencies over buildtime dependencies. If a package needs
+			# to be uninstalled in order to solve a blocker, then it should prefer to
+			# do this before it uses an installed package to break a cycle.
+			ResolverPlaygroundTestCase(
+				["app-misc/some-app-a", "app-misc/some-app-b", "app-misc/some-app-c", "app-misc/circ-buildtime-a", "app-misc/blocker-buildtime-unbuilt-a", "media-libs/mesa", "x11-base/xorg-server", "app-misc/circ-direct-a", "app-misc/circ-direct-b", "app-misc/circ-satisfied-a", "app-misc/circ-satisfied-b", "app-misc/circ-satisfied-c"],
+				success = True,
+				mergelist = ['app-misc/circ-post-runtime-a-1', 'app-misc/circ-post-runtime-c-1', 'app-misc/circ-post-runtime-b-1', 'app-misc/some-app-b-1', 'app-misc/circ-runtime-a-1', 'app-misc/circ-runtime-b-1', 'app-misc/circ-runtime-c-1', 'app-misc/some-app-a-1', 'app-misc/blocker-buildtime-unbuilt-a-1', '[uninstall]app-misc/installed-blocker-a-1', '!app-misc/installed-blocker-a', 'app-misc/circ-direct-a-1', 'app-misc/circ-direct-b-1', 'x11-base/xorg-server-1.14.1', 'media-libs/mesa-9.1.3', 'app-misc/circ-buildtime-a-1', 'app-misc/circ-buildtime-b-1', 'app-misc/circ-buildtime-c-1', 'app-misc/some-app-c-1', 'app-misc/circ-satisfied-a-1', 'app-misc/circ-satisfied-b-1', 'app-misc/circ-satisfied-c-1']),
 		)
 
 		playground = ResolverPlayground(ebuilds=ebuilds, installed=installed)

lib/_emerge/DepPriorityNormalRange.py
@@ -14,6 +14,7 @@ class DepPriorityNormalRange:
 	"""
 	MEDIUM      = 3
 	MEDIUM_SOFT = 2
+	MEDIUM_POST = 2
 	SOFT        = 1
 	NONE        = 0
 
@@ -37,6 +38,7 @@ class DepPriorityNormalRange:
 
 	ignore_medium      = _ignore_runtime
 	ignore_medium_soft = _ignore_runtime_post
+	ignore_medium_post = _ignore_runtime_post
 	ignore_soft        = _ignore_optional
 
 DepPriorityNormalRange.ignore_priority = (

commit 07a604537e746814613dc171a5c09072ef0266af

find_smallest_cycle: don't merge satisfied PDEPEND too early
After PDEPENDs have been neglected by the find_smallest_cycle function,
do not try to merge them too early if they are already satisfied by
an installed package. This fixes incorrect merge order for PDEPEND
cycles involving xorg-server and xorg-drivers, which was triggered
by commit 5095c2023595a75e2848f1ad3dbe25b5fb451a44 because it gave
PDEPEND higher priority than satisfied buildtime dependencies.

Fixes: 5095c2023595 ("find_smallest_cycle: enhance search prioritization")
Reported-by: josef64 in #gentoo-portage
Bug: https://bugs.gentoo.org/754903
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/DepPrioritySatisfiedRange.py
@@ -93,6 +93,7 @@ class DepPrioritySatisfiedRange:
 	ignore_medium      = _ignore_runtime
 	ignore_medium_soft = _ignore_satisfied_buildtime_slot_op
 	ignore_medium_post = _ignore_runtime_post
+	ignore_medium_post_satisifed = _ignore_satisfied_runtime_post
 	ignore_soft        = _ignore_optional
 
 

lib/_emerge/depgraph.py
@@ -8052,18 +8052,18 @@ class depgraph:
 									(selected_nodes[0],), noiselevel=-1)
 
 			if selected_nodes and ignore_priority is not None:
-				# Try to merge ignored medium_post deps as soon as possible
+				# Try to merge neglected medium_post deps as soon as possible
 				# if they're not satisfied by installed packages.
 				for node in selected_nodes:
 					children = set(mygraph.child_nodes(node))
-					soft = children.difference(
+					medium_post_satisifed = children.difference(
 						mygraph.child_nodes(node,
 							ignore_priority = \
-							DepPrioritySatisfiedRange.ignore_soft))
+							DepPrioritySatisfiedRange.ignore_medium_post_satisifed))
 					medium_post = children.difference(
 						mygraph.child_nodes(node,
 						ignore_priority=DepPrioritySatisfiedRange.ignore_medium_post))
-					medium_post -= soft
+					medium_post -= medium_post_satisifed
 					for child in medium_post:
 						if child in selected_nodes:
 							continue

lib/portage/tests/resolver/test_merge_order.py
@@ -217,12 +217,23 @@ class MergeOrderTestCase(TestCase):
 				"IUSE" : "X +encode",
 				"RDEPEND" : "|| ( >=media-video/ffmpeg-0.6.90_rc0-r2[X=,encode=] >=media-video/libav-0.6.90_rc[X=,encode=] )",
 			},
+			"x11-base/xorg-drivers-1.20-r2": {
+				"EAPI": "7",
+				"IUSE": "+video_cards_fbdev",
+				"PDEPEND": "x11-base/xorg-server video_cards_fbdev? ( x11-drivers/xf86-video-fbdev )",
+			},
 			"x11-base/xorg-server-1.14.1" : {
 				"EAPI" : "5",
 				"SLOT": "0/1.14.1",
 				"DEPEND" : "media-libs/mesa",
 				"RDEPEND" : "media-libs/mesa",
+				"PDEPEND": "x11-base/xorg-drivers",
 			},
+			"x11-drivers/xf86-video-fbdev-0.5.0-r1": {
+				"EAPI": "7",
+				"DEPEND": "x11-base/xorg-server:=",
+				"RDEPEND": "x11-base/xorg-server",
+			}
 		}
 
 		installed = {
@@ -299,12 +310,24 @@ class MergeOrderTestCase(TestCase):
 				"USE" : "encode",
 				"RDEPEND" : "|| ( >=media-video/ffmpeg-0.6.90_rc0-r2[X=,encode=] >=media-video/libav-0.6.90_rc[X=,encode=] )",
 			},
+			"x11-base/xorg-drivers-1.20-r2": {
+				"EAPI": "7",
+				"IUSE": "+video_cards_fbdev",
+				"USE": "video_cards_fbdev",
+				"PDEPEND": "x11-base/xorg-server x11-drivers/xf86-video-fbdev",
+			},
 			"x11-base/xorg-server-1.14.1" : {
 				"EAPI" : "5",
 				"SLOT": "0/1.14.1",
 				"DEPEND" : "media-libs/mesa",
 				"RDEPEND" : "media-libs/mesa",
+				"PDEPEND": "x11-base/xorg-drivers",
 			},
+			"x11-drivers/xf86-video-fbdev-0.5.0-r1": {
+				"EAPI": "7",
+				"DEPEND": "x11-base/xorg-server:0/1.14.1=",
+				"RDEPEND": "x11-base/xorg-server",
+			}
 		}
 
 		test_cases = (
@@ -486,10 +509,10 @@ class MergeOrderTestCase(TestCase):
 			# Both deps are already satisfied by installed packages, but
 			# the := dep is given higher priority in merge order.
 			ResolverPlaygroundTestCase(
-				["media-libs/mesa", "x11-base/xorg-server"],
+				["media-libs/mesa", "x11-drivers/xf86-video-fbdev", "x11-base/xorg-server"],
 				success=True,
 				all_permutations = True,
-				mergelist = ['x11-base/xorg-server-1.14.1', 'media-libs/mesa-9.1.3']),
+				mergelist = ['x11-base/xorg-server-1.14.1', 'media-libs/mesa-9.1.3', 'x11-drivers/xf86-video-fbdev-0.5.0-r1']),
 			# Test prioritization of the find_smallest_cycle function, which should
 			# minimize the use of installed packages to break cycles. If installed
 			# packages must be used to break cycles, then it should prefer to do this