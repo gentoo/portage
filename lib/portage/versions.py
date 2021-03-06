gentoo/portage

commit 5e9fe0f2a5afb71a2aa8425c33103c36bfc1a238

Eliminate basestring/long/_unicode py3 compat
Replace basestring and _unicode hacks with str, and long with int.

Reviewed-by: Zac Medico <zmedico@gentoo.org>
Closes: https://github.com/gentoo/portage/pull/570
Signed-off-by: Michał Górny <mgorny@gentoo.org>

lib/portage/versions.py
@@ -13,11 +13,6 @@ import sys
 import warnings
 from functools import lru_cache
 
-if sys.hexversion < 0x3000000:
-	_unicode = unicode
-else:
-	_unicode = str
-	long = int
 
 import portage
 portage.proxy.lazyimport.lazyimport(globals(),
@@ -346,7 +341,7 @@ def catpkgsplit(mydata, silent=1, eapi=None):
 	retval = (cat, p_split[0], p_split[1], p_split[2])
 	return retval
 
-class _pkg_str(_unicode):
+class _pkg_str(str):
 	"""
 	This class represents a cpv. It inherits from str (unicode in python2) and
 	has attributes that cache results for use by functions like catpkgsplit and
@@ -365,15 +360,15 @@ class _pkg_str(_unicode):
 	def __new__(cls, cpv, metadata=None, settings=None, eapi=None,
 		repo=None, slot=None, build_time=None, build_id=None,
 		file_size=None, mtime=None, db=None):
-		return _unicode.__new__(cls, cpv)
+		return str.__new__(cls, cpv)
 
 	def __init__(self, cpv, metadata=None, settings=None, eapi=None,
 		repo=None, slot=None, build_time=None, build_id=None,
 		file_size=None, mtime=None, db=None):
-		if not isinstance(cpv, _unicode):
-			# Avoid TypeError from _unicode.__init__ with PyPy.
+		if not isinstance(cpv, str):
+			# Avoid TypeError from str.__init__ with PyPy.
 			cpv = _unicode_decode(cpv)
-		_unicode.__init__(cpv)
+		str.__init__(cpv)
 		if metadata is not None:
 			self.__dict__['_metadata'] = metadata
 			slot = metadata.get('SLOT', slot)
@@ -438,7 +433,7 @@ class _pkg_str(_unicode):
 	def _long(var, default):
 		if var is not None:
 			try:
-				var = long(var)
+				var = int(var)
 			except ValueError:
 				if var:
 					var = -1

commit af100c65ebf7fd84307a84819602a934ebb0741c

Remove outdated mention of Python 2 from comment
Fixes: 5e9fe0f2a ("Eliminate basestring/long/_unicode py3 compat")
Signed-off-by: Matt Turner <mattst88@gentoo.org>
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/versions.py
@@ -341,11 +341,11 @@ def catpkgsplit(mydata, silent=1, eapi=None):
 
 class _pkg_str(str):
 	"""
-	This class represents a cpv. It inherits from str (unicode in python2) and
-	has attributes that cache results for use by functions like catpkgsplit and
-	cpv_getkey which are called frequently (especially in match_from_list).
-	Instances are typically created in dbapi.cp_list() or the Atom contructor,
-	and propagate from there. Generally, code that pickles these objects will
+	This class represents a cpv. It inherits from str and has attributes
+	that cache results for use by functions like catpkgsplit and cpv_getkey
+	which are called frequently (especially in match_from_list).  Instances
+	are typically created in dbapi.cp_list() or the Atom contructor, and
+	propagate from there. Generally, code that pickles these objects will
 	manually convert them to a plain unicode object first.
 
 	Instances of this class will have missing attributes for metadata that