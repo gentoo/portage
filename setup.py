gentoo/portage

commit c495b6f6745317f2b4c8d51b64ddfcc328806f3e

setup.py: prefix repos.conf paths for venv
Prefix repos.conf location and sync-openpgp-key-path values for venv. There
is no intention to do this for normal EPREFIX. This behavior is intended
only when create_entry_points is True, so that if emerge --sync runs in a
venv, the operation will not accidentally modify paths outside of sys.prefix.

Signed-off-by: Zac Medico <zmedico@gentoo.org>

setup.py
@@ -423,6 +423,22 @@ class x_install_data(install_data):
 			('subst_paths', 'paths'))
 
 	def run(self):
+		def re_sub_file(path, pattern, repl):
+			print('Rewriting %s' % path)
+			with codecs.open(path, 'r', 'utf-8') as f:
+				data = f.read()
+			data = re.sub(pattern, repl, data, flags=re.MULTILINE)
+			with codecs.open(path, 'w', 'utf-8') as f:
+				f.write(data)
+
+		if create_entry_points:
+			# Prefix location and sync-openpgp-key-path for venv. There
+			# is no intention to do this for normal EPREFIX. This behavior
+			# is intended only when create_entry_points is True, so that
+			# if emerge --sync runs in a venv, the operation will not
+			# accidentally modify paths outside of sys.prefix.
+			re_sub_file('cnf/repos.conf', r'= /', '= {}/'.format(eprefix))
+
 		self.run_command('build_man')
 
 		def process_data_files(df):

commit 6ca34493df931d858ee1e568d9c7cf3a15ec6ab7

setup.py: prefix paths in repos.conf and make.globals for pip venv install
Prefix paths in repos.conf and make.globals for pip venv install so
that that default paths will all be located inside the venv. The
EPREFIX value must be symbolic, since it cannot be known until
runtime.

Fixes: c495b6f67453 ("setup.py: prefix repos.conf paths for venv")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

setup.py
@@ -429,6 +429,10 @@ class x_install_data(install_data):
 			with codecs.open(path, 'w', 'utf-8') as f:
 				f.write(data)
 
+		if create_entry_points:
+			re_sub_file('cnf/repos.conf', r'= /', '= %(EPREFIX)s/')
+			re_sub_file('cnf/make.globals', r'DIR="/', 'DIR="${EPREFIX}/')
+
 		self.run_command('build_man')
 
 		def process_data_files(df):