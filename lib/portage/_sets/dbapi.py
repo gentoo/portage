a0ff6013f28d181fa97177f98a871d520e96a950
Dave Hughes <davidhughes205@gmail.com>
2021-04-08 20:59:07 -0400

    lib/portage/_sets/dbapi.py: add glob support to exclude-files parameter cnf/sets/portage.conf: add exclude-files=/usr/src/linux* in module-rebuild set
    Signed-off-by: Dave Hughes <davidhughes205@gmail.com>

--- lib/portage/_sets/dbapi.py
+++ lib/portage/_sets/dbapi.py
@@ -79,6 +79,12 @@ class OwnerSet(PackageSet):
 				glob.iglob(os.path.join(eroot, p.lstrip(os.sep))))
 		paths = expanded_paths
 
+		expanded_exclude_paths = []
+		for p in exclude_paths:
+			expanded_exclude_paths.extend(expanded_exc_p[len(eroot)-1:] for expanded_exc_p in
+				glob.iglob(os.path.join(eroot, p.lstrip(os.sep))))
+		exclude_paths = expanded_exclude_paths
+
 		pkg_str = vardb._pkg_str
 		if exclude_paths is None:
 			for link, p in vardb._owners.iter_owners(paths):
