a429f74ca669d42cdb5d6e15e4aceb5c8b426940
Zac Medico <zmedico@gentoo.org>
2021-05-24 00:33:37 -0700

    Updates for portage-3.0.19 release
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- setup.py
+++ setup.py
@@ -719,7 +719,7 @@ class build_ext(_build_ext):
 
 setup(
 	name = 'portage',
-	version = '3.0.18',
+	version = '3.0.19',
 	url = 'https://wiki.gentoo.org/wiki/Project:Portage',
 	project_urls = {
 		'Release Notes': 'https://gitweb.gentoo.org/proj/portage.git/plain/RELEASE-NOTES',
