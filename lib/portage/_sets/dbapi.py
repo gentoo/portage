gentoo/portage

commit 38d3ff6abba53f664e52d0bfafff5ab49d052bb8

OwnerSet: handle missing or empty exclude-files parameter (bug 796584)
Fixes: f55156d167a6 ("lib/portage/_sets/dbapi.py: add glob support to exclude-files parameter cnf/sets/portage.conf: add exclude-files=/usr/src/linux* in module-rebuild set")
Bug: https://bugs.gentoo.org/796584
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/_sets/dbapi.py
@@ -80,13 +80,13 @@ class OwnerSet(PackageSet):
 		paths = expanded_paths
 
 		expanded_exclude_paths = []
-		for p in exclude_paths:
+		for p in (exclude_paths or ()):
 			expanded_exclude_paths.extend(expanded_exc_p[len(eroot)-1:] for expanded_exc_p in
 				glob.iglob(os.path.join(eroot, p.lstrip(os.sep))))
 		exclude_paths = expanded_exclude_paths
 
 		pkg_str = vardb._pkg_str
-		if exclude_paths is None:
+		if exclude_paths:
 			for link, p in vardb._owners.iter_owners(paths):
 				pkg = pkg_str(link.mycpv, None)
 				rValue.add("%s:%s" % (pkg.cp, pkg.slot))

commit 693f6bf5a54e2424e2ad49e1838b61f76bf78e40

OwnerSet: fix inverted exclude_paths condition
Bug: https://bugs.gentoo.org/796584
Fixes: 38d3ff6abba5 ("OwnerSet: handle missing or empty exclude-files parameter (bug 796584)")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/_sets/dbapi.py
@@ -86,7 +86,7 @@ class OwnerSet(PackageSet):
 		exclude_paths = expanded_exclude_paths
 
 		pkg_str = vardb._pkg_str
-		if exclude_paths:
+		if not exclude_paths:
 			for link, p in vardb._owners.iter_owners(paths):
 				pkg = pkg_str(link.mycpv, None)
 				rValue.add("%s:%s" % (pkg.cp, pkg.slot))