gentoo/portage

commit f55156d167a651229518b226e1e064fee47b73a2

lib/portage/_sets/dbapi.py: add glob support to exclude-files parameter cnf/sets/portage.conf: add exclude-files=/usr/src/linux* in module-rebuild set
Closes: https://github.com/gentoo/portage/pull/694
Signed-off-by: Dave Hughes <davidhughes205@gmail.com>
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/_sets/dbapi.py
@@ -79,6 +79,12 @@ class OwnerSet(PackageSet):
 				glob.iglob(os.path.join(eroot, p.lstrip(os.sep))))
 		paths = expanded_paths
 
+		expanded_exclude_paths = []
+		for p in exclude_paths:
+			expanded_exclude_paths.extend(expanded_exc_p[len(eroot)-1:] for expanded_exc_p in
+				glob.iglob(os.path.join(eroot, p.lstrip(os.sep))))
+		exclude_paths = expanded_exclude_paths
+
 		pkg_str = vardb._pkg_str
 		if exclude_paths is None:
 			for link, p in vardb._owners.iter_owners(paths):

cnf/sets/portage.conf
@@ -71,6 +71,7 @@ includes = bzr cvs darcs git-2 git-r3 golang-vcs mercurial subversion
 [module-rebuild]
 class = portage.sets.dbapi.OwnerSet
 files = /lib/modules
+exclude-files = /usr/src/linux*
 
 # Installed packages that own files inside /usr/lib/xorg/modules,
 # excluding the package that owns /usr/bin/Xorg.

commit 38d3ff6abba53f664e52d0bfafff5ab49d052bb8

OwnerSet: handle missing or empty exclude-files parameter (bug 796584)
Fixes: f55156d167a6 ("lib/portage/_sets/dbapi.py: add glob support to exclude-files parameter cnf/sets/portage.conf: add exclude-files=/usr/src/linux* in module-rebuild set")
Bug: https://bugs.gentoo.org/796584
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/_sets/dbapi.py
@@ -80,13 +80,13 @@ class OwnerSet(PackageSet):
 		paths = expanded_paths
 
 		expanded_exclude_paths = []
-		for p in exclude_paths:
+		for p in (exclude_paths or ()):
 			expanded_exclude_paths.extend(expanded_exc_p[len(eroot)-1:] for expanded_exc_p in
 				glob.iglob(os.path.join(eroot, p.lstrip(os.sep))))
 		exclude_paths = expanded_exclude_paths
 
 		pkg_str = vardb._pkg_str
-		if exclude_paths is None:
+		if exclude_paths:
 			for link, p in vardb._owners.iter_owners(paths):
 				pkg = pkg_str(link.mycpv, None)
 				rValue.add("%s:%s" % (pkg.cp, pkg.slot))