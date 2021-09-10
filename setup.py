7af41cc626a146d370cbb67f4f72f64a552062de
Michał Górny <mgorny@gentoo.org>
2021-09-10 08:02:07 +0200

    Prepare for 3.0.23 release
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- setup.py
+++ setup.py
@@ -783,7 +783,7 @@ def get_data_files(regular_files, venv_files):
 
 setup(
 	name = 'portage',
-	version = '3.0.22',
+	version = '3.0.23',
 	url = 'https://wiki.gentoo.org/wiki/Project:Portage',
 	project_urls = {
 		'Release Notes': 'https://gitweb.gentoo.org/proj/portage.git/plain/RELEASE-NOTES',
