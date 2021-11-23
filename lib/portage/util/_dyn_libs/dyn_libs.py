gentoo/portage

commit cba2156dba89a22f2858238013469b4d80208854

portage/util/_dyn_libs/dyn_libs.py: add helper
Needed for binpkg/image verification commits coming...

Bug: https://bugs.gentoo.org/811462
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/util/_dyn_libs/dyn_libs.py
@@ -1,0 +1,23 @@
+# Copyright 2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+import glob
+
+from portage.output import colorize
+
+
+def check_dyn_libs_inconsistent(directory, provides):
+    """Checks directory for whether any dynamic libraries were installed and
+    if PROVIDES corresponds."""
+
+    # Let's check if we've got inconsistent results.
+    # If we're installing dynamic libraries (.so files), we should
+    # really have a PROVIDES.
+    # (This is a complementary check at the point of ingestion for the
+    # creation check in doebuild.py)
+    # Note: we could check a non-empty PROVIDES against the list of .sos,
+    # but this doesn't gain us anything. We're interested in failure
+    # to properly parse the installed files at all, which should really
+    # be a global problem (e.g. bug #811462)
+    installed_dynlibs = glob.glob(directory + "/**/*.so", recursive=True)
+    return installed_dynlibs and not provides

commit 3b3503bb2769b390e22a4fea42fa56fe4db4ddec

portage/util/_dyn_libs/dyn_libs.py: fix symlink recursion issue
Python's glob.glob() function follows symlinks recursively. Use
os.walk() instead.

Also re-order the operands to short-circuit the directory walk when
PROVIDES is not empty.

Bug: https://bugs.gentoo.org/826982
Fixes: cba2156dba89a22f2858238013469b4d80208854
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/util/_dyn_libs/dyn_libs.py
@@ -1,7 +1,15 @@
 # Copyright 2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-import glob
+import os
+
+
+def installed_dynlibs(directory):
+    for _dirpath, _dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith(".so"):
+                return True
+    return False
 
 
 def check_dyn_libs_inconsistent(directory, provides):
@@ -17,5 +25,4 @@ def check_dyn_libs_inconsistent(directory, provides):
     # but this doesn't gain us anything. We're interested in failure
     # to properly parse the installed files at all, which should really
     # be a global problem (e.g. bug #811462)
-    installed_dynlibs = glob.glob(directory + "/**/*.so", recursive=True)
-    return installed_dynlibs and not provides
+    return not provides and installed_dynlibs(directory)