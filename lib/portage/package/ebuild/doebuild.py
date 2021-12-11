gentoo/portage

commit 98a8a4af2c77477c327b22621b8a0aed6d15f574

doebuild.py: check for inconsistent PROVIDES/image post-src_install
This is part of a series of fixes for the linked bug (failure
to preserve libraries in some situations).

At the point of installation (even if not merging), we need
to detect inconsistent metadata: PROVIDES should be populated
if we're installing any dynamic libraries. This suggests that
e.g. scanelf malfunctioned or some corruption occurred.

Bug: https://bugs.gentoo.org/811462
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -3,6 +3,7 @@
 
 __all__ = ["doebuild", "doebuild_environment", "spawn", "spawnebuild"]
 
+import glob
 import grp
 import gzip
 import errno
@@ -118,6 +119,7 @@ from portage.util.futures import asyncio
 from portage.util.futures.executor.fork import ForkExecutor
 from portage.util.path import first_existing
 from portage.util.socks5 import get_socks5_proxy
+from portage.util._dyn_libs.dyn_libs import check_dyn_libs_inconsistent
 from portage.versions import _pkgsplit
 from _emerge.BinpkgEnvExtractor import BinpkgEnvExtractor
 from _emerge.EbuildBuildDir import EbuildBuildDir
@@ -125,7 +127,6 @@ from _emerge.EbuildPhase import EbuildPhase
 from _emerge.EbuildSpawnProcess import EbuildSpawnProcess
 from _emerge.Package import Package
 from _emerge.RootConfig import RootConfig
-
 
 _unsandboxed_phases = frozenset(
     [
@@ -3091,6 +3092,16 @@ def _post_src_install_soname_symlinks(mysettings, out):
             errors="strict",
         ) as f:
             f.write(soname_deps.provides)
+    else:
+        if check_dyn_libs_inconsistent(image_dir, soname_deps.provides):
+            self._writemsg_level(
+                colorize(
+                    "BAD",
+                    "!!! Error! Installing dynamic libraries (.so) with blank PROVIDES!",
+                ),
+                noiselevel=-1,
+                level=logging.ERROR,
+            )
 
     if unrecognized_elf_files:
         qa_msg = ["QA Notice: Unrecognized ELF file(s):"]

commit 80a90acd3da4f24df4d6f66b070c785616c1ee34

lib/portage/package/ebuild/doebuild.py: fix undefined self
Fixes: 98a8a4af2c77477c327b22621b8a0aed6d15f574
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -3096,7 +3096,7 @@ def _post_src_install_soname_symlinks(mysettings, out):
             f.write(soname_deps.provides)
     else:
         if check_dyn_libs_inconsistent(image_dir, soname_deps.provides):
-            self._writemsg_level(
+            portage.util._writemsg_level(
                 colorize(
                     "BAD",
                     "!!! Error! Installing dynamic libraries (.so) with blank PROVIDES!",