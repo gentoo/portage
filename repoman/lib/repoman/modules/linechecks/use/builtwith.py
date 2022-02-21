283ab62c7c25ae2b5f2aed8484207c6e913f9789
Matt Turner <mattst88@gentoo.org>
2022-02-21 12:59:34 -0800

    repoman: Remove
    RepoMan sez: So long and thanks for all the fish! I'll be enjoying my
                 retirement now. Haven't I done enough to deserve that?
    
    Bug: https://bugs.gentoo.org/835013
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- repoman/lib/repoman/modules/linechecks/use/builtwith.py
+++
@@ -1,9 +0,0 @@
-import re  # pylint: disable=unused-import
-
-from repoman.modules.linechecks.base import LineCheck
-
-
-class BuiltWithUse(LineCheck):
-    repoman_check_name = "ebuild.minorsyn"
-    re = re.compile(r"(^|.*\b)built_with_use\b")
-    error = "BUILT_WITH_USE"
