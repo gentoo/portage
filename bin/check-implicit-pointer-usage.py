gentoo/portage

commit 41f4f6d25019c4f944b8d9576d96482f92231fda

Use '/usr/bin/env python' shebangs
Signed-off-by: John Helmert III <ajak@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

bin/archive-conf
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/binhost-snapshot
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 2010-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/check-implicit-pointer-usage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 
 # Ripped from HP and updated from Debian
 # Update by Gentoo to support unicode output

bin/chmod-lite.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/chpathtool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 2011-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/clean_locks
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/dispatch-conf
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/dohtml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -b
+#!/usr/bin/env python -b
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

commit f60baab89a05125d3763d4ea13c0a689cb6a96ff

Drop -b from shebangs
The switch to using '/usr/bin/env python' shebangs doesn't naively
work with arguments after 'python', so the shebangs were broken. '-b'
is of dubious necessity anyway, so let's just drop it.

Fixes: 41f4f6d25019c4f944b8d9576d96482f92231fda
Signed-off-by: John Helmert III <ajak@gentoo.org>

bin/archive-conf
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/binhost-snapshot
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2010-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/check-implicit-pointer-usage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 
 # Ripped from HP and updated from Debian
 # Update by Gentoo to support unicode output

bin/chmod-lite.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2015 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/chpathtool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2011-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/clean_locks
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/dispatch-conf
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/dohtml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/doins.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2017-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 #

bin/ebuild
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/ebuild-ipc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2010-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 #

bin/egencache
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2009-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/emaint
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2005-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/emerge
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2006-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/emirrordist
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2013-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/env-update
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/filter-bash-environment.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/fixpackages
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/glsa-check
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/gpkg-helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/install.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2013-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/lock-helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2010-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/portageq
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/quickpkg
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/regenworld
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/shelve-utils
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2020-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

bin/xattr-helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2012-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

bin/xpak-helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2009-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 

lib/portage/util/changelog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -b
+#!/usr/bin/env python
 # Copyright 2009-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 

lib/portage/tests/runTests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python -bWd
+#!/usr/bin/env python -Wd
 # runTests.py -- Portage Unit Test Functionality
 # Copyright 2006-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2