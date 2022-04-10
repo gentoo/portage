gentoo/portage

commit 7c8875c38b187fbd2e0fa5fc39bbb38c1588d0fb

Begin modernizing the string usage
These files are updated to use the newer f-string feature (String
Interpolation) that has been added since Python 3.6.

The benefits to using String Interpolation are:
* Avoids incurring bugs from using Old Style formatting.
* Makes it easier to understand how the string is being formatted.
* Using embedded expressions into strings removes the need for string
concatenation and will execute much faster for the same effect.

Also, a few global string constants with Old Style formatting had their
literals inlined to their respective function arguments while
transitioning them be f-strings. When Python searches for variables, it
looks them up in the local namespace first, which is the namespace
specific a function or class method. If not found, then it searches in
the global namespace. Removing the need for global strings will also
improve performance a bit along the way.

The Portage codebase as a whole still needs this kind of work.

Signed-off-by: Kenneth Raplee <kenrap@kennethraplee.com>
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dispatch_conf.py
@@ -25,9 +25,6 @@ RCS_BRANCH = "1.1.1"
 RCS_LOCK = "rcs -ko -M -l"
 RCS_PUT = 'ci -t-"Archived config file." -m"dispatch-conf update."'
 RCS_GET = "co"
-RCS_MERGE = "rcsmerge -p -r" + RCS_BRANCH + " '%s' > '%s'"
-
-DIFF3_MERGE = "diff3 -mE '%s' '%s' '%s' > '%s'"
 _ARCHIVE_ROTATE_MAX = 9
 
 
@@ -75,18 +72,18 @@ def diff_mixed(func, file1, file2):
 
                 if tempdir is None:
                     tempdir = tempfile.mkdtemp()
-                diff_files[i] = os.path.join(tempdir, "%d" % i)
+                diff_files[i] = os.path.join(tempdir, f"{i}")
                 if st is None:
                     content = "/dev/null\n"
                 elif stat.S_ISLNK(st.st_mode):
                     link_dest = os.readlink(files[i])
-                    content = "SYM: %s -> %s\n" % (file1, link_dest)
+                    content = f"SYM: {file1} -> {link_dest}\n"
                 elif stat.S_ISDIR(st.st_mode):
-                    content = "DIR: %s\n" % (file1,)
+                    content = f"DIR: {file1}\n"
                 elif stat.S_ISFIFO(st.st_mode):
-                    content = "FIF: %s\n" % (file1,)
+                    content = f"FIF: {file1}\n"
                 else:
-                    content = "DEV: %s\n" % (file1,)
+                    content = f"DEV: {file1}\n"
                 with io.open(
                     diff_files[i], mode="w", encoding=_encodings["stdio"]
                 ) as f:
@@ -124,10 +121,7 @@ def read_config(mandatory_opts):
     loader = KeyValuePairFileLoader(config_path, None)
     opts, _errors = loader.load()
     if not opts:
-        print(
-            _("dispatch-conf: Error reading {}; fatal").format(config_path),
-            file=sys.stderr,
-        )
+        print(_(f"dispatch-conf: Error reading {config_path}; fatal"), file=sys.stderr)
         sys.exit(1)
 
     # Handle quote removal here, since KeyValuePairFileLoader doesn't do that.
@@ -143,9 +137,8 @@ def read_config(mandatory_opts):
             else:
                 print(
                     _(
-                        'dispatch-conf: Missing option "%s" in /etc/dispatch-conf.conf; fatal'
-                    )
-                    % (key,),
+                        f'dispatch-conf: Missing option "{key}" in /etc/dispatch-conf.conf; fatal'
+                    ),
                     file=sys.stderr,
                 )
 
@@ -160,8 +153,9 @@ def read_config(mandatory_opts):
         os.chmod(opts["archive-dir"], 0o700)
     elif not os.path.isdir(opts["archive-dir"]):
         print(
-            _("dispatch-conf: Config archive dir [%s] must exist; fatal")
-            % (opts["archive-dir"],),
+            _(
+                rf"""dispatch-conf: Config archive dir [{opts["archive-dir"]}] must exist; fatal"""
+            ),
             file=sys.stderr,
         )
         sys.exit(1)
@@ -195,11 +189,7 @@ def _archive_copy(src_st, src_path, dest_path):
             shutil.copy2(src_path, dest_path)
     except EnvironmentError as e:
         portage.util.writemsg(
-            _(
-                "dispatch-conf: Error copying %(src_path)s to "
-                "%(dest_path)s: %(reason)s\n"
-            )
-            % {"src_path": src_path, "dest_path": dest_path, "reason": e},
+            f"dispatch-conf: Error copying {src_path} to {dest_path}: {e}\n",
             noiselevel=-1,
         )
 
@@ -226,9 +216,9 @@ def rcs_archive(archive, curconf, newconf, mrgconf):
     ):
         _archive_copy(curconf_st, curconf, archive)
 
-    if os.path.lexists(archive + ",v"):
-        os.system(RCS_LOCK + " " + archive)
-    os.system(RCS_PUT + " " + archive)
+    if os.path.lexists(f"{archive},v"):
+        os.system(f"{RCS_LOCK} {archive}")
+    os.system(f"{RCS_PUT} {archive}")
 
     ret = 0
     mystat = None
@@ -241,17 +231,17 @@ def rcs_archive(archive, curconf, newconf, mrgconf):
     if mystat is not None and (
         stat.S_ISREG(mystat.st_mode) or stat.S_ISLNK(mystat.st_mode)
     ):
-        os.system(RCS_GET + " -r" + RCS_BRANCH + " " + archive)
+        os.system(f"{RCS_GET} -r{RCS_BRANCH} {archive}")
         has_branch = os.path.lexists(archive)
         if has_branch:
-            os.rename(archive, archive + ".dist")
+            os.rename(archive, f"{archive}.dist")
 
         _archive_copy(mystat, newconf, archive)
 
         if has_branch:
             if mrgconf and os.path.isfile(archive) and os.path.isfile(mrgconf):
                 # This puts the results of the merge into mrgconf.
-                ret = os.system(RCS_MERGE % (archive, mrgconf))
+                ret = os.system(f"rcsmerge -p -r{RCS_BRANCH} '{archive}' > '{mrgconf}'")
                 os.chmod(mrgconf, mystat.st_mode)
                 os.chown(mrgconf, mystat.st_uid, mystat.st_gid)
         os.rename(archive, archive + ".dist.new")
@@ -273,8 +263,7 @@ def _file_archive_rotate(archive):
         for max_suf, max_st, max_path in (
             (suf, os.lstat(path), path)
             for suf, path in (
-                (suf, "%s.%s" % (archive, suf))
-                for suf in range(1, _ARCHIVE_ROTATE_MAX + 1)
+                (suf, f"{archive}.{suf}") for suf in range(1, _ARCHIVE_ROTATE_MAX + 1)
             )
         ):
             pass
@@ -290,7 +279,7 @@ def _file_archive_rotate(archive):
             # Removing a directory might destroy something important,
             # so rename it instead.
             head, tail = os.path.split(archive)
-            placeholder = tempfile.NamedTemporaryFile(prefix="%s." % tail, dir=head)
+            placeholder = tempfile.NamedTemporaryFile(prefix=f"{tail}.", dir=head)
             placeholder.close()
             os.rename(max_path, placeholder.name)
         else:
@@ -300,9 +289,9 @@ def _file_archive_rotate(archive):
         max_suf -= 1
 
     for suf in range(max_suf + 1, 1, -1):
-        os.rename("%s.%s" % (archive, suf - 1), "%s.%s" % (archive, suf))
+        os.rename(f"{archive}.{suf - 1}", f"{archive}.{suf}")
 
-    os.rename(archive, "%s.1" % (archive,))
+    os.rename(archive, f"{archive}.1")
 
 
 def _file_archive_ensure_dir(parent_dir):
@@ -347,7 +336,7 @@ def file_archive(archive, curconf, newconf, mrgconf):
     # Archive the current config file if it isn't already saved
     if (
         os.path.lexists(archive)
-        and len(diffstatusoutput_mixed("diff -aq '%s' '%s'", curconf, archive)[1]) != 0
+        and len(diffstatusoutput_mixed(f"diff -aq '{curconf}' '{archive}'")[1]) != 0
     ):
         _file_archive_rotate(archive)
 
@@ -372,7 +361,7 @@ def file_archive(archive, curconf, newconf, mrgconf):
         stat.S_ISREG(mystat.st_mode) or stat.S_ISLNK(mystat.st_mode)
     ):
         # Save off new config file in the archive dir with .dist.new suffix
-        newconf_archive = archive + ".dist.new"
+        newconf_archive = f"{archive}.dist.new"
         if os.path.isdir(newconf_archive) and not os.path.islink(newconf_archive):
             _file_archive_rotate(newconf_archive)
         _archive_copy(mystat, newconf, newconf_archive)
@@ -382,11 +371,11 @@ def file_archive(archive, curconf, newconf, mrgconf):
             mrgconf
             and os.path.isfile(curconf)
             and os.path.isfile(newconf)
-            and os.path.isfile(archive + ".dist")
+            and os.path.isfile(f"{archive}.dist")
         ):
             # This puts the results of the merge into mrgconf.
             ret = os.system(
-                DIFF3_MERGE % (curconf, archive + ".dist", newconf, mrgconf)
+                f"diff3 -mE '{curconf}' '{archive}.dist' '{newconf}' > '{mrgconf}'"
             )
             os.chmod(mrgconf, mystat.st_mode)
             os.chown(mrgconf, mystat.st_uid, mystat.st_gid)
@@ -397,24 +386,24 @@ def file_archive(archive, curconf, newconf, mrgconf):
 def rcs_archive_post_process(archive):
     """Check in the archive file with the .dist.new suffix on the branch
     and remove the one with the .dist suffix."""
-    os.rename(archive + ".dist.new", archive)
-    if os.path.lexists(archive + ".dist"):
+    os.rename(f"{archive}.dist.new", archive)
+    if os.path.lexists(f"{archive}.dist"):
         # Commit the last-distributed version onto the branch.
-        os.system(RCS_LOCK + RCS_BRANCH + " " + archive)
-        os.system(RCS_PUT + " -r" + RCS_BRANCH + " " + archive)
-        os.unlink(archive + ".dist")
+        os.system(f"{RCS_LOCK}{RCS_BRANCH} {archive}")
+        os.system(f"{RCS_PUT} -r{RCS_BRANCH} {archive}")
+        os.unlink(f"{archive}.dist")
     else:
         # Forcefully commit the last-distributed version onto the branch.
-        os.system(RCS_PUT + " -f -r" + RCS_BRANCH + " " + archive)
+        os.system(f"{RCS_PUT} -f -r{RCS_BRANCH} {archive}")
 
 
 def file_archive_post_process(archive):
     """Rename the archive file with the .dist.new suffix to a .dist suffix"""
-    if os.path.lexists(archive + ".dist.new"):
-        dest = "%s.dist" % archive
+    if os.path.lexists(f"{archive}.dist.new"):
+        dest = f"{archive}.dist"
         if os.path.isdir(dest) and not os.path.islink(dest):
             _file_archive_rotate(dest)
-        os.rename(archive + ".dist.new", dest)
+        os.rename(f"{archive}.dist.new", dest)
 
 
 def perform_conf_update_hooks(kind, conf):

lib/_emerge/main.py
@@ -81,7 +81,7 @@ shortmapping = {
 
 COWSAY_MOO = r"""
 
-  Larry loves Gentoo (%s)
+  Larry loves Gentoo ({})
 
  _______________________
 < Have you mooed today? >
@@ -97,7 +97,7 @@ COWSAY_MOO = r"""
 
 def multiple_actions(action1, action2):
     sys.stderr.write("\n!!! Multiple actions requested... Please choose one only.\n")
-    sys.stderr.write("!!! '%s' or '%s'\n\n" % (action1, action2))
+    sys.stderr.write(f"!!! '{action1}' or '{action2}'\n\n")
     sys.exit(1)
 
 
@@ -745,7 +745,7 @@ def parse_opts(tmpcmdline, silent=False):
 
     for action_opt in actions:
         parser.add_argument(
-            "--" + action_opt,
+            f"--{action_opt}",
             action="store_true",
             dest=action_opt.replace("-", "_"),
             default=False,
@@ -759,7 +759,7 @@ def parse_opts(tmpcmdline, silent=False):
         )
     for shortopt, longopt in shortmapping.items():
         parser.add_argument(
-            "-" + shortopt,
+            f"-{shortopt}",
             action="store_true",
             dest=longopt.lstrip("--").replace("-", "_"),
             default=False,
@@ -832,9 +832,9 @@ def parse_opts(tmpcmdline, silent=False):
     if myoptions.buildpkg_exclude:
         bad_atoms = _find_bad_atoms(myoptions.buildpkg_exclude, less_strict=True)
         if bad_atoms and not silent:
+            invalid_atoms = ",".join(bad_atoms)
             parser.error(
-                "Invalid Atom(s) in --buildpkg-exclude parameter: '%s'\n"
-                % (",".join(bad_atoms),)
+                f"Invalid Atom(s) in --buildpkg-exclude parameter: '{invalid_atoms}'\n"
             )
 
     if myoptions.changed_deps is not None:
@@ -1014,7 +1014,7 @@ def parse_opts(tmpcmdline, silent=False):
             backtrack = None
             if not silent:
                 parser.error(
-                    "Invalid --backtrack parameter: '%s'\n" % (myoptions.backtrack,)
+                    f"Invalid --backtrack parameter: '{myoptions.backtrack}'\n"
                 )
 
         myoptions.backtrack = backtrack
@@ -1032,7 +1032,7 @@ def parse_opts(tmpcmdline, silent=False):
         if deep is not True and deep < 0:
             deep = None
             if not silent:
-                parser.error("Invalid --deep parameter: '%s'\n" % (myoptions.deep,))
+                parser.error(f"Invalid --deep parameter: '{myoptions.deep}'\n")
 
         myoptions.deep = deep
 
@@ -1049,7 +1049,7 @@ def parse_opts(tmpcmdline, silent=False):
         if jobs is not True and jobs < 1:
             jobs = None
             if not silent:
-                parser.error("Invalid --jobs parameter: '%s'\n" % (myoptions.jobs,))
+                parser.error(f"Invalid --jobs parameter: '{myoptions.jobs}'\n")
 
         myoptions.jobs = jobs
 
@@ -1066,8 +1066,7 @@ def parse_opts(tmpcmdline, silent=False):
             load_average = None
             if not silent:
                 parser.error(
-                    "Invalid --load-average parameter: '%s'\n"
-                    % (myoptions.load_average,)
+                    f"Invalid --load-average parameter: '{myoptions.load_average}'\n"
                 )
 
         myoptions.load_average = load_average
@@ -1082,8 +1081,7 @@ def parse_opts(tmpcmdline, silent=False):
             rebuilt_binaries_timestamp = 0
             if not silent:
                 parser.error(
-                    "Invalid --rebuilt-binaries-timestamp parameter: '%s'\n"
-                    % (myoptions.rebuilt_binaries_timestamp,)
+                    f"Invalid --rebuilt-binaries-timestamp parameter: '{myoptions.rebuilt_binaries_timestamp}'\n"
                 )
 
         myoptions.rebuilt_binaries_timestamp = rebuilt_binaries_timestamp
@@ -1093,14 +1091,12 @@ def parse_opts(tmpcmdline, silent=False):
             search_similarity = float(myoptions.search_similarity)
         except ValueError:
             parser.error(
-                "Invalid --search-similarity parameter "
-                "(not a number): '{}'\n".format(myoptions.search_similarity)
+                f"Invalid --search-similarity parameter (not a number): '{myoptions.search_similarity}'\n"
             )
 
         if search_similarity < 0 or search_similarity > 100:
             parser.error(
-                "Invalid --search-similarity parameter "
-                "(not between 0 and 100): '{}'\n".format(myoptions.search_similarity)
+                f"Invalid --search-similarity parameter (not between 0 and 100): '{myoptions.search_similarity}'\n"
             )
 
         myoptions.search_similarity = search_similarity
@@ -1181,7 +1177,7 @@ def profile_check(trees, myaction):
             "--help, --info, --search, --sync, and --version."
         )
         writemsg_level(
-            "".join("!!! %s\n" % l for l in textwrap.wrap(msg, 70)),
+            "".join(f"!!! {l}\n" for l in textwrap.wrap(msg, 70)),
             level=logging.ERROR,
             noiselevel=-1,
         )
@@ -1203,7 +1199,7 @@ def emerge_main(args=None):
     try:
         locale.setlocale(locale.LC_ALL, "")
     except locale.Error as e:
-        writemsg_level("setlocale: %s\n" % e, level=logging.WARN)
+        writemsg_level(f"setlocale: {e}\n", level=logging.WARN)
 
     # Disable color until we're sure that it should be enabled (after
     # EMERGE_DEFAULT_OPTS has been parsed).
@@ -1234,7 +1230,7 @@ def emerge_main(args=None):
         emerge_help()
         return os.EX_OK
     if myaction == "moo":
-        print(COWSAY_MOO % platform.system())
+        print(COWSAY_MOO.format(platform.system()))
         return os.EX_OK
     if myaction == "sync":
         # need to set this to True now in order for the repository config
@@ -1311,7 +1307,7 @@ def emerge_main(args=None):
     try:
         locale.setlocale(locale.LC_ALL, "")
     except locale.Error as e:
-        writemsg_level("setlocale: %s\n" % e, level=logging.WARN)
+        writemsg_level(f"setlocale: {e}\n", level=logging.WARN)
 
     tmpcmdline = []
     if "--ignore-default-opts" not in myopts:

lib/portage/__init__.py
@@ -48,7 +48,7 @@ except ImportError as e:
     sys.stderr.write(
         "!!! gone wrong. Here is the information we got for this exception:\n"
     )
-    sys.stderr.write("    " + str(e) + "\n\n")
+    sys.stderr.write(f"    {e}\n\n")
     raise
 
 try:
@@ -93,7 +93,7 @@ try:
         + "doebuild_environment,spawn,spawnebuild",
         "portage.package.ebuild.config:autouse,best_from_dict,"
         + "check_config_instance,config",
-        "portage.package.ebuild.deprecated_profile_check:" + "deprecated_profile_check",
+        "portage.package.ebuild.deprecated_profile_check:deprecated_profile_check",
         "portage.package.ebuild.digestcheck:digestcheck",
         "portage.package.ebuild.digestgen:digestgen",
         "portage.package.ebuild.fetch:fetch",
@@ -184,7 +184,7 @@ except ImportError as e:
         "!!! There is a README.RESCUE file that details the steps required to perform\n"
     )
     sys.stderr.write("!!! a recovery of portage.\n")
-    sys.stderr.write("    " + str(e) + "\n\n")
+    sys.stderr.write(f"    {e}\n\n")
     raise
 
 
@@ -409,7 +409,7 @@ try:
     _selinux_merge = _unicode_module_wrapper(_selinux, encoding=_encodings["merge"])
 except (ImportError, OSError) as e:
     if isinstance(e, OSError):
-        sys.stderr.write("!!! SELinux not loaded: %s\n" % str(e))
+        sys.stderr.write(f"!!! SELinux not loaded: {e}\n")
     del e
     _selinux = None
     selinux = None
@@ -482,10 +482,10 @@ def _shell_quote(s):
     """
     if _shell_quote_re.search(s) is None:
         return s
-    for letter in '\\"$`':
+    for letter in r"\"$`":
         if letter in s:
-            s = s.replace(letter, "\\" + letter)
-    return '"%s"' % s
+            s = s.replace(letter, rf"\{letter}")
+    return f'"{s}"'
 
 
 bsd_chflags = None
@@ -534,7 +534,7 @@ def abssymlink(symlink, target=None):
         mylink = os.readlink(symlink)
     if mylink[0] != "/":
         mydir = os.path.dirname(symlink)
-        mylink = mydir + "/" + mylink
+        mylink = f"{mydir}/{mylink}"
     return os.path.normpath(mylink)
 
 
@@ -597,7 +597,7 @@ def _parse_eapi_ebuild_head(f):
 def _movefile(src, dest, **kwargs):
     """Calls movefile and raises a PortageException if an error occurs."""
     if movefile(src, dest, **kwargs) is None:
-        raise portage.exception.PortageException("mv '%s' '%s'" % (src, dest))
+        raise portage.exception.PortageException(f"mv '{src}' '{dest}'")
 
 
 auxdbkeys = (
@@ -726,12 +726,11 @@ if VERSION == "HEAD":
                     BASH_BINARY,
                     "-c",
                     (
-                        "cd %s ; git describe --match 'portage-*' || exit $? ; "
-                        + 'if [ -n "`git diff-index --name-only --diff-filter=M HEAD`" ] ; '
-                        + "then echo modified ; git rev-list --format=%%ct -n 1 HEAD ; fi ; "
-                        + "exit 0"
-                    )
-                    % _shell_quote(PORTAGE_BASE_PATH),
+                        f"cd {_shell_quote(PORTAGE_BASE_PATH)} ; git describe --match 'portage-*' || exit $? ; "
+                        'if [ -n "`git diff-index --name-only --diff-filter=M HEAD`" ] ; '
+                        "then echo modified ; git rev-list --format=%%ct -n 1 HEAD ; fi ; "
+                        "exit 0"
+                    ),
                 ]
                 cmd = [
                     _unicode_encode(x, encoding=encoding, errors="strict") for x in cmd
@@ -750,7 +749,7 @@ if VERSION == "HEAD":
                             patchlevel = False
                             if len(version_split) > 2:
                                 patchlevel = True
-                                VERSION = "%s_p%s" % (VERSION, version_split[2])
+                                VERSION = f"{VERSION}_p{version_split[2]}"
                             if len(output_lines) > 1 and output_lines[1] == "modified":
                                 head_timestamp = None
                                 if len(output_lines) > 3:
@@ -765,8 +764,8 @@ if VERSION == "HEAD":
                                 ):
                                     timestamp = timestamp - head_timestamp
                                 if not patchlevel:
-                                    VERSION = "%s_p0" % (VERSION,)
-                                VERSION = "%s_p%d" % (VERSION, timestamp)
+                                    VERSION = f"{VERSION}_p0"
+                                VERSION = f"{VERSION}_p{timestamp}"
                             return VERSION
             VERSION = "HEAD"
             return VERSION

lib/portage/_global_updates.py
@@ -118,14 +118,14 @@ def _do_global_updates(trees, prev_mtimes, quiet=False, if_mtime_changed=True):
                 myupd.extend(valid_updates)
                 if not quiet:
                     writemsg_stdout(bold(mykey))
-                    writemsg_stdout(len(valid_updates) * "." + "\n")
+                    writemsg_stdout(f"{len(valid_updates) * '.'}\n")
                 if len(errors) == 0:
                     # Update our internal mtime since we
                     # processed all of our directives.
                     timestamps[mykey] = mystat[stat.ST_MTIME]
                 else:
                     for msg in errors:
-                        writemsg("%s\n" % msg, noiselevel=-1)
+                        writemsg(f"{msg}\n", noiselevel=-1)
             if myupd:
                 retupd = True
 

lib/portage/checksum.py
@@ -44,7 +44,7 @@ def _open_file(filename):
             _unicode_encode(filename, encoding=_encodings["fs"], errors="strict"), "rb"
         )
     except IOError as e:
-        func_call = "open('%s')" % _unicode_decode(filename)
+        func_call = f"open('{_unicode_decode(filename)}')"
         if e.errno == errno.EPERM:
             raise portage.exception.OperationNotPermitted(func_call)
         elif e.errno == errno.EACCES:
@@ -525,12 +525,11 @@ def verify_all(filename, mydict, calc_prelink=0, strict=0):
             if mydict[x] != myhash:
                 if strict:
                     raise portage.exception.DigestException(
-                        ("Failed to verify '$(file)s' on " + "checksum type '%(type)s'")
-                        % {"file": filename, "type": x}
+                        f"Failed to verify '{filename}' on checksum type '{x}'"
                     )
                 else:
                     file_is_ok = False
-                    reason = (("Failed on %s verification" % x), myhash, mydict[x])
+                    reason = (f"Failed on {x} verification", myhash, mydict[x])
                     break
 
     return file_is_ok, reason
@@ -578,8 +577,7 @@ def perform_checksum(filename, hashname="MD5", calc_prelink=0):
         try:
             if hashname not in hashfunc_keys:
                 raise portage.exception.DigestException(
-                    hashname
-                    + " hash function not available (needs dev-python/pycrypto)"
+                    f"{hashname} hash function not available (needs dev-python/pycrypto)"
                 )
             myhash, mysize = hashfunc_map[hashname].checksum_file(myfilename)
         except (OSError, IOError) as e:
@@ -618,8 +616,7 @@ def perform_multiple_checksums(filename, hashes=["MD5"], calc_prelink=0):
     for x in hashes:
         if x not in hashfunc_keys:
             raise portage.exception.DigestException(
-                x
-                + " hash function not available (needs dev-python/pycrypto or >=dev-lang/python-2.5)"
+                f"{x} hash function not available (needs dev-python/pycrypto)"
             )
         rVal[x] = perform_checksum(filename, x, calc_prelink)[0]
     return rVal
@@ -638,6 +635,6 @@ def checksum_str(data, hashname="MD5"):
     """
     if hashname not in hashfunc_keys:
         raise portage.exception.DigestException(
-            hashname + " hash function not available (needs dev-python/pycrypto)"
+            f"{hashname} hash function not available (needs dev-python/pycrypto)"
         )
     return hashfunc_map[hashname].checksum_str(data)

lib/portage/const.py
@@ -29,32 +29,32 @@ import sys
 
 # variables used with config_root (these need to be relative)
 USER_CONFIG_PATH = "etc/portage"
-BINREPOS_CONF_FILE = USER_CONFIG_PATH + "/binrepos.conf"
-MAKE_CONF_FILE = USER_CONFIG_PATH + "/make.conf"
-MODULES_FILE_PATH = USER_CONFIG_PATH + "/modules"
-CUSTOM_PROFILE_PATH = USER_CONFIG_PATH + "/profile"
-USER_VIRTUALS_FILE = USER_CONFIG_PATH + "/virtuals"
-EBUILD_SH_ENV_FILE = USER_CONFIG_PATH + "/bashrc"
-EBUILD_SH_ENV_DIR = USER_CONFIG_PATH + "/env"
-CUSTOM_MIRRORS_FILE = USER_CONFIG_PATH + "/mirrors"
-COLOR_MAP_FILE = USER_CONFIG_PATH + "/color.map"
-PROFILE_PATH = USER_CONFIG_PATH + "/make.profile"
-MAKE_DEFAULTS_FILE = PROFILE_PATH + "/make.defaults"  # FIXME: not used
-DEPRECATED_PROFILE_FILE = PROFILE_PATH + "/deprecated"
+BINREPOS_CONF_FILE = f"{USER_CONFIG_PATH}/binrepos.conf"
+MAKE_CONF_FILE = f"{USER_CONFIG_PATH}/make.conf"
+MODULES_FILE_PATH = f"{USER_CONFIG_PATH}/modules"
+CUSTOM_PROFILE_PATH = f"{USER_CONFIG_PATH}/profile"
+USER_VIRTUALS_FILE = f"{USER_CONFIG_PATH}/virtuals"
+EBUILD_SH_ENV_FILE = f"{USER_CONFIG_PATH}/bashrc"
+EBUILD_SH_ENV_DIR = f"{USER_CONFIG_PATH}/env"
+CUSTOM_MIRRORS_FILE = f"{USER_CONFIG_PATH}/mirrors"
+COLOR_MAP_FILE = f"{USER_CONFIG_PATH}/color.map"
+PROFILE_PATH = f"{USER_CONFIG_PATH}/make.profile"
+MAKE_DEFAULTS_FILE = f"{PROFILE_PATH}/make.defaults"  # FIXME: not used
+DEPRECATED_PROFILE_FILE = f"{PROFILE_PATH}/deprecated"
 
 # variables used with targetroot (these need to be absolute, but not
 # have a leading '/' since they are used directly with os.path.join on EROOT)
 VDB_PATH = "var/db/pkg"
 CACHE_PATH = "var/cache/edb"
 PRIVATE_PATH = "var/lib/portage"
-WORLD_FILE = PRIVATE_PATH + "/world"
-WORLD_SETS_FILE = PRIVATE_PATH + "/world_sets"
-CONFIG_MEMORY_FILE = PRIVATE_PATH + "/config"
+WORLD_FILE = f"{PRIVATE_PATH}/world"
+WORLD_SETS_FILE = f"{PRIVATE_PATH}/world_sets"
+CONFIG_MEMORY_FILE = f"{PRIVATE_PATH}/config"
 NEWS_LIB_PATH = "var/lib/gentoo"
 
 # these variables get EPREFIX prepended automagically when they are
 # translated into their lowercase variants
-DEPCACHE_PATH = "/var/cache/edb/dep"
+DEPCACHE_PATH = f"/{CACHE_PATH}/dep"
 GLOBAL_CONFIG_PATH = "/usr/share/portage/config"
 
 # these variables are not used with target_root or config_root
@@ -64,11 +64,11 @@ GLOBAL_CONFIG_PATH = "/usr/share/portage/config"
 # fmt:off
 PORTAGE_BASE_PATH = os.path.join(os.sep, os.sep.join(os.path.realpath(__file__.rstrip("co")).split(os.sep)[:-3]))
 # fmt:on
-PORTAGE_BIN_PATH = PORTAGE_BASE_PATH + "/bin"
+PORTAGE_BIN_PATH = f"{PORTAGE_BASE_PATH}/bin"
 PORTAGE_PYM_PATH = os.path.realpath(os.path.join(__file__, "../.."))
-LOCALE_DATA_PATH = PORTAGE_BASE_PATH + "/locale"  # FIXME: not used
-EBUILD_SH_BINARY = PORTAGE_BIN_PATH + "/ebuild.sh"
-MISC_SH_BINARY = PORTAGE_BIN_PATH + "/misc-functions.sh"
+LOCALE_DATA_PATH = f"{PORTAGE_BASE_PATH}/locale"  # FIXME: not used
+EBUILD_SH_BINARY = f"{PORTAGE_BIN_PATH}/ebuild.sh"
+MISC_SH_BINARY = f"{PORTAGE_BIN_PATH}/misc-functions.sh"
 SANDBOX_BINARY = "/usr/bin/sandbox"
 FAKEROOT_BINARY = "/usr/bin/fakeroot"
 BASH_BINARY = "/bin/bash"
@@ -78,7 +78,7 @@ PRELINK_BINARY = "/usr/sbin/prelink"
 INVALID_ENV_FILE = "/etc/spork/is/not/valid/profile.env"
 MERGING_IDENTIFIER = "-MERGING-"
 REPO_NAME_FILE = "repo_name"
-REPO_NAME_LOC = "profiles" + "/" + REPO_NAME_FILE
+REPO_NAME_LOC = f"profiles/{REPO_NAME_FILE}"
 
 PORTAGE_PACKAGE_ATOM = "sys-apps/portage"
 LIBC_PACKAGE_ATOM = "virtual/libc"

lib/portage/data.py
@@ -78,7 +78,7 @@ def _target_root():
         # Handle either empty or unset ROOT.
         root = os.sep
     root = portage.util.normalize_path(root)
-    return root.rstrip(os.sep) + os.sep
+    return f"{root.rstrip(os.sep)}{os.sep}"
 
 
 def portage_group_warning():

commit caf5ceea15e73bdd2681d86f522da6f715df65ff

dispatch-conf: fix diffstatusoutput_mixed() usage
This function should accept a command placeholder and two arguments.

The command placeholder is used in 2 places:
1. User config: dispatch-conf.conf
2. Inline in portage.dispatch_conf.file_archive()

We need to maintain the command placeholder format becaused of usage 1.

Fixes: 7c8875c38b187fbd2e0fa5fc39bbb38c1588d0fb
Bug: https://bugs.gentoo.org/836447
Bug: https://bugs.gentoo.org/837656
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/dispatch_conf.py
@@ -340,7 +340,7 @@ def file_archive(archive, curconf, newconf, mrgconf):
     # Archive the current config file if it isn't already saved
     if (
         os.path.lexists(archive)
-        and len(diffstatusoutput_mixed(f"diff -aq '{curconf}' '{archive}'")[1]) != 0
+        and len(diffstatusoutput_mixed("diff -aq '%s' '%s'", curconf, archive)[1]) != 0
     ):
         _file_archive_rotate(archive)
 