gentoo/portage

commit bb09a2d4db4cd0f85f8ae8ceaddc05ae2585aba3

portage: sets: VariableSet: parse *DEPEND in includes/excludes
VariableSet takes a metadata variable and checks
its contents based on 'includes' or 'excludes'
from the set definition.

Unfortunately, until now, it didn't parse/expand the chosen variable
and would only match it literally (it'd also not check effective
metadata, just what's listed in the ebuild -- no *DEPEND
from an eclass, for example).

If variable is *DEPEND, actually parse includes/excludes
so we can effecitvely match say, includes="dev-lang/go"
against ">=dev-lang/go-1.18" in an ebuild.

Bug: https://bugs.gentoo.org/827974
Bug: https://bugs.gentoo.org/865115
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/_sets/dbapi.py
@@ -168,14 +168,31 @@ class VariableSet(EverythingSet):
             return False
         (values,) = self._metadatadb.aux_get(ebuild, [self._variable])
         values = values.split()
+
+        if "DEPEND" in self._variable:
+            include_atoms = []
+            for include in self._includes:
+                include_atoms.append(Atom(include))
+
+            for x in use_reduce(values, token_class=Atom):
+                if not isinstance(x, Atom):
+                    continue
+
+                for include_atom in include_atoms:
+                    if include_atom.match(x):
+                        return True
+
+            return False
+
         if self._includes and not self._includes.intersection(values):
             return False
+
         if self._excludes and self._excludes.intersection(values):
             return False
+
         return True
 
     def singleBuilder(cls, options, settings, trees):
-
         variable = options.get("variable")
         if variable is None:
             raise SetConfigError(_("missing required attribute: 'variable'"))

commit 15dee372ef874d4d8826630583f3a6dfeea7f4e8

portage: sets: Fix DeprecationWarning.
DeprecationWarning:

lib/portage/_sets/dbapi.py:177: DeprecationWarning: Passing paren_reduced dep arrays to portage.dep.use_reduce is deprecated. Pass the original dep string instead.
  for x in use_reduce(values, token_class=Atom):

Fixes: bb09a2d4db4cd0f85f8ae8ceaddc05ae2585aba3
Signed-off-by: Arfrever Frehtes Taifersar Arahesis <Arfrever@Apache.Org>

lib/portage/_sets/dbapi.py
@@ -167,7 +167,7 @@ class VariableSet(EverythingSet):
         if not ebuild:
             return False
         (values,) = self._metadatadb.aux_get(ebuild, [self._variable])
-        values = values.split()
+        values_list = values.split()
 
         if "DEPEND" in self._variable:
             include_atoms = []
@@ -184,10 +184,10 @@ class VariableSet(EverythingSet):
 
             return False
 
-        if self._includes and not self._includes.intersection(values):
+        if self._includes and not self._includes.intersection(values_list):
             return False
 
-        if self._excludes and self._excludes.intersection(values):
+        if self._excludes and self._excludes.intersection(values_list):
             return False
 
         return True