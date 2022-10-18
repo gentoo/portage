gentoo/portage

commit 800e09a4a1a630548ea8316e5c906c6f48af3105

info: support more info compression suffixes
Bug: https://bugs.gentoo.org/757525
Signed-off-by: Sam James <sam@gentoo.org>

bin/ebuild-helpers/prepinfo
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 source "${PORTAGE_BIN_PATH}"/isolated-functions.sh || exit 1
 
-if ! ___eapi_has_prefix_variables; then
+if ! ___eapi_has_prefix_variables ; then
 	ED=${D}
 fi
 
@@ -32,7 +32,7 @@ find "${ED%/}/${infodir#/}" -type d -print0 | while read -r -d $'\0' x ; do
 		[[ -e ${f} ]] && continue 2
 	done
 
-	rm -f "${x}"/dir{,.info}{,.gz,.bz2}
+	rm -f "${x}"/dir{,.info}{,.gz,.bz2,.xz,.lz,.zst,.lzma,.lz4}
 done
 
 exit 0

bin/misc-functions.sh
@@ -253,7 +253,7 @@ install_qa_check() {
 	fi
 
 	# Portage regenerates this on the installed system.
-	rm -f "${ED%/}"/usr/share/info/dir{,.gz,.bz2} || die "rm failed!"
+	rm -f "${ED%/}"/usr/share/info/dir{,.gz,.bz2,.xz,.lz,.zst,.lzma,.lz4} || die "rm failed!"
 }
 
 __dyn_instprep() {

lib/portage/util/_info_files.py
@@ -36,7 +36,7 @@ def chk_updated_info_files(root, infodirs, prev_mtimes):
             if portage.util.noiselimit >= 0:
                 out.einfo("Regenerating GNU info directory index...")
 
-            dir_extensions = ("", ".gz", ".bz2")
+            dir_extensions = ("", ".gz", ".bz2", ".xz", ".lz", ".lz4", ".zst", ".lzma")
             icount = 0
             badcount = 0
             errmsg = ""

commit c81e799a3bc8de79ade7c5e2610ed305581c73cc

info: drop .lz4/.lzo for info, use sorted list
Use same ordering of suffixes as we do elsewhere for easy comparison.

(I threw in .lz4 & .lzo last-minute earlier but on reflection, it's
misleading to put it here if it's not supported yet by texinfo.)

Fixes: 800e09a4a1a630548ea8316e5c906c6f48af3105
Bug: https://bugs.gentoo.org/757525
Signed-off-by: Sam James <sam@gentoo.org>

bin/ebuild-helpers/prepinfo
@@ -32,7 +32,7 @@ find "${ED%/}/${infodir#/}" -type d -print0 | while read -r -d $'\0' x ; do
 		[[ -e ${f} ]] && continue 2
 	done
 
-	rm -f "${x}"/dir{,.info}{,.gz,.bz2,.xz,.lz,.zst,.lzma,.lz4}
+	rm -f "${x}"/dir{,.info}{,.Z,.gz,.bz2,.lzma,.lz,.xz,.zst}
 done
 
 exit 0

bin/misc-functions.sh
@@ -253,7 +253,7 @@ install_qa_check() {
 	fi
 
 	# Portage regenerates this on the installed system.
-	rm -f "${ED%/}"/usr/share/info/dir{,.gz,.bz2,.xz,.lz,.zst,.lzma,.lz4} || die "rm failed!"
+	rm -f "${ED%/}"/usr/share/info/dir{,.Z,.gz,.bz2,.lzma,.lz,.xz,.zst} || die "rm failed!"
 }
 
 __dyn_instprep() {