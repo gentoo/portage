3a349232febc295565dee8dd1b25f9a76726b50a
Michał Górny <mgorny@gentoo.org>
2022-12-30 11:37:03 +0100

    Inline Whirlpool tests in test_whirlpool
    Inline the Whirlpool tests in the test case instead of spawning them
    as a subprocess.
    
    Signed-off-by: Michał Górny <mgorny@gentoo.org>
    Signed-off-by: Sam James <sam@gentoo.org>

--- lib/portage/tests/util/test_whirlpool.py
+++ lib/portage/tests/util/test_whirlpool.py
@@ -1,23 +1,27 @@
-# Copyright 2011-2014 Gentoo Foundation
+# Copyright 2011-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-import subprocess
-
-import portage
-from portage import os
-from portage.const import PORTAGE_PYM_PATH
 from portage.tests import TestCase
+from portage.util.whirlpool import Whirlpool
 
 
 class WhirlpoolTestCase(TestCase):
     def testBundledWhirlpool(self):
-        # execute the tests bundled with the whirlpool module
-        retval = subprocess.call(
-            [
-                portage._python_interpreter,
-                "-b",
-                "-Wd",
-                os.path.join(PORTAGE_PYM_PATH, "portage/util/whirlpool.py"),
-            ]
+        self.assertEqual(
+            Whirlpool(b"The quick brown fox jumps over the lazy dog").hexdigest(),
+            "b97de512e91e3828b40d2b0fdce9ceb3c4a71f9bea8d88e75c4fa854df36725fd2b52eb6544edcacd6f8beddfea403cb55ae31f03ad62a5ef54e42ee82c3fb35",
         )
-        self.assertEqual(retval, os.EX_OK)
+        self.assertEqual(
+            Whirlpool(b"The quick brown fox jumps over the lazy eog").hexdigest(),
+            "c27ba124205f72e6847f3e19834f925cc666d0974167af915bb462420ed40cc50900d85a1f923219d832357750492d5c143011a76988344c2635e69d06f2d38c",
+        )
+        self.assertEqual(
+            Whirlpool(b"").hexdigest(),
+            "19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3",
+        )
+        w = Whirlpool()
+        w.update(b"")
+        self.assertEqual(
+            w.hexdigest(),
+            "19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3",
+        )
