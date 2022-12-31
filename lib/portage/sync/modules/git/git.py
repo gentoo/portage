gentoo/portage

commit da8e7d193563a2b0c88a523083b2a3466ff4526e

sync: git: only perform shallow updates if the repository is shallow
With the new default of shallow cloning/updating git repositories,
added with f2207e41792d ("GitSync.update: default to "--depth 1" (bug
824782 comment 17)"), existing *non-shallow* repositories would become
shallow after a "emerge --sync".

This adds a check to see if the repository is already shallow, and
only in this case performs a shallow clone.

Thanks to Douglas Freed for pointing out that this should consider
whether or not the user explicitly set sync-depth.

Thanks-to: Douglas Freed <dwfreed@mtu.edu>
Bug: https://bugs.gentoo.org/824782
Bug: https://bugs.gentoo.org/887025
Closes: https://github.com/gentoo/portage/pull/960
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/sync/modules/git/git.py
@@ -148,12 +148,31 @@ class GitSync(NewBase):
 
         if self.settings.get("PORTAGE_QUIET") == "1":
             git_cmd_opts += " --quiet"
+
+        # Default: Perform shallow updates (but only if the target is
+        # already a shallow repository).
+        sync_depth = 1
         if self.repo.sync_depth is not None:
-            if self.repo.sync_depth != 0:
-                git_cmd_opts += " --depth %d" % self.repo.sync_depth
+            sync_depth = self.repo.sync_depth
         else:
-            # default
-            git_cmd_opts += " --depth 1"
+            # If sync-depth is not explicitly set by the user,
+            # then check if the target repository is already a
+            # shallow one. And do not perform a shallow update if
+            # the target repository is not shallow.
+            is_shallow_cmd = ["git", "rev-parse", "--is-shallow-repository"]
+            is_shallow_res = portage._unicode_decode(
+                subprocess.check_output(
+                    is_shallow_cmd,
+                    cwd=portage._unicode_encode(self.repo.location),
+                )
+            )
+            if is_shallow_res == "false":
+                sync_depth = 0
+
+        shallow = False
+        if sync_depth > 0:
+            git_cmd_opts += f" --depth {sync_depth}"
+            shallow = True
 
         if self.repo.module_specific_options.get("sync-git-pull-extra-opts"):
             git_cmd_opts += (
@@ -181,7 +200,6 @@ class GitSync(NewBase):
             writemsg_level(msg + "\n", level=logging.ERROR, noiselevel=-1)
             return (e.returncode, False)
 
-        shallow = self.repo.sync_depth is None or self.repo.sync_depth != 0
         if shallow:
             # For shallow fetch, unreachable objects may need to be pruned
             # manually, in order to prevent automatic git gc calls from

commit 16246ea32371ff70c8941756af75ddefb8c97f4e

git.py: fix "is-shallow-repository" check by stripping \n
The output of git commands has a trailing newline character which we
usually strip.

Fixes: da8e7d193563a2b0c88a523083b2a3466ff4526e ("sync: git: only perform shallow updates if the repository is shallow")
Signed-off-by: Florian Schmaus <flow@gentoo.org>
Closes: https://github.com/gentoo/portage/pull/968
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/sync/modules/git/git.py
@@ -237,7 +237,7 @@ class GitSync(NewBase):
                         is_shallow_cmd,
                         cwd=portage._unicode_encode(self.repo.location),
                     )
-                )
+                ).rstrip("\n")
                 if is_shallow_res == "false":
                     sync_depth = 0
             else: