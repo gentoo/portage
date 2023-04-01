c7c532ccb84c6e562807b6b8335361a11412d839
Oskari Pirhonen <xxc3ncoredxx@gmail.com>
2023-03-31 23:49:31 -0500

    ci: add basic pypy support to setup-python.sh
    Install pypy by specifying 'pypy3' in the PYTHON_VERSIONS.
    
    ... and fix indents and ensure system python has venv module
    
    Signed-off-by: Oskari Pirhonen <xxc3ncoredxx@gmail.com>
    Signed-off-by: Sam James <sam@gentoo.org>

--- .builds/setup-python.sh
+++ .builds/setup-python.sh
@@ -4,17 +4,24 @@
 set -ex
 
 install_versions=( "${@/#/python}" )
+# Fix any pypy versions
+install_versions=( "${install_versions[@]/#pythonpypy/pypy}" )
 
 sudo apt-get install -y --no-install-recommends \
-    python-is-python3 \
-    "${install_versions[@]}" \
-    "${install_versions[@]/%/-venv}"
+        python-is-python3 \
+        python3-venv \
+        "${install_versions[@]}" \
+        "${install_versions[@]/%/-venv}"
 
-for py in "${@}"; do
-  "python$py" -m venv ".venv-$py"
-  source ".venv-$py/bin/activate"
-  pip install --upgrade pip
-  deactivate
+for py in "$@"; do
+    if [[ "$py" != pypy* ]]; then
+        "python$py" -m venv ".venv-$py"
+    else
+        "$py" -m venv ".venv-$py"
+    fi
+    source ".venv-$py/bin/activate"
+    pip install --upgrade pip
+    deactivate
 done
 
 python -m venv .venv
