gentoo/portage

commit ab538e7750fb85d57a3eb9d9b440848c1f2c074a

ebuild-helpers/portageq: Deprecate
Calling portageq from ebuilds was never allowed.
Last usage was removed from Gentoo repository on 2019-03-14.

Bug: https://bugs.gentoo.org/906129
Signed-off-by: Ulrich Müller <ulm@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

bin/ebuild-helpers/portageq
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-# Copyright 2009-2015 Gentoo Foundation
+# Copyright 2009-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 scriptpath=${BASH_SOURCE[0]}
 scriptname=${scriptpath##*/}
+
+source "${PORTAGE_BIN_PATH}"/isolated-functions.sh || exit 1
+
+eqawarn "QA Notice: '${0##*/}' is not allowed in ebuild scope"
 
 # Use safe cwd, avoiding unsafe import for bug #469338.
 cd "${PORTAGE_PYM_PATH}" || exit 1

commit 4bc369c549c34a757c799e3f6df1214cd1da12ef

portageq-wrapper: New wrapper script
Move the internal portageq wrapper script out of the ebuild-helpers
directory. Call the new script from best_version and has_version in
the case when IPC is disabled.

The goal is to ban portageq from the ebuild environment after some
transition time.

Bug: https://bugs.gentoo.org/906129
Fixes: ab538e7750fb85d57a3eb9d9b440848c1f2c074a
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

bin/ebuild-helpers/portageq
@@ -2,29 +2,7 @@
 # Copyright 2009-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-scriptpath=${BASH_SOURCE[0]}
-scriptname=${scriptpath##*/}
-
 source "${PORTAGE_BIN_PATH}"/isolated-functions.sh || exit 1
 
 eqawarn "QA Notice: '${0##*/}' is not allowed in ebuild scope"
-
-# Use safe cwd, avoiding unsafe import for bug #469338.
-cd "${PORTAGE_PYM_PATH}" || exit 1
-
-IFS=':'
-set -f # in case ${PATH} contains any shell glob characters
-
-for path in ${PATH}; do
-	[[ -x ${path}/${scriptname} ]] || continue
-	[[ ${path} == */portage/*/ebuild-helpers* ]] && continue
-	[[ ${path}/${scriptname} -ef ${scriptpath} ]] && continue
-
-	PYTHONPATH=${PORTAGE_PYTHONPATH:-${PORTAGE_PYM_PATH}} \
-		exec "${PORTAGE_PYTHON:-/usr/bin/python}" \
-			"${path}/${scriptname}" "$@"
-done
-
-unset IFS
-echo "${scriptname}: command not found" 1>&2
-exit 127
+exec "${PORTAGE_BIN_PATH}"/portageq-wrapper "$@"

bin/phase-helpers.sh
@@ -963,7 +963,7 @@ ___best_version_and_has_version_common() {
 	if [[ -n ${PORTAGE_IPC_DAEMON} ]] ; then
 		cmd+=("${PORTAGE_BIN_PATH}"/ebuild-ipc "${FUNCNAME[1]}" "${root}" "${atom}")
 	else
-		cmd+=("${PORTAGE_BIN_PATH}"/ebuild-helpers/portageq "${FUNCNAME[1]}" "${root}" "${atom}")
+		cmd+=("${PORTAGE_BIN_PATH}"/portageq-wrapper "${FUNCNAME[1]}" "${root}" "${atom}")
 	fi
 
 	"${cmd[@]}"

bin/portageq-wrapper
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright 2009-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+# Use safe cwd, avoiding unsafe import for bug #469338.
+cd "${PORTAGE_PYM_PATH}" || exit 1
+
+IFS=":"
+set -f # in case ${PATH} contains any shell glob characters
+
+for path in ${PATH}; do
+	[[ ${path} == */portage/*/ebuild-helpers* ]] && continue
+	[[ -x ${path}/portageq ]] || continue
+	PYTHONPATH=${PORTAGE_PYTHONPATH:-${PORTAGE_PYM_PATH}} \
+		exec "${PORTAGE_PYTHON:-/usr/bin/python}" "${path}/portageq" "$@"
+done
+
+echo "portageq: command not found" >&2
+exit 127