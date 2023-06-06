383da241cca50190eeb34582a8ac9e5470542d88
Berin Aniesh <berinaniesh@gmail.com>
2023-06-06 11:24:33 +0530

    portage.util: Fix types and add add more details to docstring
    Signed-Off-by: Berin Aniesh <berinaniesh@gmail.com>

--- lib/_emerge/main.py
+++ lib/_emerge/main.py
@@ -21,6 +21,7 @@ portage.proxy.lazyimport.lazyimport(
 from portage import os
 from portage.sync import _SUBMODULE_PATH_MAP
 
+from typing import Optional, List
 
 options = [
     "--alphabetical",
@@ -1161,12 +1162,12 @@ def profile_check(trees, myaction):
     return os.EX_OK
 
 
-def emerge_main(args=None):
+def emerge_main(args: Optional[List[str]] = None):
     """
     Entry point of emerge
 
-    Processes command line arguments and decides what the current run of emerge
-    should by creating `emerge_config`
+    Processes command line arguments (default: sys.argv[1:]) and decides
+    what the current run of emerge should by creating `emerge_config`
     """
     if args is None:
         args = sys.argv[1:]
