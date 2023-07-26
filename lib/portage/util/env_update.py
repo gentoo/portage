gentoo/portage

commit 3bba408e214ae27bdf924ba90ad4b0919a85f3c8

lib: env_update: port to subprocess
os.system is a bit janky here, let's use subprocess so we're not injecting
multiple commands.

Thanks to mgorny for the suggestion.

Bug: https://bugs.gentoo.org/910376
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/util/env_update.py
@@ -6,7 +6,7 @@ __all__ = ["env_update"]
 import errno
 import glob
 import stat
-import sys
+import subprocess
 import time
 
 import portage
@@ -364,18 +364,24 @@ def _env_update(makelinks, target_root, prev_mtimes, contents, env, writemsg_lev
             writemsg_level(
                 _(">>> Regenerating %setc/ld.so.cache...\n") % (target_root,)
             )
-            ret = os.system(f"cd / ; {ldconfig} -X -r '{target_root}'")
+            ret = subprocess.run(
+                [f"{ldconfig}", "-X", "-r", f"{target_root}"], cwd="/"
+            ).returncode
         elif ostype in ("FreeBSD", "DragonFly"):
             writemsg_level(
                 _(">>> Regenerating %svar/run/ld-elf.so.hints...\n") % target_root
             )
-            ret = os.system(
-                (
-                    "cd / ; %s -elf -i "
-                    + "-f '%svar/run/ld-elf.so.hints' '%setc/ld.so.conf'"
-                )
-                % (ldconfig, target_root, target_root)
-            )
+            ret = subprocess.run(
+                [
+                    f"{ldconfig}",
+                    "-elf",
+                    "-i",
+                    "-f",
+                    f"{target_root}var/run/ld-elf.so.hints",
+                    f"{target_root}etc/ld.so.conf",
+                ],
+                cwd="/",
+            ).returncode
 
         ret = os.waitstatus_to_exitcode(ret)
         if ret > 0:

commit 581f923b9f888efc3313b6e06ba8a3cf84641d75

env_update: drop os.waitstatus_to_exitcode
The subprocess.run() function does this translation already.

Fixes: 3bba408e214ae27bdf924ba90ad4b0919a85f3c8
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/util/env_update.py
@@ -381,7 +381,6 @@ def _env_update(makelinks, target_root, prev_mtimes, contents, env, writemsg_lev
                 cwd="/",
             ).returncode
 
-        ret = os.waitstatus_to_exitcode(ret)
         if ret > 0:
             writemsg(f"!!! ldconfig failed with exit status {ret}\n", noiselevel=-1)
         if ret < 0: