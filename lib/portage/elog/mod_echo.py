gentoo/portage

commit 2e572b9edc48a6d4623df1c2c46ba34316f7e72a

elog: mod_echo: use binpkg coloring
With the 'echo' elog module, we get messages like:
```
 * Messages for package X
 * Log file: /var/log/portage/build/[...]
```

Colorize 'X' based on whether it's a binpkg or not, rather than always using
green as if it was built from source.

Bug: https://bugs.gentoo.org/914159
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/elog/mod_echo.py
@@ -22,7 +22,9 @@ def process(mysettings, key, logentries, fulltext):
         and "PORTAGE_LOG_FILE" in mysettings
     ):
         logfile = mysettings["PORTAGE_LOG_FILE"]
-    _items.append((mysettings["ROOT"], key, logentries, logfile))
+
+    binary = mysettings.configdict["pkg"]["MERGE_TYPE"] == "binary"
+    _items.append((mysettings["ROOT"], key, logentries, logfile, binary))
 
 
 def finalize():
@@ -42,14 +44,17 @@ def finalize():
 def _finalize():
     global _items
     printer = EOutput()
-    for root, key, logentries, logfile in _items:
+    for root, key, logentries, logfile, binary in _items:
+        color = "PKG_BINARY_MERGE" if binary else "INFORM"
+
         print()
+
         if root == "/":
-            printer.einfo(_("Messages for package %s:") % colorize("INFORM", key))
+            printer.einfo(_("Messages for package %s:") % colorize(color, key))
         else:
             printer.einfo(
                 _("Messages for package %(pkg)s merged to %(root)s:")
-                % {"pkg": colorize("INFORM", key), "root": root}
+                % {"pkg": colorize(color, key), "root": root}
             )
         if logfile is not None:
             printer.einfo(_("Log file: %s") % colorize("INFORM", logfile))

commit 7fe73e2e2d72192afe63668d60d2a0945ee3fd67

elog: mod_echo: handle unmerging for binary coloring
Fixes: 2e572b9edc48a6d4623df1c2c46ba34316f7e72a
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/elog/mod_echo.py
@@ -23,7 +23,10 @@ def process(mysettings, key, logentries, fulltext):
     ):
         logfile = mysettings["PORTAGE_LOG_FILE"]
 
-    binary = mysettings.configdict["pkg"]["MERGE_TYPE"] == "binary"
+    try:
+        binary = mysettings.configdict["pkg"]["MERGE_TYPE"] == "binary"
+    except KeyError:
+        binary = False
     _items.append((mysettings["ROOT"], key, logentries, logfile, binary))
 
 