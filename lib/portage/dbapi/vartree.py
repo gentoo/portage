gentoo/portage

commit 1fc674667de01944269dfeccd70472facf83e7ba

vartree: don't clobber pkgfiles
Fixes: 743722ccfa234dd9d4a54f7fbfb14cc2ddf6f0f4
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/vartree.py
@@ -2687,7 +2687,7 @@ class dblink:
         unmerge_orphans = "unmerge-orphans" in self.settings.features
         calc_prelink = "prelink-checksums" in self.settings.features
 
-        pkgfiles = self.getcontents()
+        pkgfiles = self.getcontents() if not pkgfiles
         if pkgfiles:
             self.updateprotect()
             mykeys = list(pkgfiles)

commit 6293f7ba670fbfae5fd4bb6f4ba9fe3f822d19c3

vartree: fix syntax
Fixes: 1fc674667de01944269dfeccd70472facf83e7ba
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/vartree.py
@@ -2687,7 +2687,7 @@ class dblink:
         unmerge_orphans = "unmerge-orphans" in self.settings.features
         calc_prelink = "prelink-checksums" in self.settings.features
 
-        pkgfiles = self.getcontents() if not pkgfiles
+        pkgfiles = pkgfiles if pkgfiles else self.getcontents()
         if pkgfiles:
             self.updateprotect()
             mykeys = list(pkgfiles)