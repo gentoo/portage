gentoo/portage

commit 743722ccfa234dd9d4a54f7fbfb14cc2ddf6f0f4

vartree: drop 'Grabbing a set' useless noise
Prompted by genr8eofl asking about culling it.

Fascinatingly, both the message *and* people agreeing it's futile goes
back many years:
* bf01d6c02087c2b363617ce6ac208d09ea81de25 ('don't display useless messages when using --quiet')
* d9fc4acc572c6647a4f27b838d35d27d805d190e (svn migration)
* 4ba2baf8456c0eeda0df1bf52a1a3d712d5ac9b7 ('Portage-2.0.41 release. LOTS of bug fixes. Check the ChangeLog.', 2002)

This isn't useful for the user at all, so drop it.

Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/vartree.py
@@ -2660,10 +2660,6 @@ class dblink:
         ignored_unlink_errnos = self._ignored_unlink_errnos
         ignored_rmdir_errnos = self._ignored_rmdir_errnos
 
-        if not pkgfiles:
-            showMessage(_("No package files given... Grabbing a set.\n"))
-            pkgfiles = self.getcontents()
-
         if others_in_slot is None:
             others_in_slot = []
             slot = self.vartree.dbapi._pkg_str(self.mycpv, None).slot
@@ -2691,6 +2687,7 @@ class dblink:
         unmerge_orphans = "unmerge-orphans" in self.settings.features
         calc_prelink = "prelink-checksums" in self.settings.features
 
+        pkgfiles = self.getcontents()
         if pkgfiles:
             self.updateprotect()
             mykeys = list(pkgfiles)

commit 1fc674667de01944269dfeccd70472facf83e7ba

vartree: don't clobber pkgfiles
Fixes: 743722ccfa234dd9d4a54f7fbfb14cc2ddf6f0f4
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/vartree.py
@@ -2687,7 +2687,7 @@ class dblink:
         unmerge_orphans = "unmerge-orphans" in self.settings.features
         calc_prelink = "prelink-checksums" in self.settings.features
 
-        pkgfiles = self.getcontents()
+        pkgfiles = self.getcontents() if not pkgfiles
         if pkgfiles:
             self.updateprotect()
             mykeys = list(pkgfiles)