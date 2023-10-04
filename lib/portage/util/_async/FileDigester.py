3dfe5e326eaaca877bc7a45ec84d6b39fc8d137a
Zac Medico <zmedico@gentoo.org>
2023-10-03 19:56:10 -0700

    FileDigester: Migrate to AsyncFunction
    Bug: https://bugs.gentoo.org/915099
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- lib/portage/util/_async/FileDigester.py
+++ lib/portage/util/_async/FileDigester.py
@@ -1,13 +1,13 @@
-# Copyright 2013 Gentoo Foundation
+# Copyright 2013-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-from portage import os
+import functools
+
 from portage.checksum import perform_multiple_checksums
-from portage.util._async.ForkProcess import ForkProcess
-from _emerge.PipeReader import PipeReader
+from portage.util._async.AsyncFunction import AsyncFunction
 
 
-class FileDigester(ForkProcess):
+class FileDigester(AsyncFunction):
     """
     Asynchronously generate file digests. Pass in file_path and
     hash_names, and after successful execution, the digests
@@ -17,64 +17,15 @@ class FileDigester(ForkProcess):
 
     __slots__ = (
         "file_path",
-        "digests",
         "hash_names",
-        "_digest_pipe_reader",
-        "_digest_pw",
     )
 
     def _start(self):
-        pr, pw = os.pipe()
-        self.fd_pipes = {}
-        self.fd_pipes[pw] = pw
-        self._digest_pw = pw
-        self._digest_pipe_reader = PipeReader(
-            input_files={"input": pr}, scheduler=self.scheduler
+        self.target = functools.partial(
+            perform_multiple_checksums, self.file_path, hashes=self.hash_names
         )
-        self._digest_pipe_reader.addExitListener(self._digest_pipe_reader_exit)
-        self._digest_pipe_reader.start()
-        ForkProcess._start(self)
-        os.close(pw)
+        super()._start()
 
-    def _run(self):
-        digests = perform_multiple_checksums(self.file_path, hashes=self.hash_names)
-
-        buf = "".join("%s=%s\n" % item for item in digests.items()).encode("utf_8")
-
-        while buf:
-            buf = buf[os.write(self._digest_pw, buf) :]
-
-        return os.EX_OK
-
-    def _parse_digests(self, data):
-        digests = {}
-        for line in data.decode("utf_8").splitlines():
-            parts = line.split("=", 1)
-            if len(parts) == 2:
-                digests[parts[0]] = parts[1]
-
-        self.digests = digests
-
-    def _async_waitpid(self):
-        # Ignore this event, since we want to ensure that we
-        # exit only after _digest_pipe_reader has reached EOF.
-        if self._digest_pipe_reader is None:
-            ForkProcess._async_waitpid(self)
-
-    def _digest_pipe_reader_exit(self, pipe_reader):
-        self._parse_digests(pipe_reader.getvalue())
-        self._digest_pipe_reader = None
-        if self.pid is None:
-            self._unregister()
-            self._async_wait()
-        else:
-            self._async_waitpid()
-
-    def _unregister(self):
-        ForkProcess._unregister(self)
-
-        pipe_reader = self._digest_pipe_reader
-        if pipe_reader is not None:
-            self._digest_pipe_reader = None
-            pipe_reader.removeExitListener(self._digest_pipe_reader_exit)
-            pipe_reader.cancel()
+    @property
+    def digests(self):
+        return self.result
