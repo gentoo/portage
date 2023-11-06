gentoo/portage

commit bb82666b48e18f448661a1a8bf6a39b773cc4b1c

emerge: avoid spamming too much info about unused binpkgs
Specifically:

- the binhost might have multiple binpkgs for the same cpv, same USE
  flags, but with different build ids, in which case the same line is
  repeated twice

- many packages can be participating in the recursive deptree, but not
  get selected for actual merging, and may not even be installed at all;
  these were still reported as mismatched-USE although the packages
  themselves were not shown in show_merge_list

Both cases are useless noise. Prune them before printing a status
report.

Before:
```
Local copy of remote index is up-to-date and will be used.

Local copy of remote index is up-to-date and will be used.

These are the packages that would be merged, in order:

Calculating dependencies... done!
Dependency resolution took 17.29 s.

[binary     U  ] sys-apps/hwdata-0.374-1 [0.373]
[binary  r  U  ] dev-libs/libsodium-1.0.19-r1-1 [1.0.18_p20220618]
[binary     U  ] net-dns/libidn2-2.3.4-r1-1 [2.3.4]
[ebuild   R    ] dev-python/fs-2.4.16  PYTHON_TARGETS="(-python3_12%*)"
[ebuild     U  ] sys-apps/iproute2-6.5.0 [6.4.0]
[binary     U  ] media-video/ffmpeg-6.0-r9-2 [6.0-r6]
[ebuild  rR    ] app-editors/vim-9.0.1627

The following packages are causing rebuilds:

  (dev-libs/libsodium-1.0.19-r1-1:0/26::gentoo, binary scheduled for merge) causes rebuilds for:
    (app-editors/vim-9.0.1627:0/0::gentoo, ebuild scheduled for merge)

!!! The following binary packages have been ignored due to non matching USE:

    =app-shells/bash-5.1_p16-r6 -plugins
    =sys-apps/util-linux-2.38.1-r2 -caps split-usr
    =sys-apps/util-linux-2.38.1-r2 -caps split-usr
    =sys-process/procps-3.3.17-r2 split-usr
    =sys-process/procps-3.3.17-r2 split-usr -systemd
    =app-editors/vim-9.0.1627 -python
    =app-editors/vim-9.0.1627 -python
    =dev-lang/python-3.11.5 -lto -pgo
    =dev-lang/python-3.11.5 -lto -pgo
    =dev-lang/python-3.11.5 -lto -pgo
    =media-fonts/noto-20230831 X

lib/_emerge/depgraph.py
@@ -1232,25 +1232,38 @@ class depgraph:
             self._show_ignored_binaries_changed_deps(ignored_binaries["changed_deps"])
 
     def _show_ignored_binaries_respect_use(self, respect_use):
-        writemsg(
-            "\n!!! The following binary packages have been ignored "
-            + "due to non matching USE:\n\n",
-            noiselevel=-1,
-        )
-
+        seen = {}
+        messages = []
+        merging = {pkg.cpv for pkg in self._dynamic_config._displayed_list}
         for pkg, flags in respect_use.items():
+            # Don't include recursive deps which aren't in the merge list anyway.
+            if pkg.cpv not in merging:
+                continue
             flag_display = []
             for flag in sorted(flags):
                 if flag not in pkg.use.enabled:
                     flag = "-" + flag
                 flag_display.append(flag)
             flag_display = " ".join(flag_display)
-            # The user can paste this line into package.use
-            writemsg(f"    ={pkg.cpv} {flag_display}", noiselevel=-1)
-            if pkg.root_config.settings["ROOT"] != "/":
-                writemsg(f" # for {pkg.root}", noiselevel=-1)
-            writemsg("\n", noiselevel=-1)
+            # We don't want to list the same USE flags for multiple build IDs
+            if pkg.cpv not in seen or flag_display not in seen[pkg.cpv]:
+                seen.setdefault(pkg.cpv, set()).add(flag_display)
+                # The user can paste this line into package.use
+                messages.append(f"    ={pkg.cpv} {flag_display}")
+                if pkg.root_config.settings["ROOT"] != "/":
+                    messages.append(f" # for {pkg.root}")
+                messages.append("\n")
 
+        if not messages:
+            return
+
+        writemsg(
+            "\n!!! The following binary packages have been ignored "
+            + "due to non matching USE:\n\n",
+            noiselevel=-1,
+        )
+        for line in messages:
+            writemsg(line, noiselevel=-1)
         msg = [
             "",
             "NOTE: The --binpkg-respect-use=n option will prevent emerge",

commit 262b8c9213c6698be4a7e2ce88536568899e6c2a

emerge: fix binpkg-respect-use notice with blockers
Items in _dynamic_config._displayed_list might be blockers, so filter those out.

Bug: https://bugs.gentoo.org/916336
Fixes: bb82666b48e18f448661a1a8bf6a39b773cc4b1c
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/depgraph.py
@@ -1234,7 +1234,11 @@ class depgraph:
     def _show_ignored_binaries_respect_use(self, respect_use):
         seen = {}
         messages = []
-        merging = {pkg.cpv for pkg in self._dynamic_config._displayed_list}
+        merging = {
+            pkg.cpv
+            for pkg in self._dynamic_config._displayed_list
+            if isinstance(pkg, Package)
+        }
         for pkg, flags in respect_use.items():
             # Don't include recursive deps which aren't in the merge list anyway.
             if pkg.cpv not in merging:

lib/portage/tests/resolver/test_useflags.py
@@ -142,3 +142,90 @@ class UseFlagsTestCase(TestCase):
                         )
                 finally:
                     playground.cleanup()
+
+    def testBlockerBinpkgRespectUse(self):
+        """
+        Test for bug #916336 where we tried to check properties of a blocker
+        object which isn't a Package to be merged.
+        """
+
+        ebuilds = {
+            "dev-libs/A-1": {
+                "EAPI": "7",
+                "IUSE": "abi_x86_32",
+                "RDEPEND": "dev-libs/B",
+            },
+            "dev-libs/B-1": {
+                "EAPI": "7",
+                "IUSE": "abi_x86_32",
+            },
+            "dev-libs/A-2": {
+                "EAPI": "7",
+                "IUSE": "abi_x86_32",
+                "RDEPEND": "!<dev-libs/B-2",
+            },
+            "dev-libs/B-2": {
+                "EAPI": "7",
+                "IUSE": "abi_x86_32",
+            },
+        }
+        installed = {
+            "dev-libs/A-1": {
+                "IUSE": "abi_x86_32",
+                "USE": "abi_x86_32",
+            },
+            "dev-libs/B-1": {
+                "IUSE": "abi_x86_32",
+                "USE": "abi_x86_32",
+            },
+        }
+        binpkgs = ebuilds.copy()
+
+        user_config = {
+            "make.conf": (
+                'FEATURES="binpkg-multi-instance"',
+                'USE="abi_x86_32 abi_x86_32"',
+            ),
+        }
+
+        world = ("dev-libs/A",)
+
+        test_cases = (
+            ResolverPlaygroundTestCase(
+                ["dev-libs/A"],
+                options={
+                    "--verbose": "y",
+                    "--update": True,
+                    "--deep": True,
+                    "--complete-graph": True,
+                    "--usepkg": True,
+                    "--autounmask": "n",
+                    "--autounmask-backtrack": "n",
+                    "--autounmask-use": "n",
+                },
+                success=True,
+                mergelist=["dev-libs/A-2", "[uninstall]dev-libs/B-1", "!<dev-libs/B-2"],
+            ),
+        )
+
+        for binpkg_format in SUPPORTED_GENTOO_BINPKG_FORMATS:
+            with self.subTest(binpkg_format=binpkg_format):
+                print(colorize("HILITE", binpkg_format), end=" ... ")
+                sys.stdout.flush()
+                user_config["make.conf"] += (f'BINPKG_FORMAT="{binpkg_format}"',)
+                playground = ResolverPlayground(
+                    ebuilds=ebuilds,
+                    binpkgs=binpkgs,
+                    installed=installed,
+                    user_config=user_config,
+                    world=world,
+                )
+
+                try:
+                    for test_case in test_cases:
+                        playground.run_TestCase(test_case)
+                        self.assertEqual(
+                            test_case.test_success, True, test_case.fail_msg
+                        )
+                finally:
+                    playground.cleanup()