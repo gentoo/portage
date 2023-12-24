gentoo/portage

commit a7bbb4fc4d38f770fc943f3b856c5de56e315fe4

Fix move_ent with signed binpkg
The gpkg file that cannot be updated will be removed.

[sam: We did discuss stripping the signatures to re-use them but that
feels messier and likely unexpected by the user. We also don't distinguish
between locally-made vs fetched binpkgs (where perhaps for local ones, we could
just re-sign if signed).

It was also raised that for fetched binpkgs, we could not worry once it's
been verified the first time, but that's again complicated.

Simply dropping these binpkgs seems like the best solution and to allow
the binhost to re-sign it on updating instead.]

Bug: https://bugs.gentoo.org/919419
Signed-off-by: Sheng Yu <syu.os@protonmail.com>
Closes: https://github.com/gentoo/portage/pull/1043
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/bintree.py
@@ -291,6 +291,24 @@ class bindbapi(fakedbapi):
         elif binpkg_format == "gpkg":
             mybinpkg = portage.gpkg.gpkg(self.settings, cpv_str, binpkg_path)
             mydata = mybinpkg.get_metadata()
+            if mybinpkg.signature_exist:
+                writemsg(
+                    colorize(
+                        "WARN",
+                        f"Binpkg update ignored for signed package: {binpkg_path}, "
+                        "the file will be removed.",
+                    )
+                )
+                try:
+                    os.remove(binpkg_path)
+                except OSError as err:
+                    writemsg(
+                        colorize(
+                            "WARN",
+                            f"Failed to remove moved signed package: {binpkg_path} {str(err)}",
+                        )
+                    )
+                return
             encoding_key = False
         else:
             raise InvalidBinaryPackageFormat(
@@ -687,7 +705,6 @@ class binarytree:
                 )
                 continue
 
-            moves += 1
             binpkg_format = get_binpkg_format(binpkg_path)
             if binpkg_format == "xpak":
                 mytbz2 = portage.xpak.tbz2(binpkg_path)
@@ -707,6 +724,8 @@ class binarytree:
                 decode_metadata_name = True
             else:
                 continue
+
+            moves += 1
 
             updated_items = update_dbentries([mylist], mydata, parent=mycpv)
             mydata.update(updated_items)

lib/portage/tests/update/test_move_slot_ent.py
@@ -86,7 +86,10 @@ class MoveSlotEntTestCase(TestCase):
                     ebuilds=ebuilds,
                     installed=installed,
                     user_config={
-                        "make.conf": (f'BINPKG_FORMAT="{binpkg_format}"',),
+                        "make.conf": (
+                            f'BINPKG_FORMAT="{binpkg_format}"',
+                            'FEATURES="-binpkg-signing"',
+                        ),
                     },
                 )
 
@@ -142,6 +145,149 @@ class MoveSlotEntTestCase(TestCase):
                     # 0/1 -> 1 (equivalent to 1/1)
                     self.assertEqual("1", vardb.aux_get("dev-libs/C-1", ["SLOT"])[0])
                     self.assertEqual("1", bindb.aux_get("dev-libs/C-1", ["SLOT"])[0])
+
+                    # dont_apply_updates
+                    self.assertEqual(
+                        "0/2.30", bindb.aux_get("dev-libs/A-2", ["SLOT"])[0]
+                    )
+                    self.assertEqual("0", bindb.aux_get("dev-libs/B-2", ["SLOT"])[0])
+                    self.assertEqual(
+                        "0/2.1", bindb.aux_get("dev-libs/C-2.1", ["SLOT"])[0]
+                    )
+
+                finally:
+                    playground.cleanup()
+
+    def testMoveSlotEntWithSignature(self):
+        ebuilds = {
+            "dev-libs/A-2::dont_apply_updates": {
+                "EAPI": "5",
+                "SLOT": "0/2.30",
+            },
+            "dev-libs/B-2::dont_apply_updates": {
+                "SLOT": "0",
+            },
+            "dev-libs/C-2.1::dont_apply_updates": {
+                "EAPI": "5",
+                "SLOT": "0/2.1",
+            },
+        }
+
+        installed = {
+            "dev-libs/A-1::test_repo": {
+                "EAPI": "5",
+                "SLOT": "0/2.30",
+            },
+            "dev-libs/B-1::test_repo": {
+                "SLOT": "0",
+            },
+            "dev-libs/C-1::test_repo": {
+                "EAPI": "5",
+                "SLOT": "0/1",
+            },
+        }
+
+        binpkgs = {
+            "dev-libs/A-1::test_repo": {
+                "EAPI": "5",
+                "SLOT": "0/2.30",
+            },
+            "dev-libs/A-2::dont_apply_updates": {
+                "EAPI": "5",
+                "SLOT": "0/2.30",
+            },
+            "dev-libs/B-1::test_repo": {
+                "SLOT": "0",
+            },
+            "dev-libs/B-2::dont_apply_updates": {
+                "SLOT": "0",
+            },
+            "dev-libs/C-1::test_repo": {
+                "EAPI": "5",
+                "SLOT": "0/1",
+            },
+            "dev-libs/C-2.1::dont_apply_updates": {
+                "EAPI": "5",
+                "SLOT": "0/2.1",
+            },
+        }
+
+        updates = textwrap.dedent(
+            """
+			slotmove dev-libs/A 0 2
+			slotmove dev-libs/B 0 1
+			slotmove dev-libs/C 0 1
+		"""
+        )
+
+        for binpkg_format in ("gpkg",):
+            with self.subTest(binpkg_format=binpkg_format):
+                print(colorize("HILITE", binpkg_format), end=" ... ")
+                sys.stdout.flush()
+                playground = ResolverPlayground(
+                    binpkgs=binpkgs,
+                    ebuilds=ebuilds,
+                    installed=installed,
+                    user_config={
+                        "make.conf": (
+                            f'BINPKG_FORMAT="{binpkg_format}"',
+                            'FEATURES="binpkg-signing"',
+                        ),
+                    },
+                )
+
+                settings = playground.settings
+                trees = playground.trees
+                eroot = settings["EROOT"]
+                test_repo_location = settings.repositories["test_repo"].location
+                portdb = trees[eroot]["porttree"].dbapi
+                vardb = trees[eroot]["vartree"].dbapi
+                bindb = trees[eroot]["bintree"].dbapi
+
+                updates_dir = os.path.join(test_repo_location, "profiles", "updates")
+
+                try:
+                    ensure_dirs(updates_dir)
+                    with open(os.path.join(updates_dir, "1Q-2010"), "w") as f:
+                        f.write(updates)
+
+                    # Create an empty updates directory, so that this
+                    # repo doesn't inherit updates from the main repo.
+                    ensure_dirs(
+                        os.path.join(
+                            portdb.getRepositoryPath("dont_apply_updates"),
+                            "profiles",
+                            "updates",
+                        )
+                    )
+
+                    global_noiselimit = portage.util.noiselimit
+                    portage.util.noiselimit = -2
+                    try:
+                        _do_global_updates(trees, {})
+                    finally:
+                        portage.util.noiselimit = global_noiselimit
+
+                    # Workaround for cache validation not working
+                    # correctly when filesystem has timestamp precision
+                    # of 1 second.
+                    vardb._clear_cache()
+
+                    # 0/2.30 -> 2/2.30
+                    self.assertEqual(
+                        "2/2.30", vardb.aux_get("dev-libs/A-1", ["SLOT"])[0]
+                    )
+                    self.assertEqual(
+                        "0/2.30", bindb.aux_get("dev-libs/A-1", ["SLOT"])[0]
+                    )
+
+                    # 0 -> 1
+                    self.assertEqual("1", vardb.aux_get("dev-libs/B-1", ["SLOT"])[0])
+                    self.assertEqual("0", bindb.aux_get("dev-libs/B-1", ["SLOT"])[0])
+
+                    # 0/1 -> 1 (equivalent to 1/1)
+                    self.assertEqual("1", vardb.aux_get("dev-libs/C-1", ["SLOT"])[0])
+                    self.assertEqual("0/1", bindb.aux_get("dev-libs/C-1", ["SLOT"])[0])
 
                     # dont_apply_updates
                     self.assertEqual(

lib/portage/tests/update/test_update_dbentry.py
@@ -235,7 +235,10 @@ class UpdateDbentryTestCase(TestCase):
                     installed=installed,
                     world=world,
                     user_config={
-                        "make.conf": (f'BINPKG_FORMAT="{binpkg_format}"',),
+                        "make.conf": (
+                            f'BINPKG_FORMAT="{binpkg_format}"',
+                            'FEATURES="-binpkg-signing"',
+                        ),
                     },
                 )
 
@@ -286,6 +289,153 @@ class UpdateDbentryTestCase(TestCase):
                     rdepend = bindb.aux_get("dev-libs/A-1", ["RDEPEND"])[0]
                     self.assertTrue(old_pattern.search(rdepend) is None)
                     self.assertTrue("dev-libs/M-moved" in rdepend)
+                    rdepend = vardb.aux_get("dev-libs/B-1", ["RDEPEND"])[0]
+                    self.assertTrue(old_pattern.search(rdepend) is None)
+                    self.assertTrue("dev-libs/M-moved" in rdepend)
+                    rdepend = vardb.aux_get("dev-libs/B-1", ["RDEPEND"])[0]
+                    self.assertTrue(old_pattern.search(rdepend) is None)
+                    self.assertTrue("dev-libs/M-moved" in rdepend)
+
+                    # dont_apply_updates
+                    rdepend = vardb.aux_get("dev-libs/A-2", ["RDEPEND"])[0]
+                    self.assertTrue("dev-libs/M" in rdepend)
+                    self.assertTrue("dev-libs/M-moved" not in rdepend)
+                    rdepend = bindb.aux_get("dev-libs/A-2", ["RDEPEND"])[0]
+                    self.assertTrue("dev-libs/M" in rdepend)
+                    self.assertTrue("dev-libs/M-moved" not in rdepend)
+
+                    selected_set.load()
+                    self.assertTrue("dev-libs/M" not in selected_set)
+                    self.assertTrue("dev-libs/M-moved" in selected_set)
+
+                finally:
+                    playground.cleanup()
+
+    def testUpdateDbentryDbapiTestCaseWithSignature(self):
+        ebuilds = {
+            "dev-libs/A-2::dont_apply_updates": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+            "dev-libs/B-2::dont_apply_updates": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+        }
+
+        installed = {
+            "dev-libs/A-1::test_repo": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+            },
+            "dev-libs/A-2::dont_apply_updates": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+            "dev-libs/B-1::test_repo": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+            },
+            "dev-libs/M-1::test_repo": {
+                "EAPI": "4",
+            },
+            "dev-libs/N-1::test_repo": {
+                "EAPI": "4",
+            },
+            "dev-libs/N-2::test_repo": {
+                "EAPI": "4",
+            },
+        }
+
+        binpkgs = {
+            "dev-libs/A-1::test_repo": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+            },
+            "dev-libs/A-2::dont_apply_updates": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+            "dev-libs/B-1::test_repo": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+            },
+        }
+
+        world = ["dev-libs/M", "dev-libs/N"]
+
+        updates = textwrap.dedent(
+            """
+			move dev-libs/M dev-libs/M-moved
+		"""
+        )
+
+        for binpkg_format in ("gpkg",):
+            with self.subTest(binpkg_format=binpkg_format):
+                print(colorize("HILITE", binpkg_format), end=" ... ")
+                sys.stdout.flush()
+                playground = ResolverPlayground(
+                    binpkgs=binpkgs,
+                    ebuilds=ebuilds,
+                    installed=installed,
+                    world=world,
+                    user_config={
+                        "make.conf": (f'BINPKG_FORMAT="{binpkg_format}"',),
+                    },
+                )
+
+                settings = playground.settings
+                trees = playground.trees
+                eroot = settings["EROOT"]
+                test_repo_location = settings.repositories["test_repo"].location
+                portdb = trees[eroot]["porttree"].dbapi
+                vardb = trees[eroot]["vartree"].dbapi
+                bindb = trees[eroot]["bintree"].dbapi
+                setconfig = trees[eroot]["root_config"].setconfig
+                selected_set = setconfig.getSets()["selected"]
+
+                updates_dir = os.path.join(test_repo_location, "profiles", "updates")
+
+                try:
+                    ensure_dirs(updates_dir)
+                    with open(os.path.join(updates_dir, "1Q-2010"), "w") as f:
+                        f.write(updates)
+
+                    # Create an empty updates directory, so that this
+                    # repo doesn't inherit updates from the main repo.
+                    ensure_dirs(
+                        os.path.join(
+                            portdb.getRepositoryPath("dont_apply_updates"),
+                            "profiles",
+                            "updates",
+                        )
+                    )
+
+                    global_noiselimit = portage.util.noiselimit
+                    portage.util.noiselimit = -2
+                    try:
+                        _do_global_updates(trees, {})
+                    finally:
+                        portage.util.noiselimit = global_noiselimit
+
+                    # Workaround for cache validation not working
+                    # correctly when filesystem has timestamp precision
+                    # of 1 second.
+                    vardb._clear_cache()
+
+                    # M -> M-moved
+                    old_pattern = re.compile(r"\bdev-libs/M(\s|$)")
+                    rdepend = vardb.aux_get("dev-libs/A-1", ["RDEPEND"])[0]
+                    self.assertTrue(old_pattern.search(rdepend) is None)
+                    self.assertTrue("dev-libs/M-moved" in rdepend)
+                    rdepend = bindb.aux_get("dev-libs/A-1", ["RDEPEND"])[0]
+                    print(old_pattern.search(rdepend) is None)
+                    self.assertFalse(old_pattern.search(rdepend) is None)
+                    self.assertFalse("dev-libs/M-moved" in rdepend)
                     rdepend = vardb.aux_get("dev-libs/B-1", ["RDEPEND"])[0]
                     self.assertTrue(old_pattern.search(rdepend) is None)
                     self.assertTrue("dev-libs/M-moved" in rdepend)

lib/portage/exception.py
@@ -197,6 +197,10 @@ class CompressorOperationFailed(PortagePackageException):
     """An error occurred during external operation"""
 
 
+class SignedPackage(PortagePackageException):
+    """Unable to update a signed package"""
+
+
 class InvalidAtom(PortagePackageException):
     """Malformed atom spec"""
 

lib/portage/gpkg.py
@@ -34,6 +34,7 @@ from portage.exception import (
     DigestException,
     MissingSignature,
     InvalidSignature,
+    SignedPackage,
 )
 from portage.output import colorize, EOutput
 from portage.util._urlopen import urlopen
@@ -991,13 +992,16 @@ class gpkg:
                     finally:
                         image_tar.kill()
 
-    def update_metadata(self, metadata, new_basename=None):
+    def update_metadata(self, metadata, new_basename=None, force=False):
         """
         Update metadata in the gpkg file.
         """
         self._verify_binpkg()
         self.checksums = []
         old_basename = self.prefix
+
+        if self.signature_exist and not force:
+            raise SignedPackage("Cannot update a signed gpkg file")
 
         if new_basename is None:
             new_basename = old_basename

lib/portage/tests/gpkg/test_gpkg_metadata_update.py
@@ -16,7 +16,7 @@ class test_gpkg_metadata_case(TestCase):
     def test_gpkg_update_metadata(self):
         playground = ResolverPlayground(
             user_config={
-                "make.conf": ('BINPKG_COMPRESS="gzip"',),
+                "make.conf": ('BINPKG_COMPRESS="gzip"', 'FEATURES="-binpkg-signing"'),
             }
         )
         tmpdir = tempfile.mkdtemp()

lib/portage/tests/update/test_move_ent.py
@@ -122,3 +122,111 @@ class MoveEntTestCase(TestCase):
 
                 finally:
                     playground.cleanup()
+
+    def testMoveEntWithSignature(self):
+        ebuilds = {
+            "dev-libs/A-2::dont_apply_updates": {
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+        }
+
+        installed = {
+            "dev-libs/A-1::test_repo": {
+                "EAPI": "4",
+            },
+            "dev-libs/A-2::dont_apply_updates": {
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+        }
+
+        binpkgs = {
+            "dev-libs/A-1::test_repo": {
+                "EAPI": "4",
+            },
+            "dev-libs/A-2::dont_apply_updates": {
+                "EAPI": "4",
+                "SLOT": "2",
+            },
+        }
+
+        updates = textwrap.dedent(
+            """
+			move dev-libs/A dev-libs/A-moved
+		"""
+        )
+
+        for binpkg_format in ("gpkg",):
+            with self.subTest(binpkg_format=binpkg_format):
+                print(colorize("HILITE", binpkg_format), end=" ... ")
+                sys.stdout.flush()
+                playground = ResolverPlayground(
+                    binpkgs=binpkgs,
+                    ebuilds=ebuilds,
+                    installed=installed,
+                    user_config={
+                        "make.conf": (f'BINPKG_FORMAT="{binpkg_format}"',),
+                    },
+                )
+
+                settings = playground.settings
+                trees = playground.trees
+                eroot = settings["EROOT"]
+                test_repo_location = settings.repositories["test_repo"].location
+                portdb = trees[eroot]["porttree"].dbapi
+                vardb = trees[eroot]["vartree"].dbapi
+                bindb = trees[eroot]["bintree"].dbapi
+
+                updates_dir = os.path.join(test_repo_location, "profiles", "updates")
+
+                try:
+                    ensure_dirs(updates_dir)
+                    with open(os.path.join(updates_dir, "1Q-2010"), "w") as f:
+                        f.write(updates)
+
+                    # Create an empty updates directory, so that this
+                    # repo doesn't inherit updates from the main repo.
+                    ensure_dirs(
+                        os.path.join(
+                            portdb.getRepositoryPath("dont_apply_updates"),
+                            "profiles",
+                            "updates",
+                        )
+                    )
+
+                    global_noiselimit = portage.util.noiselimit
+                    portage.util.noiselimit = -2
+                    try:
+                        _do_global_updates(trees, {})
+                    finally:
+                        portage.util.noiselimit = global_noiselimit
+
+                    # Workaround for cache validation not working
+                    # correctly when filesystem has timestamp precision
+                    # of 1 second.
+                    vardb._clear_cache()
+
+                    # A -> A-moved
+                    self.assertRaises(KeyError, vardb.aux_get, "dev-libs/A-1", ["EAPI"])
+                    vardb.aux_get("dev-libs/A-moved-1", ["EAPI"])
+                    # The original package should still exist because a binary
+                    # package move is a copy on write operation.
+                    bindb.aux_get("dev-libs/A-1", ["EAPI"])
+                    print(bindb.aux_get("dev-libs/A-1", "PF"))
+                    self.assertRaises(
+                        KeyError, bindb.aux_get, "dev-libs/A-moved-1", ["EAPI"]
+                    )
+
+                    # dont_apply_updates
+                    self.assertRaises(
+                        KeyError, vardb.aux_get, "dev-libs/A-moved-2", ["EAPI"]
+                    )
+                    vardb.aux_get("dev-libs/A-2", ["EAPI"])
+                    self.assertRaises(
+                        KeyError, bindb.aux_get, "dev-libs/A-moved-2", ["EAPI"]
+                    )
+                    bindb.aux_get("dev-libs/A-2", ["EAPI"])
+
+                finally:
+                    playground.cleanup()

commit d31f0fee089eb0883c7b48faabee4c47d80cbdf8

bindbapi: Update state for package remove in aux_update
When removing a signed gpkg in aux_update, update internal
state including $PKGDIR/Packages (important especially for
FEATURES=pkgdir-index-trusted).

Bug: https://bugs.gentoo.org/920095
Fixes: a7bbb4fc4d38 ("Fix move_ent with signed binpkg")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/dbapi/bintree.py
@@ -302,15 +302,7 @@ class bindbapi(fakedbapi):
                         "the file will be removed.",
                     )
                 )
-                try:
-                    os.remove(binpkg_path)
-                except OSError as err:
-                    writemsg(
-                        colorize(
-                            "WARN",
-                            f"Failed to remove moved signed package: {binpkg_path} {str(err)}",
-                        )
-                    )
+                self.bintree.remove(cpv)
                 return
             encoding_key = False
         else:
@@ -1788,6 +1780,57 @@ class binarytree:
         cpv._metadata["MD5"] = d["MD5"]
 
         return cpv
+
+    def remove(self, cpv: portage.versions._pkg_str) -> None:
+        """
+        Remove a package instance and update internal state including
+        the package index. This will raise a KeyError if cpv is not
+        found in the internal state. It will display a warning message
+        if the package file was not found on disk, since it could have
+        been removed by another process before this method could
+        acquire a lock.
+
+        @param cpv: The cpv of the existing package to remove
+        @type cpv: portage.versions._pkg_str
+        @rtype: None
+        @return: None
+        @raise KeyError: If cpv does not exist in the internal state
+        """
+        if not self.populated:
+            self.populate()
+        os.makedirs(self.pkgdir, exist_ok=True)
+        pkgindex_lock = lockfile(self._pkgindex_file, wantnewlockfile=1)
+        try:
+            # Will raise KeyError if the package is not found.
+            instance_key = self.dbapi._instance_key(cpv)
+            pkg_path = self.getname(cpv)
+            self.dbapi.cpv_remove(cpv)
+            self._pkg_paths.pop(instance_key, None)
+            if self._remotepkgs is not None:
+                self._remotepkgs.pop(instance_key, None)
+            pkgindex = self._load_pkgindex()
+            if not self._pkgindex_version_supported(pkgindex):
+                pkgindex = self._new_pkgindex()
+
+            path = pkg_path[len(self.pkgdir) + 1 :]
+            for i in range(len(pkgindex.packages) - 1, -1, -1):
+                d = pkgindex.packages[i]
+                if cpv == d.get("CPV"):
+                    if path == d.get("PATH", ""):
+                        del pkgindex.packages[i]
+
+            self._pkgindex_write(pkgindex)
+            try:
+                os.remove(pkg_path)
+            except OSError as err:
+                writemsg(
+                    colorize(
+                        "WARN",
+                        f"Failed to remove package: {binpkg_path} {str(err)}",
+                    )
+                )
+        finally:
+            unlockfile(pkgindex_lock)
 
     def _read_metadata(self, filename, st, keys=None, binpkg_format=None):
         """

lib/portage/tests/update/test_move_slot_ent.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2019 Gentoo Authors
+# Copyright 2012-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import sys
@@ -277,17 +277,24 @@ class MoveSlotEntTestCase(TestCase):
                     self.assertEqual(
                         "2/2.30", vardb.aux_get("dev-libs/A-1", ["SLOT"])[0]
                     )
-                    self.assertEqual(
-                        "0/2.30", bindb.aux_get("dev-libs/A-1", ["SLOT"])[0]
-                    )
+
+                    # Stale signed packages removed since a7bbb4fc4d38.
+                    self.assertRaises(KeyError, bindb.aux_get, "dev-libs/A-1", ["SLOT"])
+                    # self.assertEqual(
+                    #    "0/2.30", bindb.aux_get("dev-libs/A-1", ["SLOT"])[0]
+                    # )
 
                     # 0 -> 1
                     self.assertEqual("1", vardb.aux_get("dev-libs/B-1", ["SLOT"])[0])
-                    self.assertEqual("0", bindb.aux_get("dev-libs/B-1", ["SLOT"])[0])
+                    # Stale signed packages removed since a7bbb4fc4d38.
+                    self.assertRaises(KeyError, bindb.aux_get, "dev-libs/B-1", ["SLOT"])
+                    # self.assertEqual("0", bindb.aux_get("dev-libs/B-1", ["SLOT"])[0])
 
                     # 0/1 -> 1 (equivalent to 1/1)
                     self.assertEqual("1", vardb.aux_get("dev-libs/C-1", ["SLOT"])[0])
-                    self.assertEqual("0/1", bindb.aux_get("dev-libs/C-1", ["SLOT"])[0])
+                    # Stale signed packages removed since a7bbb4fc4d38.
+                    self.assertRaises(KeyError, bindb.aux_get, "dev-libs/C-1", ["SLOT"])
+                    # self.assertEqual("0/1", bindb.aux_get("dev-libs/C-1", ["SLOT"])[0])
 
                     # dont_apply_updates
                     self.assertEqual(

lib/portage/tests/update/test_update_dbentry.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2013 Gentoo Foundation
+# Copyright 2012-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import sys
@@ -432,10 +432,13 @@ class UpdateDbentryTestCase(TestCase):
                     rdepend = vardb.aux_get("dev-libs/A-1", ["RDEPEND"])[0]
                     self.assertTrue(old_pattern.search(rdepend) is None)
                     self.assertTrue("dev-libs/M-moved" in rdepend)
-                    rdepend = bindb.aux_get("dev-libs/A-1", ["RDEPEND"])[0]
-                    print(old_pattern.search(rdepend) is None)
-                    self.assertFalse(old_pattern.search(rdepend) is None)
-                    self.assertFalse("dev-libs/M-moved" in rdepend)
+                    # Stale signed packages removed since a7bbb4fc4d38.
+                    self.assertRaises(
+                        KeyError, bindb.aux_get, "dev-libs/A-1", ["RDEPEND"]
+                    )
+                    # rdepend = bindb.aux_get("dev-libs/A-1", ["RDEPEND"])[0]
+                    # self.assertFalse(old_pattern.search(rdepend) is None)
+                    # self.assertFalse("dev-libs/M-moved" in rdepend)
                     rdepend = vardb.aux_get("dev-libs/B-1", ["RDEPEND"])[0]
                     self.assertTrue(old_pattern.search(rdepend) is None)
                     self.assertTrue("dev-libs/M-moved" in rdepend)