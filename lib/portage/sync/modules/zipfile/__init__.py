80445d9b00bfcd1eb4955cf3ecb397b4c02663ba
Alexey Gladkov <legion@kernel.org>
2024-02-12 13:59:40 +0000

    sync: Add method to download zip archives
    Add a simple method for synchronizing repository as a snapshot in a zip
    archive. The implementation does not require external utilities to
    download and unpack archive. This makes the method very cheap.
    
    The main usecase being considered is obtaining snapshots of github
    repositories, but many other web interfaces for git also support
    receiving snapshots in zip format.
    
    For example, to get a snapshot of the master branch:
    
      https://github.com/gentoo/portage/archive/refs/heads/master.zip
      https://gitweb.gentoo.org/proj/portage.git/snapshot/portage-master.zip
    
    or a link to a snapshot of the tag:
    
      https://github.com/gentoo/portage/archive/refs/tags/portage-3.0.61.zip
    
    Signed-off-by: Alexey Gladkov <legion@kernel.org>
    Signed-off-by: Sam James <sam@gentoo.org>

---
+++ lib/portage/sync/modules/zipfile/__init__.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024  Alexey Gladkov <gladkov.alexey@gmail.com>
+
+doc = """Zipfile plug-in module for portage.
+Performs a http download of a portage snapshot and unpacks it to the repo
+location."""
+__doc__ = doc[:]
+
+
+import os
+
+from portage.sync.config_checks import CheckSyncConfig
+
+
+module_spec = {
+    "name": "zipfile",
+    "description": doc,
+    "provides": {
+        "zipfile-module": {
+            "name": "zipfile",
+            "sourcefile": "zipfile",
+            "class": "ZipFile",
+            "description": doc,
+            "functions": ["sync"],
+            "func_desc": {
+                "sync": "Performs an archived http download of the "
+                + "repository, then unpacks it.",
+            },
+            "validate_config": CheckSyncConfig,
+            "module_specific_options": (),
+        },
+    },
+}
