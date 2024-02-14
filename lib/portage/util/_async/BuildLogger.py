gentoo/portage

commit df212738bbb209356472911cda79902f0e25918e

BuildLogger: Close self._stdin after fork
In order to ensure that we can observe EOF on the read end of the pipe,
close self._stdin after fork. Since portage.locks._close_fds() already
does something similar for _lock_manager instances which have a close()
method, it will also work with these _file_close_wrapper objects.

The portage.locks._close_fds() function calls close after fork, in
the ForkProcess._bootstrap method. For more general fork coverage,
we could move the _close_fds() call to the _ForkWatcher.hook method
in portage/__init__.py, but I've reserved that for a later change
since _close_fds() has been working fine for us where we call it now.

Bug: https://bugs.gentoo.org/919072
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/util/_async/BuildLogger.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2021 Gentoo Authors
+# Copyright 2020-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import functools
@@ -6,11 +6,39 @@ import subprocess
 
 from _emerge.AsynchronousTask import AsynchronousTask
 
+import portage
 from portage import os
+from portage.proxy.objectproxy import ObjectProxy
 from portage.util import shlex_split
 from portage.util._async.PipeLogger import PipeLogger
 from portage.util._async.PopenProcess import PopenProcess
 from portage.util.futures import asyncio
+
+
+class _file_close_wrapper(ObjectProxy):
+    """
+    Prevent fd inheritance via fork, ensuring that we can observe
+    EOF on the read end of the pipe (bug 919072).
+    """
+
+    __slots__ = ("_file",)
+
+    def __init__(self, file):
+        ObjectProxy.__init__(self)
+        object.__setattr__(self, "_file", file)
+        portage.locks._open_fds[file.fileno()] = self
+
+    def _get_target(self):
+        return object.__getattribute__(self, "_file")
+
+    def close(self):
+        file = object.__getattribute__(self, "_file")
+        if not file.closed:
+            # This must only be called if the file is open,
+            # which ensures that file.fileno() does not
+            # collide with an open lock file descriptor.
+            del portage.locks._open_fds[file.fileno()]
+            file.close()
 
 
 class BuildLogger(AsynchronousTask):
@@ -67,7 +95,7 @@ class BuildLogger(AsynchronousTask):
                     os.close(log_input)
                     os.close(filter_output)
                 else:
-                    self._stdin = os.fdopen(stdin, "wb", 0)
+                    self._stdin = _file_close_wrapper(os.fdopen(stdin, "wb", 0))
                     os.close(filter_input)
                     os.close(filter_output)
 
@@ -76,7 +104,7 @@ class BuildLogger(AsynchronousTask):
             # that is missing or broken somehow, create a pipe that
             # logs directly to pipe_logger.
             log_input, stdin = os.pipe()
-            self._stdin = os.fdopen(stdin, "wb", 0)
+            self._stdin = _file_close_wrapper(os.fdopen(stdin, "wb", 0))
 
         # Set background=True so that pipe_logger does not log to stdout.
         pipe_logger = PipeLogger(

commit 038ad1029ea574b106906380e47479db1041bee2

BuildLogger: Fix portage.locks._open_fds memory leak
The _file_close_wrapper __getattribute__ method needs to
be overridden to expose its close method, otherwise the
underlying file's close method is called and the closed
file object remains as a memory leak in the global
portage.locks._open_fds dict. For reference, see similar
classes like portage.util.atomic_ofstream which overrides
methods in the same way.

Bug: https://bugs.gentoo.org/919072
Fixes: df212738bbb2 ("BuildLogger: Close self._stdin after fork")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/util/_async/BuildLogger.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2023 Gentoo Authors
+# Copyright 2020-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import functools
@@ -30,6 +30,11 @@ class _file_close_wrapper(ObjectProxy):
 
     def _get_target(self):
         return object.__getattribute__(self, "_file")
+
+    def __getattribute__(self, attr):
+        if attr == "close":
+            return object.__getattribute__(self, attr)
+        return getattr(object.__getattribute__(self, "_file"), attr)
 
     def close(self):
         file = object.__getattribute__(self, "_file")