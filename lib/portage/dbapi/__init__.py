gentoo/portage

commit fb1d0a22f65747b750143080536a4129e8654f97

dbapi: KeyError tolerance during package moves
Raise a new CorruptionKeyError exception type instead
of a plain KeyError when os.stat fails. Treat this
type of exception as a warning during package moves.

Also fix this error that the test case triggered in
the binarytree.remove method:

  NameError: name 'binpkg_path' is not defined

Bug: https://bugs.gentoo.org/920828
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/dbapi/__init__.py
@@ -25,7 +25,11 @@ from portage.const import MERGING_IDENTIFIER
 from portage import os
 from portage import auxdbkeys
 from portage.eapi import _get_eapi_attrs
-from portage.exception import InvalidBinaryPackageFormat, InvalidData
+from portage.exception import (
+    CorruptionKeyError,
+    InvalidBinaryPackageFormat,
+    InvalidData,
+)
 from portage.localization import _
 from _emerge.Package import Package
 
@@ -424,7 +428,7 @@ class dbapi:
             if metadata_updates:
                 try:
                     aux_update(cpv, metadata_updates)
-                except InvalidBinaryPackageFormat as e:
+                except (InvalidBinaryPackageFormat, CorruptionKeyError) as e:
                     warnings.warn(e)
                 if onUpdate:
                     onUpdate(maxval, i + 1)
@@ -470,5 +474,9 @@ class dbapi:
             ):
                 newslot = f"{newslot}/{mycpv.sub_slot}"
             mydata = {"SLOT": newslot + "\n"}
-            self.aux_update(mycpv, mydata)
+            try:
+                self.aux_update(mycpv, mydata)
+            except CorruptionKeyError as e:
+                warnings.warn(e)
+                continue
         return moves

lib/portage/dbapi/bintree.py
@@ -37,6 +37,7 @@ from portage.dbapi.virtual import fakedbapi
 from portage.dep import Atom, use_reduce, paren_enclose
 from portage.exception import (
     AlarmSignal,
+    CorruptionKeyError,
     InvalidPackageName,
     InvalidBinaryPackageFormat,
     ParseError,
@@ -210,9 +211,9 @@ class bindbapi(fakedbapi):
                 raise KeyError(mycpv)
             binpkg_path = os.path.join(self.bintree.pkgdir, binpkg_path)
             try:
-                st = os.lstat(binpkg_path)
-            except OSError:
-                raise KeyError(mycpv)
+                st = os.stat(binpkg_path)
+            except OSError as oe:
+                raise CorruptionKeyError(mycpv) from oe
 
             binpkg_format = get_binpkg_format(binpkg_path)
             if binpkg_format == "xpak":
@@ -283,8 +284,10 @@ class bindbapi(fakedbapi):
             cpv_str += f"-{build_id}"
 
         binpkg_path = self.bintree.getname(cpv)
-        if not os.path.exists(binpkg_path):
-            raise KeyError(cpv)
+        try:
+            os.stat(binpkg_path)
+        except OSError as oe:
+            raise CorruptionKeyError(cpv) from oe
 
         binpkg_format = get_binpkg_format(binpkg_path)
         if binpkg_format == "xpak":
@@ -694,7 +697,18 @@ class binarytree:
                 continue
 
             binpkg_path = self.getname(mycpv)
-            if os.path.exists(binpkg_path) and not os.access(binpkg_path, os.W_OK):
+            try:
+                os.stat(binpkg_path)
+            except FileNotFoundError:
+                writemsg(_("!!! File not found: %s\n") % binpkg_path, noiselevel=-1)
+                continue
+            except OSError as oe:
+                writemsg(
+                    _("!!! File os error (path %s): %s\n") % (binpkg_path, oe),
+                    noiselevel=-1,
+                )
+                continue
+            if not os.access(binpkg_path, os.W_OK):
                 writemsg(
                     _("!!! Cannot update readonly binary: %s\n") % mycpv, noiselevel=-1
                 )
@@ -1829,7 +1843,7 @@ class binarytree:
                 writemsg(
                     colorize(
                         "WARN",
-                        f"Failed to remove package: {binpkg_path} {str(err)}",
+                        f"Failed to remove package: {pkg_path} {str(err)}",
                     )
                 )
         finally:

lib/portage/dbapi/vartree.py
@@ -1,4 +1,4 @@
-# Copyright 1998-2021 Gentoo Authors
+# Copyright 1998-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 __all__ = ["vardbapi", "vartree", "dblink"] + ["write_contents", "tar_contents"]
@@ -60,6 +60,7 @@ from portage.const import (
 from portage.dbapi import dbapi
 from portage.exception import (
     CommandNotFound,
+    CorruptionKeyError,
     InvalidData,
     InvalidLocation,
     InvalidPackageName,
@@ -990,8 +991,10 @@ class vardbapi(dbapi):
 
     def aux_update(self, cpv, values):
         mylink = self._dblink(cpv)
-        if not mylink.exists():
-            raise KeyError(cpv)
+        try:
+            os.stat(mylink.dbdir)
+        except OSError as oe:
+            raise CorruptionKeyError(cpv) from oe
         self._bump_mtime(cpv)
         self._clear_pkg_cache(mylink)
         for k, v in values.items():

lib/portage/exception.py
@@ -1,4 +1,4 @@
-# Copyright 1998-2020 Gentoo Authors
+# Copyright 1998-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import signal
@@ -28,6 +28,10 @@ class PortageKeyError(KeyError, PortageException):
 
 class CorruptionError(PortageException):
     """Corruption indication"""
+
+
+class CorruptionKeyError(CorruptionError, PortageKeyError):
+    """KeyError raised when corruption is detected (cause should be accesssible as __cause__)"""
 
 
 class InvalidDependString(PortageException):

lib/portage/tests/update/test_update_dbentry.py
@@ -1,6 +1,7 @@
 # Copyright 2012-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+import shutil
 import sys
 import re
 import textwrap
@@ -9,6 +10,7 @@ import portage
 from portage import os
 from portage.const import SUPPORTED_GENTOO_BINPKG_FORMATS
 from portage.dep import Atom
+from portage.exception import CorruptionKeyError
 from portage.tests import TestCase
 from portage.tests.resolver.ResolverPlayground import ResolverPlayground
 from portage.update import update_dbentry
@@ -186,6 +188,11 @@ class UpdateDbentryTestCase(TestCase):
                 "EAPI": "4",
                 "SLOT": "2",
             },
+            "dev-libs/B-2::test_repo": {
+                "SLOT": "2",
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+            },
             "dev-libs/B-1::test_repo": {
                 "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
                 "EAPI": "4",
@@ -212,6 +219,11 @@ class UpdateDbentryTestCase(TestCase):
                 "SLOT": "2",
             },
             "dev-libs/B-1::test_repo": {
+                "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
+                "EAPI": "4",
+            },
+            "dev-libs/B-2::test_repo": {
+                "SLOT": "2",
                 "RDEPEND": "dev-libs/M dev-libs/N dev-libs/P",
                 "EAPI": "4",
             },
@@ -269,6 +281,34 @@ class UpdateDbentryTestCase(TestCase):
                         )
                     )
 
+                    # Delete some things in order to trigger CorruptionKeyError during package moves.
+                    corruption_atom = Atom("dev-libs/B:2")
+                    # Demonstrate initial state.
+                    self.assertEqual(bindb.match(corruption_atom), ["dev-libs/B-2"])
+                    for cpv in bindb.match(corruption_atom):
+                        os.unlink(bindb.bintree.getname(cpv))
+                        self.assertRaises(
+                            CorruptionKeyError,
+                            bindb.aux_update,
+                            cpv,
+                            {"RDEPEND": "dev-libs/M-moved"},
+                        )
+                    # Demonstrate corrupt state.
+                    self.assertEqual(bindb.match(corruption_atom), ["dev-libs/B-2"])
+
+                    # Demonstrate initial state.
+                    self.assertEqual(vardb.match(corruption_atom), ["dev-libs/B-2"])
+                    for cpv in vardb.match(corruption_atom):
+                        shutil.rmtree(vardb.getpath(cpv))
+                        self.assertRaises(
+                            CorruptionKeyError,
+                            vardb.aux_update,
+                            cpv,
+                            {"RDEPEND": "dev-libs/M-moved"},
+                        )
+                    # Demonstrate correct state because vardbapi checks the disk.
+                    self.assertEqual(vardb.match(corruption_atom), [])
+
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
@@ -303,6 +343,26 @@ class UpdateDbentryTestCase(TestCase):
                     rdepend = bindb.aux_get("dev-libs/A-2", ["RDEPEND"])[0]
                     self.assertTrue("dev-libs/M" in rdepend)
                     self.assertTrue("dev-libs/M-moved" not in rdepend)
+
+                    # Demonstrate that match still returns stale results
+                    # due to intentional corruption.
+                    self.assertEqual(bindb.match(corruption_atom), ["dev-libs/B-2"])
+
+                    # Update bintree state so aux_get will properly raise KeyError.
+                    for cpv in bindb.match(corruption_atom):
+                        # Demonstrate that aux_get returns stale results.
+                        self.assertEqual(
+                            ["dev-libs/M dev-libs/N dev-libs/P"],
+                            bindb.aux_get(cpv, ["RDEPEND"]),
+                        )
+                        bindb.bintree.remove(cpv)
+                    self.assertEqual(bindb.match(corruption_atom), [])
+                    self.assertRaises(
+                        KeyError, bindb.aux_get, "dev-libs/B-2", ["RDEPEND"]
+                    )
+                    self.assertRaises(
+                        KeyError, vardb.aux_get, "dev-libs/B-2", ["RDEPEND"]
+                    )
 
                     selected_set.load()
                     self.assertTrue("dev-libs/M" not in selected_set)

commit d4ca4f7796d41b55d4d5959be3e2bf4f1ec594b2

dbapi: Fix TypeError when passing Exception to warnings.warn
If an Exception is passed as a message to warnings.warn then
it triggers this TypeError:

            if metadata_updates:
                try:
                    aux_update(cpv, metadata_updates)
                except (InvalidBinaryPackageFormat, CorruptionKeyError) as e:
>                   warnings.warn(e)
E                   TypeError: expected string or bytes-like object, got 'CorruptionKeyError'

Fixes: fb1d0a22f657 ("dbapi: KeyError tolerance during package moves")
Bug: https://bugs.gentoo.org/922935
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/dbapi/__init__.py
@@ -1,11 +1,12 @@
-# Copyright 1998-2023 Gentoo Authors
+# Copyright 1998-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 __all__ = ["dbapi"]
 
 import functools
+import logging
 import re
-import warnings
+import sys
 from typing import Any, Dict, List, Optional, Tuple
 from collections.abc import Sequence
 
@@ -429,7 +430,9 @@ class dbapi:
                 try:
                     aux_update(cpv, metadata_updates)
                 except (InvalidBinaryPackageFormat, CorruptionKeyError) as e:
-                    warnings.warn(e)
+                    logging.warning(
+                        f"{e.__class__.__name__}: {e}", exc_info=sys.exc_info()
+                    )
                 if onUpdate:
                     onUpdate(maxval, i + 1)
             if onProgress:
@@ -477,6 +480,6 @@ class dbapi:
             try:
                 self.aux_update(mycpv, mydata)
             except CorruptionKeyError as e:
-                warnings.warn(e)
+                logging.warning(f"{e.__class__.__name__}: {e}", exc_info=sys.exc_info())
                 continue
         return moves