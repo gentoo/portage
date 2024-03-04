gentoo/portage

commit e8b31c86eaed645a8740fb2844e2935aee161e43

ForkProcess: Prevent redundant pipe and set_term_size recursion
When process.spawn is updated to call ForkProcess
for bug 916566, it needs to avoid recursion via
set_term_size.

Bug: https://bugs.gentoo.org/916566
Bug: https://bugs.gentoo.org/923750
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/SpawnProcess.py
@@ -41,7 +41,7 @@ class SpawnProcess(SubProcess):
     )
 
     __slots__ = (
-        ("args", "log_filter_file")
+        ("args", "create_pipe", "log_filter_file")
         + _spawn_kwarg_names
         + (
             "_main_task",
@@ -60,15 +60,30 @@ class SpawnProcess(SubProcess):
         else:
             self.fd_pipes = self.fd_pipes.copy()
         fd_pipes = self.fd_pipes
+        log_file_path = None
 
         if fd_pipes or self.logfile or not self.background:
-            master_fd, slave_fd = self._pipe(fd_pipes)
+            if self.create_pipe is not False:
+                master_fd, slave_fd = self._pipe(fd_pipes)
 
-            can_log = self._can_log(slave_fd)
-            if can_log:
-                log_file_path = self.logfile
+                can_log = self._can_log(slave_fd)
+                if can_log:
+                    log_file_path = self.logfile
             else:
-                log_file_path = None
+                if self.logfile:
+                    raise NotImplementedError(
+                        "logfile conflicts with create_pipe=False"
+                    )
+                # When called via process.spawn and ForkProcess._start,
+                # SpawnProcess will have created a pipe earlier, so it
+                # would be redundant to do it here (it could also trigger
+                # spawn recursion via set_term_size as in bug 923750).
+                # Use /dev/null for master_fd, triggering early return
+                # of _main, followed by _async_waitpid.
+                # TODO: Optimize away the need for master_fd here.
+                master_fd = os.open(os.devnull, os.O_RDONLY)
+                slave_fd = None
+                can_log = False
 
             null_input = None
             if not self.background or 0 in fd_pipes:
@@ -97,7 +112,9 @@ class SpawnProcess(SubProcess):
 
             fd_pipes_orig = fd_pipes.copy()
 
-            if log_file_path is not None or self.background:
+            if slave_fd is None:
+                pass
+            elif log_file_path is not None or self.background:
                 fd_pipes[1] = slave_fd
                 fd_pipes[2] = slave_fd
 

lib/portage/util/_async/ForkProcess.py
@@ -75,12 +75,29 @@ class ForkProcess(SpawnProcess):
                 self.fd_pipes.setdefault(0, portage._get_stdin().fileno())
                 self.fd_pipes.setdefault(1, sys.__stdout__.fileno())
                 self.fd_pipes.setdefault(2, sys.__stderr__.fileno())
-                stdout_fd = os.dup(self.fd_pipes[1])
+                if self.create_pipe is not False:
+                    stdout_fd = os.dup(self.fd_pipes[1])
 
             if self._HAVE_SEND_HANDLE:
-                master_fd, slave_fd = self._pipe(self.fd_pipes)
-                self.fd_pipes[1] = slave_fd
-                self.fd_pipes[2] = slave_fd
+                if self.create_pipe is not False:
+                    master_fd, slave_fd = self._pipe(self.fd_pipes)
+                    self.fd_pipes[1] = slave_fd
+                    self.fd_pipes[2] = slave_fd
+                else:
+                    if self.logfile:
+                        raise NotImplementedError(
+                            "logfile conflicts with create_pipe=False"
+                        )
+                    # When called via process.spawn, SpawnProcess
+                    # will have created a pipe earlier, so it would be
+                    # redundant to do it here (it could also trigger spawn
+                    # recursion via set_term_size as in bug 923750). Use
+                    # /dev/null for master_fd, triggering early return
+                    # of _main, followed by _async_waitpid.
+                    # TODO: Optimize away the need for master_fd here.
+                    master_fd = os.open(os.devnull, os.O_RDONLY)
+                    slave_fd = None
+
                 self._files = self._files_dict(connection=connection, slave_fd=slave_fd)
 
                 # Create duplicate file descriptors in self._fd_pipes
@@ -167,7 +184,8 @@ class ForkProcess(SpawnProcess):
                     self._files.connection.close()
                     del self._files.connection
                 if hasattr(self._files, "slave_fd"):
-                    os.close(self._files.slave_fd)
+                    if self._files.slave_fd is not None:
+                        os.close(self._files.slave_fd)
                     del self._files.slave_fd
 
         await super()._main(build_logger, pipe_logger, loop=loop)

commit 6f9a10d38259dd61b948837e193b047464791845

SpawnProcess: Optimize away null input for create_pipe=False
When create_pipe=False support was added in commit e8b31c86eaed,
a null input file descriptor was used for PipeLogger and BuildLogger
instances. Optimize this away, eliminating the unnecessary loggers.

Fixes: e8b31c86eaed ("ForkProcess: Prevent redundant pipe and set_term_size recursion")
Bug: https://bugs.gentoo.org/916566
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/SpawnProcess.py
@@ -79,10 +79,7 @@ class SpawnProcess(SubProcess):
                 # SpawnProcess will have created a pipe earlier, so it
                 # would be redundant to do it here (it could also trigger
                 # spawn recursion via set_term_size as in bug 923750).
-                # Use /dev/null for master_fd, triggering early return
-                # of _main, followed by _async_waitpid.
-                # TODO: Optimize away the need for master_fd here.
-                master_fd = os.open(os.devnull, os.O_RDONLY)
+                master_fd = None
                 slave_fd = None
                 can_log = False
 
@@ -166,23 +163,27 @@ class SpawnProcess(SubProcess):
         self._registered = True
 
     def _start_main_task(self, pr, log_file_path=None, stdout_fd=None):
-        build_logger = BuildLogger(
-            env=self.env,
-            log_path=log_file_path,
-            log_filter_file=self.log_filter_file,
-            scheduler=self.scheduler,
-        )
-        build_logger.start()
+        if pr is None:
+            build_logger = None
+            pipe_logger = None
+        else:
+            build_logger = BuildLogger(
+                env=self.env,
+                log_path=log_file_path,
+                log_filter_file=self.log_filter_file,
+                scheduler=self.scheduler,
+            )
+            build_logger.start()
 
-        pipe_logger = PipeLogger(
-            background=self.background,
-            scheduler=self.scheduler,
-            input_fd=pr,
-            log_file_path=build_logger.stdin,
-            stdout_fd=stdout_fd,
-        )
+            pipe_logger = PipeLogger(
+                background=self.background,
+                scheduler=self.scheduler,
+                input_fd=pr,
+                log_file_path=build_logger.stdin,
+                stdout_fd=stdout_fd,
+            )
 
-        pipe_logger.start()
+            pipe_logger.start()
 
         self._main_task_cancel = functools.partial(
             self._main_cancel, build_logger, pipe_logger
@@ -198,18 +199,18 @@ class SpawnProcess(SubProcess):
             await self._pty_ready
             self._pty_ready = None
         try:
-            if pipe_logger.poll() is None:
+            if pipe_logger is not None and pipe_logger.poll() is None:
                 await pipe_logger.async_wait()
-            if build_logger.poll() is None:
+            if build_logger is not None and build_logger.poll() is None:
                 await build_logger.async_wait()
         except asyncio.CancelledError:
             self._main_cancel(build_logger, pipe_logger)
             raise
 
     def _main_cancel(self, build_logger, pipe_logger):
-        if pipe_logger.poll() is None:
+        if pipe_logger is not None and pipe_logger.poll() is None:
             pipe_logger.cancel()
-        if build_logger.poll() is None:
+        if build_logger is not None and build_logger.poll() is None:
             build_logger.cancel()
 
     def _main_exit(self, main_task):

lib/portage/util/_async/ForkProcess.py
@@ -91,11 +91,8 @@ class ForkProcess(SpawnProcess):
                     # When called via process.spawn, SpawnProcess
                     # will have created a pipe earlier, so it would be
                     # redundant to do it here (it could also trigger spawn
-                    # recursion via set_term_size as in bug 923750). Use
-                    # /dev/null for master_fd, triggering early return
-                    # of _main, followed by _async_waitpid.
-                    # TODO: Optimize away the need for master_fd here.
-                    master_fd = os.open(os.devnull, os.O_RDONLY)
+                    # recursion via set_term_size as in bug 923750).
+                    master_fd = None
                     slave_fd = None
 
                 self._files = self._files_dict(connection=connection, slave_fd=slave_fd)