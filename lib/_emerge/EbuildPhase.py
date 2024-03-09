gentoo/portage

commit d55d415a4f68b0d2b0c4dca00cce2d9aee934faa

EbuildPhase/EbuildBinpkg: Ensure PKGDIR subdirectory permissions
Bug: https://bugs.gentoo.org/712222
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/EbuildPhase.py
@@ -254,6 +254,9 @@ class EbuildPhase(CompositeTask):
                     )
                 else:
                     raise InvalidBinaryPackageFormat(binpkg_format)
+                self.settings.mycpv._db.bintree._ensure_dir(
+                    os.path.dirname(self.settings["PORTAGE_BINPKG_TMPFILE"])
+                )
 
     def _async_start_exit(self, task):
         task.future.cancelled() or task.future.result()

bin/misc-functions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 #
 # Miscellaneous shell functions that make use of the ebuild env but don't need
@@ -511,7 +511,11 @@ __dyn_package() {
 	export SANDBOX_ON="0"
 	[[ -z "${PORTAGE_BINPKG_TMPFILE}" ]] && \
 		die "PORTAGE_BINPKG_TMPFILE is unset"
-	mkdir -p "${PORTAGE_BINPKG_TMPFILE%/*}" || die "mkdir failed"
+	if [[ ! -d ${PORTAGE_BINPKG_TMPFILE%/*} ]]; then
+		# Warn because we don't set PKGDIR directory permissions here.
+		ewarn "__dyn_package: Creating PORTAGE_BINPKG_TMPFILE parent dir: ${PORTAGE_BINPKG_TMPFILE%/*}"
+		mkdir -p "${PORTAGE_BINPKG_TMPFILE%/*}" || die "mkdir failed"
+	fi
 
 	if [[ ! -z "${BUILD_ID}" ]]; then
 		echo -n "${BUILD_ID}" > "${PORTAGE_BUILDDIR}"/build-info/BUILD_ID

lib/_emerge/EbuildBinpkg.py
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import io
@@ -29,6 +29,7 @@ class EbuildBinpkg(CompositeTask):
         pkg_allocated_path, build_id = bintree.getname_build_id(
             pkg.cpv, allocate_new=True
         )
+        bintree._ensure_dir(os.path.dirname(pkg_allocated_path))
 
         self.pkg_allocated_path = pkg_allocated_path
         self._binpkg_tmpfile = self.pkg_allocated_path + "." + str(portage.getpid())

lib/portage/dbapi/bintree.py
@@ -72,6 +72,7 @@ import traceback
 import warnings
 from gzip import GzipFile
 from itertools import chain
+from pathlib import PurePath
 from urllib.parse import urlparse
 
 
@@ -839,11 +840,18 @@ class binarytree:
             return
         pkgdir_gid = pkgdir_st.st_gid
         pkgdir_grp_mode = 0o2070 & pkgdir_st.st_mode
-        try:
-            ensure_dirs(path, gid=pkgdir_gid, mode=pkgdir_grp_mode, mask=0)
-        except PortageException:
-            if not os.path.isdir(path):
-                raise
+
+        components = []
+        for component in PurePath(path).relative_to(self.pkgdir).parts:
+            components.append(component)
+            component_path = os.path.join(self.pkgdir, *components)
+            try:
+                ensure_dirs(
+                    component_path, gid=pkgdir_gid, mode=pkgdir_grp_mode, mask=0
+                )
+            except PortageException:
+                if not os.path.isdir(component_path):
+                    raise
 
     def _file_permissions(self, path):
         try:

commit c3ed026be40b4cf857b33854e70f59f7e016e3b5

_setup_repo_revisions: Skip async_aux_get for INHERITED
Since INHERITED is always available for the ebuild environment,
get it from settings.configdict["pkg"]["INHERITED"].

Fixes: d55d415a4f68 ("EbuildPhase/EbuildBinpkg: Ensure PKGDIR subdirectory permissions")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/EbuildPhase.py
@@ -92,17 +92,10 @@ async def _setup_repo_revisions(settings):
         and repo_name
         and "PORTAGE_REPO_REVISIONS" not in settings.configdict["pkg"]
     ):
-        inherits = frozenset(
-            (
-                await db.async_aux_get(
-                    settings.mycpv,
-                    ["INHERITED"],
-                    myrepo=repo_name,
-                )
-            )[0].split()
-        )
         repo = db.repositories[repo_name]
-        ec_dict = repo.eclass_db.get_eclass_data(inherits)
+        ec_dict = repo.eclass_db.get_eclass_data(
+            settings.configdict["pkg"]["INHERITED"].split()
+        )
         referenced_repos = {repo.name: repo}
         for ec_info in ec_dict.values():
             ec_repo = db.repositories.get_repo_for_location(