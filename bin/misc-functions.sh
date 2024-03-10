gentoo/portage

commit 8eb2502bf264d54dfbf180222f162f7a593b947d

phase-functions: prematurely delete WORKDIR if FEATURES=merge-wait
Using the merge-wait feature together with many parallel emerge jobs
potentially leads to a high disk space usage due to the created
portage build directories waiting for their packages to be merged into
the live filesystem prior portage will clean them. This can easily
lead to out-of-space errors.

Prematurely deleting WORKDIR at the end of src_install() helps
reducing the temporarily used disk space. All that portage need to
merge a package into the live filesystem are the files in the image/
directory (D). (At least in theory)

Signed-off-by: Florian Schmaus <flow@gentoo.org>
Closes: https://github.com/gentoo/portage/pull/1302
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-functions.sh
@@ -723,6 +723,13 @@ __dyn_install() {
 		>> DEBUGBUILD
 	fi
 	trap - SIGINT SIGQUIT
+
+	# Prematurely delete WORKDIR in case merge-wait is enabled to
+	# decrease the space used by portage build directories until the
+	# packages are merged and cleaned.
+	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
+		rm -rf "${WORKDIR}"
+	fi
 }
 
 __dyn_help() {

commit 8ac72ee300c1e9a55f9c1ee96438e8f12d64dd20

install_qa_check: prematurely delete WORKDIR if FEATURES=merge-wait
Delay this until after the install qa hooks so that WORKDIR is
accessible to these hooks:

* bin/install-qa-check.d/90config-impl-decl
* gentoo/metadata/install-qa-check.d/60python-tests

Bug: https://github.com/gentoo/portage/pull/1302#issuecomment-1987303879
Fixes: 8eb2502bf264 ("phase-functions: prematurely delete WORKDIR if FEATURES=merge-wait")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

bin/misc-functions.sh
@@ -255,6 +255,13 @@ install_qa_check() {
 			"${PORTAGE_BIN_PATH}"/estrip --prepallstrip
 		fi
 	fi
+
+	# Prematurely delete WORKDIR in case merge-wait is enabled to
+	# decrease the space used by portage build directories until the
+	# packages are merged and cleaned.
+	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
+		rm -rf "${WORKDIR}"
+	fi
 }
 
 __dyn_instprep() {

bin/phase-functions.sh
@@ -724,13 +724,6 @@ __dyn_install() {
 		>> DEBUGBUILD
 	fi
 	trap - SIGINT SIGQUIT
-
-	# Prematurely delete WORKDIR in case merge-wait is enabled to
-	# decrease the space used by portage build directories until the
-	# packages are merged and cleaned.
-	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
-		rm -rf "${WORKDIR}"
-	fi
 }
 
 __dyn_help() {