3796ccdf04122cbd1a03e023dff2a4a684e42bc6
Kerin Millar <kfm@plushkava.net>
2024-07-27 21:22:30 +0100

    phase-helpers.sh: check the exit status of 7z directly
    There's no need to check $? specifically. While at it, use printf
    instead of echo.
    
    Signed-off-by: Kerin Millar <kfm@plushkava.net>
    Signed-off-by: Sam James <sam@gentoo.org>

--- bin/phase-helpers.sh
+++ bin/phase-helpers.sh
@@ -426,9 +426,8 @@ unpack() {
 				;;
 			7z)
 				local my_output
-				my_output="$(7z x -y "${srcdir}${f}")"
-				if [[ $? -ne 0 ]]; then
-					echo "${my_output}" >&2
+				if ! my_output=$(7z x -y "${srcdir}${f}"); then
+					printf '%s\n' "${my_output}" >&2
 					die "${myfail}"
 				fi
 				;;
