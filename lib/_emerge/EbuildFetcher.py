gentoo/portage

commit 74e29110d86a55ac74fdde3e1b79506b3fb695c2

_EbuildFetcherProcess: Suppress CancelledError
Suppress CancelledError when attempting to cache the
result in the _async_uri_map method. The cancelled
result is returned for the caller to handle.

Bug: https://bugs.gentoo.org/937888
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/EbuildFetcher.py
@@ -373,7 +373,7 @@ class _EbuildFetcherProcess(ForkProcess):
         def cache_result(result):
             try:
                 self._uri_map = result.result()
-            except Exception:
+            except (CancelledError, Exception):
                 # The caller handles this when it retrieves the result.
                 pass
 

commit e4f0fa4a316aba2cf2b1a47c9d2974710a1d8bd3

_EbuildFetcherProcess: Suppress asyncio.CancelledError
Fixes: 74e29110d86a ("_EbuildFetcherProcess: Suppress CancelledError")
Bug: https://bugs.gentoo.org/937888
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/_emerge/EbuildFetcher.py
@@ -373,7 +373,7 @@ class _EbuildFetcherProcess(ForkProcess):
         def cache_result(result):
             try:
                 self._uri_map = result.result()
-            except (CancelledError, Exception):
+            except (asyncio.CancelledError, Exception):
                 # The caller handles this when it retrieves the result.
                 pass
 