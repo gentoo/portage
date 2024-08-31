gentoo/portage

commit a28a0fd6600242a2e062a0fd2d7d5be95296b7ae

JobStatusDisplay: increase the default width to 100
Conservatively modernize the default width value from 80 to
100. Usually, I do not care much about the width, but the job status
display holds more information these days, compared to the times where
this value was set.

Plus, self._isatty has the tendency to return false, even though
portage is actually run within a tty. The prime examples causing this
are systemd-run (and run0). Therefore, a conservatively increasing the
value is sensible.

Signed-off-by: Florian Schmaus <flow@gentoo.org>
Closes: https://github.com/gentoo/portage/pull/1348
Signed-off-by: James Le Cuirot <chewi@gentoo.org>

lib/_emerge/JobStatusDisplay.py
@@ -65,7 +65,7 @@ class JobStatusDisplay:
         if self._isatty:
             width = portage.output.get_term_size()[1]
         else:
-            width = 80
+            width = 100
         self._set_width(width)
 
     def _set_width(self, width):

commit 2b47b684dc6f4814c0b66bbb93617048df06b433

JobStatusDisplay: fix setting the default width to 100
This fixes a28a0fd66002 ("JobStatusDisplay: increase the default width
to 100") which attempted to set the default width to 100. However, the
out of bounds check in _set_width() was not adjusted. Therefore, the
default width was effectively still 80.

Fixes: a28a0fd6600242a2e062a0fd2d7d5be95296b7ae
Signed-off-by: Florian Schmaus <flow@gentoo.org>

lib/_emerge/JobStatusDisplay.py
@@ -71,8 +71,8 @@ class JobStatusDisplay:
     def _set_width(self, width):
         if width == getattr(self, "width", None):
             return
-        if width <= 0 or width > 80:
-            width = 80
+        if width <= 0 or width > 100:
+            width = 100
         object.__setattr__(self, "width", width)
         object.__setattr__(self, "_jobs_column_width", width - 32)
 