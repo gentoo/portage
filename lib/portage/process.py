gentoo/portage

commit 7218c9cea6185c82fbc1719489a2d8e814d05ff1

Fix asyncio.iscoroutinefunction DeprecationWarning in Python 3.14.0a1
DeprecationWarning: 'asyncio.iscoroutinefunction' is deprecated and slated for removal in Python 3.16; use inspect.iscoroutinefunction() instead

Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/process.py
@@ -23,6 +23,11 @@ from dataclasses import dataclass
 from functools import lru_cache, partial
 from typing import Any, Optional, Callable, Union
 
+try:
+    from inspect import iscoroutinefunction
+except ImportError:
+    iscoroutinefunction = _asyncio.iscoroutinefunction
+
 from portage import os
 from portage import _encodings
 from portage import _unicode_encode
@@ -203,7 +208,7 @@ def atexit_register(func, *args, **kargs):
     manually by calling the run_exitfuncs() function in this module."""
     # The internal asyncio wrapper module would trigger a circular import
     # if used here.
-    if _asyncio.iscoroutinefunction(func):
+    if iscoroutinefunction(func):
         # Add this coroutine function to the exit handlers for the loop
         # which is associated with the current thread.
         global_event_loop()._coroutine_exithandlers.append((func, args, kargs))

lib/portage/util/futures/_asyncio/__init__.py
@@ -38,12 +38,16 @@ from asyncio import (
     FIRST_EXCEPTION,
     Future,
     InvalidStateError,
-    iscoroutinefunction,
     Lock as _Lock,
     shield,
     TimeoutError,
     wait_for,
 )
+
+try:
+    from inspect import iscoroutinefunction
+except ImportError:
+    iscoroutinefunction = _asyncio.iscoroutinefunction
 
 import threading
 from typing import Optional

commit ea889ce3eef083ff1e2658d0e70e7bed38015332

Alway import iscoroutinefunction from inspect
This is available in all supported python versions.

Fixes: 7218c9cea618 ("Fix asyncio.iscoroutinefunction DeprecationWarning in Python 3.14.0a1")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/process.py
@@ -22,11 +22,7 @@ import warnings
 from dataclasses import dataclass
 from functools import lru_cache, partial
 from typing import Any, Optional, Callable, Union
-
-try:
-    from inspect import iscoroutinefunction
-except ImportError:
-    iscoroutinefunction = _asyncio.iscoroutinefunction
+from inspect import iscoroutinefunction
 
 from portage import os
 from portage import _encodings

lib/portage/util/futures/_asyncio/__init__.py
@@ -44,11 +44,7 @@ from asyncio import (
     wait_for,
 )
 
-try:
-    from inspect import iscoroutinefunction
-except ImportError:
-    iscoroutinefunction = _asyncio.iscoroutinefunction
-
+from inspect import iscoroutinefunction
 import threading
 from typing import Optional
 