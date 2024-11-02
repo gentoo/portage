gentoo/portage

commit 320df528974c09338c7fef12e41ce66c5593c101

gpkg: avoid os.path.islink where lstat is available
Signed-off-by: Etienne Buira <etienne.buira@free.fr>
Bug: https://bugs.gentoo.org/942512
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/gpkg.py
@@ -1943,7 +1943,7 @@ class gpkg:
 
                 file_stat = os.lstat(f)
 
-                if os.path.islink(f):
+                if stat.S_ISLNK(file_stat):
                     path_link = os.readlink(f)
                     path_link_length = len(
                         os.fsencode(path_link)
@@ -2035,7 +2035,7 @@ class gpkg:
 
             file_stat = os.lstat(path)
 
-            if os.path.islink(path):
+            if stat.S_ISLNK(file_stat):
                 path_link = os.readlink(path)
                 path_link_length = len(
                     _unicode_encode(

commit ac88048bb1d6a28dbd415223e2e0fc3ec8b16a6c

gpkg: Fix S_ISLNK usage
Fixes: 320df528974c ("gpkg: avoid os.path.islink where lstat is available")
Bug: https://bugs.gentoo.org/942512
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/gpkg.py
@@ -1943,7 +1943,7 @@ class gpkg:
 
                 file_stat = os.lstat(f)
 
-                if stat.S_ISLNK(file_stat):
+                if stat.S_ISLNK(file_stat.st_mode):
                     path_link = os.readlink(f)
                     path_link_length = len(
                         os.fsencode(path_link)
@@ -2035,7 +2035,7 @@ class gpkg:
 
             file_stat = os.lstat(path)
 
-            if stat.S_ISLNK(file_stat):
+            if stat.S_ISLNK(file_stat.st_mode):
                 path_link = os.readlink(path)
                 path_link_length = len(
                     _unicode_encode(