6648a0d37131534767f15542f1d2efc3d7201f55
Zac Medico <zmedico@gentoo.org>
2024-11-04 12:50:54 -0800

    install_qa_check: Fix noclean interaction with merge-wait
    Fixes: 8ac72ee300c1 ("install_qa_check: prematurely delete WORKDIR if FEATURES=merge-wait")
    Bug: https://bugs.gentoo.org/942760
    Signed-off-by: Zac Medico <zmedico@gentoo.org>

--- bin/misc-functions.sh
+++ bin/misc-functions.sh
@@ -259,7 +259,9 @@ install_qa_check() {
 	# Prematurely delete WORKDIR in case merge-wait is enabled to
 	# decrease the space used by portage build directories until the
 	# packages are merged and cleaned.
-	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
+	if has merge-wait ${FEATURES} &&
+		! has keepwork ${FEATURES} &&
+		! has noclean ${FEATURES} ; then
 		rm -rf "${WORKDIR}"
 	fi
 }

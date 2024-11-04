gentoo/portage

commit 8ac72ee300c1e9a55f9c1ee96438e8f12d64dd20

install_qa_check: prematurely delete WORKDIR if FEATURES=merge-wait
Delay this until after the install qa hooks so that WORKDIR is
accessible to these hooks:

* bin/install-qa-check.d/90config-impl-decl
* gentoo/metadata/install-qa-check.d/60python-tests

Bug: https://github.com/gentoo/portage/pull/1302#issuecomment-1987303879
Fixes: 8eb2502bf264 ("phase-functions: prematurely delete WORKDIR if FEATURES=merge-wait")
Signed-off-by: Zac Medico <zmedico@gentoo.org>

bin/misc-functions.sh
@@ -255,6 +255,13 @@ install_qa_check() {
 			"${PORTAGE_BIN_PATH}"/estrip --prepallstrip
 		fi
 	fi
+
+	# Prematurely delete WORKDIR in case merge-wait is enabled to
+	# decrease the space used by portage build directories until the
+	# packages are merged and cleaned.
+	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
+		rm -rf "${WORKDIR}"
+	fi
 }
 
 __dyn_instprep() {

bin/phase-functions.sh
@@ -724,13 +724,6 @@ __dyn_install() {
 		>> DEBUGBUILD
 	fi
 	trap - SIGINT SIGQUIT
-
-	# Prematurely delete WORKDIR in case merge-wait is enabled to
-	# decrease the space used by portage build directories until the
-	# packages are merged and cleaned.
-	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
-		rm -rf "${WORKDIR}"
-	fi
 }
 
 __dyn_help() {

commit 6648a0d37131534767f15542f1d2efc3d7201f55

install_qa_check: Fix noclean interaction with merge-wait
Fixes: 8ac72ee300c1 ("install_qa_check: prematurely delete WORKDIR if FEATURES=merge-wait")
Bug: https://bugs.gentoo.org/942760
Signed-off-by: Zac Medico <zmedico@gentoo.org>

bin/misc-functions.sh
@@ -259,7 +259,9 @@ install_qa_check() {
 	# Prematurely delete WORKDIR in case merge-wait is enabled to
 	# decrease the space used by portage build directories until the
 	# packages are merged and cleaned.
-	if has merge-wait ${FEATURES} && ! has keepwork ${FEATURES}; then
+	if has merge-wait ${FEATURES} &&
+		! has keepwork ${FEATURES} &&
+		! has noclean ${FEATURES} ; then
 		rm -rf "${WORKDIR}"
 	fi
 }