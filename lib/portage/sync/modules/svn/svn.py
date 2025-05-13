gentoo/portage

commit 002cd09b455eae1790f113438a030828edf3ef14

Remove portage._shell_quote function
Replaced by shlex.quote, shlex.join, or subprocess.run as appropriate.

Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/sync/modules/svn/svn.py
@@ -2,6 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 import logging
+import shlex
 
 import portage
 from portage import os
@@ -33,14 +34,14 @@ class SVNSync(NewBase):
         exitcode = portage.process.spawn_bash(
             "cd %s; exec svn co %s ."
             % (
-                portage._shell_quote(self.repo.location),
-                portage._shell_quote(svn_root),
+                shlex.quote(self.repo.location),
+                shlex.quote(svn_root),
             ),
             **self.spawn_kwargs,
         )
         if exitcode != os.EX_OK:
             msg = "!!! svn checkout error; exiting."
-            self.logger(self.xterm_titles, msg)
+            selflogger(self.xterm_titles, msg)
             writemsg_level(msg + "\n", noiselevel=-1, level=logging.ERROR)
         return (exitcode, False)
 
@@ -59,7 +60,7 @@ class SVNSync(NewBase):
 
         # svn update
         exitcode = portage.process.spawn_bash(
-            f"cd {portage._shell_quote(self.repo.location)}; exec svn update",
+            f"cd {shlex.quote(self.repo.location)}; exec svn update",
             **self.spawn_kwargs,
         )
         if exitcode != os.EX_OK:
@@ -77,7 +78,7 @@ class SVNSync(NewBase):
         @rtype: (int, bool)
         """
         exitcode = portage.process.spawn_bash(
-            f"cd {portage._shell_quote(self.repo.location)}; exec svn upgrade",
+            f"cd {shlex.quote(self.repo.location)}; exec svn upgrade",
             **self.spawn_kwargs,
         )
         if exitcode != os.EX_OK:

bin/binhost-snapshot
@@ -4,6 +4,7 @@
 
 import argparse
 import os
+import subprocess
 import sys
 import textwrap
 
@@ -107,20 +108,18 @@ def main(argv):
     if not os.path.isdir(binhost_dir):
         parser.error(f"binhost_dir could not be created: '{binhost_dir}'")
 
-    cp_opts = "RP"
+    cp_opts = "-RP"
     if options.hardlinks == "n":
         cp_opts += "p"
     else:
         cp_opts += "l"
 
-    cp_cmd = "cp -{} {} {}".format(
-        cp_opts,
-        portage._shell_quote(src_pkg_dir),
-        portage._shell_quote(snapshot_dir),
-    )
+    try:
+        result = subprocess.run(["cp", cp_opts, src_pkg_dir, snapshot_dir])
+    except OSError:
+        result = None
 
-    ret = os.system(cp_cmd)
-    if not (os.WIFEXITED(ret) and os.WEXITSTATUS(ret) == os.EX_OK):
+    if result is None or result.returncode != 0:
         return 1
 
     infile = open(

bin/ebuild
@@ -49,7 +49,6 @@ import portage
 portage._internal_caller = True
 from portage import os
 from portage import _encodings
-from portage import _shell_quote
 from portage import _unicode_encode
 from portage.const import VDB_PATH
 from portage.exception import (
@@ -165,7 +164,7 @@ def main():
     if ebuild_portdir != vdb_path and ebuild_portdir not in portage.portdb.porttrees:
         portdir_overlay = portage.settings.get("PORTDIR_OVERLAY", "")
         os.environ["PORTDIR_OVERLAY"] = (
-            portdir_overlay + " " + _shell_quote(ebuild_portdir)
+            portdir_overlay + " " + shlex.quote(ebuild_portdir)
         )
 
         print(f"Appending {ebuild_portdir} to PORTDIR_OVERLAY...")

bin/install.py
@@ -174,7 +174,7 @@ def main(args):
         if returncode != os.EX_OK:
             portage.util.writemsg(
                 "!!! install: copy_xattrs failed with the "
-                f"following arguments: {' '.join(portage._shell_quote(x) for x in args)}\n",
+                f"following arguments: {shlex.join(args)}\n",
                 noiselevel=-1,
             )
     return returncode

bin/portageq
@@ -33,6 +33,7 @@ try:
     signal.signal(signal.SIGUSR1, debug_signal)
 
     import argparse
+    import shlex
     import sys
     import types
 
@@ -882,7 +883,7 @@ try:
                 exit_status = 1
 
             if verbose:
-                print(arg + "=" + portage._shell_quote(value))
+                print(arg + "=" + shlex.quote(value))
             else:
                 print(value)
 
@@ -1441,7 +1442,7 @@ try:
         def elog(elog_funcname, lines):
             cmd = f"source '{os.environ['PORTAGE_BIN_PATH']}/isolated-functions.sh' ; "
             for line in lines:
-                cmd += f"{elog_funcname} {portage._shell_quote(line)} ; "
+                cmd += f"{elog_funcname} {shlex.quote(line)} ; "
             subprocess.call([portage.const.BASH_BINARY, "-c", cmd])
 
     else:

lib/_emerge/BinpkgEnvExtractor.py
@@ -2,10 +2,11 @@
 # Distributed under the terms of the GNU General Public License v2
 
 import errno
+import shlex
 
 from _emerge.CompositeTask import CompositeTask
 from _emerge.SpawnProcess import SpawnProcess
-from portage import os, _shell_quote, _unicode_encode
+from portage import os, _unicode_encode
 from portage.const import BASH_BINARY
 
 
@@ -31,7 +32,7 @@ class BinpkgEnvExtractor(CompositeTask):
     def _start(self):
         saved_env_path = self._get_saved_env_path()
         dest_env_path = self._get_dest_env_path()
-        shell_cmd = f"${{PORTAGE_BUNZIP2_COMMAND:-${{PORTAGE_BZIP2_COMMAND}} -d}} -c -- {_shell_quote(saved_env_path)} > {_shell_quote(dest_env_path)}"
+        shell_cmd = f"${{PORTAGE_BUNZIP2_COMMAND:-${{PORTAGE_BZIP2_COMMAND}} -d}} -c -- {shlex.quote(saved_env_path)} > {shlex.quote(dest_env_path)}"
 
         logfile = None
         if self.settings.get("PORTAGE_BACKGROUND") != "subprocess":

lib/_emerge/BinpkgExtractorAsync.py
@@ -45,7 +45,7 @@ class BinpkgExtractorAsync(SpawnProcess):
             if b"--xattrs" in output:
                 tar_options = ["--xattrs", "--xattrs-include='*'"]
                 for x in shlex.split(self.env.get("PORTAGE_XATTR_EXCLUDE", "")):
-                    tar_options.append(portage._shell_quote(f"--xattrs-exclude={x}"))
+                    tar_options.append(shlex.quote(f"--xattrs-exclude={x}"))
                 tar_options = " ".join(tar_options)
 
         decomp = _compressors.get(compression_probe(self.pkg_path))
@@ -119,9 +119,9 @@ class BinpkgExtractorAsync(SpawnProcess):
             "-c",
             textwrap.dedent(
                 f"""
-                    cmd0=(head -c {pkg_xpak.filestat.st_size - pkg_xpak.xpaksize} -- {portage._shell_quote(self.pkg_path)})
+                    cmd0=(head -c {pkg_xpak.filestat.st_size - pkg_xpak.xpaksize} -- {shlex.quote(self.pkg_path)})
                     cmd1=({decomp_cmd})
-                    cmd2=(tar -xp {tar_options} -C {portage._shell_quote(self.image_dir)} -f -);
+                    cmd2=(tar -xp {tar_options} -C {shlex.quote(self.image_dir)} -f -);
                 """
                 """
                     "${cmd0[@]}" | "${cmd1[@]}" | "${cmd2[@]}";

lib/_emerge/MiscFunctionsProcess.py
@@ -1,6 +1,8 @@
 # Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
+import shlex
+
 from _emerge.AbstractEbuildProcess import AbstractEbuildProcess
 import portage
 
@@ -22,7 +24,7 @@ class MiscFunctionsProcess(AbstractEbuildProcess):
             portage_bin_path, os.path.basename(portage.const.MISC_SH_BINARY)
         )
 
-        self.args = [portage._shell_quote(misc_sh_binary)] + self.commands
+        self.args = [shlex.quote(misc_sh_binary)] + self.commands
         if (
             self.logfile is None
             and self.settings.get("PORTAGE_BACKGROUND") != "subprocess"

commit a393a5fefcdfb463a373eed813485f671ba0bbcd

Restore missing period
Fixes: 002cd09b455eae1790f113438a030828edf3ef14
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

lib/portage/sync/modules/svn/svn.py
@@ -41,7 +41,7 @@ class SVNSync(NewBase):
         )
         if exitcode != os.EX_OK:
             msg = "!!! svn checkout error; exiting."
-            selflogger(self.xterm_titles, msg)
+            self.logger(self.xterm_titles, msg)
             writemsg_level(msg + "\n", noiselevel=-1, level=logging.ERROR)
         return (exitcode, False)
 