gentoo/portage

commit 002cd09b455eae1790f113438a030828edf3ef14

Remove portage._shell_quote function
Replaced by shlex.quote, shlex.join, or subprocess.run as appropriate.

Signed-off-by: Mike Gilbert <floppym@gentoo.org>

bin/install.py
@@ -174,7 +174,7 @@ def main(args):
         if returncode != os.EX_OK:
             portage.util.writemsg(
                 "!!! install: copy_xattrs failed with the "
-                f"following arguments: {' '.join(portage._shell_quote(x) for x in args)}\n",
+                f"following arguments: {shlex.join(args)}\n",
                 noiselevel=-1,
             )
     return returncode

bin/binhost-snapshot
@@ -4,6 +4,7 @@
 
 import argparse
 import os
+import subprocess
 import sys
 import textwrap
 
@@ -107,20 +108,18 @@ def main(argv):
     if not os.path.isdir(binhost_dir):
         parser.error(f"binhost_dir could not be created: '{binhost_dir}'")
 
-    cp_opts = "RP"
+    cp_opts = "-RP"
     if options.hardlinks == "n":
         cp_opts += "p"
     else:
         cp_opts += "l"
 
-    cp_cmd = "cp -{} {} {}".format(
-        cp_opts,
-        portage._shell_quote(src_pkg_dir),
-        portage._shell_quote(snapshot_dir),
-    )
+    try:
+        result = subprocess.run(["cp", cp_opts, src_pkg_dir, snapshot_dir])
+    except OSError:
+        result = None
 
-    ret = os.system(cp_cmd)
-    if not (os.WIFEXITED(ret) and os.WEXITSTATUS(ret) == os.EX_OK):
+    if result is None or result.returncode != 0:
         return 1
 
     infile = open(

bin/ebuild
@@ -49,7 +49,6 @@ import portage
 portage._internal_caller = True
 from portage import os
 from portage import _encodings
-from portage import _shell_quote
 from portage import _unicode_encode
 from portage.const import VDB_PATH
 from portage.exception import (
@@ -165,7 +164,7 @@ def main():
     if ebuild_portdir != vdb_path and ebuild_portdir not in portage.portdb.porttrees:
         portdir_overlay = portage.settings.get("PORTDIR_OVERLAY", "")
         os.environ["PORTDIR_OVERLAY"] = (
-            portdir_overlay + " " + _shell_quote(ebuild_portdir)
+            portdir_overlay + " " + shlex.quote(ebuild_portdir)
         )
 
         print(f"Appending {ebuild_portdir} to PORTDIR_OVERLAY...")

bin/portageq
@@ -33,6 +33,7 @@ try:
     signal.signal(signal.SIGUSR1, debug_signal)
 
     import argparse
+    import shlex
     import sys
     import types
 
@@ -882,7 +883,7 @@ try:
                 exit_status = 1
 
             if verbose:
-                print(arg + "=" + portage._shell_quote(value))
+                print(arg + "=" + shlex.quote(value))
             else:
                 print(value)
 
@@ -1441,7 +1442,7 @@ try:
         def elog(elog_funcname, lines):
             cmd = f"source '{os.environ['PORTAGE_BIN_PATH']}/isolated-functions.sh' ; "
             for line in lines:
-                cmd += f"{elog_funcname} {portage._shell_quote(line)} ; "
+                cmd += f"{elog_funcname} {shlex.quote(line)} ; "
             subprocess.call([portage.const.BASH_BINARY, "-c", cmd])
 
     else:

lib/_emerge/BinpkgEnvExtractor.py
@@ -2,10 +2,11 @@
 # Distributed under the terms of the GNU General Public License v2
 
 import errno
+import shlex
 
 from _emerge.CompositeTask import CompositeTask
 from _emerge.SpawnProcess import SpawnProcess
-from portage import os, _shell_quote, _unicode_encode
+from portage import os, _unicode_encode
 from portage.const import BASH_BINARY
 
 
@@ -31,7 +32,7 @@ class BinpkgEnvExtractor(CompositeTask):
     def _start(self):
         saved_env_path = self._get_saved_env_path()
         dest_env_path = self._get_dest_env_path()
-        shell_cmd = f"${{PORTAGE_BUNZIP2_COMMAND:-${{PORTAGE_BZIP2_COMMAND}} -d}} -c -- {_shell_quote(saved_env_path)} > {_shell_quote(dest_env_path)}"
+        shell_cmd = f"${{PORTAGE_BUNZIP2_COMMAND:-${{PORTAGE_BZIP2_COMMAND}} -d}} -c -- {shlex.quote(saved_env_path)} > {shlex.quote(dest_env_path)}"
 
         logfile = None
         if self.settings.get("PORTAGE_BACKGROUND") != "subprocess":

lib/_emerge/BinpkgExtractorAsync.py
@@ -45,7 +45,7 @@ class BinpkgExtractorAsync(SpawnProcess):
             if b"--xattrs" in output:
                 tar_options = ["--xattrs", "--xattrs-include='*'"]
                 for x in shlex.split(self.env.get("PORTAGE_XATTR_EXCLUDE", "")):
-                    tar_options.append(portage._shell_quote(f"--xattrs-exclude={x}"))
+                    tar_options.append(shlex.quote(f"--xattrs-exclude={x}"))
                 tar_options = " ".join(tar_options)
 
         decomp = _compressors.get(compression_probe(self.pkg_path))
@@ -119,9 +119,9 @@ class BinpkgExtractorAsync(SpawnProcess):
             "-c",
             textwrap.dedent(
                 f"""
-                    cmd0=(head -c {pkg_xpak.filestat.st_size - pkg_xpak.xpaksize} -- {portage._shell_quote(self.pkg_path)})
+                    cmd0=(head -c {pkg_xpak.filestat.st_size - pkg_xpak.xpaksize} -- {shlex.quote(self.pkg_path)})
                     cmd1=({decomp_cmd})
-                    cmd2=(tar -xp {tar_options} -C {portage._shell_quote(self.image_dir)} -f -);
+                    cmd2=(tar -xp {tar_options} -C {shlex.quote(self.image_dir)} -f -);
                 """
                 """
                     "${cmd0[@]}" | "${cmd1[@]}" | "${cmd2[@]}";

lib/_emerge/MiscFunctionsProcess.py
@@ -1,6 +1,8 @@
 # Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
+import shlex
+
 from _emerge.AbstractEbuildProcess import AbstractEbuildProcess
 import portage
 
@@ -22,7 +24,7 @@ class MiscFunctionsProcess(AbstractEbuildProcess):
             portage_bin_path, os.path.basename(portage.const.MISC_SH_BINARY)
         )
 
-        self.args = [portage._shell_quote(misc_sh_binary)] + self.commands
+        self.args = [shlex.quote(misc_sh_binary)] + self.commands
         if (
             self.logfile is None
             and self.settings.get("PORTAGE_BACKGROUND") != "subprocess"

lib/_emerge/PackagePhase.py
@@ -1,6 +1,7 @@
 # Copyright 2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
+import shlex
 
 from _emerge.CompositeTask import CompositeTask
 from _emerge.EbuildProcess import EbuildProcess
@@ -60,7 +61,7 @@ class PackagePhase(CompositeTask):
                             "rm -rf {PROOT}; "
                             'cp -pPR $(cp --help | grep -q -- "^[[:space:]]*-l," && echo -l)'
                             ' "${{D}}" {PROOT}'
-                        ).format(PROOT=portage._shell_quote(self._proot)),
+                        ).format(PROOT=shlex.quote(self._proot)),
                     ],
                     background=self.background,
                     env=self.settings.environ(),

commit 22aa61876b841217f59a8438dd89a1c2bef693ba

fix missing shlex import in error path
Regression in commit 002cd09b455eae1790f113438a030828edf3ef14. Since the
function we need isn't invisibly exposed via portage.*, we need to
distinctly import it. We did in most cases, but not here.

Caught by flake8's missing imports linter. Unfortunately, the overall
usefulness of flake8 here is effectively nil, because lazyimport means
*all* imports are "missing". It is too noisy to believe. But I actually
spotted this error, it's not a false positive, and we might as well fix
it immediately.

Fixes: 002cd09b455eae1790f113438a030828edf3ef14
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

bin/install.py
@@ -4,6 +4,7 @@
 
 import argparse
 import os
+import shlex
 import stat
 import sys
 import subprocess