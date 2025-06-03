gentoo/portage

commit d11490f9d83baefc1dad02d9be63c74e4c1da04f

phase-helpers.sh: don't perform a dry run in _eapply_patch()
Presently, the _eapply_patch() function executes a dry run of patch(1)
to determine whether the patch can be applied without fuzz. It then
proceeds to execute patch(1) for application, instructing the utility to
be silent in the case that a fuzz factor of zero is permissible.

Going about it in this way is wasteful. Instead, execute patch(1) just
once while capturing its output. In the case that the patch is applied
successfully, convey the output to STDOUT only if it matches a pattern
identifying a fuzz factor. Otherwise, if the patch was unsuccessfully
applied, convey the output to STDERR.

Also, localise IFS so as to guarantee that the positional parameters are
joined by a space in the diagnostic message.

Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-helpers.sh
@@ -982,8 +982,7 @@ if ___eapi_has_eapply; then
 		local -a operands options
 
 		_eapply_patch() {
-			local prefix=$1 patch=$2
-			local -a patch_opts
+			local prefix=$1 patch=$2 output IFS
 			shift 2
 
 			ebegin "${prefix:-Applying }${patch##*/}"
@@ -991,16 +990,17 @@ if ___eapi_has_eapply; then
 			# -f to avoid interactivity
 			# -g0 to guarantee no VCS interaction
 			# --no-backup-if-mismatch not to pollute the sources
-			patch_opts=( -p1 -f -g0 --no-backup-if-mismatch "$@" )
-
-			# Try applying with -F0 first, output a verbose warning
-			# if fuzz factor is necessary
-			if "${patch_cmd}" "${patch_opts[@]}" --dry-run -s -F0 < "${patch}" &>/dev/null; then
-				patch_opts+=( -s -F0 )
-			fi
-
-			"${patch_cmd}" "${patch_opts[@]}" < "${patch}"
-			if ! eend "$?"; then
+			set -- -p1 -f -g0 --no-backup-if-mismatch "$@"
+			if output=$(LC_ALL= LC_MESSAGES=C "${patch_cmd}" "$@" < "${patch}" 2>&1); then
+				# The patch was successfully applied. Maintain
+				# silence unless applied with fuzz.
+				if [[ ${output} == *[0-9]' with fuzz '[0-9]* ]]; then
+					printf '%s\n' "${output}"
+				fi
+				eend 0
+			else
+				printf '%s\n' "${output}" >&2
+				eend 1
 				__helpers_die "patch -p1 $* failed with ${patch}"
 			fi
 		}

commit a1e6f32ab5034457730a165774b6d34f92e64fc6

Don't report a phantom -p1 option in _eapply_patch() exceptions
In the course of recently re-factoring the eapply() and _eapply_patch()
functions, one of the changes made was to have the patch(1) arguments be
reported exactly as they are in the case that an exception is thrown.
However, I forgot to remove a hard-coded instance of "-p1" in the
message template, which is harmless but confusing. Remove it.

Also, use the ${param@Q} form of expansion to convey both the arguments
and the name of the patch. Doing so is acceptable, given a target of
>=bash-4.4.

Fixes: d11490f9d83baefc1dad02d9be63c74e4c1da04f
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-helpers.sh
@@ -1003,7 +1003,7 @@ if ___eapi_has_eapply; then
 			else
 				printf '%s\n' "${output}" >&2
 				eend 1
-				__helpers_die "patch -p1 $* failed with ${patch}"
+				__helpers_die "patch ${*@Q} failed with ${patch@Q}"
 			fi
 		}
 