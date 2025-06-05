gentoo/portage

commit 5746a0538af080ee4953f5bb43c61852ba4fff72

phase-functions.sh: treat the {i,n}sz variables as scalar in __dyn_install()
Capture the output of du(1) without relying on word splitting behaviour.
Instead, strip the unwanted field afterwards. This eliminates two SC2207
warnings.

Rectify two SC2086 warnings by quoting the expansions conveyed to the
size() helper function.

Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-functions.sh
@@ -634,8 +634,13 @@ __dyn_install() {
 	(
 		hash du 2>/dev/null || exit 0
 
-		local nsz=( $(du -ks "${WORKDIR}") )
-		local isz=( $(du -ks "${D}") )
+		local nsz isz
+
+		nsz=$(du -ks "${WORKDIR}")
+		isz=$(du -ks "${D}")
+		nsz=${nsz%% *}
+		isz=${isz%% *}
+
 		# align $1 to the right to the width of the widest of $1 and $2
 		padl() {
 			local s1=$1
@@ -676,8 +681,8 @@ __dyn_install() {
 			fi
 			echo "${out}"
 		}
-		einfo "Final size of build directory: $(size ${nsz[0]} ${isz[0]})"
-		einfo "Final size of installed tree:  $(size ${isz[0]} ${nsz[0]})"
+		einfo "Final size of build directory: $(size "${nsz}" "${isz}")"
+		einfo "Final size of installed tree:  $(size "${isz}" "${nsz}")"
 	)
 	__vecho
 

commit e7fcb620dec7ada18eded8c805ac60849a0a09f3

phase-functions.sh: work around du(1) not conforming to POSIX
The POSIX specification clearly states that the output format of the
du(1) utility should be in the following format.

"%d %s\n", <size>, <pathname>

Alas, the prevailing implementations flout this requirement, even where
any of the following conditions hold true:

- POSIXLY_CORRECT=1 is defined in the environment
- STDOUT is something other than a terminal
- the -s option isn't specified

Instead, the offending implementations employ <tab> as a field delimiter.

# du -sk /var/empty | od -tx1 -c
0000000  30  09  2f  76  61  72  2f  65  6d  70  74  79  0a
          0  \t   /   v   a   r   /   e   m   p   t   y  \n

Work around this by stripping from the first occuring [[:blank:]] character.

Fixes: 5746a0538af080ee4953f5bb43c61852ba4fff72
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-functions.sh
@@ -638,8 +638,8 @@ __dyn_install() {
 
 		nsz=$(du -ks "${WORKDIR}")
 		isz=$(du -ks "${D}")
-		nsz=${nsz%% *}
-		isz=${isz%% *}
+		nsz=${nsz%%[[:blank:]]*}
+		isz=${isz%%[[:blank:]]*}
 
 		# align $1 to the right to the width of the widest of $1 and $2
 		padl() {