gentoo/portage

commit 3b0150e488972b02c45c71192507300d823a361f

Use contains_word() almost everywhere that it is applicable
As was mentioned by the commit introducing the contains_word() function,
it addresses a use case for which the has() function is rampantly
misappropriated. To briefly recap here, has() compares its first operand
to the following operands in a stringwise fashion. However, it is
commonly employed in the following fashion.

  # Here, wordlist contains zero or more whitespace-separated words.
  has some-word ${wordlist}

Thus, the shell is tasked with splitting the expansion of the variable -
a process which is sensitive to the prevailing value of IFS. Further, it
is exceedingly rare for such expansions to be guarded by the "noglob"
shell option, meaning that unintentional pathname expansion remains an
attendant risk.

The contains_word() function safely and efficiently addresses this
particular use case.

  # Unaffected by IFS. Immune to pathname expansion. Fast.
  contains_word some-word "${wordlist}"

This commit replaces the use of has() with contains_word() in almost
every case where it is applicable, though I elected to defer in several
instances that require further investigation. I would add that this
commit touches 18 files which previously accounted for no fewer than 175
SC2086 warnings. This number is thus reduced to 97 in one fell swoop.

See-also: 4a4631eef7186c29668a8c049d988b61469940fd
Link: https://github.com/gentoo/portage/pull/458
Link: https://www.shellcheck.net/wiki/SC2086
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/bashrc-functions.sh
@@ -6,7 +6,7 @@ register_die_hook() {
 	local hook
 
 	for hook; do
-		if ! has "${hook}" ${EBUILD_DEATH_HOOKS}; then
+		if ! contains_word "${hook}" "${EBUILD_DEATH_HOOKS}"; then
 			export EBUILD_DEATH_HOOKS+=" ${hook}"
 		fi
 	done
@@ -16,7 +16,7 @@ register_success_hook() {
 	local hook
 
 	for hook; do
-		if ! has "${hook}" ${EBUILD_SUCCESS_HOOKS}; then
+		if ! contains_word "${hook}" "${EBUILD_SUCCESS_HOOKS}"; then
 			export EBUILD_SUCCESS_HOOKS+=" ${hook}"
 		fi
 	done

bin/ebuild-helpers/doins
@@ -85,7 +85,7 @@ if [[ "${DOINSSTRICTOPTION}" == 1 ]]; then
 	DOINS_ARGS+=( --strict_option )
 fi
 
-if has xattr ${FEATURES}; then
+if contains_word xattr "${FEATURES}"; then
 	DOINS_ARGS+=(
 		--enable_copy_xattr
 		"--xattr_exclude=${PORTAGE_XATTR_EXCLUDE}"

bin/ebuild.sh
@@ -258,7 +258,7 @@ inherit() {
 			# disabled for nofetch, since that can be called by repoman and
 			# that triggers bug #407449 due to repoman not exporting
 			# non-essential variables such as INHERITED.
-			if ! has ${ECLASS} ${INHERITED} ${__INHERITED_QA_CACHE} ; then
+			if ! contains_word "${ECLASS}" "${INHERITED} ${__INHERITED_QA_CACHE}"; then
 				eqawarn "QA Notice: Eclass '${ECLASS}' inherited illegally in ${CATEGORY}/${PF} ${EBUILD_PHASE}"
 			fi
 		fi
@@ -374,7 +374,9 @@ inherit() {
 			fi
 			unset $__export_funcs_var
 
-			has $1 ${INHERITED} || export INHERITED="${INHERITED} $1"
+			if ! contains_word "$1" "${INHERITED}"; then
+				export INHERITED="${INHERITED} $1"
+			fi
 			if [[ ${ECLASS_DEPTH} -eq 1 ]]; then
 				export PORTAGE_EXPLICIT_INHERIT="${PORTAGE_EXPLICIT_INHERIT} $1"
 			fi
@@ -707,11 +709,11 @@ if [[ ${EBUILD_PHASE} != clean?(rm) ]]; then
 
 		if [[ ${EBUILD_PHASE} != depend ]] ; then
 
-			if has distcc ${FEATURES} ; then
+			if contains_word distcc "${FEATURES}"; then
 				[[ -n ${DISTCC_LOG} ]] && addwrite "${DISTCC_LOG%/*}"
 			fi
 
-			if has ccache ${FEATURES} ; then
+			if contains_word ccache "${FEATURES}"; then
 				if [[ -n ${CCACHE_DIR} ]] ; then
 					addread "${CCACHE_DIR}"
 					addwrite "${CCACHE_DIR}"
@@ -723,7 +725,7 @@ if [[ ${EBUILD_PHASE} != clean?(rm) ]]; then
 	fi
 fi
 
-if has nostrip ${FEATURES} ${PORTAGE_RESTRICT} || has strip ${PORTAGE_RESTRICT} ; then
+if contains_word nostrip "${FEATURES} ${PORTAGE_RESTRICT}" || contains_word strip "${PORTAGE_RESTRICT}"; then
 	export DEBUGBUILD=1
 fi
 

bin/emerge-webrsync
@@ -111,7 +111,8 @@ handle_pgp_setup() {
 	# WEBRSYNC_VERIFY_SIGNATURE=2: use legacy FEATURES="webrsync-gpg"
 	WEBRSYNC_VERIFY_SIGNATURE=1
 
-	has webrsync-gpg ${FEATURES} && webrsync_gpg=1 || webrsync_gpg=0
+	contains_word webrsync-gpg "${FEATURES}"
+	webrsync_gpg=$(( $? == 0 ))
 
 	repo_has_webrsync_verify=$(
 		has $(__repo_attr "${repo_name}" sync-webrsync-verify-signature	| LC_ALL=C tr '[:upper:]' '[:lower:]') true yes
@@ -412,7 +413,7 @@ sync_local() {
 	[[ ${PORTAGE_QUIET} -eq 1 ]] || einfo "Syncing local repository ..."
 
 	local ownership="${PORTAGE_USERNAME}:${PORTAGE_GRPNAME}"
-	if has usersync ${FEATURES} ; then
+	if contains_word usersync "${FEATURES}"; then
 		case "${USERLAND}" in
 			BSD)
 				ownership=$(stat -f '%Su:%Sg' "${repo_location}")
@@ -457,7 +458,7 @@ sync_local() {
 		[[ ${PORTAGE_QUIET} == 1 ]] || einfo "Cleaning up ..."
 	fi
 
-	if has metadata-transfer ${FEATURES} ; then
+	if contains_word metadata-transfer "${FEATURES}"; then
 		einfo "Updating cache ..."
 		"${emerge}" --metadata
 	fi
@@ -466,7 +467,9 @@ sync_local() {
 	[[ -x "${post_sync}" ]] && "${post_sync}"
 
 	# --quiet suppresses output if there are no relevant news items
-	has news ${FEATURES} && "${emerge}" --check-news --quiet
+	if contains_word news "${FEATURES}"; then
+		"${emerge}" --check-news --quiet
+	fi
 	return 0
 }
 
@@ -700,7 +703,7 @@ main() {
 	if [[ ! -d ${repo_location} ]]; then
 		mkdir -p "${repo_location}" || die
 
-		if has usersync ${FEATURES} ; then
+		if contains_word usersync "${FEATURES}"; then
 			chown "${PORTAGE_USERNAME}":"${PORTAGE_GRPNAME}" "${repo_location}" || die
 		fi
 	fi

bin/install-qa-check.d/05prefix
@@ -96,11 +96,10 @@ install_qa_check_prefix() {
 				echo "${fn#${D}}:${line[0]} (script ${fn##*/} installed in PATH but interpreter ${line[0]} not found)" \
 					>> "${T}"/non-prefix-shebangs-errs
 			fi
-		else
+		elif contains_word stricter "${FEATURES}"; then
 			# unprefixed/invalid shebang, but outside ${PATH}, this may be
 			# intended (e.g. config.guess) so remain silent by default
-			has stricter ${FEATURES} && \
-				eqawarn "QA Notice: invalid shebang in ${fn#${D}}: ${line[0]}"
+			eqawarn "QA Notice: invalid shebang in ${fn#${D}}: ${line[0]}"
 		fi
 	done
 	if [[ -e "${T}"/non-prefix-shebangs-errs ]] ; then

bin/install-qa-check.d/10executable-issues
@@ -4,7 +4,7 @@
 # text relocations, executable stacks
 
 elf_check() {
-	if ! type -P scanelf >/dev/null || has binchecks ${PORTAGE_RESTRICT}; then
+	if ! type -P scanelf >/dev/null || contains_word binchecks "${PORTAGE_RESTRICT}"; then
 		return
 	fi
 
@@ -12,7 +12,7 @@ elf_check() {
 	local f x
 
 	# display warnings when using stricter because we die afterwards
-	if has stricter ${FEATURES} ; then
+	if contains_word stricter "${FEATURES}"; then
 		local PORTAGE_QUIET
 	fi
 
@@ -33,7 +33,7 @@ elf_check() {
 	for dir in "${forbidden_dirs[@]}"; do
 		while read l; do
 			f+="  ${l/:/\n    RPATH: }\n"
-			if ! has stricter ${FEATURES}; then
+			if ! contains_word stricter "${FEATURES}"; then
 				__vecho "Auto fixing rpaths for ${l%%:*}"
 				TMPDIR="${dir}" scanelf -BXr "${l%%:*}" -o /dev/null
 			fi
@@ -54,7 +54,7 @@ elf_check() {
 		eqawarn " with the maintainer of the package."
 		eqawarn "${f}${f:+${x:+\n}}${x}"
 		__vecho -ne '\n'
-		if [[ -n ${x} ]] || has stricter ${FEATURES} ; then
+		if [[ -n ${x} ]] || contains_word stricter "${FEATURES}"; then
 			insecure_rpath=1
 		fi
 	fi
@@ -139,7 +139,7 @@ elf_check() {
 
 	if [[ ${insecure_rpath} -eq 1 ]] ; then
 		die "Aborting due to serious QA concerns with RUNPATH/RPATH"
-	elif [[ -n ${die_msg} ]] && has stricter ${FEATURES} ; then
+	elif [[ -n ${die_msg} ]] && contains_word stricter "${FEATURES}"; then
 		die "Aborting due to QA concerns: ${die_msg}"
 	fi
 }

bin/install-qa-check.d/10ignored-flags
@@ -3,7 +3,7 @@
 # QA checks for ignored *FLAGS.
 
 ignored_flag_check() {
-	if ! type -P scanelf >/dev/null || has binchecks ${PORTAGE_RESTRICT}; then
+	if ! type -P scanelf >/dev/null || contains_word binchecks "${PORTAGE_RESTRICT}"; then
 		return
 	fi
 
@@ -67,8 +67,9 @@ ignored_flag_check() {
 	fi
 
 	# Check for files built without respecting LDFLAGS
-	if [[ "${LDFLAGS}" == *,--defsym=__gentoo_check_ldflags__* ]] && \
-		! has binchecks ${PORTAGE_RESTRICT} ; then
+	if [[ "${LDFLAGS}" == *,--defsym=__gentoo_check_ldflags__* ]] \
+		&& ! contains_word binchecks "${PORTAGE_RESTRICT}"
+	then
 		f=$(LC_ALL=C comm -2 -3 <(scanelf -qyRF '#k%p' -k .dynsym "${ED%/}/" | LC_ALL=C sort) \
 			<(scanelf -qyRF '#s%p' -s __gentoo_check_ldflags__ "${ED%/}/" | LC_ALL=C sort))
 		if [[ -n ${f} ]] ; then

bin/install-qa-check.d/60pkgconfig
@@ -136,7 +136,7 @@ pkgconfig_check() {
 		# Skip result reporting if *_p* because for both _pN and _preN, we
 		# don't generally expect the versions to be exactly accurate, and
 		# we want to avoid false positives.
-		if [[ ${#bad_files[@]} -gt 0 && ${PV} != *_p* ]] && ! has live ${PROPERTIES} ; then
+		if [[ ${#bad_files[@]} -gt 0 && ${PV} != *_p* ]] && ! contains_word live "${PROPERTIES}"; then
 			eqawarn "QA Notice: pkg-config files with mismatched Version found!"
 			eqawarn "The Version field of the following files does not match ${QA_PKGCONFIG_VERSION}"
 			local bad_file

commit bf17d5963cead8c930d3e996f976e57b99d839f8

bashrc-functions.sh: have register_{die,success}_hook() validate their parameters
Presently, the register_die_hook() and register_success_hook() functions
make use of the contains_word() function in order to determine whether
the 'EBUILD_DEATH_HOOKS' and 'EBUILD_SUCCESS_HOOKS' variables need to be
extended. This is so as to ensure that the same hook cannot be added
more than once.

  # Adds a hook for the my_cleanup function.
  register_die_hook my_cleanup

  # This is a no-op because the hook is already registered.
  register_die_hook my_cleanup

However, the contains_word() function will always return false where its
first argument contains whitespace, for which there is a good reason.

  # We may consider this to be a scalar variable comprising four words.
  wordlist='foo bar baz quux'

  # Thus, it would not be appropriate for this to return true.
  contains_word 'bar baz' "${wordlist}"

So far, so good. However, one must then consider that, whenever
contains_word() returns false, it is effectively granting permission for
the hook to be added again, giving rise to the following edge case.

  # Two hooks will be registered by the names of "bar" and "baz".
  # Not an intended use case; two arguments should be given.
  register_die_hook 'bar baz'

  # This will register both again, despite having been added already.
  register_die_hook 'bar baz'

To remedy this, have the register_die_hook() and register_success_hook()
functions validate their parameters. The nature of the validation is
straightforward: if a given parameter be empty, or contain any
whitespace, then it shall be considered as invalid and disregarded.

Though such cases should be vanishingly rare in practice, I would have
liked to raise a warning for them. However, /etc/portage/bashrc is
sourced so often that, barring an improved warning mechanism, it is
currently impractical.

Fixes: 3b0150e488972b02c45c71192507300d823a361f
See-also: 4a4631eef7186c29668a8c049d988b61469940fd

bin/bashrc-functions.sh
@@ -6,7 +6,9 @@ register_die_hook() {
 	local hook
 
 	for hook; do
-		if ! contains_word "${hook}" "${EBUILD_DEATH_HOOKS}"; then
+		if [[ ${hook} != +([![:space:]]) ]]; then
+			:
+		elif ! contains_word "${hook}" "${EBUILD_DEATH_HOOKS}"; then
 			export EBUILD_DEATH_HOOKS+=" ${hook}"
 		fi
 	done
@@ -16,7 +18,9 @@ register_success_hook() {
 	local hook
 
 	for hook; do
-		if ! contains_word "${hook}" "${EBUILD_SUCCESS_HOOKS}"; then
+		if [[ ${hook} != +([![:space:]]) ]]; then
+			:
+		elif ! contains_word "${hook}" "${EBUILD_SUCCESS_HOOKS}"; then
 			export EBUILD_SUCCESS_HOOKS+=" ${hook}"
 		fi
 	done