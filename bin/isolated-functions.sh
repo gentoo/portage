gentoo/portage

commit 91f7b072c851e69abefafed1b43902772fb4058c

isolated-functions.sh: improve the ERE detecting -j in MAKEOPTS
In many ways, to parse -j and/or --jobs from MAKEOPTS is a fool's
errand. There is no way of going about it correctly, short of
implementing an option parser that behaves precisely as does that of
make(1) itself.

Still, the regular expression that is presently employed can be made
less worse, so to speak. This commit revises it to be as follows.

.*[[:space:]]
(
        # short options other than -j, if any, leading up to -j,
        # optionally followed by whitespace
        -[^j]*j[[:space:]]*
        |
        # --jobs followed by either = or whitespace
        --jobs(=|[[:space:]]+)
)
([0-9]+)    # the job count ...
[[:space:]] # which must not have any trailing garbage

The following table shows the increased strictness of this expression.

SUBSTRING  BEFORE   AFTER
--jobs= 4  Valid    Invalid
--jobs 4x  Valid    Invalid
-j4x       Valid    Invalid
-jj4       Valid    Invalid

Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/isolated-functions.sh
@@ -496,11 +496,8 @@ fi
 ___makeopts_jobs() {
 	local jobs
 
-	# Copied from multiprocessing.eclass:makeopts_jobs
-	# This assumes the first .* will be more greedy than the second .*
-	# since POSIX doesn't specify a non-greedy match (i.e. ".*?").
-	if [[ " ${MAKEOPTS} " =~ .*[[:space:]](-[a-z]*j|--jobs[=[:space:]])[[:space:]]*([0-9]+).* ]]; then
-		jobs=${BASH_REMATCH[2]}
+	if [[ " ${MAKEOPTS} " =~ .*[[:space:]](-[^j]*j[[:space:]]*|--jobs(=|[[:space:]]+))([0-9]+)[[:space:]] ]]; then
+		jobs=${BASH_REMATCH[3]}
 	elif jobs=$({ getconf _NPROCESSORS_ONLN || sysctl -n hw.ncpu; } 2>/dev/null); then
 		:
 	else

commit c53f2d7aee038cba983c312a12738a58e6eed5a1

isolated-functions.sh: correctly handle bundled -j options in MAKEOPTS
Recently, the extended regular expression that is used to parse -j and
--jobs out of MAKEOPTS was adjusted so as to handle some edge cases.
Yet, I inadvertently introduced a regression by employing [^j] to match
short options preceding a bundled -j option. Of course, it should be
[A-Ia-iK-Zk-z] and acted upon with the C/POSIX collation in effect. The
resulting regular expression is as follows.

.*[[:space:]]
(
        # short options other than -j, if any, leading up to -j,
        # optionally followed by whitespace
        -[A-Ia-iK-Zk-z]*j[[:space:]]*
        |
        # --jobs followed by either = or whitespace
        --jobs(=|[[:space:]]+)
)
([0-9]+)    # the job count ...
[[:space:]] # which must not have any trailing garbage

Fixes: 91f7b072c851e69abefafed1b43902772fb4058c
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/isolated-functions.sh
@@ -494,9 +494,9 @@ if [[ -z ${XARGS} ]] ; then
 fi
 
 ___makeopts_jobs() {
-	local ere jobs
+	local LC_ALL LC_COLLATE=C ere jobs
 
-	ere='.*[[:space:]](-[^j]*j[[:space:]]*|--jobs(=|[[:space:]]+))([0-9]+)[[:space:]]'
+	ere='.*[[:space:]](-[A-Ia-iK-Zk-z]*j[[:space:]]*|--jobs(=|[[:space:]]+))([0-9]+)[[:space:]]'
 
 	if [[ " ${MAKEOPTS} " =~ $ere ]]; then
 		jobs=$(( 10#${BASH_REMATCH[3]} ))