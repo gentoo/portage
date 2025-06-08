gentoo/portage

commit b6a2901f66b6c242bff58e91d600d9529c0732a3

Support cross-root package moves
Bug: https://bugs.gentoo.org/946326
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/_global_updates.py
@@ -18,7 +18,7 @@ from portage.update import (
 from portage.util import grabfile, writemsg, writemsg_stdout, write_atomic
 
 
-def _global_updates(trees, prev_mtimes, quiet=False, if_mtime_changed=True):
+def _global_updates(root, trees, prev_mtimes, quiet=False, if_mtime_changed=True):
     """
     Perform new global updates if they exist in 'profiles/updates/'
     subdirectories of all active repositories (PORTDIR + PORTDIR_OVERLAY).
@@ -35,21 +35,24 @@ def _global_updates(trees, prev_mtimes, quiet=False, if_mtime_changed=True):
     """
     # only do this if we're root and not running repoman/ebuild digest
 
-    if secpass < 2 or "SANDBOX_ACTIVE" in os.environ or len(trees) != 1:
+    if secpass < 2 or "SANDBOX_ACTIVE" in os.environ:
         return False
 
-    vardb = trees[trees._running_eroot]["vartree"].dbapi
+    vardb = trees[root]["vartree"].dbapi
     vardb.lock()
     try:
         return _do_global_updates(
-            trees, prev_mtimes, quiet=quiet, if_mtime_changed=if_mtime_changed
+            root,
+            trees,
+            prev_mtimes,
+            quiet=quiet,
+            if_mtime_changed=if_mtime_changed,
         )
     finally:
         vardb.unlock()
 
 
-def _do_global_updates(trees, prev_mtimes, quiet=False, if_mtime_changed=True):
-    root = trees._running_eroot
+def _do_global_updates(root, trees, prev_mtimes, quiet=False, if_mtime_changed=True):
     mysettings = trees[root]["vartree"].settings
     portdb = trees[root]["porttree"].dbapi
     vardb = trees[root]["vartree"].dbapi

bin/fixpackages
@@ -54,7 +54,9 @@ def main():
         portage.writemsg(f"!!! {str(e)}\n")
         del e
 
-    _global_updates(mytrees, mtimedb["updates"], if_mtime_changed=False)
+    _global_updates(
+        mysettings["EROOT"], mytrees, mtimedb["updates"], if_mtime_changed=False
+    )
 
     print()
     print("Done.")

lib/_emerge/actions.py
@@ -2956,6 +2956,12 @@ def load_emerge_config(emerge_config=None, env=None, **kargs):
     emerge_config.running_config = emerge_config.trees[
         emerge_config.trees._running_eroot
     ]["root_config"]
+    if target_eroot != emerge_config.trees._running_eroot:
+        emerge_config.running_config.mtimedb = portage.MtimeDB(
+            os.path.join(
+                emerge_config.trees._running_eroot, portage.CACHE_PATH, "mtimedb"
+            )
+        )
     QueryCommand._db = emerge_config.trees
 
     return emerge_config
@@ -3433,25 +3439,30 @@ def repo_name_duplicate_check(trees):
 
 def run_action(emerge_config):
     # skip global updates prior to sync, since it's called after sync
-    if (
-        emerge_config.action not in ("help", "info", "sync", "version")
-        and emerge_config.opts.get("--package-moves") != "n"
-        and _global_updates(
-            emerge_config.trees,
-            emerge_config.target_config.mtimedb["updates"],
-            quiet=("--quiet" in emerge_config.opts),
-        )
-    ):
-        emerge_config.target_config.mtimedb.commit()
-        # Reload the whole config from scratch.
-        load_emerge_config(emerge_config=emerge_config)
+    configs = [emerge_config.target_config]
+    if emerge_config.target_config.root != emerge_config.running_config.root:
+        configs.append(emerge_config.running_config)
+    for root_config in configs:
+        if (
+            emerge_config.action not in ("help", "info", "sync", "version")
+            and emerge_config.opts.get("--package-moves") != "n"
+            and _global_updates(
+                root_config.root,
+                emerge_config.trees,
+                root_config.mtimedb["updates"],
+                quiet=("--quiet" in emerge_config.opts),
+            )
+        ):
+            root_config.mtimedb.commit()
+            # Reload the whole config from scratch.
+            load_emerge_config(emerge_config=emerge_config)
 
-        # Let's autoclean if we applied updates, rather than always doing it
-        # bug #792195
-        emerge_config.target_config.settings.unlock()
-        emerge_config.target_config.settings["AUTOCLEAN"] = "yes"
-        emerge_config.target_config.settings.backup_changes("AUTOCLEAN")
-        emerge_config.target_config.settings.lock()
+            # Let's autoclean if we applied updates, rather than always doing it
+            # bug #792195
+            root_config.settings.unlock()
+            root_config.settings["AUTOCLEAN"] = "yes"
+            root_config.settings.backup_changes("AUTOCLEAN")
+            root_config.settings.lock()
 
     xterm_titles = "notitles" not in emerge_config.target_config.settings.features
     if xterm_titles:

lib/portage/emaint/modules/sync/sync.py
@@ -270,14 +270,24 @@ class SyncRepos:
         return (returncode, None)
 
     def _do_pkg_moves(self):
-        if self.emerge_config.opts.get("--package-moves") != "n" and _global_updates(
-            self.emerge_config.trees,
-            self.emerge_config.target_config.mtimedb["updates"],
-            quiet=("--quiet" in self.emerge_config.opts),
+        configs = [self.emerge_config.target_config]
+        if (
+            self.emerge_config.target_config.root
+            != self.emerge_config.running_config.root
         ):
-            self.emerge_config.target_config.mtimedb.commit()
-            # Reload the whole config.
-            self._reload_config()
+            configs.append(self.emerge_config.running_config)
+        for root_config in configs:
+            if self.emerge_config.opts.get(
+                "--package-moves"
+            ) != "n" and _global_updates(
+                root_config.root,
+                self.emerge_config.trees,
+                root_config.mtimedb["updates"],
+                quiet=("--quiet" in self.emerge_config.opts),
+            ):
+                root_config.mtimedb.commit()
+                # Reload the whole config.
+                self._reload_config()
 
     def _check_updates(self):
         mybestpv = self.emerge_config.target_config.trees["porttree"].dbapi.xmatch(

lib/portage/tests/update/test_move_ent.py
@@ -93,7 +93,7 @@ class MoveEntTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
 
@@ -198,7 +198,7 @@ class MoveEntTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
 
@@ -318,7 +318,7 @@ class MoveEntTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
                 finally:

lib/portage/tests/update/test_move_slot_ent.py
@@ -121,7 +121,7 @@ class MoveSlotEntTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
 
@@ -264,7 +264,7 @@ class MoveSlotEntTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
 

lib/portage/tests/update/test_update_dbentry.py
@@ -312,7 +312,7 @@ class UpdateDbentryTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
 
@@ -478,7 +478,7 @@ class UpdateDbentryTestCase(TestCase):
                     global_noiselimit = portage.util.noiselimit
                     portage.util.noiselimit = -2
                     try:
-                        _do_global_updates(trees, {})
+                        _do_global_updates(trees._running_eroot, trees, {})
                     finally:
                         portage.util.noiselimit = global_noiselimit
 

commit b55554b55f049f51eda31b191614f4a0b9b42374

_global_updates: Skip if vardb lock raises PermissionDenied
Fixes: b6a2901f66b6 ("Support cross-root package moves")
Bug: https://bugs.gentoo.org/953449
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/_global_updates.py
@@ -1,4 +1,4 @@
-# Copyright 2010-2023 Gentoo Authors
+# Copyright 2010-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import stat
@@ -6,7 +6,7 @@ import stat
 from portage import best, os
 from portage.const import WORLD_FILE
 from portage.data import secpass
-from portage.exception import DirectoryNotFound
+from portage.exception import DirectoryNotFound, PermissionDenied
 from portage.localization import _
 from portage.output import bold, colorize
 from portage.update import (
@@ -39,7 +39,10 @@ def _global_updates(root, trees, prev_mtimes, quiet=False, if_mtime_changed=True
         return False
 
     vardb = trees[root]["vartree"].dbapi
-    vardb.lock()
+    try:
+        vardb.lock()
+    except PermissionDenied:
+        return False
     try:
         return _do_global_updates(
             root,