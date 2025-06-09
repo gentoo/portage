b91897c2b3e51bcb986e5ffdce471ee2b2b07cdc
Ulrich Müller <ulm@gentoo.org>
2025-06-09 10:18:40 +0200

    EAPI 9 has edo
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- bin/save-ebuild-env.sh
+++ bin/save-ebuild-env.sh
@@ -91,6 +91,7 @@ __save_ebuild_env() (
 	___eapi_has_version_functions && unset -f ver_cut ver_rs ver_test
 	___eapi_has_pipestatus && unset -f pipestatus
 	___eapi_has_ver_replacing && unset -f ver_replacing
+	___eapi_has_edo && unset -f edo
 
 	# Clear out the triple underscore namespace as it is reserved by the PM.
 	while IFS=' ' read -r _ _ REPLY; do
