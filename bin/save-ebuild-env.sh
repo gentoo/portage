46ac22cc29505fa573e4266e9f28aba10b271019
Kerin Millar <kfm@plushkava.net>
2025-07-15 01:58:41 +0100

    save-ebuild-env.sh: filter out the patch() function
    The patch() function was added to the "phase-helpers.sh" unit by the
    referenced commit. It serves as a wrapper for the gpatch binary, where
    available. Ensure that it is filtered out by the __save_ebuild_env()
    function.
    
    See-also: 495d920b6c2dc735cde163ca58df7fafa6f76fae
    Signed-off-by: Kerin Millar <kfm@plushkava.net>
    Signed-off-by: Sam James <sam@gentoo.org>

--- bin/save-ebuild-env.sh
+++ bin/save-ebuild-env.sh
@@ -121,7 +121,7 @@ __save_ebuild_env() (
 	___eapi_has_eapply_user && REPLY+=( __readdir eapply_user )
 	___eapi_has_get_libdir && REPLY+=( get_libdir )
 	___eapi_has_in_iuse && REPLY+=( in_iuse )
-	___eapi_has_eapply && REPLY+=( eapply )
+	___eapi_has_eapply && REPLY+=( eapply patch )
 	___eapi_has_usex && REPLY+=( usex )
 
 	# Destroy the collected functions.
