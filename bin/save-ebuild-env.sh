46faf7d832ac91bb5b4cfd002be854f359a829db
Kerin Millar <kfm@plushkava.net>
2025-07-15 01:42:21 +0100

    save-ebuild-env.sh: filter out the find0() function
    The find0() function was added to the "isolated-functions.sh" unit by
    the referenced commit. Ensure that it is filtered out by the
    __save_ebuild_env() function.
    
    See-also: a0210b3c49ea346b1e999f92a7ed89802e8d6849
    Signed-off-by: Kerin Millar <kfm@plushkava.net>
    Signed-off-by: Sam James <sam@gentoo.org>

--- bin/save-ebuild-env.sh
+++ bin/save-ebuild-env.sh
@@ -100,8 +100,8 @@ __save_ebuild_env() (
 		contains_word debug-print-function debug-print-section
 		debug-print docompress default diropts docinto dostrip die
 		einstall eqawarn exeinto exeopts ebegin eerror einfon econf
-		einfo ewarn eend elog get_KV has_version hasq hasv has inherit
-		insinto insopts into libopts nonfatal portageq
+		einfo ewarn eend elog find0 get_KV has_version hasq hasv has
+		inherit insinto insopts into libopts nonfatal portageq
 		register_success_hook register_die_hook use_enable use_with
 		unpack useq usev use
 
