gentoo/portage

commit c86c1391904231287824676cc663d7e3eddd8954

Fix the assert alias to avoid 'value to great for base' messages in some cases. Thanks to Jonathan Callen <abcd@g.o> for reporting.
svn path=/main/trunk/; revision=15121

bin/isolated-functions.sh
@@ -5,9 +5,15 @@
 # We need this next line for "die" and "assert". It expands
 # It _must_ preceed all the calls to die and assert.
 shopt -s expand_aliases
-alias assert='_pipestatus="${PIPESTATUS[*]}"; [[ "${_pipestatus// /}" -eq 0 ]] || die'
 alias save_IFS='[ "${IFS:-unset}" != "unset" ] && old_IFS="${IFS}"'
 alias restore_IFS='if [ "${old_IFS:-unset}" != "unset" ]; then IFS="${old_IFS}"; unset old_IFS; else unset IFS; fi'
+
+assert() {
+	local x pipestatus=${PIPESTATUS[*]}
+	for x in $pipestatus ; do
+		[[ $x -eq 0 ]] || die "$@"
+	done
+}
 
 shopt -s extdebug
 
@@ -519,7 +525,8 @@ save_ebuild_env() {
 		done
 		unset x
 
-		unset -f dump_trace die diefunc quiet_mode vecho elog_base eqawarn elog \
+		unset -f assert dump_trace die diefunc \
+			quiet_mode vecho elog_base eqawarn elog \
 			esyslog einfo einfon ewarn eerror ebegin _eend eend KV_major \
 			KV_minor KV_micro KV_to_int get_KV unset_colors set_colors has \
 			has_phase_defined_up_to \

commit 6f9971e54dab95c86f0eced52574a586916a5e4b

isolated-functions.sh: render assert() insensitive to the value of IFS
The purpose of the assert() function is to forward the positional
parameters to the die() function, provided that any of the elements of
the 'PIPESTATUS' array variable are non-zero. However, the function is
defective because it is beholden to the prevailing value of IFS.

Consider the following.

$ IFS=1
$ set -x; true | false | true; assert
+ true
+ false
+ true
+ assert
+ local x pipestatus=01110
+ for x in ${pipestatus}
+ [[ 0 -eq 0 ]]
+ for x in ${pipestatus}
+ [[ '' -eq 0 ]]
+ for x in ${pipestatus}
+ [[ '' -eq 0 ]]
+ for x in ${pipestatus}
+ [[ 0 -eq 0 ]]

It can be seen that the elements of 'PIPESTATUS' are joined by "1",
forming the string, "01110", which is assigned to the 'pipestatus'
variable. Next, the for command is tasked with iterating over the words
produced by an unquoted expansion of this variable. Since "1" is acting
as a field terminator, the for loop iterates over two instances of "0",
and two instances of the null string, all of which are arithmetically
equal to 0. Consequently, die() is never called, though it ought to be.

Address this issue by forming an arithmetic expression from the elements
of 'PIPESTATUS' and having a single invocation of the let builtin
evaluate it.

$ IFS=1
$ set -x; true | false | true; assert
+ true
+ false
+ true
+ assert
+ IFS='|'
+ expression='0|1|0'
+ let '! expression'
+ die

Bug: https://bugs.gentoo.org/25929
Fixes: c86c1391904231287824676cc663d7e3eddd8954
See-also: 038146623c0cdca0ba2038ee7837c3a2170b35de
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/isolated-functions.sh
@@ -23,10 +23,7 @@ fi
 shopt -s expand_aliases
 
 assert() {
-	local x pipestatus=${PIPESTATUS[*]}
-	for x in ${pipestatus} ; do
-		[[ ${x} -eq 0 ]] || die "$@"
-	done
+	IFS='|' expression=${PIPESTATUS[*]} let '! expression' || die "$@"
 }
 
 shopt -s extdebug