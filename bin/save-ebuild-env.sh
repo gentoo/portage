gentoo/portage

commit 1df2556c3aeebc64f64c5e6a2f92d9f4a68eb9da

save-ebuild-env.sh: filter out variables that can influence coreutils
As of the time of writing, there are 18 variables that may influence the
behaviour of the utilities comprising the GNU coreutils project. Ensure
that they are filtered out by the __save_ebuild_env() function. Some,
such as 'HOME' and 'TMPDIR', continue to be specified elsewhere. Such is
harmless. I would sooner be thorough in the course of listing variables
that can be directly attributed to a given source.

Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/save-ebuild-env.sh
@@ -38,9 +38,14 @@ __save_ebuild_env() (
 	fi
 
 	REPLY+=(
+		# variables that can influence the behaviour of GNU coreutils
+		BLOCK_SIZE COLORTERM COLUMNS DF_BLOCK_SIZE DU_BLOCK_SIZE HOME
+		LS_BLOCK_SIZE LS_COLORS POSIXLY_CORRECT PATH PWD QUOTING_STYLE
+		SHELL TIME_STYLE TABSIZE TMPDIR TERM TZ
+
 		# misc variables inherited from the calling environment
-		COLORTERM DISPLAY EDITOR LS_COLORS LESSOPEN LOGNAME LESS PAGER
-		TERMCAP TERM USER ftp_proxy https_proxy http_proxy no_proxy
+		DISPLAY EDITOR LESSOPEN LOGNAME LESS PAGER TERMCAP USER
+		ftp_proxy https_proxy http_proxy no_proxy
 
 		# other variables inherited from the calling environment
 		CVS_RSH ECHANGELOG_USER GPG_AGENT_INFO SSH_AGENT_PID

commit 5b643fb9e8179b77f4e7cb13fc6e871cc4f5eda0

save-ebuild-env.sh: don't strip PATH
A recent commit added PATH to the list of variables we strip out when
environment saving, but this defeats e.g. llvm-r{1,2}.eclass's mangling
of PATH, showing up when building dev-lang/python[jit]. But it's pretty
common to set PATH somewhere in the ebuild and expect it to persist into
other phases.

Instances of SHELL and TZ could do with checking in ::gentoo to see if
we need to change that too.

Fixes: 1df2556c3aeebc64f64c5e6a2f92d9f4a68eb9da
Signed-off-by: Sam James <sam@gentoo.org>

bin/save-ebuild-env.sh
@@ -40,7 +40,7 @@ __save_ebuild_env() (
 	REPLY+=(
 		# variables that can influence the behaviour of GNU coreutils
 		BLOCK_SIZE COLORTERM COLUMNS DF_BLOCK_SIZE DU_BLOCK_SIZE HOME
-		LS_BLOCK_SIZE LS_COLORS POSIXLY_CORRECT PATH PWD QUOTING_STYLE
+		LS_BLOCK_SIZE LS_COLORS POSIXLY_CORRECT PWD QUOTING_STYLE
 		SHELL TIME_STYLE TABSIZE TMPDIR TERM TZ
 
 		# misc variables inherited from the calling environment