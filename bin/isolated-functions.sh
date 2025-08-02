gentoo/portage

commit e72b8b90542d1ed2bf53c2b9a0491795c12e63c2

Stop using PORTDIR_OVERLAY in bash part of Portage.

bin/isolated-functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 1999-2012 Gentoo Foundation
+# Copyright 1999-2013 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}/eapi.sh"
@@ -176,13 +176,6 @@ die() {
 	fi
 	eerror "If you need support, post the output of \`emerge --info '=$CATEGORY/$PF'\`,"
 	eerror "the complete build log and the output of \`emerge -pqv '=$CATEGORY/$PF'\`."
-	if [[ -n ${EBUILD_OVERLAY_ECLASSES} ]] ; then
-		eerror "This ebuild used the following eclasses from overlays:"
-		local x
-		for x in ${EBUILD_OVERLAY_ECLASSES} ; do
-			eerror "  ${x}"
-		done
-	fi
 	if [ "${EMERGE_FROM}" != "binary" ] && \
 		! has ${EBUILD_PHASE} prerm postrm && \
 		[ "${EBUILD#${PORTDIR}/}" == "${EBUILD}" ] ; then
@@ -480,6 +473,22 @@ has() {
 		[ "${x}" = "${needle}" ] && return 0
 	done
 	return 1
+}
+
+__repo_key() {
+	local appropriate_section=0 exit_status=1 line saved_extglob_shopt=$(shopt -p extglob)
+	shopt -s extglob
+	while read line; do
+		[[ ${appropriate_section} == 0 && ${line} == "[$1]" ]] && appropriate_section=1 && continue
+		[[ ${appropriate_section} == 1 && ${line} == "["*"]" ]] && appropriate_section=0 && continue
+		if [[ ${appropriate_section} == 1 && ${line} == $2*( )=* ]]; then
+			echo "${line##$2*( )=*( )}"
+			exit_status=0
+			break
+		fi
+	done <<< "${PORTAGE_REPOSITORIES}"
+	eval "${saved_extglob_shopt}"
+	return ${exit_status}
 }
 
 true

bin/ebuild.sh
@@ -207,8 +207,10 @@ inherit() {
 			| fmt -w 75 | while read -r ; do eqawarn "$REPLY" ; done
 	fi
 
+	local repo
+	local repo_location
 	local location
-	local olocation
+	local potential_location
 	local x
 
 	# These variables must be restored before returning.
@@ -222,8 +224,8 @@ inherit() {
 	local B_PDEPEND
 	local B_HDEPEND
 	while [ "$1" ]; do
-		location="${ECLASSDIR}/${1}.eclass"
-		olocation=""
+		location=""
+		potential_location=""
 
 		export ECLASS="$1"
 		__export_funcs_var=__export_functions_$ECLASS_DEPTH
@@ -244,24 +246,16 @@ inherit() {
 			fi
 		fi
 
-		# any future resolution code goes here
-		if [ -n "$PORTDIR_OVERLAY" ]; then
-			local overlay
-			for overlay in ${PORTDIR_OVERLAY}; do
-				olocation="${overlay}/eclass/${1}.eclass"
-				if [ -e "$olocation" ]; then
-					location="${olocation}"
-					debug-print "  eclass exists: ${location}"
-				fi
-			done
-		fi
+		for repo in $(__repo_key ${PORTAGE_REPO_NAME} masters) ${PORTAGE_REPO_NAME} $(__repo_key ${PORTAGE_REPO_NAME} eclass-overrides); do
+			repo_location="$(__repo_key ${repo} location)"
+			potential_location="${repo_location}/eclass/${1}.eclass"
+			if [[ -f ${potential_location} ]]; then
+				location="${potential_location}"
+				debug-print "  eclass exists: ${location}"
+			fi
+		done
 		debug-print "inherit: $1 -> $location"
-		[ ! -e "$location" ] && die "${1}.eclass could not be found by inherit()"
-
-		if [ "${location}" == "${olocation}" ] && \
-			! has "${location}" ${EBUILD_OVERLAY_ECLASSES} ; then
-				EBUILD_OVERLAY_ECLASSES="${EBUILD_OVERLAY_ECLASSES} ${location}"
-		fi
+		[[ -z ${location} ]] && die "${1}.eclass could not be found by inherit()"
 
 		#We need to back up the values of *DEPEND to B_*DEPEND
 		#(if set).. and then restore them after the inherit call.

bin/phase-functions.sh
@@ -29,7 +29,7 @@ PORTAGE_READONLY_VARS="D EBUILD EBUILD_PHASE EBUILD_PHASE_FUNC \
 	PORTAGE_SAVED_READONLY_VARS PORTAGE_SIGPIPE_STATUS \
 	PORTAGE_TMPDIR PORTAGE_UPDATE_ENV PORTAGE_USERNAME \
 	PORTAGE_VERBOSE PORTAGE_WORKDIR_MODE PORTAGE_XATTR_EXCLUDE \
-	PORTDIR PORTDIR_OVERLAY \
+	PORTDIR \
 	PROFILE_PATHS REPLACING_VERSIONS REPLACED_BY_VERSION T WORKDIR \
 	__PORTAGE_HELPER __PORTAGE_TEST_HARDLINK_LOCKS"
 

pym/portage/package/ebuild/doebuild.py
@@ -305,11 +305,11 @@ def doebuild_environment(myebuild, mydo, myroot=None, settings=None,
 	if hasattr(mydbapi, 'repositories'):
 		repo = mydbapi.repositories.get_repo_for_location(mytree)
 		mysettings['PORTDIR'] = repo.eclass_db.porttrees[0]
-		mysettings['PORTDIR_OVERLAY'] = ' '.join(repo.eclass_db.porttrees[1:])
 		mysettings['PORTAGE_REPOSITORIES'] = mydbapi.repositories.config_string()
 		mysettings.configdict["pkg"]["PORTAGE_REPO_NAME"] = repo.name
 
 	mysettings["PORTDIR"] = os.path.realpath(mysettings["PORTDIR"])
+	mysettings.pop("PORTDIR_OVERLAY", None)
 	mysettings["DISTDIR"] = os.path.realpath(mysettings["DISTDIR"])
 	mysettings["RPMDIR"]  = os.path.realpath(mysettings["RPMDIR"])
 

commit 3868d097a576275641d55c9fb32bfe4976385e02

isolated-functions.sh: add and integrate the configparser() function
Presently, the __repo_attr() function acts as a crude parser for the
value of the 'PORTAGE_REPOSITORIES' environment variable, which contains
the contents of files beneath the "/etc/portage/repos.conf" directory.
Its implementation is substandard, to say the least. Firstly, it suffers
from several amateurish bugs. Secondly, it is unable to detect errors in
syntax. Thirdly, it is an incomplete, incorrect implementation of a
parser for the file format that it is supposed to handle.

This commit incorporates a new function by the name of configparser().
It consumes the standard input and - as its name suggests - attempts to
parse it as the "configparser" configuration file format that is native
to python. It has been tested against the sample file provided by the
python documentation and fully supports multiline values and key/value
pairs that are separated by the <colon> character. Each key/value entry
that is encountered shall be printed in the following format:

  "%s\0%s\0%s\0", <section>, <key>, <value>

It is capable of detecting syntax errors. Upon encountering any such
error, a diagnostic message shall be printed to standard error and the
function shall immediately return a non-zero status.

This commit also revises the __repo_attr() function so as to call the
configparser() function and interpret its output.

Fixes: e72b8b90542d1ed2bf53c2b9a0491795c12e63c2
Link: https://docs.python.org/3/library/configparser.html
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/isolated-functions.sh
@@ -539,23 +539,22 @@ has() {
 }
 
 __repo_attr() {
-	local in_section exit_status=1 line saved_extglob_shopt=$(shopt -p extglob)
-	shopt -s extglob
+	local section key val i
+	local -a records
 
-	while read -r line; do
-		if (( ! in_section )) && [[ ${line} == "[$1]" ]]; then
-			in_section=1
-		elif (( in_section )) && [[ ${line} == "["*"]" ]]; then
-			in_section=0
-		elif (( in_section )) && [[ ${line} =~ ^${2}[[:space:]]*= ]]; then
-			echo "${line##$2*( )=*( )}"
-			exit_status=0
+	mapfile -td '' records < <(configparser <<<"${PORTAGE_REPOSITORIES}")
+
+	for (( i = 0; i < ${#records[@]}; i += 3 )); do
+		section=${records[i]}
+		key=${records[i + 1]}
+		if [[ ${section} == "$1" && ${key} == "$2" ]]; then
+			val=${records[i + 2]}
 			break
 		fi
-	done <<< "${PORTAGE_REPOSITORIES}"
+	done
 
-	eval "${saved_extglob_shopt}"
-	return ${exit_status}
+	# Only print the found value in the case that configparser succeeded.
+	wait "$!" && [[ -v val ]] && printf '%s\n' "${val}"
 }
 
 # eqaquote <string>
@@ -702,5 +701,98 @@ else
 		fi
 	}
 fi
+
+# Consumes the standard input and attempts to parse it as the "configparser"
+# configuration file format that is native to python. Each key/value entry
+# shall be printed in the format of "%s\0%s\0%s\0", <section>, <key>, <value>.
+# If the entirety of the input can be successfully parsed, the return value
+# shall be 0. Otherwise, a diagnostic message shall be printed to standard
+# error and the return value shall be greater than 0. Upon encountering the
+# first error of syntax, no further key/value entries shall be printed.
+configparser() {
+	local IFS next_{section,key} reset_extglob scalar_val section flush key_i line key nr i
+	local -a next_val lines val
+	local -A seen
+
+	reset_extglob=$(shopt -p extglob)
+	shopt -s extglob
+	IFS=$'\n'
+
+	# https://docs.python.org/3/library/configparser.html#supported-ini-file-structure
+	mapfile -t lines
+	for nr in "${!lines[@]}"; do
+		# Trim trailing whitespace characters.
+		line=${lines[nr++]%%+([[:space:]])}
+
+		# Count and trim leading whitespace characters.
+		[[ ${line} =~ ^[[:space:]]* ]]
+		if (( i = ${#BASH_REMATCH} )); then
+			line=${line:i}
+		fi
+
+		if [[ ${line} == [#\;]* ]]; then
+			# Encountered a comment.
+			false
+		elif (( ${#val[@]} && (${#line} == 0 || i > key_i) )); then
+			# Encountered the continuation of a multiline value.
+			val+=( "${line}" )
+			false
+		elif (( ${#line} == 0 )); then
+			# Encountered an empty line that is not a continuation.
+			true
+		elif [[ ${line} =~ ^\[(.+)\]$ ]]; then
+			# Encountered a new section.
+			next_section=${BASH_REMATCH[1]}
+			seen=()
+		elif [[ ${line} =~ ^([^:=]+)[:=][[:space:]]*(.*) ]]; then
+			# Encountered the beginning of a key/value entry.
+			next_key=${BASH_REMATCH[1]%%*([[:space:]])}
+			if let 'seen[$next_key]++'; then
+				printf >&2 \
+					'configparser: duplicate key in section %s at STDIN[%d]: %s\n' \
+					"${section@Q}" "${nr}" "${line@Q}"
+				eval "${reset_extglob}"
+				return 1
+			elif [[ ! ${section} ]]; then
+				printf >&2 \
+					'configparser: key declared before section at STDIN[%d]: %s\n' \
+					"${nr}" "${line@Q}"
+				eval "${reset_extglob}"
+				return 1
+			fi
+			next_val=( "${BASH_REMATCH[2]}" )
+			key_i=$i
+		else
+			printf >&2 \
+				'configparser: syntax error at STDIN[%d]: %s\n' \
+				"${nr}" "${line@Q}"
+			eval "${reset_extglob}"
+			return 1
+		fi && flush=1
+
+		if (( nr == ${#lines[@]} - 1 )); then
+			# Last line encountered. Flush any pending value.
+			flush=1
+		fi
+
+		if (( flush && ${#val[@]} )); then
+			# Print section, key and value, null-terminated.
+			scalar_val=${val[*]}
+			scalar_val=${scalar_val%%+($'\n')}
+			printf '%s\0' "${section}" "${key}" "${scalar_val}"
+			val=()
+		fi
+
+		section=${next_section}
+		key=${next_key}
+		if (( ${#next_val[@]} )); then
+			val=( "${next_val[@]}" )
+			next_val=()
+		fi
+		flush=0
+	done
+
+	eval "${reset_extglob}"
+}
 
 true

bin/save-ebuild-env.sh
@@ -104,13 +104,13 @@ __save_ebuild_env() (
 		__unpack_tar __vecho
 
 		addpredict addwrite adddeny addread assert best_version
-		contains_word debug-print-function debug-print-section
-		debug-print docompress default diropts docinto dostrip die
-		einstall eqawarn exeinto exeopts ebegin eerror einfon econf
-		einfo ewarn eend elog find0 get_KV has_version hasq hasv has
-		inherit insinto insopts into libopts nonfatal portageq
-		register_success_hook register_die_hook use_enable use_with
-		unpack useq usev use
+		contains_word configparser debug-print-function
+		debug-print-section debug-print docompress default diropts
+		docinto dostrip die einstall eqawarn exeinto exeopts ebegin
+		eerror einfon econf einfo ewarn eend elog find0 get_KV
+		has_version hasq hasv has inherit insinto insopts into libopts
+		nonfatal portageq register_success_hook register_die_hook
+		use_enable use_with unpack useq usev use
 
 		# Defined by the "ebuild.sh" utility.
 		${QA_INTERCEPTORS}