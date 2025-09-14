gentoo/portage

commit 4b57307865cf4bcde8ef5150303eb48465a43689

_urlopen: fix timestamp comparison by using UTC consistently
In bintree.py's _populate_remote, we call http_to_timestamp on the
timestamp header we get from the remote binhost. Unfortunately, that uses
mktime() which is documented to return *local time*:

> This is the inverse function of localtime(). Its argument is the struct_time or
> full 9-tuple (since the dst flag is needed; use -1 as the dst flag if it is unknown)
> which expresses the time in local time, not UTC.

Switch to using mktime_tz and parsedate_tz which operate in UTC instead. I've
changed timestamp_to_http as well just to be safe: I think this may fix
a potential issue with the value we send for If-Modified-Since, as
datetime.fromtimestamp() gives localtime.

This was manifesting as a warning with --getbinpkg:

> [gentoobinhost] WARNING: Service distfiles.gentoo.org did not respect If-Modified-Since.
>  Consider asking the service operator to enable support for If-Modified-Since or
>  using another service (local: 2025-09-12T12:43:46+01:00, remote: 2025-09-12T11:43:46+01:00).

... which would disappear when using TZ=UTC.

Bug: https://bugs.gentoo.org/962832
Part-of: https://github.com/gentoo/portage/pull/1467
Closes: https://github.com/gentoo/portage/pull/1467
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/util/_urlopen.py
@@ -3,8 +3,7 @@
 
 import io
 from datetime import datetime
-from time import mktime
-from email.utils import formatdate, parsedate
+from email.utils import formatdate, parsedate_tz, mktime_tz
 from urllib.request import urlopen as _urlopen
 import urllib.parse as urllib_parse
 import urllib.request as urllib_request
@@ -68,12 +67,12 @@ def urlopen(url, timeout=10, if_modified_since=None, headers={}, proxies=None):
 
 def timestamp_to_http(timestamp):
     dt = datetime.fromtimestamp(float(int(timestamp) + TIMESTAMP_TOLERANCE))
-    stamp = mktime(dt.timetuple())
+    stamp = mktime_tz(dt.timetuple())
     return formatdate(timeval=stamp, localtime=False, usegmt=True)
 
 
 def http_to_timestamp(http_datetime_string):
-    timestamp = mktime(parsedate(http_datetime_string))
+    timestamp = mktime_tz(parsedate_tz(http_datetime_string))
     return str(int(timestamp))
 
 

commit 3b46180f871d49d8624b2140b5e29565f7e80b18

_urlopen: partly revert earlier change
In 4b57307865cf4bcde8ef5150303eb48465a43689, I also made a speculative
fix to timestamp_to_http but that turns out to be wrong (though I thought
I'd tested that too):
```
$ emerge -G hello -v --jobs=1
Traceback (most recent call last):
  File "/usr/lib/python-exec/python3.13/emerge", line 57, in main
    retval = emerge_main()
  File "/usr/lib/python3.13/site-packages/_emerge/main.py", line 1314, in emerge_main
    return run_action(emerge_config)
  File "/usr/lib/python3.13/site-packages/_emerge/actions.py", line 3521, in run_action
    mytrees["bintree"].populate(
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~^
        getbinpkgs="--getbinpkg" in emerge_config.opts,
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ...<2 lines>...
        **kwargs,
        ^^^^^^^^^
    )
    ^
  File "/usr/lib/python3.13/site-packages/portage/dbapi/bintree.py", line 964, in populate
    self._populate_remote(
    ~~~~~~~~~~~~~~~~~~~~~^
        getbinpkg_refresh=getbinpkg_refresh,
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
        pretend=pretend,
        ^^^^^^^^^^^^^^^^
        verbose=verbose,
        ^^^^^^^^^^^^^^^^
    )
    ^
  File "/usr/lib/python3.13/site-packages/portage/dbapi/bintree.py", line 1463, in _populate_remote
    f = _urlopen(
        url,
        if_modified_since=local_timestamp,
        proxies=proxies,
    )
  File "/usr/lib/python3.13/site-packages/portage/util/_urlopen.py", line 50, in urlopen
    request.add_header("If-Modified-Since", timestamp_to_http(if_modified_since))
                                            ~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^
  File "/usr/lib/python3.13/site-packages/portage/util/_urlopen.py", line 70, in timestamp_to_http
    stamp = mktime_tz(dt.timetuple())
  File "/usr/lib/python3.13/email/_parseaddr.py", line 193, in mktime_tz
    if data[9] is None:
       ~~~~^^^
IndexError: tuple index out of range
```

Revert that part. I think it's unnecessary anyway because of the formatdate
call.

Bug: https://bugs.gentoo.org/962832
Fixes: 4b57307865cf4bcde8ef5150303eb48465a43689

lib/portage/util/_urlopen.py
@@ -3,6 +3,7 @@
 
 import io
 from datetime import datetime
+from time import mktime
 from email.utils import formatdate, parsedate_tz, mktime_tz
 from urllib.request import urlopen as _urlopen
 import urllib.parse as urllib_parse
@@ -67,7 +68,7 @@ def urlopen(url, timeout=10, if_modified_since=None, headers={}, proxies=None):
 
 def timestamp_to_http(timestamp):
     dt = datetime.fromtimestamp(float(int(timestamp) + TIMESTAMP_TOLERANCE))
-    stamp = mktime_tz(dt.timetuple())
+    stamp = mktime(dt.timetuple())
     return formatdate(timeval=stamp, localtime=False, usegmt=True)
 
 