gentoo/portage

commit 502bd866c85a44852bf1b214c3504d00ca50add5

isolated-functions.sh: drop the __assert_sigpipe_ok() function
The __assert_sigpipe_ok() function was introduced in 2010 as a means of
helping to handle the processing of malformed tarballs being distributed
for various sub-projects of GNOME. Among them were the tarballs for
glibmm and evolution-data-server, both of which were found to be the
improper concatenations of two tar archives of the archaic "v7" format.

As things stand now, fifteen years later, I believe it to be a hack that
has run its course. Nowadays, it is conventional for both GNU tar and
bsdtar to produce archives that are of the "pax" format, as defined by
POSIX.1-2001. This is a format unencumbered by the limitations that were
indirectly responsible for the aforesaid defective tarballs. Besides
which, the unpack() function of the "phase-helpers.sh" unit no longer
calls __assert_sigpipe_ok() (since commit 6a3cd41450).

Hence, jettison the __assert_sigpipe_ok() function. Further, jettison
the 'PORTAGE_SIGPIPE_STATUS' environment variable that had hitherto been
defined by the "doebuild.py" unit. The purpose of this variable was to
render the function aware of the numerical value of SIGPIPE. Yet, the
shell requires no assistance in determining this information.

$ read -ra sigs < <(set -o posix; kill -l)
$ for i in "${!sigs[@]}"; do [[ ${sigs[i++]} == PIPE ]] && break; done
$ echo "$i"
13

Link: https://bugs.gentoo.org/309001
Link: https://bugzilla.gnome.org/show_bug.cgi?id=613808#c9
See-also: 6a3cd41450a78422248f10e1272fdd529bd8bc98
See-also: 0ab6f83070c9a32a24454053319c99559c2654ca
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/save-ebuild-env.sh
@@ -84,8 +84,8 @@ __save_ebuild_env() (
 	REPLY=(
 		EXPORT_FUNCTIONS KV_to_int KV_major KV_micro KV_minor
 
-		__assert_sigpipe_ok __abort_configure __abort_compile
-		__abort_handler __abort_install __abort_prepare __abort_test
+		__abort_configure __abort_compile __abort_handler
+		__abort_install __abort_prepare __abort_test
 		__check_bash_version __compose_bzip2_cmd __dyn_configure
 		__dyn_compile __dyn_install __dyn_prepare __dyn_pretend
 		__dump_trace __dyn_unpack __dyn_clean __dyn_setup __dyn_help

bin/isolated-functions.sh
@@ -29,32 +29,6 @@ assert() {
 	done
 }
 
-__assert_sigpipe_ok() {
-	# When extracting a tar file like this:
-	#
-	#     bzip2 -dc foo.tar.bz2 | tar xof -
-	#
-	# For some tar files (see bug #309001), tar will
-	# close its stdin pipe when the decompressor still has
-	# remaining data to be written to its stdout pipe. This
-	# causes the decompressor to be killed by SIGPIPE. In
-	# this case, we want to ignore pipe writers killed by
-	# SIGPIPE, and trust the exit status of tar. We refer
-	# to the bash manual section "3.7.5 Exit Status"
-	# which says, "When a command terminates on a fatal
-	# signal whose number is N, Bash uses the value 128+N
-	# as the exit status."
-
-	local x pipestatus=${PIPESTATUS[*]}
-	for x in ${pipestatus} ; do
-		# Allow SIGPIPE through (128 + 13)
-		[[ ${x} -ne 0 && ${x} -ne ${PORTAGE_SIGPIPE_STATUS:-141} ]] && die "$@"
-	done
-
-	# Require normal success for the last process (tar).
-	[[ ${x} -eq 0 ]] || die "$@"
-}
-
 shopt -s extdebug
 
 # __dump_trace([number of funcs on stack to skip],

bin/phase-functions.sh
@@ -20,20 +20,19 @@ portage_readonly_vars=(
 	PORTAGE_EBUILD_EXIT_FILE PORTAGE_ECLASS_LOCATIONS
 	PORTAGE_EXPLICIT_INHERIT PORTAGE_OVERRIDE_EPREFIX
 	PORTAGE_BINPKG_TAR_OPTS PORTAGE_INTERNAL_CALLER PORTAGE_ACTUAL_DISTDIR
-	PORTAGE_BINPKG_TMPFILE PORTAGE_SIGPIPE_STATUS PORTAGE_XATTR_EXCLUDE
-	PORTAGE_REPOSITORIES PORTAGE_WORKDIR_MODE PORTAGE_BINPKG_FILE
-	PORTAGE_BUILD_GROUP PORTAGE_DEPCACHEDIR PM_EBUILD_HOOK_DIR
-	PORTAGE_BUILD_USER PORTAGE_CONFIGROOT PORTAGE_IPC_DAEMON
-	PORTAGE_PROPERTIES PORTAGE_PYTHONPATH PORTAGE_UPDATE_ENV
-	PORTAGE_REPO_NAME PORTAGE_ARCHLIST PORTAGE_BIN_PATH PORTAGE_BUILDDIR
-	PORTAGE_COLORMAP PORTAGE_INST_GID PORTAGE_INST_UID PORTAGE_LOG_FILE
-	PORTAGE_PYM_PATH PORTAGE_RESTRICT PORTAGE_USERNAME PORTAGE_GRPNAME
-	PORTAGE_VERBOSE PORTAGE_BASHRC PORTAGE_PYTHON PORTAGE_TMPDIR
-	PORTAGE_DEBUG PORTAGE_IUSE PORTAGE_GID REPLACED_BY_VERSION
-	REPLACING_VERSIONS T WORKDIR __PORTAGE_TEST_HARDLINK_LOCKS
-	__PORTAGE_HELPER portage_mutable_filtered_vars
-	portage_saved_readonly_vars portage_readonly_metadata
-	portage_readonly_vars
+	PORTAGE_BINPKG_TMPFILE PORTAGE_XATTR_EXCLUDE PORTAGE_REPOSITORIES
+	PORTAGE_WORKDIR_MODE PORTAGE_BINPKG_FILE PORTAGE_BUILD_GROUP
+	PORTAGE_DEPCACHEDIR PM_EBUILD_HOOK_DIR PORTAGE_BUILD_USER
+	PORTAGE_CONFIGROOT PORTAGE_IPC_DAEMON PORTAGE_PROPERTIES
+	PORTAGE_PYTHONPATH PORTAGE_UPDATE_ENV PORTAGE_REPO_NAME
+	PORTAGE_ARCHLIST PORTAGE_BIN_PATH PORTAGE_BUILDDIR PORTAGE_COLORMAP
+	PORTAGE_INST_GID PORTAGE_INST_UID PORTAGE_LOG_FILE PORTAGE_PYM_PATH
+	PORTAGE_RESTRICT PORTAGE_USERNAME PORTAGE_GRPNAME PORTAGE_VERBOSE
+	PORTAGE_BASHRC PORTAGE_PYTHON PORTAGE_TMPDIR PORTAGE_DEBUG PORTAGE_IUSE
+	PORTAGE_GID REPLACED_BY_VERSION REPLACING_VERSIONS T WORKDIR
+	__PORTAGE_TEST_HARDLINK_LOCKS __PORTAGE_HELPER
+	portage_mutable_filtered_vars portage_saved_readonly_vars
+	portage_readonly_metadata portage_readonly_vars
 )
 
 portage_saved_readonly_vars=(

lib/portage/package/ebuild/_config/special_env_vars.py
@@ -171,7 +171,6 @@ environ_whitelist = frozenset(
         "PORTAGE_REPO_NAME",
         "PORTAGE_REPOSITORIES",
         "PORTAGE_RESTRICT",
-        "PORTAGE_SIGPIPE_STATUS",
         "PORTAGE_SOCKS5_PROXY",
         "PORTAGE_TMPDIR",
         "PORTAGE_UPDATE_ENV",

lib/portage/package/ebuild/doebuild.py
@@ -472,11 +472,6 @@ def doebuild_environment(
     # Set requested Python interpreter for Portage helpers.
     mysettings["PORTAGE_PYTHON"] = portage._python_interpreter
 
-    # This is used by assert_sigpipe_ok() that's used by the ebuild
-    # unpack() helper. SIGPIPE is typically 13, but its better not
-    # to assume that.
-    mysettings["PORTAGE_SIGPIPE_STATUS"] = str(128 + signal.SIGPIPE)
-
     # We are disabling user-specific bashrc files.
     mysettings["BASH_ENV"] = INVALID_ENV_FILE
 

commit 3e89139fae34c9bd2e2b4c0490512f71d1d78546

phase-helpers.sh: refactor unpack() and have tar execute its decompressors
This commit subjects the unpack() function to an overall refactoring
while incorporating a fix for a regression that I was aware of and which
has since been independently identified and filed as bug #962874. The
changes are described herewith.

Declare all of the supported archive suffixes in a fashion that mirrors
the language of PMS. To wit, all documented suffixes are initially
declared as elements of the 'suffixes' array, case-sensitively. In turn,
that means that the following suffixes are now considered as first class
entities, as opposed to being supported as a consequence of the
machinations of the __unpack_tar() function.

- tar.bz
- tar.bz2
- tar.gz
- tar.lzma
- tar.Z

After having composed the 'suffixes' array, declare an associative array
by the name of 'suffix_by', where each key of the latter is an element
of the former. In the case that the effective EAPI is found not to be
case-sensitive, exploit the 'l' attribute of the declare/typeset
builtin, by which bash is able to induce a lowercase conversion upon
assignment. The latter array is prepared before the main loop begins and
makes it easy to determine whether a given suffix is recognised, whilst
doing away with the code pertaining to the now defunct 'suffix_known'
variable (which was not particularly digestible).

Declare and localise the 'XZ_OPT' variable in such a way that it is
rendered exportable. Do so before the main loop begins, duly
guaranteeing that the __makeopts_jobs() function shall be called just
once and obviating the need to incorporate the -T option of the xz(1)
utility at a later juncture.

Employ a regular expression to extract the suffix of each filename that
is considered. The pattern matches what is commonly referred to as an
extension whilst permitting for it to be preceded by "tar.". Once the
suffix has been extracted, employ a trivial -v test so as to determine
whether it exists as a key in the 'suffix_by' array.

Jettison the __unpack_tar() function. Not only was it responsible for
operating on files that are _not_ tarballs but it was also responsible
for further scrutinising the nature of the suffix. Long had I considered
it to be in poor taste. Instead, have each case pattern be accompanied
by the appropriate command.

To compensate for the aforementioned change, introduce the following
case patterns.

- tar.bz|tar.bz2|tbz|tbz2)
- tar|tar.*|tgz)

Further, jettison the following case patterns.

- tar)
- tbz|tbz2)
- tgz)

Jettison the __compose_bzip2_cmd() function. Instead, determine which
command may be employed to handle bzip2-compressed streams before the
main loop begins, unconditionally. Further, refrain from throwing an
exception in the event that both the 'PORTAGE_BUNZIP2_COMMAND' and
'PORTAGE_BZIP2_COMMAND' variables are defined as blank values.

Refrain entirely from employing shell pipelines to combine the tar(1)
utility with any particular decompression utility. Instead, delegate the
responsibility of executing the decompressor entirely to tar(1). The
rationale for this is twofold.

Firstly, GNU tar is already perfectly capable of recognising a
considerable number of file suffixes in the case that the -f option is
specified. No fewer than 16, in fact. Based on these suffixes, it is
further capable of determining whether to execute any of 8 distinct
decompression utilities: compress(1), bzip2(1), gzip(1), lzip(1),
lzma(1), lzop(1), xz(1) and zstd(1). Ergo, there is no sense in
maintaining equivalnt bash code that amounts merely to busywork.

Secondly, there have been several portage bugs concerning the use of
shell pipelines over the years. Some pertain to the matter of knowing
the exit status of the decompressor, while others pertain to the
consumption of malformed streams that cause tar(1) to close its end of
the pipe while the decompressor is still writing. By shifting the burden
of responsibility to tar(1), this entire class of problem is obviated.

It is worth noting that bzip2-compressed tarballs are handled as a
special case. The reason for this is straightforward: there is the
continued need to support the 'PORTAGE_BUNZIP2_COMMAND' and
'PORTAGE_BZIP2_COMMAND' portage variables. To accommodate these, the -I
option is used to convey a suitable decompression command. For instance,
to specify -I "lbzip2 -c" would imply to tar(1) that it must execute
lbzip2(1) with both the -c and -d options in effect. Though the -I
option-argument is not conveyed to a shell, it is parsed in a manner
that somewhat resembles that of a shell. Therefore, the values of the
aforementioned portage variables shall only be accepted if they consist
entirely of one or more non-blank characters that are neither the single
quote nor the double quote.

Fixes: 502bd866c85a44852bf1b214c3504d00ca50add5

bin/phase-helpers.sh
@@ -322,47 +322,65 @@ use_enable() {
 }
 
 unpack() {
-	local created_symlink suffix_known basename output srcdir suffix f -
-	local -a bzip2_cmd
+	local created_symlink bzip2_cmd basename output srcdir suffix name f
+	local -A suffix_by
+	local -a suffixes
+	local -x XZ_OPT
 
 	if (( $# == 0 )); then
 		die "unpack: too few arguments (got 0; expected at least 1)"
 	fi
 
-	__unpack_tar() {
-		local inner_suffix
+	# Define an array of supported suffixes, case-sensitively.
+	# https://projects.gentoo.org/pms/8/pms.html#x1-13500012.3.15
+	suffixes=(
+		a
+		bz
+		bz2
+		deb
+		gz
+		jar
+		lzma
+		tar
+		tar.bz
+		tar.bz2
+		tar.gz
+		tar.lzma
+		tar.Z
+		tbz
+		tbz2
+		tgz
+		Z
+		zip
+		ZIP
+	)
+	___eapi_unpack_supports_7z  && suffixes+=( 7z 7Z )
+	___eapi_unpack_supports_lha && suffixes+=( lha LHa LHA lzh )
+	___eapi_unpack_supports_rar && suffixes+=( rar RAR )
+	___eapi_unpack_supports_txz && suffixes+=( tar.xz txz )
+	___eapi_unpack_supports_xz  && suffixes+=( xz )
 
-		inner_suffix=${basename%.*} inner_suffix=${inner_suffix##*.}
-		if ! ___eapi_unpack_is_case_sensitive; then
-			inner_suffix=${inner_suffix,,}
+	# Compose a finalised dictionary of supported suffixes.
+	if ! ___eapi_unpack_is_case_sensitive; then
+		# Induce lowercase conversion upon all subsequent assignments.
+		typeset -l suffix
+	fi
+	for suffix in "${suffixes[@]}"; do
+		suffix_by[$suffix]=
+	done
+
+	# Honour the user's choice of bzip2 decompressor, if specified.
+	for name in PORTAGE_BUNZIP2_CMD PORTAGE_BZIP2_CMD; do
+		if [[ ${!name} == +([![:blank:]\"\']) ]]; then
+			bzip2_cmd=${!name}
+			break
 		fi
-		if [[ ${inner_suffix} == tar ]]; then
-			"$@" -c -- "${srcdir}${f}" | tar xof -
-		else
-			"$@" -c -- "${srcdir}${f}" > "${basename%.*}"
-		fi
-	}
+	done
 
-	__compose_bzip2_cmd() {
-		local IFS
+	# Ensure that xz(1) operates in its multi-threaded mode.
+	XZ_OPT="-T$(___makeopts_jobs)"
 
-		read -rd '' -a bzip2_cmd <<<"${PORTAGE_BUNZIP2_COMMAND}"
-		if (( ! ${#bzip2_cmd[@]} )); then
-			read -rd '' -a bzip2_cmd <<<"${PORTAGE_BZIP2_COMMAND}"
-			if (( ${#bzip2_cmd[@]} )); then
-				bzip2_cmd+=( -d )
-			else
-				die "unpack: both PORTAGE_BUNZIP2_COMMAND and PORTAGE_BZIP2_COMMAND specify null commands"
-			fi
-		fi
-	}
-
-	shopt -o -s pipefail
-
-	for f in "$@"; do
-		basename=${f##*/}
-		suffix=${basename##*.}
-
+	for f; do
 		# wrt PMS 12.3.15 Misc Commands
 		if [[ ${f} != */* ]]; then
 			# filename without path of any kind
@@ -391,22 +409,16 @@ unpack() {
 			die "unpack: ${f@Q} cannot be unpacked because it is an empty file"
 		fi
 
-		case ${suffix,,} in
-			tar|tgz|tbz2|tbz|zip|jar|gz|z|bz2|bz|a|deb|lzma) ;;
-			7z)      ___eapi_unpack_supports_7z  ;;
-			rar)     ___eapi_unpack_supports_rar ;;
-			lha|lzh) ___eapi_unpack_supports_lha ;;
-			xz)      ___eapi_unpack_supports_xz  ;;
-			txz)     ___eapi_unpack_supports_txz ;;
-			*)       false                       ;;
-		esac \
-		&& suffix_known=1
 
-		___eapi_unpack_is_case_sensitive \
-		&& [[ ${suffix} != @("${suffix,,}"|ZIP|Z|7Z|RAR|LH[Aa]) ]] \
-		&& suffix_known=0
+		# Extract the suffix of the filename.
+		basename=${f##*/}
+		suffix=
+		if [[ ${basename} =~ \.([Tt][Aa][Rr]\.)?[^.]+$ ]]; then
+			suffix=${BASH_REMATCH[0]#.}
+		fi
 
-		if (( suffix_known )); then
+		# Skip any files bearing unsupported suffixes.
+		if [[ -v 'suffix_by[$suffix]' ]]; then
 			__vecho ">>> Unpacking ${f@Q} to ${PWD}"
 		else
 			__vecho "=== Skipping unpack of ${f@Q}"
@@ -424,8 +436,7 @@ unpack() {
 				ar x "${srcdir}${f}"
 				;;
 			bz|bz2)
-				(( ${#bzip2_cmd[@]} )) || __compose_bzip2_cmd
-				__unpack_tar "${bzip2_cmd[@]}"
+				"${bzip2_cmd-bzip2}" -dc -- "${srcdir}${f}" > "${basename%.*}"
 				;;
 			deb)
 				# Unpacking .deb archives can not always be done with
@@ -451,7 +462,7 @@ unpack() {
 				fi
 				;;
 			gz|z)
-				__unpack_tar gzip -d
+				gzip -dc -- "${srcdir}${f}" > "${basename%.*}"
 				;;
 			jar|zip)
 				# unzip will interactively prompt under some error conditions,
@@ -463,26 +474,26 @@ unpack() {
 				lha xfq "${srcdir}${f}"
 				;;
 			lzma)
-				__unpack_tar lzma -d
+				xz -F lzma -dc -- "${srcdir}${f}" > "${basename%.*}"
 				;;
 			rar)
 				unrar x -idq -o+ "${srcdir}${f}"
 				;;
-			tar)
-				tar xof "${srcdir}${f}"
+			tar.bz|tar.bz2|tbz|tbz2)
+				tar -I "${bzip2_cmd-bzip2} -c" -xof "${srcdir}${f}"
 				;;
-			tbz|tbz2)
-				(( ${#bzip2_cmd[@]} )) || __compose_bzip2_cmd
-				"${bzip2_cmd[@]}" -c -- "${srcdir}${f}" | tar xof -
-				;;
-			tgz)
-				tar xozf "${srcdir}${f}"
+			tar|tar.*|tgz)
+				# GNU tar recognises various file suffixes, for
+				# which it is able to execute the appropriate
+				# decompressor. They are documented by the
+				# (info) manual for the -a option.
+				tar --warning=decompress-program -xof "${srcdir}${f}"
 				;;
 			txz)
-				XZ_OPT="-T$(___makeopts_jobs)" tar xof "${srcdir}${f}"
+				tar -xJof "${srcdir}${f}"
 				;;
 			xz)
-				__unpack_tar xz -T"$(___makeopts_jobs)" -d
+				xz -dc -- "${srcdir}${f}" > "${basename%.*}"
 				;;
 		esac || die "unpack: failure unpacking ${f@Q}"
 	done

bin/save-ebuild-env.sh
@@ -184,7 +184,6 @@ __save_ebuild_env() (
 		__abort_prepare
 		__abort_test
 		__check_bash_version
-		__compose_bzip2_cmd
 		__dump_trace
 		__dyn_clean
 		__dyn_compile
@@ -223,7 +222,6 @@ __save_ebuild_env() (
 		__start_distcc
 		__strip_duplicate_slashes
 		__try_source
-		__unpack_tar
 		__unset_colors
 		__vecho
 		__ver_compare