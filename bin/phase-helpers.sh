gentoo/portage

commit 7ff579ccb92b2f2512933a41e9a0a204b0601201

phase-helpers.sh: refactor the einstalldocs() function
Declare the entire function as a compound command of the ( … ) form,
which induces a subshell.

Rename the d variable to f, since it is being employed with the
expectation that its value will be the pathname of a regular file.

Use the -v test operator to determine whether the DOCS variable is set.
This feature was introduced by bash-4.2.

Refrain from using the ___is_indexed_array_var() function to determine
whether variables are indexed array variables. Instead, use the
${param@a} form of expansion. This feature was introduced by bash-4.4.

Reduce the use of the && control operator so as to clarify the overall
flow control.

In the course of testing for a scalar variable, test not only that the
variable isn't an indexed array variable, but also that it isn't an
associative array.

Suppress SC2086 warnings where word splitting is being wilfully performed.

Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-helpers.sh
@@ -949,31 +949,31 @@ if ___eapi_has_get_libdir; then
 fi
 
 if ___eapi_has_einstalldocs; then
-	einstalldocs() {
-		(
-			if [[ $(declare -p DOCS 2>/dev/null) != *=* ]]; then
-				local d
-				for d in README* ChangeLog AUTHORS NEWS TODO CHANGES \
-						THANKS BUGS FAQ CREDITS CHANGELOG ; do
-					[[ -f ${d} && -s ${d} ]] && docinto / && dodoc "${d}"
-				done
-			elif ___is_indexed_array_var DOCS ; then
-				[[ ${#DOCS[@]} -gt 0 ]] && docinto / && dodoc -r "${DOCS[@]}"
-			else
-				[[ ${DOCS} ]] && docinto / && dodoc -r ${DOCS}
-			fi
-		)
+	einstalldocs() (
+		local f
 
-		(
-			if ___is_indexed_array_var HTML_DOCS ; then
-				[[ ${#HTML_DOCS[@]} -gt 0 ]] && \
-					docinto html && dodoc -r "${HTML_DOCS[@]}"
-			else
-				[[ ${HTML_DOCS} ]] && \
-					docinto html && dodoc -r ${HTML_DOCS}
-			fi
-		)
-	}
+		if [[ ! -v DOCS ]]; then
+			for f in README* ChangeLog AUTHORS NEWS TODO CHANGES \
+				THANKS BUGS FAQ CREDITS CHANGELOG
+			do
+				if [[ -f ${f} && -s ${f} ]]; then
+					docinto / && dodoc "${f}"
+				fi
+			done
+		elif [[ ${DOCS@a} == *a* ]] && (( ${#DOCS[@]} )); then
+			docinto / && dodoc -r "${DOCS[@]}"
+		elif [[ ${DOCS@a} != *[aA]* && ${DOCS} ]]; then
+			# shellcheck disable=2086
+			docinto / && dodoc -r ${DOCS}
+		fi
+
+		if [[ ${HTML_DOCS@a} == *a* ]] && (( ${#HTML_DOCS[@]} )); then
+			docinto html && dodoc -r "${HTML_DOCS[@]}"
+		elif [[ ${HTML_DOCS@a} != *[aA]* && ${HTML_DOCS} ]]; then
+			# shellcheck disable=2086
+			docinto html && dodoc -r ${HTML_DOCS}
+		fi
+	)
 fi
 
 if ___eapi_has_eapply; then

commit 5ee2d40e97c2aa21a3d322f486fe6783a150bbda

phase-helpers.sh: install default docs where DOCS is undeclared, not unset
EAPI 6 introduced the einstalldocs() function. A recent commit altered
the behaviour of the function in such a way that, if the 'DOCS' variable
is found to be unset, a default set of documentation files shall be
installed. The prior behaviour was to do so in the event that the
variable is undeclared.

It transpires that there is a disagreement between the implementation
and the specification. Below is an excerpt of the pseudocode presented
by PMS, with some redactions made for the purpose of legibility.

 7: else if the DOCS variable is unset then
 8:   for all d matching the filename expansion of README* ChangeLog AUTHORS ...
 9:     if file d exists and has a size greater than zero then
10:       call dodoc with d as argument
11:     end if
12:   end for
13: end if

Note the use of the term, unset. In bash, it is quite possible to
declare a variable that remains in an unset state.

# ( DOCS=(); test -v DOCS || echo unset )
unset
# ( f() { local -a DOCS; test -v DOCS || echo unset; }; f )
unset

Effectively, PMS is in the wrong. By focusing unduly on the
specification, I inadvertently introduced a regression whereby, if
'DOCS' is set as an empty array, the function proceeds to install the
default documentation.

Address this issue by restoring the prior behaviour of checking whether
'DOCS' is undeclared. Further, perform the test by taking advantage of
the ${param@A} form of expansion, rather than parse the output of the
declare builtin as the prior implementation did. Not only does this
approach avoid an unnecessary subshell but it addresses an adjacent bug.
Imagine that 'DOCS' is declared as a function-local array that is empty.

src_install() {
	# This declares a local array that is unset and empty
	local -a DOCS
	# Imagine that einstalldocs() continues to parse declare -p.
	# Here, it will proceed to install the default documentation.
	einstalldocs
}

This long-standing bug was caused by testing in the fashion of
[[ $(declare -p DOCS) != *=* ]], which falsely assumes that the
<equals-sign> character is present for a declared variable.

# ( DOCS=(); declare -p DOCS; )
declare -a DOCS=()
# ( f() { local -a DOCS; declare -p DOCS; }; f ) # no <equals-sign>
declare -a DOCS

Fixes: 7ff579ccb92b2f2512933a41e9a0a204b0601201

bin/phase-helpers.sh
@@ -962,7 +962,11 @@ if ___eapi_has_einstalldocs; then
 	einstalldocs() (
 		local f
 
-		if [[ ! -v DOCS ]]; then
+		# The implementation deviates from PMS, which purports to be
+		# concerned with whether the "DOCS variable is unset". In fact,
+		# the implementation checks whether the variable is undeclared.
+		# However, it is PMS that is in the wrong. See bug #962934.
+		if [[ ! ${DOCS@A} ]]; then
 			for f in README* ChangeLog AUTHORS NEWS TODO CHANGES \
 				THANKS BUGS FAQ CREDITS CHANGELOG
 			do