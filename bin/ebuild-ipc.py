gentoo/portage

commit d628848bfa2734db71f56f671cb7e66bdf5c5638

bin: drop old Python compat shim
Signed-off-by: Sam James <sam@gentoo.org>

bin/ebuild
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import os
 import signal
-
-# For compatibility with Python < 3.8
-raise_signal = getattr(
-    signal, "raise_signal", lambda signum: os.kill(os.getpid(), signum)
-)
 
 
 # Inherit from KeyboardInterrupt to avoid a traceback from asyncio.

bin/ebuild-ipc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2010-2022 Gentoo Authors
+# Copyright 2010-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 #
 # This is a helper which ebuild processes can use
@@ -7,11 +7,6 @@
 
 import os
 import signal
-
-# For compatibility with Python < 3.8
-raise_signal = getattr(
-    signal, "raise_signal", lambda signum: os.kill(os.getpid(), signum)
-)
 
 
 # Inherit from KeyboardInterrupt to avoid a traceback from asyncio.

bin/egencache
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-# Copyright 2009-2022 Gentoo Authors
+# Copyright 2009-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import os
 import signal
-
-# For compatibility with Python < 3.8
-raise_signal = getattr(
-    signal, "raise_signal", lambda signum: os.kill(os.getpid(), signum)
-)
 
 
 # Inherit from KeyboardInterrupt to avoid a traceback from asyncio.

bin/emaint
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2005-2023 Gentoo Authors
+# Copyright 2005-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 """System health checks and maintenance utilities.
@@ -7,11 +7,6 @@
 
 import os
 import signal
-
-# For compatibility with Python < 3.8
-raise_signal = getattr(
-    signal, "raise_signal", lambda signum: os.kill(os.getpid(), signum)
-)
 
 
 # Inherit from KeyboardInterrupt to avoid a traceback from asyncio.

bin/emerge
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
-# Copyright 2006-2023 Gentoo Authors
+# Copyright 2006-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import os
 import signal
 import sys
-
-# For compatibility with Python < 3.8
-raise_signal = getattr(
-    signal, "raise_signal", lambda signum: os.kill(os.getpid(), signum)
-)
 
 
 # Inherit from KeyboardInterrupt to avoid a traceback from asyncio.

bin/portageq
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 import os
 import signal
-
-# For compatibility with Python < 3.8
-raise_signal = getattr(
-    signal, "raise_signal", lambda signum: os.kill(os.getpid(), signum)
-)
 
 
 # Inherit from KeyboardInterrupt to avoid a traceback from asyncio.

commit 26cc6dd4a30019a58841518d667fef2c30db145e

bin: fix signal_raise call
Fixes: d628848bfa2734db71f56f671cb7e66bdf5c5638
Signed-off-by: Sam James <sam@gentoo.org>

bin/ebuild
@@ -440,4 +440,4 @@ if __name__ == "__main__":
         # Prevent traceback on ^C
         signum = getattr(e, "signum", signal.SIGINT)
         signal.signal(signum, signal.SIG_DFL)
-        raise_signal(signum)
+        signal.raise_signal(signum)

bin/ebuild-ipc.py
@@ -308,4 +308,4 @@ except KeyboardInterrupt as e:
     # Prevent traceback on ^C
     signum = getattr(e, "signum", signal.SIGINT)
     signal.signal(signum, signal.SIG_DFL)
-    raise_signal(signum)
+    signal.raise_signal(signum)

bin/egencache
@@ -1386,4 +1386,4 @@ except KeyboardInterrupt as e:
     # Prevent traceback on ^C
     signum = getattr(e, "signum", signal.SIGINT)
     signal.signal(signum, signal.SIG_DFL)
-    raise_signal(signum)
+    signal.raise_signal(signum)

bin/emaint
@@ -67,4 +67,4 @@ except KeyboardInterrupt as e:
     # Prevent traceback on ^C
     signum = getattr(e, "signum", signal.SIGINT)
     signal.signal(signum, signal.SIG_DFL)
-    raise_signal(signum)
+    signal.raise_signal(signum)

bin/emerge
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         signal.signal(signum, signal.SIG_DFL)
         sys.stderr.write(f"\n\nExiting on signal {signum}\n")
         sys.stderr.flush()
-        raise_signal(signum)
+        signal.raise_signal(signum)
     finally:
         # Only close the event loop for __main__,
         # since outside of __main__ it would close the

bin/portageq
@@ -1573,4 +1573,4 @@ except KeyboardInterrupt as e:
     # Prevent traceback on ^C
     signum = getattr(e, "signum", signal.SIGINT)
     signal.signal(signum, signal.SIG_DFL)
-    raise_signal(signum)
+    signal.raise_signal(signum)