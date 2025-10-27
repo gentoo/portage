gentoo/portage

commit bbfe590be38ca79fe198f58bdf83542fd676cf84

doebuild: avoid forkserver permission issue due to userfetch
Since we typically drop privileges for userfetch here,
a forkserver shared with the parent would open privilege
escalation issues that are better to avoid, therefore
force the multiprocessing start method to spawn.

Bug: https://bugs.gentoo.org/965138
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -1654,6 +1654,14 @@ def doebuild(
 
 
 def _fetch_subprocess(fetchme, mysettings, listonly, dist_digests, fetchonly):
+
+    if sys.version_info >= (3, 14):
+        # Since we typically drop privileges for userfetch here,
+        # a forkserver shared with the parent would open privilege
+        # escalation issues that are better to avoid, therefore
+        # force the multiprocessing start method to spawn.
+        multiprocessing.set_start_method("spawn", force=True)
+
     # For userfetch, drop privileges for the entire fetch call, in
     # order to handle DISTDIR on NFS with root_squash for bug 601252.
     if _want_userfetch(mysettings):

commit 48b91a2758897bb5c77fa8e26a7dcb9feb89a031

doebuild: import multiprocessing
Fixes: bbfe590be38ca79fe198f58bdf83542fd676cf84
Bug: https://bugs.gentoo.org/965138
Signed-off-by: Zac Medico <zmedico@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -1,4 +1,4 @@
-# Copyright 2010-2024 Gentoo Authors
+# Copyright 2010-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 __all__ = ["doebuild", "doebuild_environment", "spawn", "spawnebuild"]
@@ -9,6 +9,7 @@ import errno
 import fnmatch
 from itertools import chain
 import logging
+import multiprocessing
 import os as _os
 import platform
 import pwd