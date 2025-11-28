gentoo/portage

commit afaf72e840d910c17e23335c8069c5bd40faf4f7

EAPI 9 bans assert
When rebasing, I had to partially revert commit 6f9971e54d, because
the value of PIPESTATUS must be saved before the feature test call

Signed-off-by: Ulrich Müller <ulm@gentoo.org>

bin/isolated-functions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 # shellcheck disable=2128,2185,2219
 
@@ -23,7 +23,11 @@ fi
 shopt -s expand_aliases
 
 assert() {
-	IFS='|' expression=${PIPESTATUS[*]} let '! expression' || die "$@"
+	local x pipestatus=( "${PIPESTATUS[@]}" )
+	___eapi_has_assert || die "'${FUNCNAME}' banned in EAPI ${EAPI}"
+	for x in "${pipestatus[@]}"; do
+		[[ ${x} -eq 0 ]] || die "$@"
+	done
 }
 
 shopt -s extdebug

bin/eapi.sh
@@ -24,6 +24,7 @@ ___eapi_has_prefix_variables()         [[ ${1-${EAPI-0}} != [0-2] ]]
 
 # HELPERS PRESENCE
 
+___eapi_has_assert()             [[ ${1-${EAPI-0}} == [0-8] ]]
 ___eapi_has_docompress()         [[ ${1-${EAPI-0}} != [0-3] ]]
 ___eapi_has_dohard()             [[ ${1-${EAPI-0}} == [0-3] ]]
 ___eapi_has_doheader()           [[ ${1-${EAPI-0}} != [0-4] ]]

commit 21b9458ec44fab14d915f64476dd8b48f7081cc5

*.sh: Replace internal assert calls
assert() cannot be used in EAPI 9. Instead, define an internal
__pipestatus() function.

Thanks to Florian Schmaus for pointing this out.

Fixes: afaf72e840d910c17e23335c8069c5bd40faf4f7
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

bin/isolated-functions.sh
@@ -30,6 +30,15 @@ assert() {
 	done
 }
 
+# Simplified version of pipestatus() for internal use
+__pipestatus() {
+	local status=( "${PIPESTATUS[@]}" ) s ret=0
+	for s in "${status[@]}"; do
+		[[ ${s} -ne 0 ]] && ret=${s}
+	done
+	return "${ret}"
+}
+
 shopt -s extdebug
 
 # __dump_trace([number of funcs on stack to skip],

bin/misc-functions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2128
 
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 #
 # Miscellaneous shell functions that make use of the ebuild env but don't need
@@ -540,7 +540,8 @@ __dyn_package() {
 
 		gtar ${tar_options} -cf - ${PORTAGE_BINPKG_TAR_OPTS} -C "${D}" . | \
 			${PORTAGE_COMPRESSION_COMMAND} > "${PORTAGE_BINPKG_TMPFILE}"
-		assert "failed to pack binary package: '${PORTAGE_BINPKG_TMPFILE}'"
+		__pipestatus \
+			|| die "failed to pack binary package: '${PORTAGE_BINPKG_TMPFILE}'"
 
 		PYTHONPATH=${PORTAGE_PYTHONPATH:-${PORTAGE_PYM_PATH}} \
 			"${PORTAGE_PYTHON:-/usr/bin/python}" "${PORTAGE_BIN_PATH}"/xpak-helper.py recompose \

bin/phase-functions.sh
@@ -753,7 +753,7 @@ __dyn_install() {
 	__save_ebuild_env --exclude-init-phases | __filter_readonly_variables \
 		--filter-path --filter-sandbox --allow-extra-vars > \
 		"${PORTAGE_BUILDDIR}"/build-info/environment
-	assert "__save_ebuild_env failed"
+	__pipestatus || die "__save_ebuild_env failed"
 	cd "${PORTAGE_BUILDDIR}"/build-info || die
 
 	${PORTAGE_BZIP2_COMMAND} -f9 environment
@@ -1072,7 +1072,7 @@ __ebuild_main() {
 				__filter_readonly_variables --filter-path \
 				--filter-sandbox --allow-extra-vars \
 				| ${PORTAGE_BZIP2_COMMAND} -c -f9 > "${PORTAGE_UPDATE_ENV}"
-			assert "__save_ebuild_env failed"
+			__pipestatus || die "__save_ebuild_env failed"
 		fi
 		;;
 	unpack|prepare|configure|compile|test|clean|install)
@@ -1166,7 +1166,7 @@ __ebuild_main() {
 		cd "${PORTAGE_PYM_PATH}"
 		__save_ebuild_env | __filter_readonly_variables \
 			--filter-features > "${T}/environment"
-		assert "__save_ebuild_env failed"
+		__pipestatus || die "__save_ebuild_env failed"
 
 		chgrp "${PORTAGE_GRPNAME:-portage}" "${T}/environment"
 		chmod g+w "${T}/environment"