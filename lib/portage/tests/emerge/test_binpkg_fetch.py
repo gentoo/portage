gentoo/portage

commit 14a21e6e4a6c5ef9ad9f84bae6b65fff5c00fdda

Always wrap binpkg verification failures in eerror
Use the same logic as from 08a2bc3800ea2e997716903244ec041339b45b06. This
also fixes that in some cases, we weren't emitting gpg output at all,
just 'GPG verify failed'.

Bug: https://bugs.gentoo.org/927632
Bug: https://bugs.gentoo.org/936322
Bug: https://bugs.gentoo.org/941770
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/Scheduler.py
@@ -2,6 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 from collections import deque
+import io
 import gc
 import gzip
 import logging
@@ -1001,19 +1002,35 @@ class Scheduler(PollScheduler):
                     if fetched and bintree.get_local_repo_location(x.cpv):
                         os.rename(fetched, fetcher.pkg_allocated_path)
                     elif fetched:
-                        if not bintree.inject(
-                            x.cpv,
-                            current_pkg_path=fetched,
-                            allocated_pkg_path=fetcher.pkg_allocated_path,
-                        ):
-                            eerror(
-                                "Binary package is not usable",
-                                phase="pretend",
-                                key=x.cpv,
+                        injected_pkg = None
+                        stdout_orig = sys.stdout
+                        stderr_orig = sys.stderr
+                        out = io.StringIO()
+                        try:
+                            sys.stdout = out
+                            sys.stderr = out
+
+                            injected_pkg = bintree.inject(
+                                x.cpv,
+                                current_pkg_path=fetched,
+                                allocated_pkg_path=fetcher.pkg_allocated_path,
                             )
-                            failures += 1
-                            self._record_pkg_failure(x, settings, 1)
-                            continue
+                        finally:
+                            sys.stdout = stdout_orig
+                            sys.stderr = stderr_orig
+
+                        output_value = out.getvalue()
+                        if output_value:
+                            if injected_pkg is None:
+                                msg = ["Binary package is not usable:"]
+                                msg.extend(
+                                    "\t" + line for line in output_value.splitlines()
+                                )
+                                self._elog("eerror", msg)
+
+                        failures += 1
+                        self._record_pkg_failure(x, settings, 1)
+                        continue
 
                     infloc = os.path.join(build_dir_path, "build-info")
                     ensure_dirs(infloc)

lib/portage/gpkg.py
@@ -555,11 +555,16 @@ class checksum_helper:
                 trust_signature = True
 
         if (not good_signature) or (not trust_signature):
-            writemsg(
-                colorize(
-                    "BAD", f"!!!\n{self.gpg_result.decode('UTF-8', errors='replace')}"
-                )
+            msg = ["Binary package is not usable:"]
+            msg.extend(
+                "\t" + line
+                for line in self.gpg_result.decode(
+                    "UTF-8", errors="replace"
+                ).splitlines()
             )
+            out = portage.output.EOutput()
+            [out.eerror(line) for line in msg]
+
             raise InvalidSignature("GPG verify failed")
 
     def update(self, data):
@@ -599,18 +604,25 @@ class checksum_helper:
                 if self.gpg_operation == checksum_helper.VERIFY:
                     self._check_gpg_status(self.gpg_result)
             else:
-                writemsg(
-                    colorize(
-                        "BAD",
-                        f"!!!\n{self.gpg_result.decode('UTF-8', errors='replace')}",
-                    )
+                msg = ["Binary package is not usable:"]
+                msg.extend(
+                    "\t" + line
+                    for line in self.gpg_result.decode(
+                        "UTF-8", errors="replace"
+                    ).splitlines()
                 )
+                out = portage.output.EOutput()
+                [out.eerror(line) for line in msg]
+
                 if self.gpg_operation == checksum_helper.SIGNING:
-                    writemsg(
-                        colorize(
-                            "BAD", self.gpg_output.decode("UTF-8", errors="replace")
-                        )
+                    msg = ["Binary package is not usable (signing failed):"]
+                    msg.extend(
+                        "\t" + line
+                        for line in self.gpg_output.decode(
+                            "UTF-8", errors="replace"
+                        ).splitlines()
                     )
+                    [out.eerror(line) for line in msg]
                     raise GPGException("GPG signing failed")
                 elif self.gpg_operation == checksum_helper.VERIFY:
                     raise InvalidSignature("GPG verify failed")

commit f41f766f3e1d0c60c34e4cb6a9e6b06c67d18fe1

Scheduler: add test case for pkg_pretend bug
Add a testcase for the pkg_pretend --getbinpkg issue introduced in
14a21e6e4a6c5ef9ad9f84bae6b65fff5c00fdda and fixed by
a3f0843a3256ffe3f1248b903702d5b58c7ac892.

Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/tests/emerge/test_binpkg_fetch.py
@@ -5,6 +5,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import textwrap
 
 import portage
 from portage import os
@@ -131,6 +132,209 @@ class BinpkgFetchtestCase(TestCase):
             "PORTAGE_INST_GID": str(os.getgid()),
             "PORTAGE_INST_UID": str(os.getuid()),
             "FEATURES": "-pkgdir-index-trusted",
+        }
+
+        dirs = [
+            playground.distdir,
+            fake_bin,
+            portage_tmpdir,
+            user_config_dir,
+            var_cache_edb,
+        ]
+
+        true_symlinks = ["chown", "chgrp"]
+
+        needed_binaries = {
+            "true": (find_binary("true"), True),
+        }
+
+        try:
+            for d in dirs:
+                ensure_dirs(d)
+            for x in true_symlinks:
+                os.symlink(needed_binaries["true"][0], os.path.join(fake_bin, x))
+
+            with open(os.path.join(var_cache_edb, "counter"), "wb") as f:
+                f.write(b"100")
+
+            if debug:
+                # The subprocess inherits both stdout and stderr, for
+                # debugging purposes.
+                stdout = None
+            else:
+                # The subprocess inherits stderr so that any warnings
+                # triggered by python -Wd will be visible.
+                stdout = subprocess.PIPE
+
+            for i, step in enumerate(test_commands):
+                if isinstance(step, FunctionStep):
+                    try:
+                        step.function(i)
+                    except Exception as e:
+                        if isinstance(e, AssertionError) and f"step {i}" in str(e):
+                            raise
+                        raise AssertionError(
+                            f"step {i} raised {e.__class__.__name__}"
+                        ) from e
+                    continue
+
+                env["PORTAGE_BINHOST"] = f"file:///{tmppkgdir_suffix}"
+                proc = subprocess.Popen(
+                    step.command,
+                    env=dict(env.items(), **(step.env or {})),
+                    cwd=step.cwd,
+                    stdout=stdout,
+                )
+
+                if debug:
+                    proc.wait()
+                else:
+                    output = proc.stdout.readlines()
+                    proc.wait()
+                    proc.stdout.close()
+                    if proc.returncode != step.returncode:
+                        for line in output:
+                            sys.stderr.write(portage._unicode_decode(line))
+
+                self.assertEqual(
+                    step.returncode,
+                    proc.returncode,
+                    f"{step.command} (step {i}) failed with exit code {proc.returncode}",
+                )
+        finally:
+            playground.debug = False
+            playground.cleanup()
+            tmppkgdir.cleanup()
+
+    def testFetchBinpkgWithPkgPretend(self):
+        """
+        Make sure error handling for pkg_pretend w/ --getbinpkg works
+        correctly (fixed by a3f0843a3256ffe3f1248b903702d5b58c7ac892).
+        """
+        debug = False
+
+        pkg_pretend = textwrap.dedent(
+            """
+        S="${WORKDIR}"
+
+        pkg_pretend() {
+            einfo "Hello world!"
+        }
+        """
+        )
+
+        ebuilds = {
+            "dev-libs/A-1::local": {
+                "EAPI": "7",
+                "SLOT": "0",
+                "MISC_CONTENT": pkg_pretend,
+            },
+            "dev-libs/B-1::local": {
+                "EAPI": "7",
+                "SLOT": "0",
+                "MISC_CONTENT": pkg_pretend,
+            },
+        }
+
+        playground = ResolverPlayground(ebuilds=ebuilds, debug=debug)
+        settings = playground.settings
+        eprefix = settings["EPREFIX"]
+        eroot = settings["EROOT"]
+        trees = playground.trees
+        bindb = trees[eroot]["bintree"].dbapi
+        var_cache_edb = os.path.join(eprefix, "var", "cache", "edb")
+        user_config_dir = os.path.join(eprefix, USER_CONFIG_PATH)
+
+        portage_python = portage._python_interpreter
+        emerge_cmd = (
+            portage_python,
+            "-b",
+            "-Wd",
+            os.path.join(str(self.bindir), "emerge"),
+        )
+
+        tmppkgdir = tempfile.TemporaryDirectory()
+        tmppkgdir_suffix = os.path.join(tmppkgdir.name, "binpkg")
+
+        test_commands = (
+            # Create two trivial binpkgs first.
+            CommandStep(
+                returncode=os.EX_OK,
+                command=emerge_cmd
+                + ("--oneshot", "--verbose", "--buildpkg", "dev-libs/A", "dev-libs/B"),
+            ),
+            # Copy to a new PKGDIR which we'll use as PORTAGE_BINHOST
+            # then delete the old PKGDIR.
+            FunctionStep(
+                function=lambda _: shutil.copytree(
+                    bindb.bintree.pkgdir, tmppkgdir_suffix
+                )
+                or True,
+            ),
+            FunctionStep(
+                function=lambda _: os.unlink(
+                    os.path.join(
+                        bindb.bintree.pkgdir, "dev-libs", "A", "A-1-1.gpkg.tar"
+                    )
+                )
+            ),
+            FunctionStep(
+                function=lambda _: os.unlink(
+                    os.path.join(
+                        bindb.bintree.pkgdir, "dev-libs", "B", "B-1-1.gpkg.tar"
+                    )
+                )
+            ),
+            # Both dev-libs/A and dev-libs/B have a (passing) pkg_pretend to force
+            # taking the path that was broken before.
+            CommandStep(
+                returncode=os.EX_OK,
+                command=emerge_cmd
+                + (
+                    "--oneshot",
+                    "--verbose",
+                    "--jobs=2",
+                    "--getbinpkgonly",
+                    "dev-libs/A",
+                    "dev-libs/B",
+                ),
+            ),
+        )
+
+        fake_bin = os.path.join(eprefix, "bin")
+        portage_tmpdir = os.path.join(eprefix, "var", "tmp", "portage")
+
+        path = settings.get("PATH")
+        if path is not None and not path.strip():
+            path = None
+        if path is None:
+            path = ""
+        else:
+            path = ":" + path
+        path = fake_bin + path
+
+        pythonpath = os.environ.get("PYTHONPATH")
+        if pythonpath is not None and not pythonpath.strip():
+            pythonpath = None
+        if pythonpath is not None and pythonpath.split(":")[0] == PORTAGE_PYM_PATH:
+            pass
+        else:
+            if pythonpath is None:
+                pythonpath = ""
+            else:
+                pythonpath = ":" + pythonpath
+            pythonpath = PORTAGE_PYM_PATH + pythonpath
+
+        env = {
+            "PORTAGE_OVERRIDE_EPREFIX": eprefix,
+            "PATH": path,
+            "PORTAGE_PYTHON": portage_python,
+            "PORTAGE_REPOSITORIES": settings.repositories.config_string(),
+            "PYTHONDONTWRITEBYTECODE": os.environ.get("PYTHONDONTWRITEBYTECODE", ""),
+            "PYTHONPATH": pythonpath,
+            "PORTAGE_INST_GID": str(os.getgid()),
+            "PORTAGE_INST_UID": str(os.getuid()),
+            "FEATURES": "-parallel-fetch -pkgdir-index-trusted",
         }
 
         dirs = [