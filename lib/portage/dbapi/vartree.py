e2aa30bb619b54494d91be8134cda342a08f8adf
Michał Górny <mgorny@gentoo.org>
2026-06-02 11:23:38 +0200

    Workaround another import problem on update
    Bug: https://bugs.gentoo.org/976616
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- lib/portage/dbapi/vartree.py
+++ lib/portage/dbapi/vartree.py
@@ -45,6 +45,7 @@ from ._VdbMetadataDelta import VdbMetadataDelta
 
 # Made global to fix importing on Python version upgrade:
 # https://bugs.gentoo.org/970375
+from portage.util.env_update import env_update
 from ._SyncfsProcess import SyncfsProcess
 
 from _emerge.EbuildBuildDir import EbuildBuildDir
@@ -2355,7 +2356,6 @@ class dblink:
         )
         from portage.util import write_atomic
         from portage.util._async.SchedulerInterface import SchedulerInterface
-        from portage.util.env_update import env_update
         from portage.versions import catsplit, cpv_getkey
 
         if trimworld is not None:
@@ -4365,7 +4365,6 @@ class dblink:
             normalize_path,
             write_atomic,
         )
-        from portage.util.env_update import env_update
         from portage.util.install_mask import InstallMask, install_mask_dir
         from portage.util.writeable_check import get_ro_checker
         from portage.package.ebuild.prepare_build_dirs import prepare_build_dirs
