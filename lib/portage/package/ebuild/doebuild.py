gentoo/portage

commit 5d6bbf3549a22d581bfe3eaa796a0f7d11a2c518

doebuild: place PORTAGE_EBUILD_EXTRA_SOURCE under PORTAGE_TMPDIR
Under EAPI 9, spawn() writes unexported PMS variables to a file
referenced by PORTAGE_EBUILD_EXTRA_SOURCE. For the depend phase, that
file is created beneath a directory returned by tempfile.mkdtemp(),
whose parent is selected from the process environment or from Python's
platform-specific fallback.

This approach is unsuitable for phases that are subject to reduced
privileges. In particular, sudo with pam_mktemp can place the temporary
directory under /var/tmp/.private/root, while FEATURES=userpriv causes
the depend phase to run as the portage user. Since that user cannot
traverse the path, PORTAGE_EBUILD_EXTRA_SOURCE cannot be sourced.

Address this issue by creating PORTAGE_EBUILD_EXTRA_SOURCE under
${PORTAGE_TMPDIR}/portage.

Fixes: c75b61b523457f8798c66afe4bcdc1708408ea00
Bug: https://bugs.gentoo.org/977245
Reported-by: Stuart Shelton <srcshelton@gmail.com>
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: James Le Cuirot <chewi@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -80,6 +80,7 @@ from portage.exception import (
     InvalidData,
     InvalidDependString,
     PermissionDenied,
+    PortageException,
     UnsupportedAPIException,
 )
 from portage.localization import _
@@ -2244,8 +2245,25 @@ def spawn(
         else:  # case B and C
             global _emerge_tmpdir
             if _emerge_tmpdir is None:
+                # Keep this directory beneath ${PORTAGE_TMPDIR}/portage so that
+                # dropped-privilege phases can traverse the path and source
+                # PORTAGE_EBUILD_EXTRA_SOURCE (bug #977245).
+                build_prefix = os.path.join(mysettings["PORTAGE_TMPDIR"], "portage")
+                portage.util.ensure_dirs(build_prefix)
+                try:
+                    apply_secpass_permissions(
+                        build_prefix,
+                        gid=portage_gid,
+                        uid=portage_uid,
+                        mode=0o700,
+                        mask=0,
+                    )
+                except PortageException:
+                    if not os.path.isdir(build_prefix):
+                        raise
                 _emerge_tmpdir = tempfile.mkdtemp(
-                    prefix=f"portage-tmpdir-{portage.getpid()}-"
+                    prefix=f"portage-tmpdir-{portage.getpid()}-",
+                    dir=build_prefix,
                 )
                 os.chmod(_emerge_tmpdir, 0o1775)
                 os.chown(_emerge_tmpdir, -1, int(portage_build_gid))

commit 6bfb99ed1ebe72e34132d85effdf712d3a4f7c35

doebuild: use PORTAGE_TMPDIR for _emerge_tmpdir only as root
Following the fix for bug 977245, the temporary directory referenced by
_emerge_tmpdir is created beneath ${PORTAGE_TMPDIR}/portage so that
reduced-privilege phases are able to traverse the path to
PORTAGE_EBUILD_EXTRA_SOURCE.

However, for an unprivileged --pretend operation, such as one that
incurs the depend phase on account of cached metadata being unavailable,
${PORTAGE_TMPDIR}/portage may be inaccessible to the calling user. In
that case, tempfile.mkdtemp() fails with EACCES before
PORTAGE_EBUILD_EXTRA_SOURCE can be created.

Address this issue by designating ${PORTAGE_TMPDIR}/portage as the
parent only where root permissions are in effect. For unprivileged
callers, allow tempfile.mkdtemp() to select its parent as it used to.

Fixes: 5d6bbf3549a22d581bfe3eaa796a0f7d11a2c518
Bug: https://bugs.gentoo.org/977840
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: James Le Cuirot <chewi@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -2245,26 +2245,31 @@ def spawn(
         else:  # case B and C
             global _emerge_tmpdir
             if _emerge_tmpdir is None:
-                # Keep this directory beneath ${PORTAGE_TMPDIR}/portage so that
-                # dropped-privilege phases can traverse the path and source
-                # PORTAGE_EBUILD_EXTRA_SOURCE (bug #977245).
-                build_prefix = os.path.join(mysettings["PORTAGE_TMPDIR"], "portage")
-                portage.util.ensure_dirs(build_prefix)
-                try:
-                    apply_secpass_permissions(
-                        build_prefix,
-                        gid=portage_gid,
-                        uid=portage_uid,
-                        mode=0o700,
-                        mask=0,
+                mkdtemp_kwargs = {
+                    "prefix": f"portage-tmpdir-{portage.getpid()}-",
+                }
+                # A value of 2 means root-equivalent permissions are in effect.
+                if secpass >= 2:
+                    # Keep this directory beneath ${PORTAGE_TMPDIR}/portage so
+                    # that dropped-privilege phases can traverse the path and
+                    # source PORTAGE_EBUILD_EXTRA_SOURCE (bug #977245).
+                    build_prefix = os.path.join(
+                        mysettings["PORTAGE_TMPDIR"], "portage"
                     )
-                except PortageException:
-                    if not os.path.isdir(build_prefix):
-                        raise
-                _emerge_tmpdir = tempfile.mkdtemp(
-                    prefix=f"portage-tmpdir-{portage.getpid()}-",
-                    dir=build_prefix,
-                )
+                    portage.util.ensure_dirs(build_prefix)
+                    try:
+                        apply_secpass_permissions(
+                            build_prefix,
+                            gid=portage_gid,
+                            uid=portage_uid,
+                            mode=0o700,
+                            mask=0,
+                        )
+                    except PortageException:
+                        if not os.path.isdir(build_prefix):
+                            raise
+                    mkdtemp_kwargs["dir"] = build_prefix
+                _emerge_tmpdir = tempfile.mkdtemp(**mkdtemp_kwargs)
                 os.chmod(_emerge_tmpdir, 0o1775)
                 os.chown(_emerge_tmpdir, -1, int(portage_build_gid))
                 portage.process.atexit_register(