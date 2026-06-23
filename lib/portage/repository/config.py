gentoo/portage

commit dd76020b2f921387afee7f71c9f4f5edd4fe5c2b

repository: add more sync-openpgp-key-refresh options
Make sync-openpgp-key-refresh accept the following three more options:

- wkd
- keyserver
- false-nowarn

This allows more control over how the refresh should be performed. For
example, when syncing raw ::gentoo we probably don't want to refresh
via wkd, as not all keys that could appear on the top commit are
available via wkd.

Bug: https://bugs.gentoo.org/661518
Signed-off-by: Florian Schmaus <flow@gentoo.org>
Part-of: https://github.com/gentoo/portage/pull/1474
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/repository/config.py
@@ -269,9 +269,27 @@ class RepoConfig:
 
         self.sync_openpgp_key_path = repo_opts.get("sync-openpgp-key-path", None)
 
-        self.sync_openpgp_key_refresh = repo_opts.get(
+        sync_openpgp_key_refresh = repo_opts.get(
             "sync-openpgp-key-refresh", "true"
-        ).lower() in ("true", "yes")
+        ).lower()
+        if sync_openpgp_key_refresh == "yes":
+            sync_openpgp_key_refresh = "true"
+        elif sync_openpgp_key_refresh == "no":
+            sync_openpgp_key_refresh = "false"
+        elif sync_openpgp_key_refresh not in (
+            "true",
+            "false",
+            "wkd",
+            "keyserver",
+            "false-nowarn",
+        ):
+            writemsg(
+                f"!!! Invalid sync-openpgpg-key-refresh setting for repo {name}: {sync_openpgp_key_refresh}\n",
+                noiselevel=-1,
+            )
+            sync_openpgp_key_refresh = "true"
+
+        self.sync_openpgp_key_refresh = sync_openpgp_key_refresh
 
         for k in (
             "sync_openpgp_key_refresh_retry_count",
@@ -596,8 +614,10 @@ class RepoConfig:
             repo_msg.append(indent + "location: " + self.location)
         if not self.strict_misc_digests:
             repo_msg.append(indent + "strict-misc-digests: false")
-        if not self.sync_openpgp_key_refresh:
-            repo_msg.append(indent + "sync-openpgp-key-refresh: no")
+        if self.sync_openpgp_key_refresh != "true":
+            repo_msg.append(
+                indent + "sync-openpgp-key-refresh: " + self.sync_openpgp_key_refresh
+            )
         if self.sync_type:
             repo_msg.append(indent + "sync-type: " + self.sync_type)
         if self.sync_umask:

lib/portage/sync/syncbase.py
@@ -272,7 +272,9 @@ class SyncBase:
             quiet=("--quiet" in self.options["emerge_config"].opts)
         )
 
-        if not self.repo.sync_openpgp_key_refresh:
+        if self.repo.sync_openpgp_key_refresh == "false-nowarn":
+            return
+        elif self.repo.sync_openpgp_key_refresh == "false":
             out.ewarn(
                 "Key refresh is disabled via a repos.conf sync-openpgp-key-refresh"
             )
@@ -282,11 +284,12 @@ class SyncBase:
             out.ewarn("detection of revoked keys!")
             return
 
-        out.ebegin("Refreshing keys via WKD")
-        if openpgp_env.refresh_keys_wkd():
-            out.eend(0)
-            return
-        out.eend(1)
+        if self.repo.sync_openpgp_key_refresh in ("true", "wkd"):
+            out.ebegin("Refreshing keys via WKD")
+            if openpgp_env.refresh_keys_wkd():
+                out.eend(0)
+                return
+            out.eend(1)
 
         out.ebegin(
             "Refreshing keys from keyserver{}".format(

man/portage.5
@@ -1217,8 +1217,27 @@ only for protocols supporting cryptographic verification, provided
 that the respective verification option is enabled. If unset, the user's
 keyring is used.
 .TP
-.B sync\-openpgp\-key\-refresh = yes
+.B sync\-openpgp\-key\-refresh = yes|true|wkd|keyserver|no|false|false-nowarn
 Enable OpenPGP key(ring) refresh. This option is enabled by default.
+
+The refresh method is determined by the value:
+.RS
+.TP
+\fIyes\fR (\fItrue\fR)
+Attempt refresh first via WKD (Web Key Directory), with a fallback to the configured keyserver. (Default)
+.TP
+\fIwkd\fR
+Refresh keys using WKD only.
+.TP
+\fIkeyserver\fR
+Refresh keys using the configured keyserver only.
+.TP
+\fIno\fR (\fIfalse\fR)
+Disable refresh and emit a warning about the security impact.
+.TP
+\fIfalse\-nowarn\fR
+Disable refresh without emitting a warning.
+.RE
 
 \fBWarning\fR: It is a security vulnerability to disable this option
 because this will prevent detection of revoked keys!

commit 6a3a4187f1f0dba216438bd9e938480596a807b0

RepoConfigLoader: serialise sync-openpgp-key-refresh as a string
The value of the sync-openpgp-key-refresh attribute may legally be any
of "true", "false", "wkd", "keyserver" and "false-nowarn".

However, the config_string() function currently treats this attribute as
a boolean, causing all non-empty values to be serialised as "true" in
the course of composing the PORTAGE_REPOSITORIES environment variable.

Address this issue by designating it as a string attribute.

Bug: https://bugs.gentoo.org/977915
Fixes: dd76020b2f921387afee7f71c9f4f5edd4fe5c2b
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: James Le Cuirot <chewi@gentoo.org>

lib/portage/repository/config.py
@@ -1374,7 +1374,6 @@ class RepoConfigLoader:
         bool_keys = (
             "strict_misc_digests",
             "sync_allow_hardlinks",
-            "sync_openpgp_key_refresh",
             "sync_rcu",
             "volatile",
         )
@@ -1389,6 +1388,7 @@ class RepoConfigLoader:
             "sync_openpgp_keyserver",
             "sync_openpgp_key_package",
             "sync_openpgp_key_path",
+            "sync_openpgp_key_refresh",
             "sync_openpgp_key_refresh_retry_count",
             "sync_openpgp_key_refresh_retry_delay_exp_base",
             "sync_openpgp_key_refresh_retry_delay_max",