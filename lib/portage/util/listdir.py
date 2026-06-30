gentoo/portage

commit ef82d7e97385c7d7f377c0adf6f9cec21f0856bb

util: listdir: port to os.scandir()
Replace {os.stat(), os.listdir(), os.stat() or os.lstat()} with just
os.scandir() which will reuse cached stat calls where possible, like
where we check the file type. It's also just cleaner.

Porting consumers of os.listdir() in general will require some care
to see if it's worht the churn, as it's only worth it if the
information is actually going to be reused (nicer attributes notwithstanding).

This is a bit easier to review with `-w`.

With this on emerge -p -uvDU @world for me, we go from 17746 newfstatat
calls -> 14834 newfsstatat (16%) calls, and with a total syscall count change
of 297326 -> 294345 (1%).

Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/util/listdir.py
@@ -1,56 +1,44 @@
-# Copyright 2010-2013 Gentoo Foundation
+# Copyright 2010-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 __all__ = ["cacheddir", "listdir"]
 
-import errno
-import stat
-
 import os
+
 from portage.const import VCS_DIRS
 from portage.exception import DirectoryNotFound, PermissionDenied, PortageException
 from portage.util import normalize_path
 
 
 def cacheddir(my_original_path, ignorecvs, ignorelist, followSymlinks=True):
+    fpaths = []
+    ftype = []
     mypath = normalize_path(my_original_path)
+
     try:
-        pathstat = os.stat(mypath)
-        if not stat.S_ISDIR(pathstat.st_mode):
-            raise DirectoryNotFound(mypath)
+        with os.scandir(mypath) as it:
+            for entry in it:
+                fpaths.append(entry.name)
+
+                try:
+                    if entry.is_file(follow_symlinks=followSymlinks):
+                        ftype.append(0)
+                    elif entry.is_dir(follow_symlinks=followSymlinks):
+                        ftype.append(1)
+                    elif entry.is_symlink(follow_symlinks=followSymlinks):
+                        ftype.append(2)
+                    else:
+                        ftype.append(3)
+                except OSError:
+                    ftype.append(3)
+    except NotADirectoryError:
+        raise DirectoryNotFound(mypath)
     except OSError as e:
         if e.errno == PermissionDenied.errno:
             raise PermissionDenied(mypath)
-        del e
-        return [], []
+        return fpaths, ftype
     except PortageException:
-        return [], []
-    else:
-        try:
-            fpaths = os.listdir(mypath)
-        except OSError as e:
-            if e.errno != errno.EACCES:
-                raise
-            del e
-            raise PermissionDenied(mypath)
-        ftype = []
-        for x in fpaths:
-            try:
-                if followSymlinks:
-                    pathstat = os.stat(mypath + "/" + x)
-                else:
-                    pathstat = os.lstat(mypath + "/" + x)
-
-                if stat.S_ISREG(pathstat[stat.ST_MODE]):
-                    ftype.append(0)
-                elif stat.S_ISDIR(pathstat[stat.ST_MODE]):
-                    ftype.append(1)
-                elif stat.S_ISLNK(pathstat[stat.ST_MODE]):
-                    ftype.append(2)
-                else:
-                    ftype.append(3)
-            except OSError:
-                ftype.append(3)
+        return fpaths, ftype
 
     if ignorelist or ignorecvs:
         ret_list = []

commit c28b0a920b61db5a4897c395de2d134e600854f0

util: listdir: drop invalid follow_symlinks kwarg from is_symlink()
Python's is_symlink() method takes no arguments, unlike is_dir() and
is_file(). Passing the 'follow_symlinks' keyword argument results in a
TypeError.

This bug surfaces during package tree traversals, notably crashing
"eclean distfiles" with:

  TypeError: is_symlink() takes no arguments
      elif entry.is_symlink(follow_symlinks=followSymlinks):

Fix this by removing the invalid argument.

Signed-off-by: Florian Schmaus <flow@gentoo.org>
Fixes: ef82d7e97385c7d7f377c0adf6f9cec21f0856bb

lib/portage/util/listdir.py
@@ -25,7 +25,7 @@ def cacheddir(my_original_path, ignorecvs, ignorelist, followSymlinks=True):
                         ftype.append(0)
                     elif entry.is_dir(follow_symlinks=followSymlinks):
                         ftype.append(1)
-                    elif entry.is_symlink(follow_symlinks=followSymlinks):
+                    elif entry.is_symlink():
                         ftype.append(2)
                     else:
                         ftype.append(3)