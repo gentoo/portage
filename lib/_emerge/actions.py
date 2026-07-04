gentoo/portage

commit 7e15fc7fa4c1e8f2afd5bc97508b627f07affbd1

dep: Atom: remove str subtyping
[mattst88]
Fix call sites that relied on Atom being a str subclass:
- wrap str method calls (lstrip, indexing, join, concatenation) with str()
  in BlockerDB, glsa, actions, depgraph, bintree, binrepo/config,
  repository/config
- use key=str in emaint/world sorted() call instead of adding ordering
  methods to Atom
- add TYPE_CHECKING guard and lazyimport block in versions.py

Co-authored-by: Matt Turner <mattst88@gentoo.org>
Signed-off-by: Matt Turner <mattst88@gentoo.org>

lib/_emerge/actions.py
@@ -1758,8 +1758,8 @@ def action_deselect(settings, trees, opts, atoms):
         expanded_atoms = set(atoms)
 
         for atom in atoms:
-            if not atom.startswith(SETPREFIX):
-                if atom.cp.startswith("null/"):
+            if not str(atom).startswith(SETPREFIX):
+                if atom.category == "null":
                     # try to expand category from world set
                     null_cat, pn = portage.catsplit(atom.cp)
                     for world_atom in world_atoms:
@@ -1767,7 +1767,7 @@ def action_deselect(settings, trees, opts, atoms):
                         if pn == world_pn:
                             expanded_atoms.add(
                                 Atom(
-                                    atom.replace("null", cat, 1),
+                                    str(atom).replace("null", cat, 1),
                                     allow_repo=True,
                                     allow_wildcard=True,
                                 )
@@ -1780,13 +1780,13 @@ def action_deselect(settings, trees, opts, atoms):
         discard_atoms = set()
         for atom in world_set:
             for arg_atom in expanded_atoms:
-                if arg_atom.startswith(SETPREFIX):
-                    if atom.startswith(SETPREFIX) and arg_atom == atom:
+                if str(arg_atom).startswith(SETPREFIX):
+                    if str(atom).startswith(SETPREFIX) and arg_atom == atom:
                         discard_atoms.add(atom)
                         break
                 else:
                     if (
-                        not atom.startswith(SETPREFIX)
+                        not str(atom).startswith(SETPREFIX)
                         and arg_atom.intersects(atom)
                         and not (arg_atom.slot and not atom.slot)
                         and not (arg_atom.repo and not atom.repo)
@@ -1794,13 +1794,13 @@ def action_deselect(settings, trees, opts, atoms):
                         discard_atoms.add(atom)
                         break
         if discard_atoms:
-            for atom in sorted(discard_atoms):
+            for atom in sorted(discard_atoms, key=str):
                 if pretend:
                     action_desc = "Would remove"
                 else:
                     action_desc = "Removing"
 
-                if atom.startswith(SETPREFIX):
+                if str(atom).startswith(SETPREFIX):
                     filename = "world_sets"
                 else:
                     filename = "world"
@@ -2103,7 +2103,7 @@ def action_info(settings, trees, myopts, myfiles):
                 if not atom.blocker:
                     atoms.append((x, atom))
 
-    myvars = sorted(set(atoms))
+    myvars = sorted(set(atoms), key=lambda t: str(t[0]))
 
     cp_map = {}
     cp_max_len = 0
@@ -2835,7 +2835,7 @@ def binpkg_selection_config(opts, settings):
         writemsg(
             "\n!!! The following atoms appear in both the --usepkg-exclude "
             "and --usepkg-include command line arguments:\n"
-            "\n    %s\n" % ("\n    ".join(conflicted_atoms))
+            "\n    %s\n" % ("\n    ".join(str(a) for a in conflicted_atoms))
         )
         for a in conflicted_atoms:
             usepkg_exclude.remove(a)
@@ -2847,14 +2847,16 @@ def binpkg_selection_config(opts, settings):
             writemsg(
                 "\n!!! The following --usepkg-exclude atoms are ignored due "
                 "to use of --nobindeps:\n"
-                "\n    %s\n" % ("\n    ".join(usepkg_exclude.getAtoms()))
+                "\n    %s\n"
+                % ("\n    ".join(str(a) for a in usepkg_exclude.getAtoms()))
             )
             usepkg_exclude.clear()
         if not usepkg_include.isEmpty():
             writemsg(
                 "\n!!! The following --usepkg-include atoms are ignored due "
                 "to use of --nobindeps:\n"
-                "\n    %s\n" % ("\n    ".join(usepkg_include.getAtoms()))
+                "\n    %s\n"
+                % ("\n    ".join(str(a) for a in usepkg_include.getAtoms()))
             )
             usepkg_include.clear()
         for repo in settings.repositories:
@@ -2863,7 +2865,10 @@ def binpkg_selection_config(opts, settings):
                     "\n!!! The following usepkg-exclude atoms for [%s] are "
                     "ignored due to use of --nobindeps:\n"
                     "\n    %s\n"
-                    % (repo.name, "\n    ".join(repo.usepkg_exclude.getAtoms()))
+                    % (
+                        repo.name,
+                        "\n    ".join(str(a) for a in repo.usepkg_exclude.getAtoms()),
+                    )
                 )
                 repo.usepkg_exclude.clear()
             if not repo.usepkg_include.isEmpty():
@@ -2871,7 +2876,10 @@ def binpkg_selection_config(opts, settings):
                     "\n!!! The following usepkg-include atoms for [%s] are "
                     "ignored due to use of --nobindeps:\n"
                     "\n    %s\n"
-                    % (repo.name, "\n    ".join(repo.usepkg_include.getAtoms()))
+                    % (
+                        repo.name,
+                        "\n    ".join(str(a) for a in repo.usepkg_include.getAtoms()),
+                    )
                 )
                 repo.usepkg_include.clear()
 
@@ -2884,7 +2892,8 @@ def binpkg_selection_config(opts, settings):
             writemsg(
                 "\n!!! The following usepkg-exclude atoms for [%s] have "
                 "been overridden by the --usepkg-include option:\n"
-                "\n    %s\n" % (repo.name, "\n    ".join(conflicted_exclude))
+                "\n    %s\n"
+                % (repo.name, "\n    ".join(str(a) for a in conflicted_exclude))
             )
             for a in conflicted_exclude:
                 repo.usepkg_exclude.remove(a)
@@ -2895,7 +2904,8 @@ def binpkg_selection_config(opts, settings):
             writemsg(
                 "\n!!! The following usepkg-include atoms for [%s] have "
                 "been overridden by the --usepkg-exclude option:\n"
-                "\n    %s\n" % (repo.name, "\n    ".join(conflicted_include))
+                "\n    %s\n"
+                % (repo.name, "\n    ".join(str(a) for a in conflicted_include))
             )
             for a in conflicted_include:
                 repo.usepkg_include.remove(a)
@@ -2908,20 +2918,20 @@ def binpkg_selection_config(opts, settings):
         writemsg(
             "\n!!! The following atoms appear in both the --getbinpkg-exclude "
             "and --getbinpkg-include command line arguments:\n"
-            "\n    %s\n" % ("\n    ".join(conflicted_atoms))
+            "\n    %s\n" % ("\n    ".join(str(a) for a in conflicted_atoms))
         )
         for a in conflicted_atoms:
             getbinpkg_exclude.remove(a)
             getbinpkg_include.remove(a)
 
     if not getbinpkg_exclude.isEmpty():
-        opts["--getbinpkg-exclude"] = list(getbinpkg_exclude)
+        opts["--getbinpkg-exclude"] = [str(a) for a in getbinpkg_exclude]
     if not getbinpkg_include.isEmpty():
-        opts["--getbinpkg-include"] = list(getbinpkg_include)
+        opts["--getbinpkg-include"] = [str(a) for a in getbinpkg_include]
     if not usepkg_exclude.isEmpty():
-        opts["--usepkg-exclude"] = list(usepkg_exclude)
+        opts["--usepkg-exclude"] = [str(a) for a in usepkg_exclude]
     if not usepkg_include.isEmpty():
-        opts["--usepkg-include"] = list(usepkg_include)
+        opts["--usepkg-include"] = [str(a) for a in usepkg_include]
 
 
 def display_missing_pkg_set(root_config, set_name):
@@ -4161,11 +4171,11 @@ def run_action(emerge_config):
                     # look at the ebuilds, since EAPI 4 allows running pkg_info
                     # on non-installed packages
                     valid_atom = dep_expand(x, mydb=vardb)
-                    if valid_atom.cp.split("/")[0] == "null":
+                    if valid_atom.category == "null":
                         valid_atom = dep_expand(x, mydb=portdb)
 
                     if (
-                        valid_atom.cp.split("/")[0] == "null"
+                        valid_atom.category == "null"
                         and emerge_config.opts.get("--usepkg") is True
                     ):
                         valid_atom = dep_expand(x, mydb=bindb)

lib/_emerge/depgraph.py
@@ -192,33 +192,37 @@ class _frozen_depgraph_config:
         else:
             self._required_set_names = {"world"}
 
-        atoms = " ".join(myopts.get("--exclude", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--exclude", [])).split()
         self.excluded_pkgs = WildcardPackageSet(atoms)
-        atoms = " ".join(myopts.get("--reinstall-atoms", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--reinstall-atoms", [])).split()
         self.reinstall_atoms = WildcardPackageSet(atoms)
-        atoms = " ".join(myopts.get("--usepkg-exclude", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--usepkg-exclude", [])).split()
         self.usepkg_exclude = WildcardPackageSet(atoms, allow_repo=True)
-        atoms = " ".join(myopts.get("--usepkg-include", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--usepkg-include", [])).split()
         self.usepkg_include = WildcardPackageSet(atoms, allow_repo=True)
-        atoms = " ".join(myopts.get("--useoldpkg-atoms", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--useoldpkg-atoms", [])).split()
         self.useoldpkg_atoms = WildcardPackageSet(atoms)
-        atoms = " ".join(myopts.get("--rebuild-exclude", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--rebuild-exclude", [])).split()
         self.rebuild_exclude = WildcardPackageSet(atoms)
-        atoms = " ".join(myopts.get("--rebuild-ignore", [])).split()
+        atoms = " ".join(str(a) for a in myopts.get("--rebuild-ignore", [])).split()
         self.rebuild_ignore = WildcardPackageSet(atoms)
 
         self.buildpkg_exclude = InternalPackageSet(
-            initial_atoms=" ".join(myopts.get("--buildpkg-exclude", [])).split(),
+            initial_atoms=" ".join(
+                str(a) for a in myopts.get("--buildpkg-exclude", [])
+            ).split(),
             allow_wildcard=True,
             allow_repo=True,
         )
 
         for repo in settings.repositories:
             self.usepkg_exclude.update(
-                a + _repo_separator + repo.name for a in repo.usepkg_exclude.getAtoms()
+                str(a) + _repo_separator + repo.name
+                for a in repo.usepkg_exclude.getAtoms()
             )
             self.usepkg_include.update(
-                a + _repo_separator + repo.name for a in repo.usepkg_include.getAtoms()
+                str(a) + _repo_separator + repo.name
+                for a in repo.usepkg_include.getAtoms()
             )
 
         self.rebuild_if_new_rev = "--rebuild-if-new-rev" in myopts
@@ -3671,7 +3675,7 @@ class depgraph:
                     continue
                 dep = Dependency(
                     atom=atom,
-                    blocker=False,
+                    blocker=None,
                     depth=depth,
                     parent=pkg,
                     priority=self._priority(cross=self._cross(pkg.root), runtime=True),
@@ -4709,7 +4713,7 @@ class depgraph:
                 # Note: Eventually this will check for PROPERTIES=virtual
                 # or whatever other metadata gets implemented for this
                 # purpose.
-                if x.cp.startswith("virtual/"):
+                if x.category == "virtual":
                     disjunctions.append(x)
                 else:
                     yield x
@@ -5142,8 +5146,10 @@ class depgraph:
                 if len(expanded_atoms) > 1:
                     number_of_virtuals = 0
                     for expanded_atom in expanded_atoms:
-                        if expanded_atom.cp.startswith(
-                            ("acct-group/", "acct-user/", "virtual/")
+                        if expanded_atom.category in (
+                            "acct-group",
+                            "acct-user",
+                            "virtual",
                         ):
                             number_of_virtuals += 1
                         else:
@@ -5173,7 +5179,8 @@ class depgraph:
                     if virts_p:
                         # Allow the depgraph to choose which virtual.
                         atom = Atom(
-                            null_atom.replace("null/", "virtual/", 1), allow_repo=True
+                            str(null_atom).replace("null/", "virtual/", 1),
+                            allow_repo=True,
                         )
                     else:
                         atom = null_atom
@@ -5334,7 +5341,7 @@ class depgraph:
             pprovideddict = pkgsettings.pprovideddict
             virtuals = pkgsettings.getvirtuals()
 
-            for atom in sorted(arg.pset.getAtoms()):
+            for atom in sorted(arg.pset.getAtoms(), key=str):
                 self._spinner_update()
                 dep = Dependency(atom=atom, onlydeps=onlydeps, root=myroot, parent=arg)
                 try:
@@ -5405,7 +5412,7 @@ class depgraph:
                         pprovided_match = False
                         for virt_choice in virtuals.get(atom.cp, []):
                             expanded_atom = portage.dep.Atom(
-                                atom.replace(atom.cp, virt_choice.cp, 1)
+                                str(atom).replace(atom.cp, virt_choice.cp, 1)
                             )
                             pprovided = pprovideddict.get(expanded_atom.cp)
                             if pprovided and portage.match_from_list(
@@ -5445,7 +5452,7 @@ class depgraph:
                     if atom.cp != pkg.cp:
                         # For old-style virtuals, we need to repeat the
                         # package.provided check against the selected package.
-                        expanded_atom = atom.replace(atom.cp, pkg.cp)
+                        expanded_atom = str(atom).replace(atom.cp, pkg.cp)
                         pprovided = pprovideddict.get(pkg.cp)
                         if pprovided and portage.match_from_list(
                             expanded_atom, pprovided
@@ -6007,7 +6014,7 @@ class depgraph:
         if not isinstance(atom, Atom):
             atom = Atom(atom)
 
-        if not atom.cp.startswith("virtual/"):
+        if atom.category != "virtual":
             yield atom
             return
 
@@ -6846,7 +6853,7 @@ class depgraph:
             mask_docs = True
         else:
             cp_exists = False
-            if atom.package and not atom.cp.startswith("null/"):
+            if atom.package and atom.category != "null":
                 for pkg in self._iter_match_pkgs_any(root_config, Atom(atom.cp)):
                     cp_exists = True
                     break
@@ -8566,7 +8573,7 @@ class depgraph:
 
         self._set_args(args)
         for arg in self._expand_set_args(args, add_to_digraph=True):
-            for atom in sorted(arg.pset.getAtoms()):
+            for atom in sorted(arg.pset.getAtoms(), key=str):
                 if not self._add_dep(
                     Dependency(
                         atom=atom,
@@ -8826,7 +8833,7 @@ class depgraph:
 
                     if blocker_data is None and blockers is not None:
                         # Re-use the blockers from the graph.
-                        blocker_atoms = sorted(blockers)
+                        blocker_atoms = sorted(blockers, key=str)
                         blocker_data = blocker_cache.BlockerData(
                             pkg.counter, blocker_atoms
                         )
@@ -8879,7 +8886,7 @@ class depgraph:
                             show_invalid_depstring_notice(pkg, atoms)
                             return False
                         blocker_atoms = [myatom for myatom in atoms if myatom.blocker]
-                        blocker_atoms.sort()
+                        blocker_atoms.sort(key=str)
                         blocker_cache[cpv] = blocker_cache.BlockerData(
                             pkg.counter, blocker_atoms
                         )
@@ -8938,7 +8945,9 @@ class depgraph:
                 atoms = []
                 for provider_entry in virtuals[blocker.cp]:
                     atoms.append(
-                        Atom(blocker.atom.replace(blocker.cp, provider_entry.cp, 1))
+                        Atom(
+                            str(blocker.atom).replace(blocker.cp, provider_entry.cp, 1)
+                        )
                     )
             else:
                 atoms = [blocker.atom]
@@ -11135,7 +11144,7 @@ class depgraph:
             all_added.append(s)
         all_added.extend(added_favorites)
         if all_added:
-            all_added.sort()
+            all_added.sort(key=str)
             skip = False
             if "--ask" in self._frozen_config.myopts:
                 writemsg_stdout("\n", noiselevel=-1)
@@ -11306,7 +11315,7 @@ class depgraph:
             # added via _add_pkg() so that they are included in the
             # digraph (needed at least for --tree display).
             for arg in self._expand_set_args(args, add_to_digraph=True):
-                for atom in sorted(arg.pset.getAtoms()):
+                for atom in sorted(arg.pset.getAtoms(), key=str):
                     pkg, existing_node = self._select_package(
                         arg.root_config.root, atom
                     )
@@ -11614,7 +11623,7 @@ class _dep_check_composite_db(dbapi):
         if (
             pkg is not None
             and atom.sub_slot is None
-            and pkg.cp.startswith("virtual/")
+            and pkg.category == "virtual"
             and (
                 (
                     "remove" not in self._depgraph._dynamic_config.myparams
@@ -11706,7 +11715,7 @@ class _dep_check_composite_db(dbapi):
                 if not self._depgraph._equiv_ebuild_visible(pkg):
                     return False
 
-        if pkg.cp.startswith("virtual/"):
+        if pkg.category == "virtual":
             if not self._depgraph._virt_deps_visible(pkg, ignore_use=True):
                 return False
 

bin/quickpkg
@@ -71,7 +71,8 @@ def quickpkg_atom(options, infos, arg, eout):
         eout.eerror(f"Invalid atom: {arg}")
         infos["missing"].append(arg)
         return 1
-    if atom[:1] == "=" and arg[:1] != "=":
+    # Check if dep_expand added an operator that wasn't in the original arg
+    if atom.operator == "=" and not str(arg).startswith("="):
         # dep_expand() allows missing '=' but it's really invalid
         eout.eerror(f"Invalid atom: {arg}")
         infos["missing"].append(arg)

lib/_emerge/BlockerCache.py
@@ -2,6 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 import errno
+from portage.dep import Atom
 from portage.util import writemsg
 from portage.data import secpass
 import portage
@@ -99,10 +100,10 @@ class BlockerCache(portage.cache.mappings.MutableMapping):
                     continue
                 invalid_atom = False
                 for atom in atoms:
-                    if not isinstance(atom, str):
+                    if not isinstance(atom, (str, Atom)):
                         invalid_atom = True
                         break
-                    if atom[:1] != "!" or not portage.isvalidatom(
+                    if str(atom)[:1] != "!" or not portage.isvalidatom(
                         atom, allow_blockers=True
                     ):
                         invalid_atom = True

lib/_emerge/BlockerDB.py
@@ -76,8 +76,8 @@ class BlockerDB:
                     )
                     continue
 
-                blocker_atoms = [atom for atom in atoms if atom.startswith("!")]
-                blocker_atoms.sort()
+                blocker_atoms = [atom for atom in atoms if atom.blocker]
+                blocker_atoms.sort(key=str)
                 blocker_cache[inst_pkg.cpv] = blocker_cache.BlockerData(
                     inst_pkg.counter, blocker_atoms
                 )
@@ -89,7 +89,7 @@ class BlockerDB:
         blocker_atoms = []
         for pkg in installed_pkgs:
             for blocker_atom in blocker_cache[pkg.cpv].atoms:
-                blocker_atom = blocker_atom.lstrip("!")
+                blocker_atom = str(blocker_atom).lstrip("!")
                 blocker_atoms.append(blocker_atom)
                 blocker_parents.add(blocker_atom, pkg)
 
@@ -113,7 +113,7 @@ class BlockerDB:
             show_invalid_depstring_notice(new_pkg, atoms)
             assert False
 
-        blocker_atoms = [atom.lstrip("!") for atom in atoms if atom[:1] == "!"]
+        blocker_atoms = [str(atom).lstrip("!") for atom in atoms if atom.blocker]
         if blocker_atoms:
             blocker_atoms = InternalPackageSet(initial_atoms=blocker_atoms)
             for inst_pkg in installed_pkgs:

lib/_emerge/create_world_atom.py
@@ -113,7 +113,7 @@ def create_world_atom(pkg, args_set, root_config, before_install=False):
         # gcc for example.
         system_atom = sets["system"].findAtomForPackage(pkg)
         if system_atom:
-            if not system_atom.cp.startswith("virtual/"):
+            if system_atom.category != "virtual":
                 return None
             # System virtuals aren't safe to exclude from world since they can
             # match multiple old-style virtuals but only one of them will be

lib/_emerge/resolver/output.py
@@ -953,7 +953,7 @@ def format_unmatched_atom(pkg, atom, pkg_use_enabled):
     def perform_coloring():
         atom_str = ""
         marker_str = ""
-        for ii, x in enumerate(atom):
+        for ii, x in enumerate(str(atom)):
             if ii in highlight:
                 atom_str += colorize("BAD", x)
                 marker_str += "^"
@@ -964,7 +964,7 @@ def format_unmatched_atom(pkg, atom, pkg_use_enabled):
 
     if atom.cp != pkg.cp:
         # Highlight the cp part only.
-        ii = atom.find(atom.cp)
+        ii = str(atom).find(atom.cp)
         highlight.update(range(ii, ii + len(atom.cp)))
         return perform_coloring()
 
@@ -994,7 +994,7 @@ def format_unmatched_atom(pkg, atom, pkg_use_enabled):
             highlight.update(range(len(op)))
 
         if ver is not None:
-            start = atom.rfind(ver)
+            start = str(atom).rfind(ver)
             end = start + len(ver)
             highlight.update(range(start, end))
 
@@ -1004,7 +1004,7 @@ def format_unmatched_atom(pkg, atom, pkg_use_enabled):
             slot_str += "/" + atom.sub_slot
         if atom.slot_operator:
             slot_str += atom.slot_operator
-        start = atom.find(slot_str)
+        start = str(atom).find(slot_str)
         end = start + len(slot_str)
         highlight.update(range(start, end))
 
@@ -1027,7 +1027,7 @@ def format_unmatched_atom(pkg, atom, pkg_use_enabled):
                     )
 
     if highlight_use:
-        ii = atom.find("[") + 1
+        ii = str(atom).find("[") + 1
         for token in atom.use.tokens:
             if token.lstrip("-!").rstrip("=?") in highlight_use:
                 highlight.update(range(ii, ii + len(token)))

lib/_emerge/unmerge.py
@@ -122,8 +122,9 @@ def _unmerge_display(
                 print(f"\nNo packages to {unmerge_action} have been provided.\n")
                 return 1, {}
             for x in unmerge_files:
-                arg_parts = x.split("/")
-                if x[0] not in [".", "/"] and arg_parts[-1][-7:] != ".ebuild":
+                x_str = str(x)  # this could be an Atom, stringify
+                arg_parts = x_str.split("/")
+                if x_str[0] not in [".", "/"] and arg_parts[-1][-7:] != ".ebuild":
                     # possible cat/pkg or dep; treat as such
                     candidate_catpkgs.append(x)
                 elif unmerge_action in ["prune", "clean"]:

commit d51c79eebbc775cc2e70272c2ed3a696899ea872

_emerge: fix TypeError joining Atom objects in diagnostic messages
Same class of bug as fixed in Scheduler._calc_resume_list: Atom objects
must be explicitly converted to str before being passed to str.join().

Fix actions.action_build (dropped_tasks display) and
depgraph._slot_operator_check_reverse_dependencies (debug message).

Fixes: 7e15fc7fa ("dep: Atom: remove str subtyping")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

lib/_emerge/actions.py
@@ -385,7 +385,7 @@ def action_build(
                         )
                     else:
                         writemsg(
-                            f"  {task} requires {', '.join(atoms)}\n",
+                            f"  {task} requires {', '.join(str(a) for a in atoms)}\n",
                             noiselevel=-1,
                         )
 

lib/_emerge/depgraph.py
@@ -2386,7 +2386,7 @@ class depgraph:
                         "_slot_operator_check_reverse_dependencies:",
                         f"   candidate package does not match atom '{atom}': {candidate_pkg}",
                         f"   parent: {parent}",
-                        f"   parent atoms: {' '.join(parent_atoms)}",
+                        f"   parent atoms: {' '.join(str(a) for a in parent_atoms)}",
                         "",
                     )
                     writemsg_level("\n".join(msg), noiselevel=-1, level=logging.DEBUG)