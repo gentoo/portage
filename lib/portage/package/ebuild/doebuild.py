gentoo/portage

commit cbe382c8d7b9b27adbd3de627e237011404b54c3

ebuild: ignore RESTRICT=network-sandbox for depend phase
Even if the ebuild has RESTRICT=network-sandbox, we should ignore that
for the depend phase. It's too dangerous and there shouldn't be any
justification for that.

Bug: https://bugs.gentoo.org/978846
Fixes: d56707b9d3451810562fc1e763d4916ab02dd54e
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -247,7 +247,7 @@ def _doebuild_spawn(phase, settings, actionmap=None, **kwargs):
             phase == "test" and "test_network" in settings["PORTAGE_PROPERTIES"].split()
         )
         or phase in _ipc_phases
-        or "network-sandbox" in settings["PORTAGE_RESTRICT"].split()
+        or (phase != "depend" and "network-sandbox" in settings["PORTAGE_RESTRICT"].split())
     )
     kwargs["pidns"] = (
         "pid-sandbox" in settings.features and phase not in _global_pid_phases

commit a5e34c4a35b8b87433a1e262cca2278517cdee70

Run black
Fixes: cbe382c8d ("ebuild: ignore RESTRICT=network-sandbox for depend phase")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

lib/portage/package/ebuild/doebuild.py
@@ -138,9 +138,7 @@ _ipc_phases = frozenset(
 )
 
 # phases which execute in the global PID namespace
-_global_pid_phases = frozenset(
-    ["config", "preinst", "prerm", "postinst", "postrm"]
-)
+_global_pid_phases = frozenset(["config", "preinst", "prerm", "postinst", "postrm"])
 
 _phase_func_map = {
     "config": "pkg_config",
@@ -247,7 +245,10 @@ def _doebuild_spawn(phase, settings, actionmap=None, **kwargs):
             phase == "test" and "test_network" in settings["PORTAGE_PROPERTIES"].split()
         )
         or phase in _ipc_phases
-        or (phase != "depend" and "network-sandbox" in settings["PORTAGE_RESTRICT"].split())
+        or (
+            phase != "depend"
+            and "network-sandbox" in settings["PORTAGE_RESTRICT"].split()
+        )
     )
     kwargs["pidns"] = (
         "pid-sandbox" in settings.features and phase not in _global_pid_phases