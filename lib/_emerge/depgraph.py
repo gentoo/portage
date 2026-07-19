gentoo/portage

commit 7b3cee22afb0d70322a0dd538f267aaf437dd6d0

Cancel the spinner before printing resolver diagnostics
The spinner now runs from its own animation thread, so it may write to
standard output while the dependency resolver is printing diagnostics or
ambiguous package search results.

Add a cancel() method which stops the animation without printing the
usual completion message, and call it before emitting such output. This
prevents spinner frames from interleaving with diagnostics, without
pretending that the resolver was able to complete its work.

See-also: a349f90e930978ed08022ae8f62f1dae9da031f7
Bug: https://bugs.gentoo.org/831467
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/depgraph.py
@@ -5138,7 +5138,6 @@ class depgraph:
                         expanded_atoms = [candidate]
 
                 if len(expanded_atoms) > 1:
-                    writemsg("\n\n", noiselevel=-1)
                     ambiguous_package_name(
                         x,
                         expanded_atoms,
@@ -6524,6 +6523,12 @@ class depgraph:
                 raise self._autounmask_breakage()
             else:
                 return
+
+        # Dependency resolution has failed, and diagnostics are about to be
+        # printed. Cancel the spinner first, lest its animation thread
+        # interleave with them (bug 831467).
+        if self._frozen_config.spinner is not None:
+            self._frozen_config.spinner.cancel()
 
         missing_use_reasons = []
         missing_iuse_reasons = []
@@ -11784,6 +11789,13 @@ class _dep_check_composite_db(dbapi):
 
 
 def ambiguous_package_name(arg, atoms, root_config, spinner, myopts):
+    if spinner is not None:
+        # Cancel the spinner before printing search results, lest its animation
+        # thread interleave with them (bug 831467).
+        spinner.cancel()
+
+    writemsg("\n\n", noiselevel=-1)
+
     if "--quiet" in myopts:
         writemsg(
             f'!!! The short ebuild name "{arg}" is ambiguous. Please specify\n',

lib/_emerge/stdout_spinner.py
@@ -87,6 +87,15 @@ class stdout_spinner:
     def stop(self):
         self.driver.stop()
 
+    def cancel(self):
+        # Stop the spinner without printing a completion message.
+        self.stop()
+        if self.update in (self.update_twirl, self.update_scroll):
+            sys.stdout.write("\r\x1b[K")
+            sys.stdout.flush()
+            self.show_cursor()
+        self.update = self.update_quiet
+
     def _frame_index(self):
         return int((time.monotonic() - self.start_time) / self.min_display_latency)
 

commit 0bdaf92ff2c5fa91f2fef68f6699e0c63a940994

depgraph: don't cancel spinner during USE-change probes
Silent collect_use_changes probes may run while dependency resolution
remains underway. Avoid cancelling the spinner for them. Otherwise, the
"Calculating dependencies" notice disappears prematurely.

Fixes: 7b3cee22afb0d70322a0dd538f267aaf437dd6d0
Bug: https://bugs.gentoo.org/979438
Reported-by: Sam James <sam@gentoo.org>
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/depgraph.py
@@ -6527,10 +6527,9 @@ class depgraph:
             else:
                 return
 
-        # Dependency resolution has failed, and diagnostics are about to be
-        # printed. Cancel the spinner first, lest its animation thread
-        # interleave with them (bug 831467).
-        if self._frozen_config.spinner is not None:
+        # Cancel the spinner before printing dependency failure diagnostics
+        # (bug 831467), but not for silent USE-change probes (bug 979438).
+        if self._frozen_config.spinner is not None and not collect_use_changes:
             self._frozen_config.spinner.cancel_notice()
 
         missing_use_reasons = []