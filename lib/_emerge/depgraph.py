gentoo/portage

commit c80ce97ecd3f1a6881654255e19818682f993e49

Avoid excess space after cancelling the spinner
Refrain from printing an extra pair of newlines before ambiguous-name
search results after cancelling the spinner, since the cleared spinner
line can be reused for that output.

Keep the separator for --quiet output, where no search results precede
the ambiguity diagnostic. Also, have cancel() terminate the static
spinner notice, so that subsequent diagnostics begin on a fresh line.

Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/depgraph.py
@@ -11794,9 +11794,8 @@ def ambiguous_package_name(arg, atoms, root_config, spinner, myopts):
         # thread interleave with them (bug 831467).
         spinner.cancel()
 
-    writemsg("\n\n", noiselevel=-1)
-
     if "--quiet" in myopts:
+        writemsg("\n\n", noiselevel=-1)
         writemsg(
             f'!!! The short ebuild name "{arg}" is ambiguous. Please specify\n',
             noiselevel=-1,

lib/_emerge/stdout_spinner.py
@@ -94,6 +94,9 @@ class stdout_spinner:
             sys.stdout.write("\r\x1b[K")
             sys.stdout.flush()
             self.show_cursor()
+        elif self.update == self.update_static:
+            sys.stdout.write("\n")
+            sys.stdout.flush()
         self.update = self.update_quiet
 
     def _frame_index(self):

commit cff74972d8f28edfac7ae309f60de231f409bc7b

stdout_spinner: assume responsibility for the dependency notice
Presently, the dependency resolver prints and completes the dependency
notice, while the stdout_spinner class is responsible for erasing it
upon the spinner being cancelled. This division of responsibilities
requires that the spinner infer whether a notice is still pending.

Have the stdout_spinner class record the notice itself, along with
whether it is pending. The spinner is thus able to distinguish between
beginning, completing, interrupting, resuming and cancelling the notice
according to its recorded state, rather than the display mode alone.

This prevents a late cancellation from emitting the stray newline that
formerly appeared when a static notice had already been completed.

Fixes: c80ce97ecd3f1a6881654255e19818682f993e49
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/depgraph.py
@@ -6531,7 +6531,7 @@ class depgraph:
         # printed. Cancel the spinner first, lest its animation thread
         # interleave with them (bug 831467).
         if self._frozen_config.spinner is not None:
-            self._frozen_config.spinner.cancel()
+            self._frozen_config.spinner.cancel_notice()
 
         missing_use_reasons = []
         missing_iuse_reasons = []
@@ -11804,7 +11804,7 @@ def ambiguous_package_name(arg, atoms, root_config, spinner, myopts):
     if spinner is not None:
         # Cancel the spinner before printing search results, lest its animation
         # thread interleave with them (bug 831467).
-        spinner.cancel()
+        spinner.cancel_notice()
 
     if "--quiet" in myopts:
         writemsg("\n\n", noiselevel=-1)
@@ -11890,30 +11890,15 @@ def _spinner_start(spinner, myopts):
     if "--quiet" in myopts or "--nodeps" in myopts:
         spinner.mode = spinner.QUIET
 
-    if spinner.mode == spinner.STATIC:
-        portage.writemsg_stdout("Calculating dependencies ...")
-    elif spinner.animates():
-        spinner.scroll_prefix = "Calculating dependencies "
-        portage.writemsg_stdout(spinner.scroll_prefix)
-        spinner.hide_cursor()
-    spinner.start_time = time.monotonic()
-    spinner.start()
+    spinner.begin_notice("Calculating dependencies")
 
 
 def _spinner_stop(spinner, backtracked: int = -1, max_retries: int = -1):
     if spinner is None:
         return
 
-    spinner.stop()
-
-    if not spinner.displays_notice():
+    if not spinner.end_notice():
         return
-
-    if spinner.animates():
-        portage.writemsg_stdout("\rCalculating dependencies ... done!\x1b[K\n")
-        spinner.show_cursor()
-    else:
-        portage.writemsg_stdout(" done!\n")
 
     stop_time = time.monotonic()
     time_fmt = f"{stop_time - spinner.start_time:.2f}"

lib/_emerge/stdout_spinner.py
@@ -84,7 +84,8 @@ class stdout_spinner:
         self.min_display_latency = 0.08
         self.start_time = time.monotonic()
         self.last_frame = -1
-        self.scroll_prefix = ""
+        self.notice = ""
+        self.notice_pending = False
         self.driver = _SpinnerDriver(self, self.min_display_latency)
 
     def displays_notice(self):
@@ -100,16 +101,60 @@ class stdout_spinner:
     def stop(self):
         self.driver.stop()
 
-    def cancel(self):
-        # Stop the spinner without printing a completion message.
+    def begin_notice(self, notice):
+        # Render the given notice according to the current mode, then start the
+        # animation if applicable. It is eventually completed by end_notice(),
+        # interrupted by interrupt_notice(), or withdrawn by cancel_notice().
+        self.notice = notice
+        if self.mode == self.STATIC:
+            sys.stdout.write(f"{notice} ...")
+            sys.stdout.flush()
+        elif self.animates():
+            sys.stdout.write(f"{notice} ")
+            sys.stdout.flush()
+            self.hide_cursor()
+        self.notice_pending = self.displays_notice()
+        self.start_time = time.monotonic()
+        self.last_frame = -1
+        self.start()
+
+    def resume_notice(self):
+        # Render the notice anew, after having been interrupted.
+        self.begin_notice(self.notice)
+
+    def end_notice(self):
+        # Stop the spinner, then complete the pending notice. The return value
+        # indicates whether there was a displayed notice to complete.
         self.stop()
-        if self.animates():
-            sys.stdout.write("\r\x1b[K")
-            sys.stdout.flush()
+        if not self.notice_pending:
+            return False
+        self.notice_pending = False
+        if self.mode == self.STATIC:
+            sys.stdout.write(" done!\n")
+        else:
+            sys.stdout.write(f"\r{self.notice} ... done!\x1b[K\n")
             self.show_cursor()
-        elif self.mode == self.STATIC:
+        sys.stdout.flush()
+        return True
+
+    def interrupt_notice(self):
+        # Stop the spinner, then erase or terminate the pending notice so that
+        # further output may cleanly follow. Resumption remains possible, upon
+        # which the notice may be rendered anew.
+        self.stop()
+        if not self.notice_pending:
+            return
+        self.notice_pending = False
+        if self.mode == self.STATIC:
             sys.stdout.write("\n")
-            sys.stdout.flush()
+        else:
+            sys.stdout.write("\r\x1b[K")
+            self.show_cursor()
+        sys.stdout.flush()
+
+    def cancel_notice(self):
+        # Withdraw the pending notice and render nothing further.
+        self.interrupt_notice()
         self.mode = self.QUIET
 
     def _frame_index(self):
@@ -134,7 +179,7 @@ class stdout_spinner:
             char = darkgreen(self.scroll_sequence[pos])
         else:
             char = green(self.scroll_sequence[pos])
-        sys.stdout.write(f"\r{self.scroll_prefix}{' ' * pos}{char}\x1b[K")
+        sys.stdout.write(f"\r{self.notice} {' ' * pos}{char}\x1b[K")
         self.last_frame = frame
         sys.stdout.flush()
 