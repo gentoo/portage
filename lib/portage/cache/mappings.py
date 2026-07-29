gentoo/portage

commit abf052059f05c0f04be7b17057810da1a989f2d7

*/*: misc ruff fixes
This is from `ruff check --fix` with ruff-0.16.0 then I checked the whole diff
a few times manually.

In a few places, I had to save some comments it wanted to drop, and added
some parens to make precedence clearer. I ran black at the end.

Summary overall for what ruff did:
* Sort members of tuples
* Sort imports and group them together
* Use f-strings in more places and use `s!` postfix rather than `str()`
  (and a few other such postfixes)
* Conditional `str.endswith` -> `str.removesuffix`
* Drop some useless `pass`
* Use `min`, `max` rather than their resp. patterns
* Drop unnecessary first arg to `range`
* Chain context managers
* Some combinations of branches
* logging.WARN -> logging.WARNING (apparently cpython changed quite a long
  time ago for this: 6fa635df7aa88ae9fd8b41ae42743341316c90f7)

There are some other tiny obvious changes like dropping an empty orphan
comment line that wasn't serving any purpose too where it didn't seem worth
listing those.

Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/cache/mappings.py
@@ -3,11 +3,11 @@
 # Author(s): Brian Harring (ferringb@gentoo.org)
 
 __all__ = [
+    "LazyLoad",
     "Mapping",
     "MutableMapping",
-    "UserDict",
     "ProtectedDict",
-    "LazyLoad",
+    "UserDict",
     "slot_dict_class",
 ]
 
@@ -119,11 +119,11 @@ class MutableMapping(Mapping):
             pass
         elif hasattr(other, "iteritems"):
             # Use getattr to avoid interference from 2to3.
-            for k, v in getattr(other, "iteritems")():
+            for k, v in other.iteritems():
                 self[k] = v
         elif hasattr(other, "items"):
             # Use getattr to avoid interference from 2to3.
-            for k, v in getattr(other, "items")():
+            for k, v in other.items():
                 self[k] = v
         elif hasattr(other, "keys"):
             for k in other.keys():
@@ -192,7 +192,7 @@ class ProtectedDict(MutableMapping):
     the underlying dict from changes
     """
 
-    __slots__ = ("orig", "new", "blacklist")
+    __slots__ = ("blacklist", "new", "orig")
 
     def __init__(self, orig):
         self.orig = orig
@@ -238,7 +238,7 @@ class LazyLoad(Mapping):
     Lazy loading of values for a dict
     """
 
-    __slots__ = ("pull", "d")
+    __slots__ = ("d", "pull")
 
     def __init__(self, pull_items_func, initial_items=[]):
         self.d = {}
@@ -352,11 +352,11 @@ class _SlotDict:
             pass
         elif hasattr(other, "iteritems"):
             # Use getattr to avoid interference from 2to3.
-            for k, v in getattr(other, "iteritems")():
+            for k, v in other.iteritems():
                 self[k] = v
         elif hasattr(other, "items"):
             # Use getattr to avoid interference from 2to3.
-            for k, v in getattr(other, "items")():
+            for k, v in other.items():
                 self[k] = v
         elif hasattr(other, "keys"):
             for k in other.keys():

commit ee079bc162277399aa17ace3738fcba72d1c23c7

cache: revert part of ruff changes
Restore the quirk from 8be1a1f81066241318ee5be858285ff14500884f. I'm
not sure why it's there but clearly the comment would become stale
with abf052059f05c0f04be7b17057810da1a989f2d7 and it seems like it may
still be needed if it was needed back then too (of course not for 2to3
but the question is what for to begin with).

Fixes: abf052059f05c0f04be7b17057810da1a989f2d7
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/cache/mappings.py
@@ -118,15 +118,15 @@ class MutableMapping(Mapping):
         if other is None:
             pass
         elif hasattr(other, "iteritems"):
-            # Use getattr to avoid interference from 2to3.
-            for k, v in other.iteritems():
+            for k, v in getattr(other, "iteritems")():
                 self[k] = v
         elif hasattr(other, "items"):
             # Use getattr to avoid interference from 2to3.
             for k, v in other.items():
                 self[k] = v
         elif hasattr(other, "keys"):
-            for k in other.keys():
+            # Use getattr to avoid interference from 2to3.
+            for k, v in getattr(other, "items")():
                 self[k] = other[k]
         else:
             for k, v in other: