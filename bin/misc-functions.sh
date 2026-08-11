gentoo/portage

commit 460de6219d147d9680c247405369da9f0a276bba

bin: log when creating binpkg
We already log when it's *done*, but not when we're starting. The slow
part is often compression.

Bug: https://bugs.gentoo.org/930213
Signed-off-by: Sam James <sam@gentoo.org>

bin/misc-functions.sh
@@ -546,6 +546,7 @@ __dyn_package() {
 	# in there in case any tools were built with -pg in CFLAGS.
 	cd "${T}" || die
 
+	__vecho ">>> Creating binpkg"
 	# Sandbox is disabled in case the user wants to use a symlink
 	# for ${PKGDIR} and/or ${PKGDIR}/All.
 	export SANDBOX_ON="0"

commit 5dd040302c60d6645f9011f4167489d4ba8daa4b

bin: log binpkg creation later
Don't log at the start of the phase, but rather after packdebug, otherwise
we get nested binpkg+packdebug logs, then two 'Done.' messages.

Fixes: 460de6219d147d9680c247405369da9f0a276bba
Signed-off-by: Sam James <sam@gentoo.org>

bin/misc-functions.sh
@@ -546,7 +546,6 @@ __dyn_package() {
 	# in there in case any tools were built with -pg in CFLAGS.
 	cd "${T}" || die
 
-	__vecho ">>> Creating binpkg"
 	# Sandbox is disabled in case the user wants to use a symlink
 	# for ${PKGDIR} and/or ${PKGDIR}/All.
 	export SANDBOX_ON="0"
@@ -580,6 +579,7 @@ __dyn_package() {
 		fi
 	fi
 
+	__vecho ">>> Creating binpkg"
 	if [[ "${BINPKG_FORMAT}" == "xpak" ]]; then
 		local tar_options=""
 