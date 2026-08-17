gentoo/portage

commit 8f6f099e8cc47837f530ea4a0c4fbfbc49d0d6c1

Revert "Workaround import problem after Python upgrade"
Obsolete with the fix in 1b188f43eb40a3c59c306b0fbd0407435b6cd4a7.

This reverts commit e7cf51fd6a504b2ab9d887c562c2ca3ace12d033.

Bug: https://bugs.gentoo.org/970375
Bug: https://bugs.gentoo.org/976616
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/vartree.py
@@ -63,10 +63,6 @@ from portage.util.futures import asyncio
 from portage.util.futures.executor.fork import ForkExecutor
 from ._ContentsCaseSensitivityManager import ContentsCaseSensitivityManager
 
-# Made global to fix importing on Python version upgrade:
-# https://bugs.gentoo.org/970375
-from ._SyncfsProcess import SyncfsProcess
-
 _METADATA_FILE = "metadata"
 # The exact set of fields the consolidated metadata file carries, and the set
 # vardbapi caches. Membership is bounded by what _aux_get() may serve as "" on
@@ -6101,6 +6097,8 @@ class dblink:
         disk and avoid data-loss in the event of a power failure. This method
         does nothing if FEATURES=merge-sync is disabled.
         """
+        import subprocess
+        from portage.dbapi._SyncfsProcess import SyncfsProcess
 
         if not self._device_path_map or "merge-sync" not in self.settings.features:
             return

commit c0d94add064532cc375ef6c1dcbc9eb5f57af39f

vartree: drop obsolete subprocess import
No longer needed since 2b37f59364bb2212b794adbbea108bca7cb678c8.

Fixes: 8f6f099e8cc47837f530ea4a0c4fbfbc49d0d6c1
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/dbapi/vartree.py
@@ -6097,7 +6097,6 @@ class dblink:
         disk and avoid data-loss in the event of a power failure. This method
         does nothing if FEATURES=merge-sync is disabled.
         """
-        import subprocess
         from portage.dbapi._SyncfsProcess import SyncfsProcess
 
         if not self._device_path_map or "merge-sync" not in self.settings.features: