b87fe1b8f7025bade8b0c381d0dc11f29ded140b
Matt Turner <mattst88@gentoo.org>
2026-08-18 00:03:55 -0400

    tests: cover merge order around a rebuild trigger in a cycle
    A long standing report describes a package being rebuilt before the
    upgrade that triggered the rebuild, when the upgrade is part of a
    dependency cycle.
    
    Reconstructing the reporter's graph from the debug output attached to
    the bug and reducing it leaves four packages. x11-base/xorg-server is
    rebuilt because virtual/udev pulls in a new sys-fs/udev, and that
    upgrade sits in a cycle with sys-apps/util-linux. Before commit
    5095c2023595 ("find_smallest_cycle: enhance search prioritization")
    the cycle was broken by relying on the installed udev, which let
    xorg-server merge before the upgrade it was being rebuilt for.
    
    Add the reduced graph as a regression test. It fails at 309e28b2d7ff
    and passes from 5095c2023595 onwards.
    
    Bug: https://bugs.gentoo.org/463976
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

---
+++ lib/portage/tests/resolver/test_merge_order_cycles.py
@@ -0,0 +1,70 @@
+# Copyright 2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+from portage.tests import TestCase
+from portage.tests.resolver.ResolverPlayground import (
+    ResolverPlayground,
+    ResolverPlaygroundTestCase,
+)
+
+
+class MergeOrderCyclesTestCase(TestCase):
+    def testRebuildTriggerMergeOrder(self):
+        """
+        A rebuild triggered by an upgrade must be merged after the upgrade
+        that triggered it, even when the upgrade is itself part of a cycle
+        (bug 463976).
+        """
+        ebuilds = {
+            "sys-apps/util-linux-2.22.2": {"EAPI": "8", "DEPEND": "virtual/udev"},
+            "sys-fs/udev-200": {
+                "EAPI": "8",
+                "DEPEND": "sys-apps/util-linux",
+                "PDEPEND": "virtual/udev",
+            },
+            "virtual/udev-197-r2": {"EAPI": "8", "RDEPEND": "sys-fs/udev"},
+            "x11-base/xorg-server-1.13.1": {"EAPI": "8", "DEPEND": "virtual/udev"},
+        }
+
+        installed = {
+            "sys-apps/util-linux-2.22.2": {"EAPI": "8", "DEPEND": "virtual/udev"},
+            "sys-fs/udev-197-r8": {"EAPI": "8", "PDEPEND": "virtual/udev"},
+            "virtual/udev-197-r2": {"EAPI": "8", "RDEPEND": "sys-fs/udev"},
+            "x11-base/xorg-server-1.13.1": {"EAPI": "8", "DEPEND": "virtual/udev"},
+        }
+
+        world = ["sys-apps/util-linux", "x11-base/xorg-server"]
+
+        test_cases = (
+            ResolverPlaygroundTestCase(
+                ["@world"],
+                options={
+                    "--update": True,
+                    "--deep": True,
+                    "--newuse": True,
+                    "--complete-graph": "y",
+                    "--with-bdeps": "y",
+                    "--rebuild-if-new-rev": "y",
+                },
+                success=True,
+                ambiguous_merge_order=True,
+                merge_order_assertions=(
+                    ("sys-fs/udev-200", "x11-base/xorg-server-1.13.1"),
+                ),
+                mergelist=[
+                    "sys-apps/util-linux-2.22.2",
+                    "sys-fs/udev-200",
+                    "x11-base/xorg-server-1.13.1",
+                ],
+            ),
+        )
+
+        playground = ResolverPlayground(
+            ebuilds=ebuilds, installed=installed, world=world
+        )
+        try:
+            for test_case in test_cases:
+                playground.run_TestCase(test_case)
+                self.assertEqual(test_case.test_success, True, test_case.fail_msg)
+        finally:
+            playground.cleanup()
