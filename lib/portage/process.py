gentoo/portage

commit 25346bc407b1e5e977f4d91692e27ee9cc8b9e7c

process: use context manager in has_ipv6
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/process.py
@@ -835,16 +835,11 @@ def has_ipv6():
                 # With ipv6.disable=0 and ipv6.disable_ipv6=1, socket creation
                 # succeeds, but then the bind call fails with this error:
                 # [Errno 99] Cannot assign requested address.
-                sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-                sock.bind(("::1", 0))
+                with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
+                    sock.bind(("::1", 0))
             except OSError:
                 __has_ipv6 = False
-            else:
-                __has_ipv6 = True
-            finally:
-                # python2.7 sockets do not support context management protocol
-                if sock is not None:
-                    sock.close()
+            __has_ipv6 = True
         else:
             __has_ipv6 = False
 

commit 22449ce9b58fb8a9e4d7b89cb2dc9147bfebea83

process: fix IPv6 check
When there's no runtime IPv6 support because of e.g. a sysctl, we'd
set __has_ipv6 to to True.

This may help with bug #981369 but it's not certain.

Bug: https://bugs.gentoo.org/981369
Fixes: 25346bc407b1e5e977f4d91692e27ee9cc8b9e7c
Reported-by: Matt Turner <mattst88@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

lib/portage/process.py
@@ -816,20 +816,22 @@ def has_ipv6():
     """
     global __has_ipv6
 
-    if __has_ipv6 is None:
-        if socket.has_ipv6:
-            sock = None
-            try:
-                # With ipv6.disable=0 and ipv6.disable_ipv6=1, socket creation
-                # succeeds, but then the bind call fails with this error:
-                # [Errno 99] Cannot assign requested address.
-                with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
-                    sock.bind(("::1", 0))
-            except OSError:
-                __has_ipv6 = False
+    if __has_ipv6 is not None:
+        return __has_ipv6
+
+    __has_ipv6 = False
+    if socket.has_ipv6:
+        sock = None
+        try:
+            # With ipv6.disable=0 and ipv6.disable_ipv6=1, socket creation
+            # succeeds, but then the bind call fails with this error:
+            # [Errno 99] Cannot assign requested address.
+            with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
+                sock.bind(("::1", 0))
+
             __has_ipv6 = True
-        else:
-            __has_ipv6 = False
+        except OSError:
+            pass
 
     return __has_ipv6
 