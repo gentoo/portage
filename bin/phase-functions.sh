gentoo/portage

commit 7d73f9873e3130ce92f54299a7c64dc007b566b6

special_env_vars.py: preserve QEMU_* for qemu-static
When running emerge in a cross architecture chroot via qemu-static
with extensions that are not enabled by default it fails with error:
```
Sandboxed process killed by signal: Illegal instruction
 * The ebuild phase 'unpack' has been killed by signal 4.
```
This is caused by qemu-static being invoked without the QEMU_* vars.
They have already been stripped by environ, but not yet being added
back in _grab_pkg_env. Thus qemu-static falls back to its  default
settings for the target architecture and doesn't load the
needed cpu extensions.

Therefore `sandbox` (environ) and `bash` (__filter_readonly_variables)
fail, when execting in a cross arch chroot.

For qemu-static to work properly:
- allow QEMU_* vars through the environ filter
- add QEMU_* vars to the readonly vars filter environment

Closes: https://bugs.gentoo.org/978685
Signed-off-by: Pepper Gray <hello@peppergray.xyz>
Part-of: https://github.com/gentoo/portage/pull/1622
Closes: https://github.com/gentoo/portage/pull/1622
Signed-off-by: Sam James <sam@gentoo.org>

bin/phase-functions.sh
@@ -86,14 +86,19 @@ portage_mutable_filtered_vars=( AA HOSTNAME )
 # is to preserve various variables as they were at the time that the binary
 # package was built while protecting against the application of package renames.
 __filter_readonly_variables() {
-	local -a filtered_vars bash_vars
-	local IFS
+	local -a filtered_vars bash_vars qemu_env
+	local IFS x
+
+	# preserve QEMU vars for qemu-static
+	for x in ${!QEMU_@}; do
+		qemu_env+=( "${x}=${!x}" )
+	done
 
 	# Collect an initial list of special bash variables by instructing a
 	# hygienic instance of bash(1) to report them.
 	mapfile -t bash_vars < <(
 		# Like compgen -A variable but doesn't require readline support.
-		env -i -- "${BASH}" -c "printf %s\\\n $(printf '${!%s*} ' {A..Z} {a..z} _)" \
+		env -i -- "${qemu_env[@]}" "${BASH}" -c "printf %s\\\n $(printf '${!%s*} ' {A..Z} {a..z} _)" \
 		| grep -vx -e PATH -e SHELL
 	)
 	# Incorporate other variables that are known to either be set by or be

lib/portage/package/ebuild/_config/special_env_vars.py
@@ -247,7 +247,7 @@ environ_whitelist = frozenset(
     )
 )
 
-environ_whitelist_re = re.compile(r"^(CCACHE_|DISTCC_).*")
+environ_whitelist_re = re.compile(r"^(CCACHE_|DISTCC_|QEMU_).*")
 
 # Filter selected variables in the config.environ() method so that
 # they don't needlessly propagate down into the ebuild environment.

commit f7bfe34de11b5868846f482748d3ed0924f3c932

phase-functions.sh: don't filter ebuild-set QEMU_* variables
__filter_readonly_variables() determines the set of bash's own special
variables by asking a clean instance of bash to report them. Since
commit 7d73f9873 the QEMU_* variables of the calling environment are
passed to that instance, so bash reports them back and they end up
filtered from the saved ebuild environment.

That breaks any ebuild setting a QEMU_* variable of its own.
sys-firmware/edk2-bin sets QEMU_TARGETS at global scope; it is stripped
when the environment is saved after src_unpack(), leaving it empty for
every subsequent phase and failing the build.

Exclude the injected variables from the reported ones, as PATH and SHELL
already are. They remain visible to qemu-static.

Bug: https://bugs.gentoo.org/978685
Fixes: 7d73f9873 ("special_env_vars.py: preserve QEMU_* for qemu-static")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

bin/phase-functions.sh
@@ -86,12 +86,15 @@ portage_mutable_filtered_vars=( AA HOSTNAME )
 # is to preserve various variables as they were at the time that the binary
 # package was built while protecting against the application of package renames.
 __filter_readonly_variables() {
-	local -a filtered_vars bash_vars qemu_env
+	local -a filtered_vars bash_vars qemu_env qemu_vars
 	local IFS x
 
-	# preserve QEMU vars for qemu-static
+	# Preserve QEMU vars for qemu-static. Exclude them from the reported
+	# variables below, or an ebuild-set QEMU_* var is taken for a bash one
+	# and filtered out. See bug #978685.
 	for x in ${!QEMU_@}; do
 		qemu_env+=( "${x}=${!x}" )
+		qemu_vars+=( -e "${x}" )
 	done
 
 	# Collect an initial list of special bash variables by instructing a
@@ -99,7 +102,7 @@ __filter_readonly_variables() {
 	mapfile -t bash_vars < <(
 		# Like compgen -A variable but doesn't require readline support.
 		env -i -- "${qemu_env[@]}" "${BASH}" -c "printf %s\\\n $(printf '${!%s*} ' {A..Z} {a..z} _)" \
-		| grep -vx -e PATH -e SHELL
+		| grep -vx -e PATH -e SHELL "${qemu_vars[@]}"
 	)
 	# Incorporate other variables that are known to either be set by or be
 	# able to influence bash. This list was last updated for bash-5.3.