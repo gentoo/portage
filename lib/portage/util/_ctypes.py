a4a5e3fbd021b6645c08d57be78ed72f216f81a5
Mike Gilbert <floppym@gentoo.org>
2026-09-15 16:04:42 -0400

    ctypes: simplify load_libc()
    Avoid the need to figure out libc's soname by passing None as the
    library name. This provides a handle representing the "global symbol
    table", which will include Python binary and all shared libs loaded at
    startup.
    
    We risk a conflict if a called symbol is defined in more than one loaded
    object, but that seems unlikely for most libc-provided functions we
    would need to call.
    
    This also drops our use of ctypes.util.find_library() which has been
    "soft deprecated" as of Python 3.15.
    
    Signed-off-by: Mike Gilbert <floppym@gentoo.org>

--- lib/portage/util/_ctypes.py
+++ lib/portage/util/_ctypes.py
@@ -3,34 +3,8 @@
 
 try:
     import ctypes
-    import ctypes.util
 except ImportError:
     ctypes = None
-else:
-    try:
-        ctypes.cdll
-    except AttributeError:
-        ctypes = None
-
-_library_names = {}
-
-
-def find_library(name):
-    """
-    Calls ctype.util.find_library() if the ctypes module is available,
-    and otherwise returns None. Results are cached for future invocations.
-    """
-    filename = _library_names.get(name)
-    if filename is None:
-        if ctypes is not None:
-            filename = ctypes.util.find_library(name)
-            if filename is None:
-                filename = False
-            _library_names[name] = filename
-
-    if filename is False:
-        return None
-    return filename
 
 
 _library_handles = {}
@@ -38,7 +12,7 @@ _library_handles = {}
 
 def LoadLibrary(name):
     """
-    Calls ctypes.cdll.LoadLibrary(name) if the ctypes module is available,
+    Calls ctypes.CDLL(name) if the ctypes module is available,
     and otherwise returns None. Results are cached for future invocations.
     """
     handle = _library_handles.get(name)
@@ -55,10 +29,9 @@ def load_libc():
     Loads the C standard library, returns a tuple with the CDLL handle and
     the filename. Returns (None, None) if unavailable.
     """
-    filename = find_library("c")
-    if filename is None:
-        # find_library fails for musl where there is no soname
-        filename = "libc.so"
+    # Passing NULL to dlopen() gives access to all global symbols.
+    # This includes shared libs loaded at startup (including libc).
+    filename = None
     try:
         return (LoadLibrary(filename), filename)
     except OSError:
