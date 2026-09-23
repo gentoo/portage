gentoo/portage

commit eaaab14c782c0f5d8314dce04110e3492d710cc8

ebuild: set SANDBOX_LOG to a non-T value for the depend phase
Bug: https://bugs.gentoo.org/543774
Signed-off-by: Sam James <sam@gentoo.org>

lib/_emerge/EbuildMetadataPhase.py
@@ -1,15 +1,17 @@
 # Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-from _emerge.SubProcess import SubProcess
 import sys
 import os
-from portage.cache.mappings import slot_dict_class
-import portage
+import fcntl
+import tempfile
 
+from _emerge.SubProcess import SubProcess
+
+from portage.cache.mappings import slot_dict_class
 from portage.util.futures import asyncio
 
-import fcntl
+import portage
 
 
 class EbuildMetadataPhase(SubProcess):
@@ -122,6 +124,10 @@ class EbuildMetadataPhase(SubProcess):
         self._raw_metadata = []
         files.ebuild = master_fd
         self.scheduler.add_reader(files.ebuild, self._output_handler)
+
+        settings["SANDBOX_LOG"] = tempfile.NamedTemporaryFile(
+            prefix="sandbox", delete_on_close=False
+        ).name
 
         retval = doebuild(
             ebuild_path,

lib/portage/package/ebuild/doebuild.py
@@ -521,7 +521,9 @@ def doebuild_environment(
     mysettings["WORKDIR"] = os.path.join(mysettings["PORTAGE_BUILDDIR"], "work")
     mysettings["D"] = os.path.join(mysettings["PORTAGE_BUILDDIR"], "image") + os.sep
     mysettings["T"] = os.path.join(mysettings["PORTAGE_BUILDDIR"], "temp")
-    mysettings["SANDBOX_LOG"] = os.path.join(mysettings["T"], "sandbox.log")
+    if mydo != "depend":
+        # EbuildMetadataPhase handles it for the depend phase
+        mysettings["SANDBOX_LOG"] = os.path.join(mysettings["T"], "sandbox.log")
     mysettings["FILESDIR"] = os.path.join(settings["PORTAGE_BUILDDIR"], "files")
 
     # Prefix forward compatibility

commit 3662436772e5045151214c746998609d31dcb91c

EbuildMetadataPhase: keep the sandbox log until the phase is done
SANDBOX_LOG was set from the name of a NamedTemporaryFile that was
dropped immediately. On CPython that unlinks the file right away. On
PyPy it is unlinked whenever the object is collected, which can be
after sandbox has written a violation to it, and Python 3.14 and PyPy
also emit a ResourceWarning.

Create the file with mkstemp() and unlink it once the returncode has
been checked. Stat that path rather than settings["SANDBOX_LOG"], since
settings is handed back to the caller after doebuild() and may already
hold another phase's log.

Fixes: eaaab14c782c ("ebuild: set SANDBOX_LOG to a non-T value for the depend phase")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

lib/_emerge/EbuildMetadataPhase.py
@@ -35,6 +35,7 @@ class EbuildMetadataPhase(SubProcess):
         "_eapi",
         "_eapi_lineno",
         "_raw_metadata",
+        "_sandbox_log",
     )
 
     _file_names = ("ebuild",)
@@ -125,12 +126,9 @@ class EbuildMetadataPhase(SubProcess):
         files.ebuild = master_fd
         self.scheduler.add_reader(files.ebuild, self._output_handler)
 
-        tempfile_args = {}
-        if sys.version_info >= (3, 12):
-            tempfile_args["delete_on_close"] = False
-        settings["SANDBOX_LOG"] = tempfile.NamedTemporaryFile(
-            prefix="sandbox", **tempfile_args
-        ).name
+        sandbox_log_fd, self._sandbox_log = tempfile.mkstemp(prefix="sandbox")
+        os.close(sandbox_log_fd)
+        settings["SANDBOX_LOG"] = self._sandbox_log
 
         retval = doebuild(
             ebuild_path,
@@ -159,6 +157,7 @@ class EbuildMetadataPhase(SubProcess):
         if isinstance(retval, int):
             # doebuild failed before spawning
             self.returncode = retval
+            self._remove_sandbox_log()
             self._async_wait()
             return
 
@@ -269,11 +268,22 @@ class EbuildMetadataPhase(SubProcess):
             # (say, a missing eclass) in which case we probably want to plough
             # on, or if we have a sandbox violation, in which case we stop dead.
             try:
-                if os.stat(self.settings["SANDBOX_LOG"]).st_size > 0:
+                if os.stat(self._sandbox_log).st_size > 0:
                     self.returncode = 2
             except OSError:
                 # Some other, non-sandbox problem occurred.
                 pass
+
+        self._remove_sandbox_log()
+
+    def _remove_sandbox_log(self):
+        if self._sandbox_log is None:
+            return
+        try:
+            os.unlink(self._sandbox_log)
+        except FileNotFoundError:
+            pass
+        self._sandbox_log = None
 
     def _eapi_invalid(self, metadata):
         from portage.package.ebuild._metadata_invalid import eapi_invalid