63230df123496e6841979933f0507e70f3ed1683
Florian Schmaus <flow@gentoo.org>
2026-09-24 09:24:25 +0200

    EbuildMetadataPhase: place sandbox log in isolated directory under emerge tmpdir
    Commit 3662436772e5 ("EbuildMetadataPhase: keep the sandbox log until
    the phase is done") changed SANDBOX_LOG initialization to create a
    file via tempfile.mkstemp(prefix="sandbox") in /tmp. When running
    emerge as root, mkstemp() creates the file with root:root ownership
    and 0600 mode.
    
    When FEATURES="userpriv" is active, doebuild drops privileges to the
    portage user before spawning sandbox. At startup, setup_sandbox()
    checks if the log file already exists and attempts to unlink
    it. Because /tmp has the sticky bit set, an unprivileged user cannot
    unlink a root-owned file, failing with EPERM and aborting the depend
    phase.
    
    This shows up as
    
    sandbox:setup_sandbox  could not unlink old log file: /tmp/sandboxj6ktklg9: Operation not permitted
    sandbox:main  failed to setup sandbox
    
    in portage output.
    
    Furthermore, using an unlinked name directly in world-writable /tmp
    creates a potential TOCTOU / local denial-of-service issue, and under
    sudo with pam_mktemp /tmp may not be traversable by the portage user
    (bug #977245).
    
    Address this by placing SANDBOX_LOG inside an isolated subdirectory
    created beneath the process-level emerge_tmpdir (under PORTAGE_TMPDIR
    when running as root, or standard tempdir when unprivileged). The
    directory has mode 0750 and, when running as root, is owned by the
    portage user. sandbox.log is created on demand if a violation occurs.
    
    Fixes: 3662436772e5 ("EbuildMetadataPhase: keep the sandbox log until the phase is done")
    Signed-off-by: Florian Schmaus <flow@gentoo.org>
    Part-of: https://github.com/gentoo/portage/pull/1713
    Closes: https://github.com/gentoo/portage/pull/1713
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- lib/_emerge/EbuildMetadataPhase.py
+++ lib/_emerge/EbuildMetadataPhase.py
@@ -3,6 +3,7 @@
 
 import fcntl
 import os
+import shutil
 import sys
 import tempfile
 
@@ -35,6 +36,7 @@ class EbuildMetadataPhase(SubProcess):
         "_eapi",
         "_eapi_lineno",
         "_raw_metadata",
+        "_sandbox_dir",
         "_sandbox_log",
     )
 
@@ -48,7 +50,7 @@ class EbuildMetadataPhase(SubProcess):
         self._registered = True
 
     async def _async_start(self):
-        from portage.package.ebuild.doebuild import doebuild
+        from portage.package.ebuild.doebuild import doebuild, get_emerge_tmpdir
 
         from _emerge.EbuildPhase import _setup_locale
 
@@ -87,6 +89,25 @@ class EbuildMetadataPhase(SubProcess):
         await _setup_locale(self.settings)
 
         debug = settings.get("PORTAGE_DEBUG") == "1"
+
+        # Create the sandbox log directory before any file descriptors are
+        # opened, so that a failure here does not leak them.
+        emerge_tmpdir = get_emerge_tmpdir(settings)
+        self._sandbox_dir = tempfile.mkdtemp(prefix="sandbox-", dir=emerge_tmpdir)
+        try:
+            portage.util.apply_secpass_permissions(
+                self._sandbox_dir,
+                uid=portage.portage_uid,
+                gid=portage.portage_gid,
+                mode=0o750,
+            )
+        except Exception:
+            shutil.rmtree(self._sandbox_dir, ignore_errors=True)
+            self._sandbox_dir = None
+            raise
+        self._sandbox_log = os.path.join(self._sandbox_dir, "sandbox.log")
+        settings["SANDBOX_LOG"] = self._sandbox_log
+
         master_fd = None
         slave_fd = None
         fd_pipes = None
@@ -126,10 +147,6 @@ class EbuildMetadataPhase(SubProcess):
         files.ebuild = master_fd
         self.scheduler.add_reader(files.ebuild, self._output_handler)
 
-        sandbox_log_fd, self._sandbox_log = tempfile.mkstemp(prefix="sandbox")
-        os.close(sandbox_log_fd)
-        settings["SANDBOX_LOG"] = self._sandbox_log
-
         retval = doebuild(
             ebuild_path,
             "depend",
@@ -157,7 +174,7 @@ class EbuildMetadataPhase(SubProcess):
         if isinstance(retval, int):
             # doebuild failed before spawning
             self.returncode = retval
-            self._remove_sandbox_log()
+            self._remove_sandbox_dir()
             self._async_wait()
             return
 
@@ -274,15 +291,14 @@ class EbuildMetadataPhase(SubProcess):
                 # Some other, non-sandbox problem occurred.
                 pass
 
-        self._remove_sandbox_log()
+        self._remove_sandbox_dir()
 
-    def _remove_sandbox_log(self):
-        if self._sandbox_log is None:
+    def _remove_sandbox_dir(self):
+        if self._sandbox_dir is None:
             return
-        try:
-            os.unlink(self._sandbox_log)
-        except FileNotFoundError:
-            pass
+
+        shutil.rmtree(self._sandbox_dir, ignore_errors=True)
+        self._sandbox_dir = None
         self._sandbox_log = None
 
     def _eapi_invalid(self, metadata):
